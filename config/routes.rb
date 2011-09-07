e2a42e3bdaf98234bfa1e41695c6d089e8d96570
Frances Berriman <phaeness@Phae.local>
2011-09-07 12:21:56 +0100

    A section template (unfinished + needs static pull)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Frontend::Application.routes.draw do
   root :to => "root#index"
   match "/help", :to => "root#help"
+  match "/section", :to => "root#section"
   match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match ":slug(/:part)",:as=>"publication", :to => "root#publication"
 end

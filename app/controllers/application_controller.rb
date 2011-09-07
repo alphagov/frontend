1a927b23b9c6bc6654aa14033e1b99419c5c4db7
Ben Griffiths <bengriffiths@gmail.com>
2011-09-07 12:20:55 +0100

    Environment specific api host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ require 'publisher_api'
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def api_host
+    env = ENV['RAILS_ENV'] || 'development'
+    case env 
+    when 'development','test'
+      "local.alphagov.co.uk:3000"
+    when 'production'
+       "api.alpha.gov.uk"
+    else
+       "guides.#{env}.alphagov.co.uk:8080"
+    end
+  end
+
+
   def api
-    @api ||= PublisherApi.new("http://local.alphagov.co.uk:3000")
+    @api ||= PublisherApi.new("http://#{api_host}")
   end
 end

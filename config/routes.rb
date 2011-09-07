6ebe8c37d763a749cce68e408bf7cdc75f52280b
Frances Berriman <phaeness@Phae.local>
2011-09-07 16:07:59 +0100

    Added a basic /search template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Frontend::Application.routes.draw do
   root :to => "root#index"
   match "/help", :to => "root#help"
   match "/section", :to => "root#section"
+  match "/search", :to => "root#search"
   match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match ":slug(/:part)",:as=>"publication", :to => "root#publication"
 end

85a574174f3fa4f4a5bea031db7aa99a326ef952
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-08 11:51:15 +0000

    remove all actionmailer calls as it's been removed

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,3 @@
 require File.expand_path('production.rb', File.dirname(__FILE__))
 Frontend::Application.configure do
-  config.action_mailer.default_url_options = { :host => "demo.alphagov.co.uk" }
-  config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
 end

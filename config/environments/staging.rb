b9f2f9d42fb10208d870ae2682b4a92fbaaa7cd2
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-08 11:43:21 +0000

    correct application name in saging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,4 @@
 require File.expand_path('production.rb', File.dirname(__FILE__))
-
-Publisher::Application.configure do
+Frontend::Application.configure do
   config.action_mailer.default_url_options = { :host => "demo.alphagov.co.uk" }
 end

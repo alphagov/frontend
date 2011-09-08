8d906f82b206e2b003b382da9bc7e4b2a189a416
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-08 12:37:37 +0000

    add asset hostish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Frontend::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://demo.alphagov.co.uk"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

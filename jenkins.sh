1b5d88553f57bc56f0a6047441170f15109deb80
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-09-09 11:43:21 +0000

    added rails stats for jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 source '/usr/local/lib/rvm'
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+bundle exec rake stats
 bundle exec rake ci:setup:testunit test
 

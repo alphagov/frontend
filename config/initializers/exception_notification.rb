c4584e0d769778414c07391c0199f9fce457dfed
James Stewart <jys@ketlai.co.uk>
2011-09-12 19:40:15 +0100

    Fix exception notification

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
 unless Rails.env.development? or Rails.env.test?
-  Publisher::Application.config.middleware.use ExceptionNotifier,
+  Frontend::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Front End] ",
     :sender_address => %{"Winston Smith-Churchill" <winston@alphagov.co.uk>},
     :exception_recipients => %w{dev@alphagov.co.uk}

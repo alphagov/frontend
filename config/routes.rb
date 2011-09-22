14fd45ef221e065d316f8ea8ebab263ed8b51222
Frances Berriman <phaeness@Phae.local>
2011-09-22 15:44:47 +0100

    Placeholder pages for planner WIP

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Frontend::Application.routes.draw do
   match "/help", :to => "root#help"
   match "/section", :to => "root#section"
   match "/search", :to => "root#search"
+  match "/planner", :to => "root#planner"
   match "/autocomplete", :to => "root#autocomplete"
   match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match "/places/load_places/:slug", :as=>"load_places", :to => "root#load_places"

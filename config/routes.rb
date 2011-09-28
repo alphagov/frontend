109751aec618d65c42a46d74fac9f2677bf7b9e9
Frances Berriman <phaeness@Phae.local>
2011-09-28 10:31:57 +0100

    Layout changes for planner

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Frontend::Application.routes.draw do
   match "/section", :to => "root#section"
   match "/search", :to => "root#search"
   match "/planner", :to => "root#planner"
+  match "/plannerresults", :to => "root#plannerresults"
   match "/autocomplete", :to => "root#autocomplete"
   match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match "/places/load_places/:slug", :as=>"load_places", :to => "root#load_places"

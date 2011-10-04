60621c1d7c93b76e8e5e0f082dbbfaffed56788a
Frances Berriman <phaeness@phae.lan>
2011-10-04 11:23:26 +0100

    Removed refs to dummy files

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Frontend::Application.routes.draw do
   match "/help", :to => "root#help"
   match "/section", :to => "root#section"
   match "/search", :to => "root#search"
-  match "/planner", :to => "root#planner"
-  match "/plannerresults", :to => "root#plannerresults"
   match "/autocomplete", :to => "root#autocomplete"
   match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match "/places/load_places/:slug", :as=>"load_places", :to => "root#load_places"

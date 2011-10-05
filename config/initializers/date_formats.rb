156685cbcbf8948f3170c0adc1dc5497be0bcaba
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-10-05 12:52:27 +0100

    removing lambda from date format as it is no longer needed

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,2 +1,2 @@
-Date::DATE_FORMATS[:short_ordinal] = lambda { |date| date.strftime("%d %B %Y") }
-Time::DATE_FORMATS[:short_ordinal] = lambda { |date| date.strftime("%d %B %Y") }
+Date::DATE_FORMATS[:short_ordinal] = "%d %B %Y"
+Time::DATE_FORMATS[:short_ordinal] = "%d %B %Y"

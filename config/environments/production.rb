c10fd32450ee45d760e0a80d126501e5061912db
Gareth Rushgrove <gareth@alphagov.co.uk>
2011-10-18 20:03:25 +0000

    add asset host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,6 @@ Frontend::Application.configure do
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   config.action_controller.asset_host = "http://static.production.alphagov.co.uk"
-  config.action_controller.asset_path = CdnHelpers::AssetPath
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

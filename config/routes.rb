65994856100a5001180cc1d9499387a2f74949a1
James Stewart <jys@ketlai.co.uk>
2011-12-06 16:36:13 +0000

    Update route for identifying councils

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Frontend::Application.routes.draw do
   root :to => "root#index"
   match "/help", :to => "root#help"
   match "/section", :to => "root#section"
-  match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
+  match "/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match "/places/load_places/:slug", :as=>"load_places", :to => "root#load_places"
   match ":slug(/:part)",:as=>"publication", :to => "root#publication"
 end

8b227edf7909751268fc6cd71cbbfe74ce6605ba
Frances Berriman <fberriman@gmail.com>
2011-12-09 11:51:32 +0000

    Platform page stuv

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Frontend::Application.routes.draw do
   root :to => "root#index"
   match "/help", :to => "root#help"
+  match "/platform", :to => "root#platform"
   match "/section", :to => "root#section"
   match "/transactions/identify_council/:slug", :as=>"identify_council", :to => "root#identify_council"
   match "/places/load_places/:slug", :as=>"load_places", :to => "root#load_places"

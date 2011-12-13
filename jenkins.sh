eb8ccd10e770fdf127d93fdae895df2bc874e77b
James Stewart <jys@ketlai.co.uk>
2011-12-13 16:09:55 +0000

    Update jenkins.sh with display settings

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x
+export FACTER_govuk_platform=test
+export RAILS_ENV=test
+export DISPLAY=":99"
+
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 bundle exec rake stats
 bundle exec rake ci:setup:testunit test
+RESULT=$?
+exit $RESULT

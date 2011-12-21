46bccd9995ba3ea6384af34fdcc997537f109e58
Craig R Webster <craig@barkingiguana.com>
2011-12-21 17:04:31 +0000

    Use SSL for assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Frontend::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = Plek.current.find('assets')
+  config.action_controller.asset_host = Plek.current.find('assets').gsub(/^http\:/, 'https:')
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

7ab7d6c0587cf4613905cbfb08966b54aaeae738
Paul Battley <pbattley@gmail.com>
2011-12-21 10:51:27 +0000

    Improve test coverage of title.

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
-class ApplicationHelperContainer
-  include ApplicationHelper
-  def request
-    format = OpenStruct.new :video? => true
-    OpenStruct.new :format => format
-  end
-end
-
 class ApplicationHelperTest < ActionView::TestCase
+  class ApplicationHelperContainer
+    include ApplicationHelper
+    def request
+      @request ||= OpenStruct.new(:format => OpenStruct.new(:video? => false))
+    end
+  end
+
   def setup
     @helper = ApplicationHelperContainer.new
   end
@@ -22,12 +21,14 @@ class ApplicationHelperTest < ActionView::TestCase
   end
 
   test "the page title doesn't contain consecutive pipes" do
-    assert ! @helper.page_title(basic_artefact).match(/\|\s*\|/)
+    assert_no_match %r{\|\s*\|}, @helper.page_title(basic_artefact)
   end
 
   test "the page title includes the publication alternative title if one's set" do
-    publication = OpenStruct.new(alternative_title: 'I am an alternative')
-    assert @helper.page_title(basic_artefact, publication).match(/I am an alternative/)
+    publication = OpenStruct.new(alternative_title: 'I am an alternative', title: 'I am not')
+    title = @helper.page_title(basic_artefact, publication)
+    assert_match %r{I am an alternative}, title
+    assert_no_match %r{I am not}, title
   end
 
   test "the page title doesn't blow up if the publication titles are nil" do
@@ -36,7 +37,32 @@ class ApplicationHelperTest < ActionView::TestCase
   end
 
   test "it correctly identifies a video guide in the wrapper classes" do
+    @helper.request.format.stubs(:video?).returns(true)
     guide = OpenStruct.new(:type => 'guide')
     assert @helper.wrapper_class(guide).split.include?('video-guide')
+  end
+
+  test "should build title from publication and artefact" do
+    publication = OpenStruct.new(title: "Title")
+    artefact = OpenStruct.new(section: "Section")
+    assert_equal "Title | Section | www.gov.uk", @helper.page_title(artefact, publication)
+  end
+
+  test "should prefix title of video with video" do
+    @helper.request.format.stubs(:video?).returns(true)
+    publication = OpenStruct.new(title: "Title")
+    assert_match /^Video - Title/, @helper.page_title(basic_artefact, publication)
+  end
+
+  test "should omit artefact section if missing" do
+    publication = OpenStruct.new(title: "Title")
+    artefact = OpenStruct.new(section: "")
+    assert_equal "Title | www.gov.uk", @helper.page_title(artefact, publication)
+  end
+
+  test "should omit first part of title if publication is omitted" do
+    @helper.request.format.stubs(:video?).returns(true)
+    artefact = OpenStruct.new(section: "Section")
+    assert_equal "Section | www.gov.uk", @helper.page_title(artefact)
   end
 end

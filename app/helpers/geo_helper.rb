ac4b55b891d0de52705b29dfeb87a85064437b07
Gareth Rushgrove <gareth@morethanseven.net>
2011-12-22 11:25:06 +0000

    fix up the test of geo

--- app/helpers/geo_helper.rb
+++ app/helpers/geo_helper.rb
@@ -10,25 +10,24 @@ module GeoHelper
   def geo_known_to?(*accuracy)
     geo_header and geo_header['fuzzy_point'] and accuracy.include?(geo_header['fuzzy_point']['accuracy'])
   end
-  
+
   def geo_friendly_name
     geo_header['friendly_name']
   end
-  
+
   def district_postcode
     geo_header['postcode'] if geo_header['postcode'].present?
   end
 
   def geo_header
-    #if request.env['HTTP_X_GOVGEO_STACK'] and request.env['HTTP_X_GOVGEO_STACK'] != ''
-    #  @geo_header ||= JSON.parse(Base64.decode64(request.env['HTTP_X_GOVGEO_STACK']))
-    #else
-    #  @geo_header ||= {}
-    #end
-    @geo_header = {}
+    if request.env['HTTP_X_GOVGEO_STACK'] and request.env['HTTP_X_GOVGEO_STACK'] != ''
+      @geo_header ||= JSON.parse(Base64.decode64(request.env['HTTP_X_GOVGEO_STACK']))
+    else
+      @geo_header ||= {}
+    end
     @geo_header
   end
-  
+
   def reset_geo_url
     callback = Addressable::URI.parse(request.url)
     callback.query_values = {:reset_geo => 'true'}

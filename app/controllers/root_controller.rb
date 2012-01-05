bcb2d36f296aada500fb5276d94ac457399ad6a4
Dafydd Vaughan <dafydd@alphagov.co.uk>
2012-01-05 14:36:39 +0000

    force NeedID custom var to be a string - Google doesn't seem to like integers

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -144,7 +144,7 @@ protected
   def set_slimmer_artefact_headers(artefact)
     set_slimmer_headers(
       section:     artefact.section,
-      need_id:     artefact.need_id,
+      need_id:     artefact.need_id.to_s,
       format:      artefact.kind,
       proposition: "citizen"
     )

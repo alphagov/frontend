cd830db586144aa66fc6154bd1f54569cb804b88
James Stewart <jys@ketlai.co.uk>
2012-01-20 11:09:12 +0000

    Whitespace

--- test/integration/page_rendering_test.rb
+++ test/integration/page_rendering_test.rb
@@ -52,16 +52,15 @@ class LoadingPlacesTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Ride a motorcycle or moped")
     assert page.has_css?("a[href='http://www.youtube.com/watch?v=iD941H0j1Z0']")
   end
-  
+
   test "rendering a help page" do
     visit "/help/accessibility"
     assert page.has_content?("I need help getting started online")
   end
-  
+
   test "rendering a platform page" do
     visit "/platform/api"
     assert page.has_content?("API")
   end
-  
 end
 

9e1762d94a06246670ae88ff492e0299e42efe26
Jordan Hatch <jordan@jordanh.net>
2012-01-25 15:18:29 +0000

    Fixed local transaction bug and test issue

--- test/functional/local_transactions_test.rb
+++ test/functional/local_transactions_test.rb
@@ -94,12 +94,13 @@ class LocalTransactionsTest < ActionController::TestCase
   end
 
   test "Should set message if no council for local transaction" do
-    councils = {'council'=>[{'ons'=>1},{'ons'=>2},{'ons'=>3}]}
+    councils = {'council'=>[{'ons'=>1}]}
     request.env["HTTP_X_GOVGEO_STACK"] = encode_stack councils
 
-    councils['council'].each do |c|
-      setup_this_local_transaction c['ons']
-    end 
+    publication_exists_for_snac(1234, {"slug" => "c-slug", "type" => "local_transaction", "name" => "THIS"})
+    panopticon_has_metadata('slug' => 'c-slug', 'id' => '12345')
+
+    stub_request(:get, "#{PUBLISHER_ENDPOINT}/publications/c-slug.json?snac=1").to_return(:body => JSON.dump({"slug" => "c-slug", "type" => "local_transaction", "name" => "THIS"}))
 
     get :publication, :slug => "c-slug"
     assert response.body.include? "couldn't find details of a provider"

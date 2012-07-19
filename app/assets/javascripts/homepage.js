b1312945d0ca528476c06c9a01d7567e12ba930c
Alex Torrance <hello@alextorrance.co.uk>
2012-07-19 14:10:42 +0100

    Add Google Analytics tracking to homepage links

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -16,8 +16,30 @@ function cycleSuggestion() {
   });
 }
 
+/*
+* Set up Google analytics event tracking for homepage analysis
+*/
+function initAnalytics() {
+  // Search Suggestions
+  $('.search-suggestions').on('click', 'a', function (e) {
+    if ( _gat && _gat._getTracker ) {
+      _gaq.push(['_trackEvent', 'homepage_analysis', 'search_suggestion', this.href]);
+    }
+  });
 
+  // Browse sections
+  $('.homepage-sections').on('click', 'h2 a', function (e) {
+    if ( _gat && _gat._getTracker ) {
+      _gaq.push(['_trackEvent', 'homepage_analysis', 'nav_links', this.href]);
+    }
+  }).on('click', '.sections-links a', function (e) {
+    if ( _gat && _gat._getTracker ) {
+      _gaq.push(['_trackEvent', 'homepage_analysis', 'page_links', this.href]);
+    }
+  });
+}
 
 $(document).ready(function() {
   initSuggestions();
+  initAnalytics();
 });

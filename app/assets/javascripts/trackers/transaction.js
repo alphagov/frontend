e9e1b9f7345dae2e6ae01adeff7902b0055f43f6
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-07-25 16:13:48 +0100

    Remove legacy google analytics tracking
    The legacy codes were listed by Ashraf Chohan and are:
    Citizen-Format-Guide
    Citizen-Format-Answer
    Citizen-Format-Benefit
    Citizen-Format-Service
    Citizen-Format-Localservice
    Citizen-Feedback
    Citizen-Format-Findmynearest
    Citizen-Accessibility

--- app/assets/javascripts/trackers/transaction.js
+++
@@ -1,9 +0,0 @@
-_gaq.push(['_trackEvent', 'Citizen-Format-Service', 'Load']);
-$(document).ready(function() {
-  // click on 'get started' CTA
-  $(".get-started a").click(function(){
-    _gaq.push(['_trackEvent', 'Citizen-Format-Service', 'Success-interaction']);
-    return true;
-  })
-  
-});

44819be0d382d09e97d60b34675f540329633470
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-08-24 16:13:32 +0100

    Don't disable slimmer in the tests.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -1,16 +1,10 @@
-require 'test_helper'
+require_relative 'test_helper'
 require 'capybara/rails'
 
 require 'gds_api/test_helpers/publisher'
 require 'gds_api/test_helpers/panopticon'
 require 'gds_api/test_helpers/imminence'
 require 'slimmer/test'
-
-class ActionController::Base
-  before_filter proc {
-    response.headers[Slimmer::SKIP_HEADER] = "true"
-  }
-end
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL

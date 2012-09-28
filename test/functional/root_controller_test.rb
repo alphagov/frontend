ad4f69b1857a226d85ecb2f41f2235da358879a9
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-28 11:28:31 +0100

    Reverse whitespace changes.

--- test/functional/root_controller_test.rb
+++ test/functional/root_controller_test.rb
@@ -4,13 +4,13 @@ class RootControllerTest < ActionController::TestCase
 
   def setup_this_answer
     publication_exists(
-        'slug' => 'c-slug',
-        'type' => 'answer',
-        'name' => 'THIS',
-        'parts' => [
-            {'slug' => 'a', 'name' => 'AA'},
-            {'slug' => 'b', 'name' => 'BB'}
-        ]
+      'slug' => 'c-slug',
+      'type' => 'answer',
+      'name' => 'THIS',
+      'parts' => [
+        {'slug' => 'a', 'name' => 'AA'},
+        {'slug' => 'b', 'name' => 'BB'}
+      ]
     )
     content_api_has_an_artefact("c-slug")
   end
@@ -29,12 +29,12 @@ class RootControllerTest < ActionController::TestCase
 
   test "should return a 404 if asked for a guide without parts" do
     publication_exists(
-        "slug" => "disability-living-allowance-guide",
-        "alternative_title" => "",
-        "overview" => "",
-        "title" => "Disability Living Allowance",
-        "parts" => [],
-        "type" => "guide"
+      "slug" => "disability-living-allowance-guide",
+      "alternative_title" => "",
+      "overview" => "",
+      "title" => "Disability Living Allowance",
+      "parts" => [],
+      "type" => "guide"
     )
     content_api_has_an_artefact("disability-living-allowance-guide")
     get :publication, :slug => "disability-living-allowance-guide"
@@ -45,7 +45,7 @@ class RootControllerTest < ActionController::TestCase
     publication_does_not_exist('slug' => 'a-slug')
     content_api_has_an_artefact("a-slug")
     prevent_implicit_rendering
-    @controller.expects(:render).with(has_entry(:status => 404))
+    @controller.expects(:render).with(has_entry(:status=>404))
     get :publication, :slug => "a-slug"
   end
 
@@ -75,7 +75,7 @@ class RootControllerTest < ActionController::TestCase
     publication_exists('slug' => 'zippy', 'type' => 'programme', 'parts' => [
         {'slug' => 'a', 'name' => 'AA'},
         {'slug' => 'further-information', 'name' => 'BB'}
-    ])
+      ])
     content_api_has_an_artefact("zippy")
     get :publication, :slug => "zippy"
     assert @response.body.include? "further-information"
@@ -85,7 +85,7 @@ class RootControllerTest < ActionController::TestCase
     publication_exists('slug' => 'george', 'type' => 'programme', 'parts' => [
         {'slug' => 'a', 'name' => 'AA'},
         {'slug' => 'b', 'name' => 'BB'}
-    ])
+      ])
     content_api_has_an_artefact("george")
     get :publication, :slug => "george"
     assert !@response.body.include?("further-information")
@@ -124,9 +124,9 @@ class RootControllerTest < ActionController::TestCase
 
   test "should return print view" do
     publication_exists(
-        'slug' => 'a-slug', 'type' => 'guide', 'name' => 'THIS', 'parts' => [
+      'slug' => 'a-slug', 'type' => 'guide', 'name' => 'THIS', 'parts' => [
         {'title' => 'Part 1', 'slug' => 'part-1', 'body' => 'Part 1 I am'}
-    ]
+      ]
     )
     content_api_has_an_artefact("a-slug")
 
@@ -183,16 +183,16 @@ class RootControllerTest < ActionController::TestCase
   end
 
   test "should not pass edition parameter on to api if it's blank" do
-    edition_id = ''
-    api = mock()
-    api.expects(:publication_for_slug).with("a-slug", {}).returns(OpenStruct.new(:type => "answer"))
-    @controller.stubs(:publisher_api).returns api
+     edition_id = ''
+     api = mock()
+     api.expects(:publication_for_slug).with("a-slug", {}).returns(OpenStruct.new(:type=>"answer"))
+     @controller.stubs(:publisher_api).returns api
 
-    content_api_has_an_artefact("a-slug")
+     content_api_has_an_artefact("a-slug")
 
-    prevent_implicit_rendering
-    @controller.stubs(:render)
-    get :publication, :slug => "a-slug", :edition => edition_id
+     prevent_implicit_rendering
+     @controller.stubs(:render)
+     get :publication, :slug => "a-slug",:edition => edition_id
   end
 
   test "should pass specific and general variables to template" do
@@ -332,6 +332,4 @@ class RootControllerTest < ActionController::TestCase
       assert_equal "contact@lambeth.gov.uk", json['council']['contact_email']
     end
   end
-
-
 end

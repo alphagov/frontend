1b0f481258573dce2d32e0b570ef9574b99c09d5
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-02 13:03:18 +0100

    Show detailed guidance sections on subsection pages

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -10,7 +10,7 @@ class BrowseController < ApplicationController
   def section
     @category = content_api.tag(params[:section])
     return error_404 unless @category
-    
+
     response = content_api.sub_sections(params[:section])
     @sub_categories = response.results.sort_by { |category| category.title }
     setup_page_title(@category.title)
@@ -25,6 +25,8 @@ class BrowseController < ApplicationController
 
     @category = @sub_category.parent
     @results = content_api.sorted_by(tag_id, "alphabetical").results
+
+    detailed_guidance_sections(tag_id)
 
     setup_page_title(@sub_category.title)
     options = {
@@ -42,6 +44,15 @@ class BrowseController < ApplicationController
 protected
   def setup_page_title(category=nil)
     @page_title = category.nil? ? "Browse | GOV.UK Beta (Test)" : "#{category} | GOV.UK Beta (Test)"
+  end
+
+  def detailed_guidance_sections(tag_id)
+    begin
+      @detailed_categories = Frontend.detailed_guidance_content_api.sub_sections(tag_id).results
+      @detailed_categories.sort_by! { |category| category.title }
+    rescue GdsApi::HTTPErrorResponse
+      @detailed_categories = []
+    end
   end
 
 end
--- config/initializers/gds_api.rb
+++ config/initializers/gds_api.rb
@@ -1,3 +1,7 @@
 require 'gds_api/base'
+require 'gds_api/content_api'
+
 GdsApi::Base.logger = Logger.new(Rails.root.join("log/#{Rails.env}.api_client.log"))
 GdsApi::Base.default_options = {disable_timeout: true}
+
+Frontend.detailed_guidance_content_api = GdsApi::ContentApi.new(Plek.current.environment, endpoint_url: "#{Plek.current.find('whitehall')}/api/specialist/")

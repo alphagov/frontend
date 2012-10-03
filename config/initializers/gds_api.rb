a3bfb462473769a1012ce9f8db3c2502f7bc646a
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-03 14:41:05 +0100

    Make note of copies existing in preview/production

--- config/initializers/gds_api.rb
+++ config/initializers/gds_api.rb
@@ -5,3 +5,6 @@ GdsApi::Base.logger = Logger.new(Rails.root.join("log/#{Rails.env}.api_client.lo
 GdsApi::Base.default_options = {disable_timeout: true}
 
 Frontend.detailed_guidance_content_api = GdsApi::ContentApi.new(Plek.current.environment, endpoint_url: "#{Plek.current.find('whitehall')}/api/specialist/")
+
+# Note that copies of this exist in both preview and production
+# to_upload directories, so make sure your changes propagate there.

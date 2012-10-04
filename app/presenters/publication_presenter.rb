ea099ea83260489b9718324bc046bed712059670
John Griffin <johnog@gmail.com>
2012-10-04 12:24:20 +0100

    Extract PartPresenter to its own file

--- app/presenters/publication_presenter.rb
+++ app/presenters/publication_presenter.rb
@@ -1,21 +1,3 @@
-class PartPresenter
-  attr_reader :part
-
-  def initialize(part)
-    @part = part
-  end
-
-  PASS_THROUGH_KEYS = [
-    :slug, :title, :body, :name
-  ]
-
-  PASS_THROUGH_KEYS.each do |key|
-    define_method key do
-      part[key.to_s]
-    end
-  end
-end
-
 class PublicationPresenter
   include GdsApi::PartMethods
 

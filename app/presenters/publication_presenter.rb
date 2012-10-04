10e319239aa34a71e450b1c4a5373fcf86fef258
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-10-04 15:35:11 +0000

    Fix place editions - displaying the results
    The JS which filled in the results was getting an error page instead
    of JSON, so would error.

--- app/presenters/publication_presenter.rb
+++ app/presenters/publication_presenter.rb
@@ -17,7 +17,7 @@ class PublicationPresenter
     :body, :introduction, :expectations, :video_url, :alternative_title,
     :overview, :name, :video_summary, :continuation_link, :licence_overview,
     :link, :will_continue_on, :more_information, :minutes_to_complete,
-    :alternate_methods
+    :alternate_methods, :place_type
   ]
 
   PASS_THROUGH_KEYS.each do |key|

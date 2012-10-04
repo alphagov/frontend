90fe87f528e59cb50be3eb8a87ad3f2e64109ecd
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-10-04 16:20:11 +0000

    Return 404 if publication is not found for slug.

--- app/controllers/exit_controller.rb
+++ app/controllers/exit_controller.rb
@@ -1,22 +1,27 @@
 class ExitController < ApplicationController
 
+  class TargetNotAllowed < Exception
+  end
+
   def exit
-    publication = params_valid?(params) ? fetch_artefact : nil
-    if publication.nil?
-      logger.info { "root#exit rejected redirect to '#{params[:target]}' from #{params[:slug]}" }
-      statsd.increment('request.exist.404')
-      error_404 and return
-    elsif not publication.raw_response_body.include?(params[:target])
-      logger.warn { "root#exit rejected redirect to '#{params[:target]}' from #{params[:slug]}" }
-      statsd.increment('request.exist.403')
-      error 403 and return
-    end
+    raise RecordNotFound unless params_valid?(params)
+    publication = fetch_artefact
+    raise RecordNotFound unless publication
+    raise TargetNotAllowed unless publication.raw_response_body.include?(params[:target])
 
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
     response.headers["Cache-Control"] = "no-cache, must-revalidate"
 
     redirect_to params[:target], :status => 302
+  rescue RecordNotFound
+    logger.info { "root#exit rejected redirect to '#{params[:target]}' from #{params[:slug]}" }
+    statsd.increment('request.exist.404')
+    error_404 and return
+  rescue TargetNotAllowed
+    logger.warn { "root#exit rejected redirect to '#{params[:target]}' from #{params[:slug]}" }
+    statsd.increment('request.exist.403')
+    error 403 and return
   end
 
   protected

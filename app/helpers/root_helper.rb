1f8d4b4adb01ca250cb70f2d573c3ba4c1e54fd7
James Stewart <jys@ketlai.co.uk>
2012-10-04 22:13:26 +0100

    Remove 4 more outdated helpers
    These aren't used any more.

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -4,30 +4,6 @@ module RootHelper
 
   include GeoHelper
 
-  def has_further_information?(publication)
-    publication.parts.collect(&:slug).include?('further-information')
-  end
-
-  def programme_parts(publication)
-    publication.parts.reject { |part| part.slug == 'further-information' }
-  end
-
-  def transaction_path(slug,council,edition)
-    unless council
-      guide_path(slug,nil,edition)
-    else
-      if edition
-        publication_path(:slug=>slug,:edition=>edition,:snac=>council)
-      else
-        publication_path(:slug=>slug,:snac=>council)
-      end
-    end
-  end
-
-  def council_lookup_path(slug)
-    identify_council_path(:slug=>slug)
-  end
-
   def to_govspeak(field)
     Govspeak::Document.new(field || "", :auto_ids=>false).to_html.html_safe
   end

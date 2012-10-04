6f3b009db406f0bcc3fa7a6d255a15dfbc01c049
John Griffin <johnog@gmail.com>
2012-10-04 12:18:04 +0100

    Use PartMethods module rather than duplicating it

--- app/presenters/publication_presenter.rb
+++ app/presenters/publication_presenter.rb
@@ -17,6 +17,8 @@ class PartPresenter
 end
 
 class PublicationPresenter
+  include GdsApi::PartMethods
+
   attr_reader :artefact
 
   def initialize(artefact)
@@ -74,39 +76,6 @@ class PublicationPresenter
   def updated_at
     date = details["updated_at"]
     DateTime.parse(date) if date
-  end
-
-  # Parts stuff
-
-  def part_index(slug)
-    parts.index { |p| p.slug == slug }
-  end
-
-  def find_part(slug)
-    return nil unless index = part_index(slug)
-    parts[index]
-  end
-
-  def has_parts?(part)
-    !! (has_previous_part?(part) || has_next_part?(part))
-  end
-
-  def has_previous_part?(part)
-    index = part_index(part.slug)
-    !! (index && index > 0)
-  end
-
-  def has_next_part?(part)
-    index = part_index(part.slug)
-    !! (index && (index + 1) < parts.length)
-  end
-
-  def part_after(part)
-    part_at(part, 1)
-  end
-
-  def part_before(part)
-    part_at(part, -1)
   end
 
 private

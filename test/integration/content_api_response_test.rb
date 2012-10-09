f03183edf7b27c918ee347a95d2252a48d03777a
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-09 14:30:55 +0000

    Add tests to check Content API status response
    These tests are stubbing all the possible response codes that the
    Content API might produce and making sure that we're responding
    consistently within the frontend app.

---
+++ test/integration/content_api_response_test.rb
@@ -0,0 +1,42 @@
+require_relative "../integration_test_helper"
+
+class ContentApiResponseTest < ActionDispatch::IntegrationTest
+  def status_for_url(url, status)
+    url = "/#{url}" if url[0] != "/"
+    response = {
+      status: status,
+      body: "{\"test\":\"bleh\"}"
+    }
+    stub_request(:get, "https://contentapi.test.alphagov.co.uk#{url}.json").to_return(response)
+  end
+
+  should "render the page with a 500 error code when receiving a 500 status" do
+    status_for_url "/my-item", 500
+    visit "/my-item"
+    assert_equal 503, page.status_code
+  end
+
+  should "render the page with a 500 error code when receiving a 501 status" do
+    status_for_url "/my-item", 501
+    visit "/my-item"
+    assert_equal 503, page.status_code
+  end
+
+  should "render the page with a 500 error code when receiving a 502 status" do
+    status_for_url "/my-item", 502
+    visit "/my-item"
+    assert_equal 503, page.status_code
+  end
+
+  should "render the page with a 500 error code when receiving a 503 status" do
+    status_for_url "/my-item", 503
+    visit "/my-item"
+    assert_equal 503, page.status_code
+  end
+
+  should "render the page with a 500 error code when receiving a 504 status" do
+    status_for_url "/my-item", 504
+    visit "/my-item"
+    assert_equal 503, page.status_code
+  end
+end

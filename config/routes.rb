346f3d100efb2633095ffa5baeacc92e03c6958c
Jordan Hatch <jordan@jordanh.net>
2012-10-10 17:50:45 +0100

    Energy help campaign page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Frontend::Application.routes.draw do
 
   # Campaign pages.
   match "/workplacepensions", :to => "campaign#workplace_pensions"
+  match "/energyhelp", :to => "campaign#energy_help"
 
   with_options(as: "publication", to: "root#publication") do |pub|
     pub.match ":slug/video", format: :video

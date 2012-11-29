e22f556b0e1f178977578ef3eaaa8aea95d77144
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-11-29 16:10:01 +0000

    Add Welsh translation of answer page.

--- test/integration/answer_rendering_test.rb
+++ test/integration/answer_rendering_test.rb
@@ -32,4 +32,27 @@ class AnswerRenderingTest < ActionDispatch::IntegrationTest
 
     assert page.has_selector?("#test-related")
   end
+
+  should "render a quick answer in welsh correctly" do
+    # Note, this is using an english piece of content set to Welsh
+    # This is fine because we're testing the page furniture, not the rendering of the content.
+    artefact = content_api_response('vat-rates')
+    artefact["details"]["language"] = "cy"
+    content_api_has_an_artefact('vat-rates', artefact)
+
+    visit "/vat-rates"
+
+    assert_equal 200, page.status_code
+
+    within '#content' do
+      within 'header' do
+        assert page.has_content?("VAT rates")
+        assert page.has_content?("Ateb cyflym")
+      end
+
+      within '.article-container' do
+        assert page.has_selector?(".modified-date", :text => "Diweddarwyd ddiwethaf: 22 Hydref 2012")
+      end
+    end # within #content
+  end
 end

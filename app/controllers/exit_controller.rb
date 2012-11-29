1a8aafa213a93491fd69fc286d4243dc85d133d9
Gareth Rogers <grogers@thoughtworks.com>
2012-11-29 16:49:45 +0000

    Exit controller now cached on server, not browser

--- app/controllers/exit_controller.rb
+++ app/controllers/exit_controller.rb
@@ -11,7 +11,7 @@ class ExitController < ApplicationController
 
     raise RecordNotFound unless publication and publication.details and publication.details.link
 
-    response.headers["Cache-Control"] = "public, max-age=1800"
+    response.headers["Cache-Control"] = "public, max-age=0, s-maxage=1800"
 
     redirect_to publication.details.link, :status => 301
   rescue RecordNotFound
--- test/functional/exit_controller_test.rb
+++ test/functional/exit_controller_test.rb
@@ -20,7 +20,7 @@ class ExitControllerTest < ActionController::TestCase
       get :exit, slug: slug, format: format
 
       assert_equal 301, response.status
-      assert_equal "public, max-age=1800", response.headers["Cache-Control"]
+      assert_equal "public, max-age=0, s-maxage=1800", response.headers["Cache-Control"]
       assert_redirected_to target
     end
 

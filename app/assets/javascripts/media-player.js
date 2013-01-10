e6c6e21cacea772fb813902add96f36a49598f77
tombye <tom.byers@digital.cabinet-office.gov.uk>
2013-01-10 17:26:36 +0000

    Replace media player with updated version
    Copied across from
    alphagov/Accessible-Media-Player@ba085cebeade95a026615571b97204775394ee0c

--- app/assets/javascripts/media-player.js
+++ app/assets/javascripts/media-player.js
@@ -1,3 +1,5 @@
-//= require media-player/jquery.loader.js
-//= require media-player/jquery.player.js
 //= require media-player/swfobject/swfobject.js
+//= require media-player/mediaplayer_decorator.js
+//= require media-player/youtube_player.js
+//= require media-player/jquery.player.js
+//= require media-player/jquery.loader.js

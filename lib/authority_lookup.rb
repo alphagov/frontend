ebd48eef66f6ca8421fd5ba0b2f827e7974c11dd
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-04 21:20:32 +0000

    Refactor complex/duplicate method bodies
    These methods essentially repeat the same body code so reduce the
    duplication into something that's simpler to follow.

--- lib/authority_lookup.rb
+++ lib/authority_lookup.rb
@@ -2,29 +2,38 @@ class AuthorityLookup
   cattr_accessor :authorities
 
   def self.find_slug_from_snac(snac)
-    self.load_authorities unless self.authorities
-    self.authorities.select {|slug, codes| codes["ons"] == snac.to_s }.keys.first || false
+    self.find_slug_from_code("ons", snac)
   end
 
   def self.find_slug_from_gss(gss)
-    self.load_authorities unless self.authorities
-    self.authorities.select {|slug, codes| codes["gss"] == gss.to_s }.keys.first || false
+    self.find_slug_from_code("gss", gss)
   end
 
   def self.find_snac(slug)
-    self.load_authorities unless self.authorities
-    return self.authorities[slug]["ons"] if self.authorities[slug]
-    return false
+    self.find_code("ons", slug)
   end
 
   def self.find_gss(slug)
-    self.load_authorities unless self.authorities
-    return self.authorities[slug]["gss"] if self.authorities[slug]
-    return false
+    self.find_code("gss", slug)
   end
 
   def self.load_authorities
-    json = File.open( Rails.root.join('lib', 'data', 'authorities.json') ).read
+    json = File.open(Rails.root.join("lib", "data", "authorities.json")).read
     self.authorities = JSON.parse(json)
+  end
+
+  private
+
+  def self.find_code(type, slug)
+    self.load_authorities unless self.authorities
+    return self.authorities[slug][type] if self.authorities[slug]
+    return false
+  end
+
+  def self.find_slug_from_code(type, code)
+    self.load_authorities unless self.authorities
+    self.authorities.select do |slug, codes|
+      codes[type] == code.to_s
+    end.keys.first || false
   end
 end

509b7c4d05c738e54254dc9d4dabcf64f6303c57
Jack Franklin <jack@jackfranklin.net>
2013-03-01 11:52:48 +0000

    Move code into DOM Ready event

--- app/assets/javascripts/views/travel-advice.js
+++ app/assets/javascripts/views/travel-advice.js
@@ -1,5 +1,5 @@
 (function ($) {
-  jQuery.expr[':'].contains = function (obj, index, meta){
+  jQuery.expr[':'].contains = function(obj, index, meta){
     return (obj.textContent || obj.innerText || "").toUpperCase().indexOf(meta[3].toUpperCase()) >= 0;
   };
 
@@ -7,36 +7,41 @@
     return $("section#" + headingFirstLetter.toUpperCase()).find("li:visible").length > 0;
   };
 
-  var input = $("form#country-filter input#country");
-  var listItems = $("ul.countries li");
-  var countryHeadings = $(".inner section").not(":first").children("h1");
+  $(function() {
+    var input = $("form#country-filter input#country"),
+        listItems = $("ul.countries li"),
+        countryHeadings = $(".inner section").not(":first").children("h1");
 
-  var filterHeadings = function() {
-    countryHeadings.each(function(index, elem) {
-      var $elem = $(elem),
-          header = $elem.text();
-      if(headingHasVisibleCountries(header)) {
-        $elem.show();
+    var filterHeadings = function() {
+      countryHeadings.each(function(index, elem) {
+        var $elem = $(elem),
+            header = $elem.text();
+
+        if(headingHasVisibleCountries(header)) {
+          $elem.show();
+        } else {
+          $elem.hide();
+        }
+
+      });
+    };
+
+    input.change(function() {
+      var filter = $(this).val();
+
+      if (filter && filter.length > 0) {
+        listItems.show();
+        listItems.filter(":not(:contains(" + filter + "))").hide();
+        filterHeadings();
       } else {
-        $elem.hide();
+        listItems.show();
+        countryHeadings.show();
       }
+
+      return false;
+
+    }).keyup(function() {
+      $(this).change();
     });
-  };
-
-  input.change(function () {
-    var filter = $(this).val();
-
-    if (filter && filter.length > 0) {
-      listItems.show();
-      listItems.filter(":not(:contains(" + filter + "))").hide();
-      filterHeadings();
-    } else {
-      listItems.show();
-      countryHeadings.show();
-    }
-
-    return false;
-  }).keyup(function () {
-    $(this).change();
   });
 }(jQuery));

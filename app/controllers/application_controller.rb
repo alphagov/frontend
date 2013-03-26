fc72febf4588ea4bfe06cef06c314d5426b49076
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-03-26 11:31:18 +0000

    Fix intentation in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,79 +32,79 @@ class ApplicationController < ActionController::Base
     render status: status_code, text: "#{status_code} error"
   end
 
-  protected
-    def statsd
-      @statsd ||= Statsd.new("localhost").tap do |c|
-        c.namespace = ENV['GOVUK_STATSD_PREFIX'].to_s
-      end
+protected
+  def statsd
+    @statsd ||= Statsd.new("localhost").tap do |c|
+      c.namespace = ENV['GOVUK_STATSD_PREFIX'].to_s
     end
+  end
 
-    def set_expiry(duration = 30.minutes)
-      unless Rails.env.development?
-        expires_in(duration, :public => true)
-      end
+  def set_expiry(duration = 30.minutes)
+    unless Rails.env.development?
+      expires_in(duration, :public => true)
     end
+  end
 
-    def set_slimmer_artefact_headers(artefact, slimmer_headers = {})
-      slimmer_headers[:format] ||= artefact["format"]
-      set_slimmer_headers(slimmer_headers)
-      set_slimmer_artefact(artefact)
+  def set_slimmer_artefact_headers(artefact, slimmer_headers = {})
+    slimmer_headers[:format] ||= artefact["format"]
+    set_slimmer_headers(slimmer_headers)
+    set_slimmer_artefact(artefact)
+  end
+
+  def fetch_artefact(snac = nil, location = nil)
+    options = { snac: snac, edition: params[:edition] }.delete_if { |k,v| v.blank? }
+    if location
+      options[:latitude]  = location.lat
+      options[:longitude] = location.lon
     end
+    artefact = content_api.artefact(params[:slug], options)
 
-    def fetch_artefact(snac = nil, location = nil)
-      options = { snac: snac, edition: params[:edition] }.delete_if { |k,v| v.blank? }
-      if location
-        options[:latitude]  = location.lat
-        options[:longitude] = location.lon
-      end
-      artefact = content_api.artefact(params[:slug], options)
-
-      unless artefact
-        logger.warn("Failed to fetch artefact #{params[:slug]} from Content API. Response code: 404")
-        raise RecordNotFound
-      end
-
-      unless supported_artefact_formats.include?(artefact['format'])
-        raise UnsupportedArtefactFormat
-      end
-      artefact
-    rescue GdsApi::HTTPErrorResponse => e
-      if e.code == 410
-        raise RecordArchived
-      elsif e.code >= 500
-        statsd.increment("content_api_error")
-      end
-      raise
-    rescue URI::InvalidURIError
-      logger.warn("Failed to fetch artefact from Content API.")
+    unless artefact
+      logger.warn("Failed to fetch artefact #{params[:slug]} from Content API. Response code: 404")
       raise RecordNotFound
     end
 
-    def content_api
-      @content_api ||= GdsApi::ContentApi.new(
-        Plek.current.find("contentapi"),
-        content_api_options
-      )
+    unless supported_artefact_formats.include?(artefact['format'])
+      raise UnsupportedArtefactFormat
     end
-
-    def supported_artefact_formats
-      %w{answer business_support completed_transaction guide licence local_transaction place programme transaction travel-advice video}
+    artefact
+  rescue GdsApi::HTTPErrorResponse => e
+    if e.code == 410
+      raise RecordArchived
+    elsif e.code >= 500
+      statsd.increment("content_api_error")
     end
+    raise
+  rescue URI::InvalidURIError
+    logger.warn("Failed to fetch artefact from Content API.")
+    raise RecordNotFound
+  end
 
-    def validate_slug_param(param_name = :slug)
-      if params[param_name].parameterize != params[param_name]
-        error 404
-      end
-    rescue ArgumentError # Triggered by malformed UTF-8
+  def content_api
+    @content_api ||= GdsApi::ContentApi.new(
+      Plek.current.find("contentapi"),
+      content_api_options
+    )
+  end
+
+  def supported_artefact_formats
+    %w{answer business_support completed_transaction guide licence local_transaction place programme transaction travel-advice video}
+  end
+
+  def validate_slug_param(param_name = :slug)
+    if params[param_name].parameterize != params[param_name]
       error 404
     end
+  rescue ArgumentError # Triggered by malformed UTF-8
+    error 404
+  end
 
-  private
-    def content_api_options
-      options = CONTENT_API_CREDENTIALS
-      unless request.format == :atom
-        options = options.merge(web_urls_relative_to: Plek.current.website_root)
-      end
-      options
+private
+  def content_api_options
+    options = CONTENT_API_CREDENTIALS
+    unless request.format == :atom
+      options = options.merge(web_urls_relative_to: Plek.current.website_root)
     end
+    options
+  end
 end

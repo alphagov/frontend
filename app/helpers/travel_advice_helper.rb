d87a9160660945c7e5c9e5ce8b5c39a7e4b83221
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-03-27 14:51:33 +0000

    Refactor atom feeds to use common partial for entries
    This involved adding a new presenter class for the index views so that the data for the index view quacks like the data for the individual country view.

--- app/helpers/travel_advice_helper.rb
+++ app/helpers/travel_advice_helper.rb
@@ -2,11 +2,12 @@ module TravelAdviceHelper
 
   def group_by_initial_letter(countries)
     countries.group_by do |country|
-      country['name'][0] if country and country['name']
+      country.name[0] if country and country.name
     end
   end
 
   def readable_time(time)
-    Time.parse(time).strftime("%e %B %Y")
+    time = DateTime.parse(time) unless time.respond_to?(:strftime)
+    time.strftime("%e %B %Y")
   end
 end
---
+++ app/views/travel_advice/_country.atom.builder
@@ -0,0 +1,9 @@
+feed.entry(country,
+           :id => "#{country.web_url}##{country.updated_at}",
+           :url => country.web_url) do |entry|
+  entry.title(country.title)
+  entry.link(:rel => "self", :type => "application/atom+xml", :href => "#{country.web_url}.atom")
+  entry.summary(:type => :xhtml) do |summary|
+    summary << numericise_html_entities(country.change_description)
+  end
+end

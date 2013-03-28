49621fdf0788fd531aa067f64ea5411b0d349917
Jack Franklin <jack@jackfranklin.net>
2013-03-28 09:44:55 +0000

    First pass at allowing filtering by synonyms

--- app/assets/javascripts/views/travel-advice.js
+++ app/assets/javascripts/views/travel-advice.js
@@ -7,44 +7,71 @@
     return $("section#" + headingFirstLetter.toUpperCase()).find("li:visible").length > 0;
   };
 
-  $(function() {
-    var input = $("form#country-filter input#country"),
-        listItems = $("ul.countries li"),
-        countryHeadings = $(".inner section").not(":first").children("h1");
+  var input = $("form#country-filter input#country"),
+      listItems = $("ul.countries li"),
+      countryHeadings = $(".inner section").not(":first").children("h1");
 
-    var filterHeadings = function() {
-      countryHeadings.each(function(index, elem) {
-        var $elem = $(elem),
-            header = $elem.text();
-
-        if(headingHasVisibleCountries(header)) {
-          $elem.show();
-        } else {
-          $elem.hide();
-        }
-
-      });
-    };
-
-    input.change(function() {
-      var filter = $(this).val();
-
-      if(filter && filter.length > 0) {
-        listItems.show();
-        listItems.filter(":not(:contains(" + filter + "))").hide();
-        filterHeadings();
-      } else {
-        listItems.show();
-        countryHeadings.show();
-      }
-
-      return false;
-    }).keyup(function() {
-      $(this).change();
-    }).keypress(function(event) {
-      if (event.which == 13) {
-        event.preventDefault();
-      }
+  var filterHeadings = function() {
+    countryHeadings.each(function(index, elem) {
+      var $elem = $(elem), header = $elem.text();
+      headingHasVisibleCountries(header) ? $elem.show() : $elem.hide();
     });
+  };
+
+  var filterListItems = function(filter) {
+    if(filter && filter.length > 0) {
+      listItems.show();
+      listItems.filter(":not(:contains(" + filter + "))").hide();
+      filterHeadings();
+    } else {
+      listItems.show();
+      countryHeadings.show();
+    }
+  };
+
+  var findSynonym = function(search) {
+    console.log("called", search);
+    // - United States -> USA
+    // - America -> USA
+    // - Dubai -> United Arab Emirates
+    // - UAE -> United Arab Emirates
+    // - East Timor -> Timor-Leste
+    // - Ivory Coast -> Côte d'Ivoire
+    // - PNG -> Papua New Guinea
+    // - Ibiza -> Spain
+    var country_synonyms = {
+      "USA": "United States",
+      "America": "United States",
+      "Dubai": "United Arab Emirates",
+      "UAE": "United Arab Emirates"
+    };
+    if(search.length > 1) {
+      for(var prop in country_synonyms) {
+        console.log("search", search, "prop", prop);
+        if(prop.toLowerCase().indexOf(search.toLowerCase()) > -1) {
+          console.log("found", prop);
+          filterListItems(country_synonyms[prop]);
+          break;
+        }
+      }
+    }
+  }
+
+
+
+  input.change(function(e) {
+    var filter = $(this).val();
+
+    filterListItems(filter);
+    findSynonym(filter);
+
+    e.preventDefault();
+
+  }).keyup(function() {
+    $(this).change();
+  }).keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+    }
   });
 }(jQuery));

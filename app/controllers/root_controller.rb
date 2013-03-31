ccf21e637ecf90b1beaa60b53396fec85d83b9ea
James Stewart <jys@ketlai.co.uk>
2013-03-30 22:10:33 -0500

    Begin breaking LicenceDetails#from_artefact into smaller methods

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -158,29 +158,35 @@ protected
 
   class LicenceDetails
     def from_artefact(artefact, licence_authority_slug, snac_code, interaction = nil)
-      licence_attributes = { licence: artefact['details']['licence'] }
+      licence = artefact['details']['licence']
+      licence_attributes = { licence: licence }
 
-      return false if licence_attributes[:licence].blank? or licence_attributes[:licence]['error'].present?
+      return false if missing_or_invalid_licence?(licence)
 
-      licence_attributes[:authority] = authority_for_licence(licence_attributes[:licence], licence_authority_slug, snac_code)
+      licence_attributes[:authority] = authority_for_licence(licence, licence_authority_slug, snac_code)
 
-      if ! licence_attributes[:authority] and (snac_code.present? || licence_authority_slug.present?)
-        raise RecordNotFound
-      end
+      authority_required = snac_code.present? || licence_authority_slug.present?
+      raise RecordNotFound if authority_required && ! licence_attributes[:authority]
 
       if licence_attributes[:authority]
         licence_attributes[:action] = interaction
         available_actions = licence_attributes[:authority]['actions'].keys + ["apply","renew","change"]
 
-        if licence_attributes[:action] && ! available_actions.include?(licence_attributes[:action])
-          raise RecordNotFound
-        end
+        raise RecordNotFound unless appropriate_action_available?(licence_attributes[:action], available_actions)
       end
 
       return licence_attributes
     end
 
     protected
+      def appropriate_action_available?(action, available_actions)
+        action.blank? || available_actions.include?(action)
+      end
+
+      def missing_or_invalid_licence?(licence)
+        licence.blank? || licence['error'].present?
+      end
+
       def authority_for_licence(licence, licence_authority_slug, snac_code)
         if licence["location_specific"]
           if snac_code

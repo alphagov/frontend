ca8cd255c7836e7b15aa60001a80fa1b2e614caa
James Stewart <jys@ketlai.co.uk>
2013-03-30 22:11:48 -0500

    Move LicenceDetails class out to its own file

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,6 +2,7 @@ require "slimmer/headers"
 require "authority_lookup"
 require "local_transaction_location_identifier"
 require "licence_location_identifier"
+require "licence_details"
 
 class RootController < ApplicationController
   include RootHelper
@@ -154,52 +155,6 @@ protected
     end
 
     artefact['details'].slice('local_authority', 'local_service', 'local_interaction')
-  end
-
-  class LicenceDetails
-    def from_artefact(artefact, licence_authority_slug, snac_code, interaction = nil)
-      licence = artefact['details']['licence']
-      licence_attributes = { licence: licence }
-
-      return false if missing_or_invalid_licence?(licence)
-
-      licence_attributes[:authority] = authority_for_licence(licence, licence_authority_slug, snac_code)
-
-      authority_required = snac_code.present? || licence_authority_slug.present?
-      raise RecordNotFound if authority_required && ! licence_attributes[:authority]
-
-      if licence_attributes[:authority]
-        licence_attributes[:action] = interaction
-        available_actions = licence_attributes[:authority]['actions'].keys + ["apply","renew","change"]
-
-        raise RecordNotFound unless appropriate_action_available?(licence_attributes[:action], available_actions)
-      end
-
-      return licence_attributes
-    end
-
-    protected
-      def appropriate_action_available?(action, available_actions)
-        action.blank? || available_actions.include?(action)
-      end
-
-      def missing_or_invalid_licence?(licence)
-        licence.blank? || licence['error'].present?
-      end
-
-      def authority_for_licence(licence, licence_authority_slug, snac_code)
-        if licence["location_specific"]
-          if snac_code
-            licence['authorities'].first
-          end
-        else
-          if licence['authorities'].size == 1
-            licence['authorities'].first
-          elsif licence_authority_slug
-            licence['authorities'].select {|authority| authority['slug'] == licence_authority_slug }.first
-          end
-        end
-      end
   end
 
   def licence_details(artefact, licence_authority_slug, snac_code)

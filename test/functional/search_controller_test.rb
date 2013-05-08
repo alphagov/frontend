38f16e8d266d361ee5d801944db9decdf2fd1b03
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-05-08 16:03:49 +0000

    Remove a long-redundant parameter
    This was added to get results including the government tab, or artificial
    inside gov results, but both of those features were enabled by default a long
    time ago.

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -111,8 +111,6 @@ class SearchControllerTest < ActionController::TestCase
     get :index, q: "search-term"
     assert_select "a[href='#mainstream-results']", text: "General results (3)"
     assert_select "a[href='#detailed-results']", text: "Detailed guidance (1)"
-    # Temporarily, Government results only visible with parameter
-    get :index, q: "search-term", government: "1"
     assert_select "a[href='#government-results']", text: "Inside Government (2)"
   end
 
@@ -273,7 +271,7 @@ class SearchControllerTest < ActionController::TestCase
 
     Frontend.mainstream_search_client.stubs(:search).returns([normal_result, inside_government_link])
 
-    get :index, { q: "bleh", government: "1" }
+    get :index, { q: "bleh" }
     assert_select '#mainstream-results li:first-child a', text: "QUEUE JUMPER"
     assert_select '#mainstream-results li:nth-child(2) a', text: 'BORING'
   end

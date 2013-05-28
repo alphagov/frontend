f7234168ee805b7b20fb4a4dd4da6871ae46eeca
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-05-28 19:21:05 +0100

    Allow rummager host to be overridden using env var

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -1,4 +1,5 @@
 require 'gds_api/rummager'
-Frontend.mainstream_search_client = GdsApi::Rummager.new(Plek.current.find('search') + '/mainstream')
-Frontend.detailed_guidance_search_client = GdsApi::Rummager.new(Plek.current.find('search') + '/detailed')
-Frontend.government_search_client = GdsApi::Rummager.new(Plek.current.find('search') + '/government')
+rummager_host = env["RUMMAGER_HOST"] || Plek.current.find('search')
+Frontend.mainstream_search_client = GdsApi::Rummager.new(rummager_host + '/mainstream')
+Frontend.detailed_guidance_search_client = GdsApi::Rummager.new(rummager_host + '/detailed')
+Frontend.government_search_client = GdsApi::Rummager.new(rummager_host + '/government')

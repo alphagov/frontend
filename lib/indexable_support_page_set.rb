a424a2637b134265715265faba7efe2ff32d6999
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-06-21 15:44:23 +0000

    Escape regex special chars
    We only want to match literal dots, not anything.

--- lib/indexable_support_page_set.rb
+++ lib/indexable_support_page_set.rb
@@ -3,7 +3,7 @@ require 'indexable_support_page'
 class IndexableSupportPageSet
   def pages
     all_files = Dir.glob("#{Rails.root}/app/views/support/**.erb")
-    files_to_index = all_files.reject { |file| file =~ /index.html.erb/ }
+    files_to_index = all_files.reject { |file| file =~ /index\.html\.erb/ }
     files_to_index.map do |filename|
       IndexableSupportPage.new(filename)
     end

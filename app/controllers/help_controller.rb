0589ee35cd5693712e25edd15c8e5fec0eeff213
Jordan Hatch <jordan@jordanh.net>
2013-08-27 15:04:23 +0100

    Remove redundant rescue_from calls from HelpController

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -4,9 +4,6 @@ class HelpController < ApplicationController
   before_filter :setup_slimmer_artefact
   before_filter :set_expiry
 
-  rescue_from AbstractController::ActionNotFound, :with => :error_404
-  rescue_from ActionView::MissingTemplate, :with => :error_404
-
   def index
   end
 

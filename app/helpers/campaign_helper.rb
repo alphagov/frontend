339ecec3163426be51332994275553b5b74a9328
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-09-20 12:28:14 +0000

    Add helper for campaign format

---
+++ app/helpers/campaign_helper.rb
@@ -0,0 +1,31 @@
+module CampaignHelper
+  def campaign_image_url(publication, size)
+    if image_attrs = publication.details["#{size}_image"]
+      image_attrs['web_url']
+    end
+  end
+
+  def formatted_organisation_name(publication)
+    organisation_attr(publication, 'formatted_name')
+  end
+
+  def organisation_url(publication)
+    organisation_attr(publication, 'url')
+  end
+
+  def organisation_crest(publication)
+    organisation_attr(publication, 'crest')
+  end
+
+  def organisation_brand_colour(publication)
+    organisation_attr(publication, 'brand_colour')
+  end
+
+private
+  
+  def organisation_attr(publication, attr_name)
+    if org_attrs = publication.details['organisation']
+      org_attrs[attr_name]
+    end
+  end
+end
---
+++ test/unit/helpers/campaign_helper_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class CampaignHelperTest < ActionView::TestCase
+  include CampaignHelper
+
+  setup do
+    @model = PublicationPresenter.new(JSON.parse(
+      File.read(
+        Rails.root.join("test/fixtures/britain-is-great.json")
+      )
+    ))
+  end
+
+  test "campaign_image_url" do
+    assert_equal "http://static.dev.gov.uk/media/52380a1e686c82231f000003/britain-is-great.jpg",
+      campaign_image_url(@model, 'large')
+    assert_equal "http://static.dev.gov.uk/media/52380a1e686c82231f000005/britain-is-great.jpg",
+      campaign_image_url(@model, 'medium')
+    assert_equal "http://static.dev.gov.uk/media/52383e4b686c82308d000001/britain-is-great.jpg",
+      campaign_image_url(@model, 'small')
+  end
+
+  test "formatted_organisation_name" do
+    assert_equal "Cabinet Office", formatted_organisation_name(@model)
+  end
+
+  test "organisation_url" do
+    assert_equal "https://www.gov.uk/government/organisations/cabinet-office", organisation_url(@model)
+  end
+
+  test "organisation_crest" do
+    assert_equal "single-identity", organisation_crest(@model)
+  end
+
+  test "organisation_brand_colour" do
+    assert_equal "cabinet-office", organisation_brand_colour(@model) 
+  end
+
+end

10c2962d5a3b14d7974cbda29828a9ffc58db3ae
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-10-14 11:28:22 +0000

    Remove unused campaign controller actions

--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -3,40 +3,9 @@ require "slimmer/headers"
 class CampaignController < ApplicationController
 
   before_filter :setup_slimmer_artefact
-  before_filter :set_expiry, :except => [:royal_mail_shares]
-
-  def unimoney
-  end
-
-  def workplace_pensions
-  end
+  before_filter :set_expiry
 
   def uk_welcomes
-  end
-
-  def sort_my_tax
-  end
-
-  def new_licence_rules
-  end
-
-  def fire_kills
-  end
-
-  def know_before_you_go
-  end
-
-  def britain_is_great
-  end
-
-  ROYAL_MAIL_SHARES_START = Time.zone.parse("2013-09-12T07:00:00+01:00")
-  # This campaign has to be live on this date, and not before
-  def royal_mail_shares
-    if Time.zone.now < ROYAL_MAIL_SHARES_START and ! params[:show_me_the_page]
-      error_404
-    else
-      set_expiry
-    end
   end
 
 protected
--- test/functional/campaign_controller_test.rb
+++ test/functional/campaign_controller_test.rb
@@ -3,18 +3,13 @@ require_relative "../test_helper"
 class CampaignControllerTest < ActionController::TestCase
 
   should "set slimmer format of campaign" do
-    get :workplace_pensions
+    get :uk_welcomes
     assert_equal "campaign",  response.headers["X-Slimmer-Format"]
   end
 
   should "set correct expiry headers" do
-    get :workplace_pensions
+    get :uk_welcomes
     assert_equal "max-age=1800, public",  response.headers["Cache-Control"]
-  end
-
-  should "load the workplace pensions campaign" do
-    get :workplace_pensions
-    assert_response :success
   end
 
   should "load the UK Welcomes campaign" do
@@ -22,63 +17,4 @@ class CampaignControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  should "load the sort my tax campaign" do
-    get :sort_my_tax
-    assert_response :success
-  end
-
-  should "load the dvla new licence rules campaign" do
-    get :new_licence_rules
-    assert_response :success
-  end
-
-  should "load the firekills campaign" do
-    get :fire_kills
-    assert_response :success
-  end
-
-  should "load the know before you go campaign" do
-    get :know_before_you_go
-    assert_response :success
-  end
-
-  context "the royal_mail_shares campaign" do
-
-    context "before the start date" do
-      setup do
-        Timecop.freeze(CampaignController::ROYAL_MAIL_SHARES_START - 40.minutes)
-      end
-
-      should "return a 404" do
-        get :royal_mail_shares
-        assert_response :missing
-      end
-
-      should "not set the cache headers" do
-        get :royal_mail_shares
-        assert_nil response.headers["Cache-Control"]
-      end
-
-      should "show the page when given 'show_me_the_page' query param" do
-        get :royal_mail_shares, :show_me_the_page => "1"
-        assert_response :success
-      end
-    end
-
-    context "after the start time" do
-      setup do
-        Timecop.freeze(CampaignController::ROYAL_MAIL_SHARES_START + 5.minutes)
-      end
-
-      should "show the page" do
-        get :royal_mail_shares
-        assert_response :success
-      end
-
-      should "set the cache headers" do
-        get :royal_mail_shares
-        assert_equal "max-age=1800, public",  response.headers["Cache-Control"]
-      end
-    end
-  end
 end

cebc2196b1d6170f503a1cdad8ae362ec1376229
Jake Benilov <benilov@gmail.com>
2013-11-12 10:14:50 +0000

    service feedback: detect if JS enabled or not

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -4,4 +4,6 @@ $(document).ready(function () {
   if ($container.find('.js-tabs').length) {
     $container.tabs();
   }
+
+  $('form#completed-transaction-form').append('<input type="hidden" name="service_feedback[javascript_enabled]" value="true"/>');
 });

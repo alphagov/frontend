7928b6c2f7631267b5daef478bd8dc9c2fd441e1
Jordan Hatch <jordan@jordanh.net>
2014-01-20 15:52:01 +0000

    Strip the sector name from artefact titles
    When the name of an artefact begins with the name of the sector (eg. 
    "Oil and gas: wells"), strip the sector from the title and upcase the 
    first character of the remaining string.

---
+++ app/helpers/industry_sectors_helper.rb
@@ -0,0 +1,18 @@
+module IndustrySectorsHelper
+
+  # This strips away the initial part of an item title if it contains the
+  # title of the current industry sector to remove any duplication. The
+  # leading character of the remainder of the title is then upcased.
+  #
+  # eg. "Oil and gas: wells" -> "Wells"
+  #
+  def industry_sector_item_title(title, sector)
+    pattern = /\A#{Regexp.escape(sector.title)}: /
+
+    if title =~ pattern
+      title = title.sub(pattern, '')
+      title[0] = title[0].upcase
+    end
+    title
+  end
+end

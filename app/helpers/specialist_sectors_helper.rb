83e9ef07f4f8b288216252782251a5eac5349e3e
Jordan Hatch <jordan@jordanh.net>
2014-02-13 12:32:40 +0000

    Rename industry_sectors to specialist_sectors
    This makes the necessary changes to Frontend to rename the tag type
    "industry_sector" to "specialist_sector".
    
    Here, I've renamed the existing IndustrySectorsController class to be
    SpecialistSectorsController, and its associated tests, view templates
    and helpers have been moved or renamed too.

--- app/helpers/specialist_sectors_helper.rb
+++ app/helpers/specialist_sectors_helper.rb
@@ -1,12 +1,12 @@
-module IndustrySectorsHelper
+module SpecialistSectorsHelper
 
   # This strips away the initial part of an item title if it contains the
-  # title of the current industry sector to remove any duplication. The
+  # title of the current specialist sector to remove any duplication. The
   # leading character of the remainder of the title is then upcased.
   #
   # eg. "Oil and gas: wells" -> "Wells"
   #
-  def industry_sector_item_title(title, sector)
+  def specialist_sector_item_title(title, sector)
     pattern = /\A#{Regexp.escape(sector.title)}: /
 
     if title =~ pattern

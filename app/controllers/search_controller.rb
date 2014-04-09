f0f7a30c4e39f244b2612898f900c4eda6b3f9ef
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-04-09 15:53:56 +0000

    WIP patch to display the organisation facets in search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -48,10 +48,12 @@ class SearchController < ApplicationController
       count: "#{requested_result_count}",
       q: @search_term,
       filter_organisations: [*params[:filter_organisations]],
+      facet_organisations: "100",
     }
     search_response = search_client.unified_search(search_params)
 
     results = UnifiedSearchResultsPresenter.new(search_response)
+    @facets = search_response["facets"]
     @spelling_suggestion = results.spelling_suggestion
     @result_count = results.result_count
     @results = results.results

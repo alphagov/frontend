26339bbbc2cde9d57db1d58d5a74448c37e1354a
Jordan Hatch <jordan@jordanh.net>
2014-05-01 14:33:54 +0100

    Stop redirecting /search.json to the Content API
    The new unified search controller has a JSON representation which is 
    used to load results in-place on the page. When we remove the 'ui' 
    feature-flag, the JSON route for unified search will conflict with 
    the existing redirect route.
    
    There are almost zero requests to /search.json in its current form, and 
    we recently changed the structure of the API response at the redirect's 
    destination without any complaint from users, so I don't think changing 
    this route is going to pose a problem.

--- test/integration/search_test.rb
+++ test/integration/search_test.rb
@@ -2,23 +2,6 @@ require_relative '../integration_test_helper'
 
 class SearchTest < ActionDispatch::IntegrationTest
 
-  context "redirecting JSON requests" do
-    should "redirect to the public API for the search term" do
-      get "/search.json?q=something"
-      assert_redirected_to "/api/search.json?q=something"
-    end
-
-    should "properly quote query params when redirecting" do
-      get "/search.json?q=does+this+work%3F"
-      assert_redirected_to "/api/search.json?q=does+this+work%3F"
-    end
-
-    should "redirect to the public API with no search term" do
-      get "/search.json"
-      assert_redirected_to "/api/search.json?q="
-    end
-  end
-
   should "return a 405 for POST requests to /search" do
     post "/search?q=foo"
     assert_equal 405, response.status

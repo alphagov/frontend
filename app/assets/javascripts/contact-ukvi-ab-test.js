afea9140b8eac5084ba77a9db440fbac9459f57f
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-05-20 14:07:25 +0100

    Refactor code and introduce JSLint
    - Removes duplication of the same method
      traversal (`window.location...`)
    - Introduces the correct front matter to get JSLint to understand our
      code
    - Changes to appease JSLint

--- app/assets/javascripts/contact-ukvi-ab-test.js
+++ app/assets/javascripts/contact-ukvi-ab-test.js
@@ -1,30 +1,39 @@
 //= require govuk/multivariate-test
-
+/*jslint browser: true,
+ indent: 2,
+ white: true
+*/
+/*global $, GOVUK */
 $(function(){
-  var disclaimer = '<p>Because of high call volumes the UK Visas and Immigration contact centre is currently not accepting calls.</p>';
-  var replacePhoneNumberWithExplanation = function() {
+  "use strict";
+  var disclaimer = '<p>Because of high call volumes the UK Visas and Immigration contact centre is currently not accepting calls.</p>',
+  replacePhoneNumberWithExplanation = function() {
     $('.contact').
       removeClass('contact').
       addClass('application-notice info-notice').
       html(disclaimer);
   };
 
+  function pathInLocation(path) {
+    return (window.location.href.indexOf(path) > -1);
+  }
+
   GOVUK.hideContactCentrePhoneNumbers = function() {
-    if (window.location.href.indexOf("/contact-ukvi/visas-and-settlement") > -1) {
+    if (pathInLocation("/contact-ukvi/visas-and-settlement")) {
       replacePhoneNumberWithExplanation();
-    } else if (window.location.href.indexOf("/contact-ukvi/british-citizenship-and-nationality") > -1) {
+    } else if (pathInLocation("/contact-ukvi/british-citizenship-and-nationality")) {
       $('.contact').
         html('<p><strong>Citizenship and nationality enquiries</strong><br><a href="mailto:ukbanationalityenquiries@ukba.gsi.gov.uk">ukbanationalityenquiries@ukba.gsi.gov.uk</a></p>').
         after('<div class="application-notice info-notice">' + disclaimer + '</div>');
-    } else if (window.location.href.indexOf("/contact-ukvi/european-nationals") > -1) {
+    } else if (pathInLocation("/contact-ukvi/european-nationals")) {
       $('.application-notice.info-notice:eq(1)').removeClass('application-notice info-notice');
       replacePhoneNumberWithExplanation();
-    };
+    }
   };
 
-  if(window.location.href.indexOf("/contact-ukvi/visas-and-settlement") > -1 ||
-     window.location.href.indexOf("/contact-ukvi/british-citizenship-and-nationality") > -1 ||
-     window.location.href.indexOf("/contact-ukvi/european-nationals") > -1 ) {
+  if(pathInLocation("/contact-ukvi/visas-and-settlement") ||
+     pathInLocation("/contact-ukvi/british-citizenship-and-nationality") ||
+     pathInLocation("/contact-ukvi/european-nationals") ) {
     new GOVUK.MultivariateTest({
       name: 'contact_ukvi_visas_and_settlement_contact_centre_50_50',
       customVarIndex: 13,

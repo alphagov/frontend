7e2d7179fbe76cd6e3719424272cc83c18d44110
steventux <steve.laing@digital.cabinet-office.gov.uk>
2014-09-15 16:13:02 +0000

    Track start page tab clicks

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -1,4 +1,20 @@
+(function () {
+
+  "use strict"
+  
+  window.GOVUK = window.GOVUK || {};
+
+  window.GOVUK.Transactions = {
+    trackStartPageTabs : function (e) {
+      var pagePath = e.target.href;
+      _gaq.push(['_trackEvent', 'startpages', 'tab', pagePath, 0, true]); 
+    }
+  };
+
+})();
+
 $(document).ready(function () {
+
   var $container = $('section.more');
 
   if ($container.find('.js-tabs').length) {
@@ -13,4 +29,7 @@ $(document).ready(function () {
     $(this).attr('disabled', 'disabled');
     $(this).parents('form').submit();
   });
+  
+  $('.transaction .nav-tabs a').click(window.GOVUK.Transactions.trackStartPageTabs);
+
 });

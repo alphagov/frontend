a125553a6c153d0b480b4132e9eb64ea8f99e1c8
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2014-12-09 13:18:37 +0000

    update the transaction_rendering test not overwrite it

--- test/integration/transaction_rendering_test.rb
+++ test/integration/transaction_rendering_test.rb
@@ -308,6 +308,20 @@ class TransactionRenderingTest < ActionDispatch::IntegrationTest
       end # within #content
     end
   end
+
+  context "exceptional tax-disc start page format" do
+    should "render a bespoke view" do
+      setup_api_responses('tax-disc')
+      visit "/tax-disc"
+      assert_equal 200, page.status_code
+      within("div.title h1") do
+        assert page.has_content?("Renew a tax disc")
+      end
+      within(".primary-apply") do
+        assert page.has_content?("Apply using the new service")
+      end
+    end
+  end
   context "exceptional vehicle-tax start page format" do
     should "render a bespoke view" do
       setup_api_responses('vehicle-tax')

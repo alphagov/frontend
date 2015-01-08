bd163882451975b7b7f8847479d6234fbb83d041
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-01-08 13:40:49 +0000

    Remove unused helpers
    None of the search helper methods were being used or tested, and the
    readable_time helper in travel advice helper wasn't being used either.
    Remove them.

--- app/helpers/travel_advice_helper.rb
+++ app/helpers/travel_advice_helper.rb
@@ -8,11 +8,6 @@ module TravelAdviceHelper
     end
   end
 
-  def readable_time(time)
-    time = DateTime.parse(time) unless time.respond_to?(:strftime)
-    time.strftime("%e %B %Y")
-  end
-
   def format_atom_change_description(text)
     # Encode basic entities([<>&'"]) as named, the rest as decimal
     simple_format(HTMLEntities.new.encode(text, :basic, :decimal))

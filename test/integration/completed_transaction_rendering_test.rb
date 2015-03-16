138850c073c8f399a30a94f80ae4c527e88b8859
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-03-16 10:25:07 +0000

    Organ donor promotion on completed transaction pg.
    https://trello.com/c/caDndyIX
    
    Completed transaction pages need to optionally show organ
    donor registration promotion. Display is toggled, and 'Join'
    link URL is configured from Publisher.

---
+++ test/integration/completed_transaction_rendering_test.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+require_relative '../integration_test_helper'
+
+class CompletedTransactionRenderingTest < ActionDispatch::IntegrationTest
+
+  context "a completed transaction edition" do
+    should "hide organ donor registration promotion when presentation toggle is not present" do
+      artefact = artefact_for_slug "no-organ-donation-registration-promotion"
+      artefact = artefact.merge({ format: "completed_transaction" })
+      content_api_has_an_artefact("no-organ-donation-registration-promotion", artefact)
+
+      visit "/no-organ-donation-registration-promotion"
+
+      assert_equal 200, page.status_code
+      within '.content-block' do
+        assert page.has_no_text?("Sign up to the NHS Organ Donor Register and help save the lives of people in need of transplants.")
+        assert page.has_no_link?("Register now")
+      end
+    end
+
+    should "show organ donor registration promotion and survey heading if related presentation toggle is turned-on" do
+      artefact = artefact_for_slug "shows-organ-donation-registration-promotion"
+      artefact = artefact.merge({
+        format: "completed_transaction",
+        details: {
+          presentation_toggles: { organ_donor_registration: { promote_organ_donor_registration: true, organ_donor_registration_url: '/organ-donor-registration-url' } }
+        }
+      })
+      content_api_has_an_artefact("shows-organ-donation-registration-promotion", artefact)
+
+      visit "/shows-organ-donation-registration-promotion"
+
+      assert_equal 200, page.status_code
+      within '.content-block' do
+        assert page.has_text?("Sign up to the NHS Organ Donor Register and help save the lives of people in need of transplants.")
+        assert page.has_link?("Register now", href: "/organ-donor-registration-url")
+        within 'h2.satisfaction-survey-heading' do
+          assert page.has_text?("Satisfaction survey")
+        end
+      end
+    end
+  end
+end

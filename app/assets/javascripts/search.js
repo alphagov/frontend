58707a13828d4103a81b57bed78e1a791ad3e93e
Kushal Pisavadia <kushi.p@gmail.com>
2015-05-26 17:19:25 +0100

    Inline search-analytics.js code into search.js
    This file originally contained all of the relevant code for providing
    metrics around search.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,6 +5,35 @@
       $ = root.jQuery,
       $searchResults = $('#results .results-list');
 
+  if (typeof root.GOVUK === 'undefined') { root.GOVUK = {}; }
+  if (typeof root.GOVUK.Search === 'undefined') { root.GOVUK.Search = {}; }
+
+  var extractSearchURLs = function ($searchResults) {
+    if ($searchResults.length <= 0) {
+      return [];
+    }
+
+    function extractSearchURL(index, element) {
+      var foundURL = $(element).find('h3 a');
+
+      if (foundURL.parents('.descoped-results').length) {
+        return {
+          'href': foundURL.attr('href'),
+          'scoped': true
+        };
+      } else {
+        return {
+          'href': foundURL.attr('href'),
+          'scoped': false
+        };
+      }
+    }
+
+    return $searchResults.children().map(extractSearchURL);
+  };
+
+  GOVUK.Search.extractSearchURLs = extractSearchURLs;
+
   if ($searchResults.length > 0) {
     $('.js-openable-filter').each(function(){
       new GOVUK.CheckboxFilter({el:$(this)});

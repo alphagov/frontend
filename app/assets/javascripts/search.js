84a2deb7dcf0a5f605909ee3deb6dd7fa3ab5aa8
Kushal Pisavadia <kushi.p@gmail.com>
2015-05-27 15:18:45 +0100

    Put self-executing functions into single namespace
    Move all of the self-executing functions in the `search.js` file into a
    single namespace `GOVUK.search` and trigger them using a single
    initialiser: `GOVUK.search.init()`. This is consistent with our
    JavaScript styleguide[1].
    
    [1] https://github.com/alphagov/styleguides/blob/master/js.md#modules

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,10 +3,25 @@
 
   window.GOVUK = window.GOVUK || {};
 
-  var $ = window.jQuery,
-      $searchResults = $('#results .results-list');
+  var $ = window.jQuery;
 
-  GOVUK.search = {
+  var search = {
+    init: function () {
+      var $searchResults = $('#results .results-list');
+
+      search.enableLiveSearchCheckbox($searchResults);
+      search.trackExternalSearchClicks($searchResults);
+      search.trackSearchClicks($searchResults);
+      search.trackSpellingSuggestions();
+    },
+    enableLiveSearchCheckbox: function ($searchResults) {
+      if ($searchResults.length > 0) {
+        $('.js-openable-filter').each(function(){
+          new GOVUK.CheckboxFilter({el:$(this)});
+        });
+        GOVUK.liveSearch.init();
+      };
+    },
     extractSearchURLs: function ($searchResults) {
       if ($searchResults.length <= 0) {
         return [];
@@ -29,78 +44,74 @@
       }
 
       return $searchResults.children().map(extractSearchURL);
-    }
-  };
-
-  if ($searchResults.length > 0) {
-    $('.js-openable-filter').each(function(){
-      new GOVUK.CheckboxFilter({el:$(this)});
-    });
-    GOVUK.liveSearch.init();
-  };
-
-  (function trackSearchResultURLs() {
-    var searchURLs = GOVUK.search.extractSearchURLs($searchResults);
-
-    if (searchURLs.length) {
-      GOVUK.analytics.trackEvent('Search Results', 'Shown', {
-        label: searchURLs,
-        nonInteraction: true
-      });
-    }
-  }());
-
-  (function trackSpellingSuggestion() {
-    var $spellingSuggestion = $('.spelling-suggestion a');
-
-    if ($spellingSuggestion.length) {
-      GOVUK.analytics.trackEvent('Search suggestions', 'Shown', {
-        label: $spellingSuggestion.text(),
-        nonInteraction: true
-      });
-    }
-  }());
-
-  (function trackSearchClicks(){
-    if($searchResults.length === 0 || !GOVUK.cookie){
-      return;
-    }
-    $searchResults.on('click', 'a', function(e){
-      var $link = $(e.target),
-          sublink = '',
-          position, href, startAt;
-
-      var getStartAtValue = function() {
-        var queryString = window.location.search,
-            startAtRegex = /(&|\?)start=([0-9]+)(&|$)/,
-            startAt = 0,
-            matches;
-
-        matches = queryString.match(startAtRegex);
-
-        if (matches !== null) {
-          startAt = parseInt(matches[2], 10);
-        }
-        return startAt;
-      };
-
-      if($link.closest('ul').hasClass('sections')){
-        href = $link.attr('href');
-        if(href.indexOf('#') > -1){
-          sublink = '&sublink='+href.split('#')[1];
-        }
-        $link = $link.closest('ul');
+    },
+    trackExternalSearchClicks: function ($searchResults) {
+      if ($searchResults.length > 0) {
+        new GOVUK.TrackExternalLinks($searchResults);
+      }
+    },
+    trackSearchClicks: function ($searchResults) {
+      if($searchResults.length === 0 || !GOVUK.cookie){
+        return;
       }
 
-      startAt = getStartAtValue();
-      position = $link.closest('li').index() + startAt + 1; // +1 so it isn't zero offset
-      GOVUK.analytics.callOnNextPage('setSearchPositionDimension', 'position=' + position + sublink);
-    });
-  }());
+      $searchResults.on('click', 'a', function(e){
+        var $link = $(e.target),
+            sublink = '',
+            position, href, startAt;
 
-  (function trackExternalSearchClicks(){
-    if($searchResults.length > 0){
-      new GOVUK.TrackExternalLinks($searchResults);
+        var getStartAtValue = function() {
+          var queryString = window.location.search,
+              startAtRegex = /(&|\?)start=([0-9]+)(&|$)/,
+              startAt = 0,
+              matches;
+
+          matches = queryString.match(startAtRegex);
+
+          if (matches !== null) {
+            startAt = parseInt(matches[2], 10);
+          }
+
+          return startAt;
+        };
+
+        if ($link.closest('ul').hasClass('sections')) {
+          href = $link.attr('href');
+
+          if (href.indexOf('#') > -1) {
+            sublink = '&sublink='+href.split('#')[1];
+          }
+
+          $link = $link.closest('ul');
+        }
+
+        startAt = getStartAtValue();
+        position = $link.closest('li').index() + startAt + 1; // +1 so it isn't zero offset
+        GOVUK.analytics.callOnNextPage('setSearchPositionDimension', 'position=' + position + sublink);
+      });
+    },
+    trackSearchResultURLs: function () {
+      var searchURLs = search.extractSearchURLs($searchResults);
+
+      if (searchURLs.length) {
+        GOVUK.analytics.trackEvent('Search Results', 'Shown', {
+          label: searchURLs,
+          nonInteraction: true
+        });
+      }
+    },
+    trackSpellingSuggestions: function () {
+      var $spellingSuggestion = $('.spelling-suggestion a');
+
+      if ($spellingSuggestion.length) {
+        GOVUK.analytics.trackEvent('Search suggestions', 'Shown', {
+          label: $spellingSuggestion.text(),
+          nonInteraction: true
+        });
+      }
     }
-  }());
+  };
+
+  GOVUK.search = search;
+  GOVUK.search.init();
 }).call(this);

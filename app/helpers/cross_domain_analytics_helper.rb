20a11a151dc677b08348f5231443c15b15e3c9a4
Paul Hayes <fofr@users.noreply.github.com>
2015-06-03 14:42:49 +0100

    Remove link to pivotal story

--- app/helpers/cross_domain_analytics_helper.rb
+++ app/helpers/cross_domain_analytics_helper.rb
@@ -3,7 +3,7 @@ module CrossDomainAnalyticsHelper
     {
       'register-to-vote' => 'UA-23066786-5',
       'accelerated-possession-eviction' => 'UA-37377084-12',
-      'renewtaxcredits' => 'UA-43414424-1', # HMRC, https://www.pivotaltracker.com/n/projects/1261204
+      'renewtaxcredits' => 'UA-43414424-1',
     }[slug]
   end
 end

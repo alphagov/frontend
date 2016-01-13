9910e6048a006306de00253f3093e40d170c5dd9
Chris Patuzzo <chris@patuzzo.co.uk>
2016-01-13 14:19:12 +0000

    Render the atom feed for the index page

--- test/integration/travel_advice_atom_test.rb
+++ test/integration/travel_advice_atom_test.rb
@@ -1,6 +1,8 @@
 require 'integration_test_helper'
+require "gds_api/test_helpers/content_store"
 
 class TravelAdviceAtomTest < ActionDispatch::IntegrationTest
+  include GdsApi::TestHelpers::ContentStore
 
   context "on a single country page" do
     should "display the atom feed for a country" do
@@ -35,7 +37,10 @@ class TravelAdviceAtomTest < ActionDispatch::IntegrationTest
 
   context "aggregate feed" do
     should "display the list of countries as an atom feed" do
-      setup_api_responses("foreign-travel-advice", :file => 'foreign-travel-advice/index2.json')
+      json = GovukContentSchemaTestHelpers::Examples.new.get('travel_advice_index', 'index')
+      content_item = JSON.parse(json)
+      base_path = content_item.fetch("base_path")
+      content_store_has_item(base_path, content_item)
 
       visit '/foreign-travel-advice.atom'
       assert_equal 200, page.status_code
@@ -45,44 +50,34 @@ class TravelAdviceAtomTest < ActionDispatch::IntegrationTest
 
       assert page.has_xpath? ".//feed/link[@rel='self' and @href='http://www.example.com/foreign-travel-advice.atom']"
       assert page.has_xpath? ".//feed/link[@rel='alternate' and @type='text/html' and @href='http://www.example.com/foreign-travel-advice']"
-      assert page.has_xpath? ".//feed/updated", :text => "2013-02-23T11:31:08+00:00"
-      assert page.has_xpath? ".//feed/entry", :count => 3
+      assert page.has_xpath? ".//feed/updated", :text => "2016-01-06T00:00:00+00:00"
+      assert page.has_xpath? ".//feed/entry", :count => 6
 
-      assert page.has_xpath? ".//feed/entry[1]/title", :text => "Syria"
-      assert page.has_xpath? ".//feed/entry[1]/id", :text => "http://frontend.dev.gov.uk/foreign-travel-advice/syria#2013-02-23T11:31:08+00:00"
-      assert page.has_xpath? ".//feed/entry[1]/link[@type='text/html' and @href='https://www.gov.uk/foreign-travel-advice/syria']"
-      assert page.has_xpath? ".//feed/entry[1]/link[@type='application/atom+xml' and @href='https://www.gov.uk/foreign-travel-advice/syria.atom']"
-      assert page.has_xpath? ".//feed/entry[1]/updated", :text => "2013-02-23T11:31:08+00:00"
-      assert page.has_xpath? ".//feed/entry[1]/summary[@type='xhtml']/div/p", :text => "Serious problems in the country."
-      assert page.has_xpath? ".//feed/entry[1]/summary[@type='xhtml']/div/p", :text => "Bad stuff is happening"
+      assert page.has_xpath? ".//feed/entry[1]/title", :text => "Spain"
+      assert page.has_xpath? ".//feed/entry[1]/id", :text => "https://www.gov.uk/foreign-travel-advice/spain#2016-01-06T00:00:00+00:00"
+      assert page.has_xpath? ".//feed/entry[1]/link[@type='text/html' and @href='https://www.gov.uk/foreign-travel-advice/spain']"
+      assert page.has_xpath? ".//feed/entry[1]/link[@type='application/atom+xml' and @href='https://www.gov.uk/foreign-travel-advice/spain.atom']"
+      assert page.has_xpath? ".//feed/entry[1]/updated", text: "2016-01-06T00:00:00+00:00"
+      assert page.has_xpath? ".//feed/entry[1]/summary[@type='xhtml']/div/p", text: "Latest update: Summary – information and advice for Manchester City fans travelling to Seville"
 
-      assert page.has_xpath? ".//feed/entry[2]/title", :text => "Luxembourg"
-      assert page.has_xpath? ".//feed/entry[2]/id", :text => "https://www.gov.uk/foreign-travel-advice/luxembourg#2013-01-15T16:48:54+00:00"
-      assert page.has_xpath? ".//feed/entry[2]/link[@type='text/html' and @href='https://www.gov.uk/foreign-travel-advice/luxembourg']"
-      assert page.has_xpath? ".//feed/entry[2]/link[@type='application/atom+xml' and @href='https://www.gov.uk/foreign-travel-advice/luxembourg.atom']"
-      assert page.has_xpath? ".//feed/entry[2]/updated", :text => "2013-01-15T16:48:54+00:00"
-      assert page.has_xpath? ".//feed/entry[2]/summary[@type='xhtml']/div/p", :text => "The issue with the Knights of Ni has been resolved."
-
-      assert page.has_xpath? ".//feed/entry[3]/title", :text => "Portugal"
-      assert page.has_xpath? ".//feed/entry[3]/id", :text => "https://www.gov.uk/foreign-travel-advice/portugal#2012-02-22T11:31:08+00:00"
-      assert page.has_xpath? ".//feed/entry[3]/link[@type='text/html' and @href='https://www.gov.uk/foreign-travel-advice/portugal']"
-      assert page.has_xpath? ".//feed/entry[3]/link[@type='application/atom+xml' and @href='https://www.gov.uk/foreign-travel-advice/portugal.atom']"
-      assert page.has_xpath? ".//feed/entry[3]/updated", :text => "2012-02-22T11:31:08+00:00"
-      assert page.has_xpath? ".//feed/entry[3]/summary[@type='xhtml']/div/p", :text => "Added information about sunburn risks."
+      assert page.has_xpath? ".//feed/entry[2]/title", text: "Malaysia"
+      assert page.has_xpath? ".//feed/entry[2]/id", text: "https://www.gov.uk/foreign-travel-advice/malaysia#2016-01-05T00:00:00+00:00"
+      assert page.has_xpath? ".//feed/entry[2]/link[@type='text/html' and @href='https://www.gov.uk/foreign-travel-advice/malaysia']"
+      assert page.has_xpath? ".//feed/entry[2]/link[@type='application/atom+xml' and @href='https://www.gov.uk/foreign-travel-advice/malaysia.atom']"
+      assert page.has_xpath? ".//feed/entry[2]/updated", text: "2016-01-05T00:00:00+00:00"
+      assert page.has_xpath? ".//feed/entry[2]/summary[@type='xhtml']/div/p", text: "Latest update: Summary - haze can cause disruption to local, regional air travel and to government and private schools"
     end
 
-    should "only include the 20 most recently updated countries" do
-      setup_api_responses("foreign-travel-advice", :file => 'foreign-travel-advice/index_large.json')
+    should "render a maximum of 20 countries" do
+      json = GovukContentSchemaTestHelpers::Examples.new.get('travel_advice_index', 'index')
+      content_item = JSON.parse(json)
+      content_item["details"]["countries"] = content_item["details"]["countries"] * 5
+      base_path = content_item.fetch("base_path")
+      content_store_has_item(base_path, content_item)
 
       visit '/foreign-travel-advice.atom'
       assert_equal 200, page.status_code
-
-      assert page.has_xpath? ".//feed/title", :text => "Travel Advice Summary"
-
-      assert page.has_xpath? ".//feed/entry", :count => 20
-
-      assert page.has_xpath? ".//feed/entry[1]/title", :text => "First"
-      assert page.has_xpath? ".//feed/entry[20]/title", :text => "Twentieth"
+      assert page.has_xpath? ".//feed/entry", count: 20
     end
   end
 end

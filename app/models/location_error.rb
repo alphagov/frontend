4d525895aefe68aac0d9b85ff1a3aae713e9bda4
Murray Steele <muz@h-lame.com>
2016-01-29 17:49:42 +0000

    Don't use definite article in front of LA names
    For most it doesn't sound correct (e.g. "the Sunderland City Council")
    and for those where it does (e.g. "the Royal Borough of Greenwich") it
    still sounds ok without and in one case the name already includes it ("The
    Highland Council").
    
    Where we said "We've matched this postcode to the <council>" we now say
    "We've matched this postcode to <council>".  However, where we said "Search
    the <council> website for this service" we leave the definite article in
    because it's the website we're talking about.
    
    This causes a complication because that text is part of a translation file
    and we are limited in what logic we can perform.  We check if the la name
    starts with a the in the LocationError and use a different translation that
    excludes it for those councils whose names already include it.

--- app/models/location_error.rb
+++ app/models/location_error.rb
@@ -3,6 +3,7 @@ class LocationError
 
   def initialize(postcode_error = nil, message_args = {})
     @postcode_error = postcode_error
+    @message_args = message_args
 
     case postcode_error
     when 'fullPostcodeNoMapitMatch'
@@ -12,17 +13,27 @@ class LocationError
       @message = 'formats.local_transaction.no_local_authority'
       @sub_message = 'formats.local_transaction.no_local_authority_sub_html'
     when 'laMatchNoLink'
-      @message = 'formats.local_transaction.local_authority_no_service_url_html'
+      @message =
+        if local_authority_name_starts_with_a_the?
+          'formats.local_transaction.local_authority_starts_with_the_no_service_url_html'
+        else
+          'formats.local_transaction.local_authority_no_service_url_html'
+        end
       @sub_message = '' #not used in the markup for this case
     when 'laMatchNoLinkNoAuthorityUrl'
-      @message = 'formats.local_transaction.local_authority_no_service_url_no_authority_link_html'
+      @message =
+        if local_authority_name_starts_with_a_the?
+          'formats.local_transaction.local_authority_starts_with_the_no_service_url_no_authority_link_html'
+        else
+          'formats.local_transaction.local_authority_no_service_url_no_authority_link_html'
+        end
+
       @sub_message = '' #not used in the markup for this case
     else # e.g. 'invalidPostcodeFormat'
       @message = 'formats.local_transaction.invalid_postcode'
       @sub_message = 'formats.local_transaction.invalid_postcode_sub'
     end
 
-    @message_args = message_args
     send_error_notification(postcode_error) if postcode_error
   end
 
@@ -32,5 +43,11 @@ class LocationError
 
   def no_location_interaction?
     %w(laMatchNoLink laMatchNoLinkNoAuthorityUrl).include? postcode_error
+  end
+
+private
+
+  def local_authority_name_starts_with_a_the?
+    message_args.fetch(:local_authority_name, '') =~ /\Athe/i
   end
 end

81e3b62e118d54b9c331405007d3380aecb959a4
David Basalla <davidbasalla@gmail.com>
2016-02-19 15:09:40 +0000

    Add module for tracking form submission on postcode search
    We implement our own custom module based on the data modules pattern as described in govuk_frontend_toolkit (https://github.com/alphagov/govuk_frontend_toolkit/blob/master/docs/javascript.md#modules)

---
+++ app/assets/javascripts/modules/track-submit.js
@@ -0,0 +1,20 @@
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function(Modules) {
+  "use strict";
+
+  Modules.TrackSubmit = function () {
+    this.start = function (element) {
+      element.on('submit', 'form', trackSubmit);
+
+      var category = element.data('track-category'),
+          action = element.data('track-action');
+
+      function trackSubmit() {
+        if (GOVUK.analytics && GOVUK.analytics.trackEvent) {
+          GOVUK.analytics.trackEvent(category, action);
+        }
+      }
+    };
+  };
+})(window.GOVUK.Modules);
---
+++ test/javascripts/unit/modules/track-submit.spec.js
@@ -0,0 +1,35 @@
+describe('A form submit tracker', function() {
+  "use strict";
+
+  var tracker,
+      element;
+
+  beforeEach(function() {
+    GOVUK.analytics = {trackEvent: function() {}};
+    tracker = new GOVUK.Modules.TrackSubmit();
+  });
+
+  afterEach(function() {
+    delete GOVUK.analytics;
+  });
+
+  it('tracks submit events', function() {
+    spyOn(GOVUK.analytics, 'trackEvent');
+
+    element = $('\
+      <div \
+        data-track-category="category"\
+        data-track-action="action">\
+        <form method="post">\
+          <button id="submit-button" type="submit">Submit</button>\
+        </form>\
+      </div>\
+    ');
+
+    tracker.start(element);
+
+    element.find('form').trigger('submit');
+
+    expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith('category', 'action');
+  });
+});

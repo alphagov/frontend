d871a5214dc4ddb48653122ce5c3906e1a55f778
Paul Bowsher <paul.bowsher@gmail.com>
2016-06-09 17:31:05 +0100

    Base cookie name on variables

--- app/assets/javascripts/register-to-vote.js
+++ app/assets/javascripts/register-to-vote.js
@@ -1,15 +1,16 @@
 //= require govuk/multivariate-test
 
 $(function(){
+  // x minutes in days: x / (60 minutes * 24 hours)
+  var minutesForCohort = 2,
+    minutesInDay = 1440; // 60 * 24
+
   if(window.location.href.indexOf("/register-to-vote") > -1) {
     // Get the current cohort or assign one if it has not been already
     GOVUK.MultivariateTest.prototype.getCohort = function() {
       var cohort = GOVUK.cookie(this.cookieName());
       if (!cohort || !this.cohorts[cohort]) {
         cohort = this.chooseRandomCohort();
-        // x minutes in days: x / (60 minutes * 24 hours)
-        var minutesForCohort = 5,
-          minutesInDay = 1440; // 60 * 24
         GOVUK.cookie(this.cookieName(), cohort, {days: minutesForCohort / minutesInDay});
       }
       return cohort;
@@ -25,16 +26,17 @@ $(function(){
     };
 
     var bContent = $('#get-started-b-content').html();
+    var pcThreshold = 25;
 
     new GOVUK.MultivariateTest({
       el: '.get-started',
-      name: 'registerToVote_rateLimit_201606',
+      name: 'registerToVote_rateLimit_201606_' + pcThreshold.toString() + 'pc_' + minutesForCohort.toString() + 'm',
       customDimensionIndex: 14,
       cohorts: {
-        a: { callback: function() {}, variantId: 0, weight: 75 },
+        a: { callback: function() {}, variantId: 0, weight: 100 - pcThreshold },
         b: { callback: function() {
           $('.get-started').replaceWith(bContent);
-        }, variantId: 1, weight: 25 },
+        }, variantId: 1, weight: pcThreshold },
       }
     });
   }

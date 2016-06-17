2fc756a08c7f7a474e101339159244f54220fbd4
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2016-06-17 12:39:00 +0100

    Remove duplication of lists of type codes
    The previous commit identified that an area type had been included in one place
    but not another. This helps a bit with that problem.
    
    It's hard to check which ones are in the list if they're randomly ordered.
    
    The ordering of the list of `authority_types` is important as licences need to
    be able to select the most specific match in the case when two local
    authorities are returned for a postcode. [This test describes the case](https://github.com/alphagov/frontend/blob/3b12a066f0787f9a926912becf04a8b440792f12/test/unit/licence_location_identifier_test.rb#L50L61)

--- lib/location_identifier.rb
+++ lib/location_identifier.rb
@@ -1,14 +1,21 @@
 class LocationIdentifier
   private
+    DISTRICT = ['DIS']
+    COUNTY = ['CTY']
+    UNITARY = %w(COI LBO LGD MTD UTA)
+
+    #It is important that DIS (district) is before CTY (county) in the list of
+    # authority types as licences want the most specific provider first. All
+    # the others are unitary so we can order them in a manner which suits us.
     def self.authority_types
-      ["DIS","LBO","UTA","CTY","LGD","MTD","COI"]
+      DISTRICT + COUNTY + UNITARY
     end
 
     def self.identify_tier(type)
       case type
-      when 'DIS' then 'district'
-      when 'CTY' then 'county'
-      when 'LBO','LGD','MTD','UTA', 'COI' then 'unitary'
+      when *DISTRICT then 'district'
+      when *COUNTY then 'county'
+      when *UNITARY then 'unitary'
       end
     end
 end

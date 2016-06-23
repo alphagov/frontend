80b7fdcb95197099731807559a30be525cb9eabe
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-06-23 11:08:59 +0000

    Remove legacy orgon donor promotion code
    There were two api's relating to promotion data. This removes one
    leaving the single api from govuk_content_models.

--- app/helpers/completed_transaction_helper.rb
+++
@@ -1,9 +0,0 @@
-module CompletedTransactionHelper
-  def promote_organ_donor_registration?
-     organ_donor_registration_attributes && organ_donor_registration_attributes['promote_organ_donor_registration']
-  end
-
-  def organ_donor_registration_attributes
-    @attributes ||= @publication.presentation_toggles && @publication.presentation_toggles['organ_donor_registration']
-  end
-end

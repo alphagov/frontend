9c8f7f70d8852898ac640862dcdbf654d7602b77
David Basalla <davidbasalla@gmail.com>
2016-09-21 16:52:57 +0100

    Remove detailed_guidance_content_api
    This api wrapper around content_api is not used anymore anywhere in the code.

--- config/initializers/gds_api.rb
+++ config/initializers/gds_api.rb
@@ -6,7 +6,5 @@ GdsApi::Base.logger = Logger.new(Rails.root.join("log/#{Rails.env}.api_client.lo
 # This file is overwritten on deployment, so this only applies to development.
 GdsApi::Base.default_options = { disable_cache: true }
 
-Frontend.detailed_guidance_content_api = GdsApi::ContentApi.new("#{Plek.new.find('whitehall')}/api/specialist/")
-
 # Note that copies of this exist in both preview and production
 # to_upload directories, so make sure your changes propagate there.

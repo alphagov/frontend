dc6adc1a547578c4d7e63e578b36c518be187046
Tom Gladhill <whoojemaflip@gmail.com>
2016-11-09 11:18:31 +0000

    Advanced Constraints for routing requests to format-specific controllers
    To enable an incremental refactor of the RootController#publication
    method, we want to split out responsibilities for individual formats
    into their own controllers. This is done by an Advanced Constraint class
    which makes a (cacheable) request to the content-api, and uses the
    response to make a decision as to where the request will be routed.
    
    In support of this new class is an Artefact Retriever Factory which
    wraps the configuration needed to make a call to the content-api.

---
+++ lib/artefact_retriever_factory.rb
@@ -0,0 +1,35 @@
+class ArtefactRetrieverFactory
+  class << self
+    def artefact_retriever(
+      options: {},
+      artefact_retriever_class: ArtefactRetriever
+    )
+
+      @options = options
+      artefact_retriever_class.new(content_api, logger, statsd)
+    end
+
+    private
+
+    def content_api
+      GdsApi::ContentApi.new(
+        Plek.new.find("contentapi"),
+        content_api_options
+      )
+    end
+
+    def content_api_options
+      CONTENT_API_CREDENTIALS.merge(@options)
+    end
+
+    def logger
+      Rails.logger
+    end
+
+    def statsd
+      Statsd.new("localhost").tap do |c|
+        c.namespace = ENV['GOVUK_STATSD_PREFIX'].to_s
+      end
+    end
+  end
+end

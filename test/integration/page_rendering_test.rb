8a6e88c50dcdf70e57ae4f43562f6fc7a6878927
David Basalla <davidbasalla@gmail.com>
2016-11-16 15:07:35 +0000

    Add integration test for video format
    We move over the small test from page_rendering_test and add tests to get better coverage for the video format, testing for preview and translation functionality. To test translation we had to add the updated_at field to the video fixture, so that it gets rendered.
    We also removed the breadcrumb tests from answers and help format integration tests, as the line was duplicated in those tests.

--- test/integration/page_rendering_test.rb
+++ test/integration/page_rendering_test.rb
@@ -90,16 +90,4 @@ class PageRenderingTest < ActionDispatch::IntegrationTest
     visit "/pagerror.gif"
     assert_equal 404, page.status_code
   end
-
-  test "viewing a video answer page" do
-    setup_api_responses('test-video')
-    visit "/test-video"
-    assert_equal 200, page.status_code
-    within '#content' do
-      assert page.has_content?("This is the video summary")
-      assert page.has_selector?("figure#video a[href='https://www.youtube.com/watch?v=fLreo24WYeQ']")
-      assert page.has_selector?("figure#video a[href='https://www.example.org/test.xml']", visible: :all)
-      assert page.has_content?("Video description")
-    end
-  end
 end

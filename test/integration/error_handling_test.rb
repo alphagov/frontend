a343417056c7cbbb15d3e52c29c896fd3a3d6651
Tom Gladhill <whoojemaflip@gmail.com>
2016-12-06 12:12:46 +0000

    Remove duplicate tests

--- test/integration/error_handling_test.rb
+++ test/integration/error_handling_test.rb
@@ -40,4 +40,17 @@ class ErrorHandlingTest < ActionDispatch::IntegrationTest
       assert_equal 503, page.status_code
     end
   end
+
+  # Crude way of handling the situation described at
+  # http://stackoverflow.com/a/3443678
+  test "requests for gifs 404" do
+    visit "/crisis-loans/refresh.gif"
+    assert_equal 404, page.status_code
+
+    visit "/refresh.gif"
+    assert_equal 404, page.status_code
+
+    visit "/pagerror.gif"
+    assert_equal 404, page.status_code
+  end
 end

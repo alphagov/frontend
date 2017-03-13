c00fe668a21e97bfbd0c23b7148a5c410ac13e29
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2017-03-13 14:00:55 +0000

    Move banner into partial
    This ensures that we can deploy most of the functionality in advance and switch over the banner content in the final deploy.
    
    As we want to be able to deploy just the content, the tests need to support
    a partial both with and without content. To do this, we override the view path to stub templates
    replacement. This is borrowed from rspec-rails’ `stub_template` method
    https://github.com/rspec/rspec-rails/blob/e8054a1cd03044f725030fe8315952
    cf3799a395/lib/rspec/rails/example/view_example_group.rb#L86-L88

--- test/functional/homepage_controller_test.rb
+++ test/functional/homepage_controller_test.rb
@@ -11,5 +11,38 @@ class HomepageControllerTest < ActionController::TestCase
       get :index
       assert_equal "max-age=1800, public", response.headers["Cache-Control"]
     end
+
+    context "with a blank promo banner partial" do
+      should "not render the promo banner" do
+        stub_template "homepage/_promo_banner.html.erb" => "<%# foo %>"
+
+        get :index
+        assert_template "homepage/_promo_banner"
+        refute @response.body.include?("homepage-promo-banner")
+      end
+    end
+
+    context "with a promo banner partial" do
+      should "render promo banner" do
+        stub_template "homepage/_promo_banner.html.erb" => <<-EOF
+        <div id="homepage-promo-banner">
+          <div class="banner-message">
+            <p><strong>Some Title</strong> On some date something has happened.
+            <a href="https://something.gov.uk" rel="external nofollow">More&nbsp;information</a></p>
+          </div>
+        </div>
+        EOF
+
+        get :index
+        assert_template "homepage/_promo_banner"
+        assert @response.body.include?("homepage-promo-banner")
+        assert @response.body.include?("Some Title")
+      end
+    end
+  end
+
+  def stub_template(hash)
+    require 'action_view/testing/resolvers'
+    @controller.view_paths.unshift(ActionView::FixtureResolver.new(hash))
   end
 end

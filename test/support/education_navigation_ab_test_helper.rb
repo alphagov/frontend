c2b3b7694753284b051cc2d5683833f65cf5fd95
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-03-16 10:35:03 +0000

    Enable taxonomy breadcrumb back link on mobile
    Pass `collapse_on_mobile` parameter to enable the collapse of the
    taxonomy breadcrumbs to a single back link.

--- test/support/education_navigation_ab_test_helper.rb
+++ test/support/education_navigation_ab_test_helper.rb
@@ -10,17 +10,17 @@ module EducationNavigationAbTestHelper
   end
 
   def expect_normal_navigation
-    GovukNavigationHelpers::NavigationHelper.any_instance.expects(:breadcrumbs)
+    GovukNavigationHelpers::NavigationHelper.any_instance.expects(:breadcrumbs).returns(breadcrumbs: [])
     GovukNavigationHelpers::NavigationHelper.any_instance.expects(:related_items)
   end
 
   def expect_new_navigation
-    GovukNavigationHelpers::NavigationHelper.any_instance.expects(:taxon_breadcrumbs)
+    GovukNavigationHelpers::NavigationHelper.any_instance.expects(:taxon_breadcrumbs).returns(breadcrumbs: [])
     GovukNavigationHelpers::NavigationHelper.any_instance.expects(:taxonomy_sidebar)
   end
 
   def expect_normal_navigation_and_old_related_links
-    GovukNavigationHelpers::NavigationHelper.any_instance.expects(:taxon_breadcrumbs)
+    GovukNavigationHelpers::NavigationHelper.any_instance.expects(:taxon_breadcrumbs).returns(breadcrumbs: [])
     GovukNavigationHelpers::NavigationHelper.any_instance.expects(:related_items)
   end
 end

c1241763f3ec6656d21fb47869f3b74acff1a996
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2017-03-16 11:00:53 +0000

    Add live flag to be consistent with other repos

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-
 bundle install
-bundle exec rails s -p 3005
+
+if [[ $1 == "--live" ]] ; then
+  GOVUK_APP_DOMAIN=www.gov.uk \
+  GOVUK_WEBSITE_ROOT=https://www.gov.uk \
+  PLEK_SERVICE_CONTENT_STORE_URI=https://www.gov.uk/api \
+  PLEK_SERVICE_CONTENTAPI_URI=https://www.gov.uk/api \
+  PLEK_SERVICE_STATIC_URI=assets.publishing.service.gov.uk \
+  bundle exec rails s -p 3005
+else
+  bundle exec rails s -p 3005
+fi

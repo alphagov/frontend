90a31a86b88bb85936347c0df29bafccf0a3d06d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-03-27 13:11:26 +0000

    Remove Campaign format rendering
    The campaign format is being retired, so we remove all code that renders the
    format along with associated tests.

--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -5,10 +5,6 @@ class CampaignController < ApplicationController
   before_filter -> { setup_content_item_and_navigation_helpers("/" + params[:slug]) }, only: :show
   before_filter :set_publication, only: :show
 
-  def show
-    render locals: { full_width: true }
-  end
-
   def uk_welcomes
     # This is a special case. It is a hardcoded campaign which is not editable
     # in Publisher.
--- test/functional/campaign_controller_test.rb
+++ test/functional/campaign_controller_test.rb
@@ -1,27 +1,6 @@
 require "test_helper"
 
 class CampaignControllerTest < ActionController::TestCase
-  context "campaigns edited normally in Publisher" do
-    context "GET show" do
-      setup do
-        @artefact = artefact_for_slug('firekills')
-        @artefact["format"] = "campaign"
-      end
-
-      context "for live content" do
-        setup do
-          content_api_and_content_store_have_page('firekills', artefact: @artefact)
-        end
-
-        should "set the cache expiry headers" do
-          get :show, slug: "firekills"
-
-          assert_equal "max-age=1800, public", response.headers["Cache-Control"]
-        end
-      end
-    end
-  end
-
   context "UK Welcomes campaign" do
     should "set correct expiry headers" do
       get :uk_welcomes

e53a0e6cb6809061bcf120f4e46052e5b3170deb
Murray Steele <muz@h-lame.com>
2017-05-12 13:49:18 +0000

    Run assisted digital survey on 2 more pages
    We want to run them on the done pages for the following 3 services:
    
    * https://www.gov.uk/done/waste-carrier-or-broker-registration
    * https://wasteexemptions.service.gov.uk/
    * https://register-flood-risk-exemption.service.gov.uk/
    
    We already had the done page for the last one, this commit adds the
    done pages for the first two.

--- lib/assisted_digital_satisfaction_survey.rb
+++ lib/assisted_digital_satisfaction_survey.rb
@@ -1,6 +1,8 @@
 module AssistedDigitalSatisfactionSurvey
   SLUGS_IN_SURVEY = [
-    'done/register-flood-risk-exemption'
+    'done/register-flood-risk-exemption',
+    'done/waste-carrier-or-broker-registration',
+    'done/register-waste-exemption',
   ].freeze
 
   def self.show_survey?(slug)

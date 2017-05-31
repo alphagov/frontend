c4387981cee9fc11631d8846cb4813714738157c
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2017-05-31 13:15:39 +0000

    Show new taxonomy sidebar for all content
    - No longer override sidebar for mainstream content
    - Overriding will be allowed by the component

--- test/functional/transaction_controller_test.rb
+++ test/functional/transaction_controller_test.rb
@@ -56,19 +56,6 @@ class TransactionControllerTest < ActionController::TestCase
           get :show, slug: "tagged"
         end
       end
-
-      should "show taxon breadcrumbs and old related links for flagged mainstream content" do
-        MainstreamContentFetcher.stubs(:with_curated_sidebar).returns(
-          ["/tagged-to-taxon"]
-        )
-
-        expect_normal_navigation_and_old_related_links
-        content_store_has_item_tagged_to_taxon(base_path: '/tagged-to-taxon', payload: {})
-
-        with_variant EducationNavigation: "B" do
-          get :show, slug: "tagged-to-taxon"
-        end
-      end
     end
   end
 

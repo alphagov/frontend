05914e4325400334af03ba73ae41b0924a169882
David Henry <david@decoybecoy.com>
2017-10-23 14:53:14 +0100

    Remove site search JS/CSS and views

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -2,12 +2,9 @@
 // Note: The ordering of these JavaScript includes matters.
 //= require transactions
 //= require media-player-loader
-//= require checkbox-filter
 //= require support
-//= require live-search
 //= require shared_mustache
 //= require templates
-//= require search
 //= require_tree ./modules
 
 $(document).ready(function() {

9c5efabc7e5cb48e247e538db9237059ec0aacfc
David Henry <david@decoybecoy.com>
2017-12-18 10:51:56 +0000

    Remove the last of rummager integration code
    We are now processing special-routes via the publishing API which means we can
    remove the last of the rummager integration code.

--- lib/services.rb
+++ lib/services.rb
@@ -1,11 +1,6 @@
-require 'gds_api/rummager'
 require 'gds_api/content_store'
 
 module Services
-  def self.rummager
-    @rummager ||= GdsApi::Rummager.new(Plek.new.find('search'))
-  end
-
   def self.content_store
     @content_store ||= GdsApi::ContentStore.new(Plek.new.find('content-store'))
   end

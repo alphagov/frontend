967854d139b3b3e404989eec61b6ec496ccc236d
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2019-02-06 16:30:50 +0000

    Ensure B variant of related links shows no related links if suggested related links do not exist.
    Previously, a user on the B variant would have seen related links of the control variant if the test variant contained no related links, as `ordered_related_items` would not have been overridden on the content item. This commit fixes that problem, to ensure a fair test.
    
    This commit also renames `RelatedLinksABTest` to `RelatedLinksABTest1` in anticipation of multiple test iterations.
    
    Solo: @karlbaker02

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,8 +68,8 @@ protected
   def content_item(base_path = "/#{params[:slug]}")
     @content_item ||= Services.content_store.content_item(base_path)
 
-    if related_links_variant.variant?('B') && @content_item.dig('links', 'suggested_ordered_related_items')
-      @content_item['links']['ordered_related_items'] = @content_item['links']['suggested_ordered_related_items']
+    if related_links_variant.variant?('B')
+      @content_item['links']['ordered_related_items'] = @content_item['links'].fetch('suggested_ordered_related_items', [])
     end
 
     @content_item
--- test/functional/transaction_controller_test.rb
+++ test/functional/transaction_controller_test.rb
@@ -23,8 +23,8 @@ class TransactionControllerTest < ActionController::TestCase
       assert_equal "DENY", @response.headers["X-Frame-Options"]
     end
 
-    should "get item from the content store and keeps ordered_related_items when running RelatedLinksABTest control variant" do
-      with_variant RelatedLinksABTest: 'A' do
+    should "get item from the content store and keeps ordered_related_items when running RelatedLinksABTest1 control variant" do
+      with_variant RelatedLinksABTest1: 'A' do
         @content_item = content_store_has_example_item('/apply-marine-licence', schema: 'transaction', example: 'apply-marine-licence')
 
         get :show, params: { slug: 'apply-marine-licence' }
@@ -34,14 +34,25 @@ class TransactionControllerTest < ActionController::TestCase
       end
     end
 
-    should "get item from the content store and replace ordered_related_items when running RelatedLinksABTest test variant" do
-      with_variant RelatedLinksABTest: 'B' do
+    should "get item from the content store and replace ordered_related_items when running RelatedLinksABTest1 test variant" do
+      with_variant RelatedLinksABTest1: 'B' do
         @content_item = content_store_has_example_item('/apply-marine-licence', schema: 'transaction', example: 'apply-marine-licence')
 
         get :show, params: { slug: 'apply-marine-licence' }
 
         assert_response :success
         assert_equal assigns[:content_item]['links']['ordered_related_items'], assigns[:content_item]['links']['suggested_ordered_related_items']
+      end
+    end
+
+    should "get item from the content store and replace ordered_related_items with empty array when running RelatedLinksABTest1 test variant" do
+      with_variant RelatedLinksABTest1: 'B' do
+        @content_item = content_store_has_example_item('/national-curriculum', schema: 'guide', example: 'guide')
+
+        get :show, params: { slug: 'national-curriculum' }
+
+        assert_response :success
+        assert_equal [], assigns[:content_item]['links']['ordered_related_items']
       end
     end
   end

1050cccfb0876199c1cdbe218acd777a5026483e
Kelvin Gan <dev@kelvingan.co.uk>
2019-05-31 14:54:58 +0100

    Add integration test for /help/cookies

---
+++ test/integration/help_cookies_test.rb
@@ -0,0 +1,23 @@
+require "integration_test_helper"
+
+class HelpCookiesTest < ActionDispatch::IntegrationTest
+  context "rendering the cookies setting page" do
+    setup do
+      payload = {
+        base_path: "/help/cookies",
+        format: "special_route",
+        title: "Cookies on GOV.UK",
+        description: "You can choose which cookies you're happy for GOV.UK to use.",
+      }
+      content_store_has_item('/help/cookies', payload)
+    end
+
+    should "render the cookies setting page correctly" do
+      visit "/help/cookies"
+
+      within '#content' do
+        assert_has_component_title "Cookies on GOV.UK"
+      end
+    end
+  end
+end

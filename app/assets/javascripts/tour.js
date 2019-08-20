97ecb99346c0e4ef260e6df81411f59aed55086d
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-08-20 09:22:17 +0100

    Rebuild the tour page
    - remove all custom CSS and JS
    - fix videos by using govspeak Youtube player component
    - put transcripts into details component
    - use govuk-frontend grid for layout, headings and fonts
    - use search component for search box

--- app/assets/javascripts/tour.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-  $('.video-transcript-toggle').removeClass('visuallyhidden');
-  $('.video-transcript-toggle').on('click', function() {
-    $($(this).attr("href")).toggle();
-      return false;
-  });
-});

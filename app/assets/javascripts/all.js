a13896a0799b9d04105c6d32aaf7be03f19d7e1a
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-10-02 10:59:29 +0100

    Remove toolkit reference from JS

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,7 +1,5 @@
 // This is a manifest file used by the runner for JavaScript unit tests.
 // It is not used in the main application.
 //
-//= require govuk_toolkit
-//
 //= require_tree ./
 //= require_tree ./views

75333c4e9da2b84e8aa5f27508d62f175c17b327
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-15 12:08:33 +0100

    Update to govuk_publishing_components 22.0.0

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,15 @@
-Rails.application.config.assets.precompile += %w( all.js ) if Rails.env.test?
+Rails.application.config.assets.precompile += %w(manifest.js)
+
+Rails.application.config.assets.precompile += %w(
+  all.js
+  frontend.js
+  support.js
+  transations.js
+  application-ie6.css
+  application-ie7.css
+  application-ie8.css
+  application.css
+  print.css
+)
+
+Rails.application.config.assets.version = "1.0"

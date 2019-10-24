b751fb8d1f2e98146376cab0a21de28e89d72615
Robbond-GDS <robert.bond@digital.cabinet-office.gov.uk>
2019-10-24 13:46:05 +0100

    added project details view

--- app/controllers/funding_form/project_details_controller.rb
+++ app/controllers/funding_form/project_details_controller.rb
@@ -8,8 +8,8 @@ class FundingForm::ProjectDetailsController < ApplicationController
     keys.each do |key|
       session[key] = params[key]
     end
-    session[:start_date] = DateTime.new(params[:start_date_year].to_i, params[:start_date_month].to_i, params[:start_date_day].to_i).strftime("%Y-%m-%d")
-    session[:end_date] = DateTime.new(params[:end_date_year].to_i, params[:end_date_month].to_i, params[:end_date_day].to_i).strftime("%Y-%m-%d")
+    session[:award_start_date] = DateTime.new(params[:start_date_year].to_i, params[:start_date_month].to_i, params[:start_date_day].to_i).strftime("%Y-%m-%d")
+    session[:award_end_date] = DateTime.new(params[:end_date_year].to_i, params[:end_date_month].to_i, params[:end_date_day].to_i).strftime("%Y-%m-%d")
     redirect_to controller: "funding_form/check_answers", action: "show"
   end
 end

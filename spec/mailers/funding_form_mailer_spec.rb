c0cfdd1f6ac854e2ca883ab71205d608a69f3c94
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-24 08:39:10 +0100

    Add FundingFormMailer
    This will be used to send out the emails for the funding form.

---
+++ spec/mailers/funding_form_mailer_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe FundingFormMailer do
+  let(:email_address) { "test@example.com" }
+
+  describe "#confirmation_email" do
+    it "creates an email for the email address" do
+      mail = described_class.confirmation_email(email_address)
+
+      expect(mail.to).to eq([email_address])
+      expect(mail.subject).to eq("Funding Form Confirmation")
+      expect(mail.body.to_s).to include("Confirmation of funding form.")
+    end
+  end
+
+  describe "#backend_email" do
+    it "creates an email for the email address" do
+      mail = described_class.backend_email(email_address)
+
+      expect(mail.to).to eq([email_address])
+      expect(mail.subject).to eq("Funding Form")
+      expect(mail.body.to_s).to include("Funding form submission.")
+    end
+  end
+end

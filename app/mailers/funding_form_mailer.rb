6d620f0c5a078b776185ab13dab82df887d961c8
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-24 10:28:46 +0100

    Add an email delivery job
    We'll use this for the funding form to retry any failures.

--- app/mailers/funding_form_mailer.rb
+++ app/mailers/funding_form_mailer.rb
@@ -1,4 +1,6 @@
 class FundingFormMailer < ApplicationMailer
+  self.delivery_job = EmailDeliveryJob
+
   def confirmation_email(email_address)
     mail(to: email_address, subject: "Funding Form Confirmation")
   end

83a175c43eb7f3d522cfa298962899d65672f491
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-25 13:53:56 +0100

    Add radio button validation
    This validates that the radio buttons are correctly selected.

--- spec/controllers/funding_form/programme_controller_spec.rb
+++ spec/controllers/funding_form/programme_controller_spec.rb
@@ -7,18 +7,28 @@ RSpec.describe FundingForm::ProgrammeController do
   end
 
   describe "POST submit" do
-    before do
+    it "sets sanitised session variables" do
       post :submit, params: {
         funding_programme: "<script></script>Erasmus+",
       }
-    end
 
-    it "sets sanitised session variables" do
       expect(session[:funding_programme]).to eq "Erasmus+"
     end
 
     it "redirects to next step" do
+      post :submit, params: {
+        funding_programme: "<script></script>Erasmus+",
+      }
+
       expect(response).to redirect_to("/brexit-eu-funding/project-details")
+    end
+
+    it "validates an option is chosen" do
+      post :submit, params: {
+        funding_programme: "",
+      }
+
+      expect(response).to render_template("funding_form/programme")
     end
   end
 end

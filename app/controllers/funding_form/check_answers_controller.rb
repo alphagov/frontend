00c5a2a4efbbce15907cbe0995283fcdc33ec36e
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-10-25 14:50:23 +0100

    Shorten random part of EU Funding form reference number
    This was previously 10 hexadecimal numbers (i.e. 20 characters).  Reducing to three
    to give a six digit string.
    
    The reference also includes date and time of submission, so the chance of conflict is
    very low.

--- app/controllers/funding_form/check_answers_controller.rb
+++ app/controllers/funding_form/check_answers_controller.rb
@@ -18,7 +18,7 @@ class FundingForm::CheckAnswersController < ApplicationController
 
   def reference_number
     timestamp = Time.zone.now.strftime("%Y%m%d-%H%M%S")
-    random_id = SecureRandom.hex(10).upcase
+    random_id = SecureRandom.hex(3).upcase
     "#{timestamp}-#{random_id}"
   end
 end

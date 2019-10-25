19e966911bd66f3a14eb8924dd8dc447637d5be1
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-10-25 11:53:34 +0100

    Add mandatory text field validation

--- spec/controllers/funding_form/project_details_controller_spec.rb
+++ spec/controllers/funding_form/project_details_controller_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe FundingForm::ProjectDetailsController do
   end
 
   describe "POST submit" do
-    before do
-      post :submit, params: {
+    let(:params) do
+      {
         project_name: "<script></script>Researching something interesting",
         total_amount_awarded: "<script></script>1000000",
         start_date_day: "10",
@@ -21,6 +21,8 @@ RSpec.describe FundingForm::ProjectDetailsController do
     end
 
     it "sets sanitised session variables" do
+      post :submit, params: params
+
       expect(session[:project_name]).to eq "Researching something interesting"
       expect(session[:total_amount_awarded]).to eq "1000000"
       expect(session[:award_start_date]).to eq "2019-06-10"
@@ -28,7 +30,16 @@ RSpec.describe FundingForm::ProjectDetailsController do
     end
 
     it "redirects to next step" do
+      post :submit, params: params
+
       expect(response).to redirect_to("/brexit-eu-funding/does-the-project-have-partners-or-participants-outside-the-uk")
+    end
+
+    it "catches missing mandatory fields" do
+      params["project_name"] = ""
+      post :submit, params: params
+
+      expect(response).to render_template("funding_form/project_details")
     end
   end
 end

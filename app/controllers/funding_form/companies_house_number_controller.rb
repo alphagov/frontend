6c169d762469c40f19c0c3d141fc2fc4459f82b2
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-10-25 11:27:36 +0100

    Update logic for Companies House number step

--- app/controllers/funding_form/companies_house_number_controller.rb
+++ app/controllers/funding_form/companies_house_number_controller.rb
@@ -4,7 +4,7 @@ class FundingForm::CompaniesHouseNumberController < ApplicationController
   end
 
   def submit
-    session[:company_house_or_charity_commision_number] = params[:company_house_or_charity_commision_number_yes].presence || params[:company_house_or_charity_commision_number]
+    session[:company_house_or_charity_commision_number] = params[:company_house_or_charity_commision_number_other].presence || params[:company_house_or_charity_commision_number]
     redirect_to controller: "funding_form/grant_agreement_number", action: "show"
   end
 end

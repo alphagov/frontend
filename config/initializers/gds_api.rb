02fe98ab645f23dfdcf7b01c16f7402635cba4db
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-11-14 10:21:30 +0000

    Remove GdsApi logger configuration
    The comment in the file lies, as it's [not overwritten by anything else](https://github.com/alphagov/govuk-app-deployment/blob/master/frontend/config/deploy.rb).
    We don't need to log these interactions because we can link them together
    using `govuk_request_id` anyway.
    
    These logs aren't ingested into Logit, so it's likely that no one uses them.
    
    Should we wish to turn these back on, [logging api interactions is documented
    in the gds-api-adapters README](https://github.com/alphagov/gds-api-adapters#logging)

--- config/initializers/gds_api.rb
+++
@@ -1,9 +0,0 @@
-require "gds_api/base"
-
-GdsApi::Base.logger = Logger.new(Rails.root.join("log/#{Rails.env}.api_client.log"))
-
-# This file is overwritten on deployment, so this only applies to development.
-GdsApi::Base.default_options = { disable_cache: true }
-
-# Note that copies of this exist in both preview and production
-# to_upload directories, so make sure your changes propagate there.

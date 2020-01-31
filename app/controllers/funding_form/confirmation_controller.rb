53beb6c562f826812f8a4050009cbe2f2e46601e
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-01-31 09:57:24 +0000

    Remove code related to EU funding form
    The service has been discontinued and it now redirects to
    https://www.gov.uk/guidance/register-as-an-organisation-getting-funding-directly-from-the-eu.

--- app/controllers/funding_form/confirmation_controller.rb
+++
@@ -1,5 +0,0 @@
-class FundingForm::ConfirmationController < ApplicationController
-  def show
-    render "funding_form/confirmation"
-  end
-end
--- spec/controllers/funding_form_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe FundingFormController do
-  describe "GET index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-  end
-end

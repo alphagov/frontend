bfc5145e6b1f303963d3df74c1ee2e4835ffc95d
Kevin Dew <kevindew@me.com>
2020-05-28 21:27:33 +0100

    Create test-dependencies.js to replace live static assets
    The tests for this app were failing after GOV.UK's assets switched from
    the assets hostname to www. This was because slimmer was actually
    embedding links to production GOV.UK assets, once the path to these
    assets changed the tests then broke.
    
    Since it is good practice for tests to be isolated I've created a
    test-dependencies.js file which loads in the basic slimmer dependencies
    from govuk_publishing_components. This has the advantage that this test
    suite no longer has external network requirement, but does have the
    disadvantage that the JS isn't the same. This trade-off seems reasonable
    since there was never anything particularly realistic about the slimmer
    test template [1] and it's mostly luck that it matches production.
    
    [1]: https://github.com/alphagov/slimmer/blob/master/lib/slimmer/test_templates/wrapper.html

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[all.js] if Rails.env.test?
+Rails.application.config.assets.precompile += %w[all.js test-dependencies.js] if Rails.env.test?

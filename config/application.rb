44a9791c8ab60e1a46967df81af7e1cf93f39cf8
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-09-23 10:16:51 +0100

    Disable active_job
    Following on from ed352bb170f0cd7070c66a3270f5435342af8be7 we don't need
    this anymore as it was only being used for sending emails.

--- config/application.rb
+++ config/application.rb
@@ -61,8 +61,6 @@ module Frontend
 
     config.action_controller.allow_forgery_protection = false
 
-    config.active_job.queue_adapter = :sidekiq
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

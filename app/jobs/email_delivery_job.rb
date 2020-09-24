e58649e0e0d95757e53a98028ef51e725344b03a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-09-24 08:14:39 +0100

    Remove EmailDeliveryJob
    Following on from ed352bb170f0cd7070c66a3270f5435342af8be7 we're no
    longer sending emails so we don't need the delivery method.

--- app/jobs/email_delivery_job.rb
+++
@@ -1,8 +0,0 @@
-class EmailDeliveryJob < ActionMailer::DeliveryJob
-  # retry at 3s, 18s, 83s, 258s, 627s
-  retry_on(
-    Notifications::Client::RequestError,
-    wait: :exponentially_longer,
-    attempts: 5,
-  )
-end

8e4ce2caed0a73ba8551a0345f6d5186bd86dafe
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-11-03 09:59:03 +0000

    Finish DfT AB test and set promo to the Z variant
    I've checked that the pages under test are set up to show the correct
    promotion.

--- app/controllers/completed_transaction_controller.rb
+++ app/controllers/completed_transaction_controller.rb
@@ -1,7 +1,6 @@
 class CompletedTransactionController < ApplicationController
   include Cacheable
   include Navigable
-  include ElectricCarAbTestable
 
   # These 2 legacy completed transactions are linked to from multiple
   # transactions. The user satisfaction survey should not be shown for these as
--- test/functional/completed_transaction_controller_test.rb
+++ test/functional/completed_transaction_controller_test.rb
@@ -24,24 +24,4 @@ class CompletedTransactionControllerTest < ActionController::TestCase
       end
     end
   end
-
-  context "A/B testing" do
-    setup do
-      content_store_has_example_item("/done/vehicle-tax", schema: "completed_transaction")
-      content_store_has_example_item("/done/weee", schema: "completed_transaction")
-    end
-
-    %w[A B C D E F G H Z].each do |variant|
-      should "show the #{variant} variant of the electric car AB test on applicable pages" do
-        with_variant ElectricCarABTest: variant do
-          get :show, params: { slug: "done/vehicle-tax" }
-        end
-      end
-    end
-
-    should "not use AB testing headers for pages that are not in scope" do
-      get :show, params: { slug: "done/weee" }
-      assert_response_not_modified_for_ab_test("ElectricCar")
-    end
-  end
 end

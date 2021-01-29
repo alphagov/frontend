becac8d232169e49325cfc450bcaecffd8da427c
Graham Lewis <gclssvglx@gmail.com>
2021-01-29 11:04:02 +0000

    Update methods for country specific content
    Update the 'unavailable?' method with the correct path to the country specific content
    
    Update the 'content' method to return the country specific content
    
    The 'content' method should be as flexible as possible - it should handle a number of variables that will be interpolated into the country specific content

--- test/unit/models/local_transaction_services_test.rb
+++ test/unit/models/local_transaction_services_test.rb
@@ -6,26 +6,70 @@ class LocalTransactionServicesTest < ActiveSupport::TestCase
       assert true, LocalTransactionServices.instance.unavailable?(461, "Scotland")
     end
 
-    should "not include Northern Ireland for service 461" do
-      assert_not LocalTransactionServices.instance.unavailable?(461, "Northern Ireland")
+    should "not include Wales for service 461" do
+      assert_not LocalTransactionServices.instance.unavailable?(461, "Wales")
     end
   end
 
   context ".content" do
-    should "return a string with the given country and local authority name in" do
-      assert_equal "This service is unavailable in Dundee, Scotland", LocalTransactionServices.instance.content(461, "Scotland", "Dundee")
+    context "Scotland" do
+      should "return a string with the given country and local authority name in" do
+        assert_equal "This service is unavailable in Dundee, Scotland", LocalTransactionServices.instance.content(461, "Scotland", { local_authority_name: "Dundee" })
+      end
+
+      should "return an empty string if the given content for the service is an empty string" do
+        assert_equal "", LocalTransactionServices.instance.content(561, "Scotland", { local_authority_name: "Dundee" })
+      end
+
+      should "return an empty string if the given content for the service is an empty" do
+        assert_equal "", LocalTransactionServices.instance.content(661, "Scotland", { local_authority_name: "Dundee" })
+      end
+
+      should "return an empty string if there is no content for the given service" do
+        assert_equal "", LocalTransactionServices.instance.content(761, "Scotland", { local_authority_name: "Dundee" })
+      end
     end
 
-    should "return an empty string if the given content for the service is an empty string" do
-      assert_equal "", LocalTransactionServices.instance.content(561, "Scotland", "Dundee")
+    context "Northern Ireland" do
+      should "return a string with the given country and local authority name in" do
+        assert_equal "This service is unavailable in Armagh City, Banbridge and Craigavon, Northern Ireland", LocalTransactionServices.instance.content(461, "Northern Ireland", { local_authority_name: "Armagh City, Banbridge and Craigavon" })
+      end
+
+      should "return an empty string if the given content for the service is an empty string" do
+        assert_equal "", LocalTransactionServices.instance.content(561, "Northern Ireland", { local_authority_name: "Armagh City, Banbridge and Craigavon" })
+      end
+
+      should "return an empty string if the given content for the service is an empty" do
+        assert_equal "", LocalTransactionServices.instance.content(661, "Northern Ireland", { local_authority_name: "Armagh City, Banbridge and Craigavon" })
+      end
+
+      should "return an empty string if there is no content for the given service" do
+        assert_equal "", LocalTransactionServices.instance.content(761, "Northern Ireland", { local_authority_name: "Armagh City, Banbridge and Craigavon" })
+      end
     end
 
-    should "return an empty string if the given content for the service is an empty" do
-      assert_equal "", LocalTransactionServices.instance.content(661, "Scotland", "Dundee")
-    end
+    context "params" do
+      should "show the un-interpolated string when no params are given" do
+        assert_equal "This service is unavailable in %{local_authority_name}, Scotland", LocalTransactionServices.instance.content(461, "Scotland")
+      end
 
-    should "return an empty string if there is no content for the given service" do
-      assert_equal "", LocalTransactionServices.instance.content(761, "Scotland", "Dundee")
+      should "show the un-interpolated string when params is nil" do
+        assert_equal "This service is unavailable in %{local_authority_name}, Scotland", LocalTransactionServices.instance.content(461, "Scotland", nil)
+      end
+
+      should "show the un-interpolated string when params is empty" do
+        assert_equal "This service is unavailable in %{local_authority_name}, Scotland", LocalTransactionServices.instance.content(461, "Scotland", {})
+      end
+
+      should "raise a missing interpolation when local authority name is missing" do
+        assert_raises I18n::MissingInterpolationArgument do
+          LocalTransactionServices.instance.content(461, "Scotland", { not_required: "Testing" })
+        end
+      end
+
+      should "show the interpolated string with the given params" do
+        assert_equal "This service is unavailable in Bedrock, Scotland", LocalTransactionServices.instance.content(461, "Scotland", { local_authority_name: "Bedrock" })
+      end
     end
   end
 end

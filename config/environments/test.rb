b3fc0472beb85be4f8ccf8392c5fc268f5193388
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-02-05 10:04:06 +0000

    Refactor local transactions services model
    This removes the need for a singleton class by creating an object per
    service, which allows simplification of the code as the lgsl and country
    name can become instance instance attributes. This also moves the config
    to the lib folder, as the top level config is reserved for Rails
    configuration.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  config.after_initialize do
+    Frontend.local_transactions_config = "test/fixtures/unavailable_services.yml"
+  end
 end
--- config/initializers/local_links_manager.rb
+++ config/initializers/local_links_manager.rb
@@ -1,3 +1,4 @@
 require "gds_api/local_links_manager"
 
 Frontend.local_links_manager_api = GdsApi::LocalLinksManager.new(Plek.new.find("local-links-manager"))
+Frontend.local_transactions_config = "lib/config/local_transaction_config.yml"

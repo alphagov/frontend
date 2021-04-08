a333e16662715f6a157627e708a18b5533abeb87
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-04-08 16:24:17 +0100

    Configure app to use memcached
    Frontend currently uses the Rails default cache implementation,
    which is to store entries to the file system at "#{root}/tmp/cache/".
    We want it to use memcached instead.
    Without setting the URL of the memcached servers, it will default to
    localhost:11211, an additional puppet PR is needed to set the URL
    as an env var.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, nil, { namespace: :frontend, compress: true } unless ENV["HEROKU_APP_NAME"]
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

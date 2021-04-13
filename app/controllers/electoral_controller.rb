88e050f4c562cfa704c47e7912126a5cd6bc940e
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-04-13 17:01:41 +0100

    Fetch repsonse from the api with uprn params
    If a postcode maps to multiple addresses, we can lookup by uprn instead.

--- app/controllers/electoral_controller.rb
+++ app/controllers/electoral_controller.rb
@@ -5,14 +5,18 @@ class ElectoralController < ApplicationController
   def show
     @publication = LocalTransactionPresenter.new(@content_item)
 
-    if postcode_params.nil?
+    if postcode_params.nil? && uprn_params.nil?
       render "local_transaction/search"
       return
     end
 
-    @postcode = postcode_params.strip
-    api_response = fetch_response(@postcode)
+    @postcode = postcode_params.strip if postcode_params
+
+    api_response =
+      postcode_params ? fetch_response(postcode: @postcode) : fetch_response(uprn: uprn_params)
+
     @presented_result = presented_result(api_response)
+
     if @presented_result.address_picker
       render :address_picker
       return
@@ -22,13 +26,18 @@ class ElectoralController < ApplicationController
 
 private
 
-  def fetch_response(postcode)
-    response = request_api("#{api_base_path}/postcode/#{postcode}")
+  def fetch_response(postcode: nil, uprn: nil)
+    endpoint = postcode ? "postcode/#{postcode}" : "address/#{uprn}"
+    response = request_api("#{api_base_path}/#{endpoint}")
     JSON.parse(response)
   end
 
   def postcode_params
     params[:postcode]
+  end
+
+  def uprn_params
+    params[:uprn]
   end
 
   def presented_result(response)
--- test/integration/electoral_look_up_test.rb
+++ test/integration/electoral_look_up_test.rb
@@ -17,8 +17,13 @@ class ElectoralLookUpTest < ActionDispatch::IntegrationTest
     File.read(path)
   end
 
-  def stub_democracy_club_api(response)
-    stub_request(:get, "https://api.ec-dc.club/api/v1/postcode/LS11UR")
+  def stub_api_postcode_lookup(response, postcode)
+    stub_request(:get, "https://api.ec-dc.club/api/v1/postcode/#{postcode}")
+      .to_return(body: response)
+  end
+
+  def stub_api_address_lookup(response, uprn)
+    stub_request(:get, "https://api.ec-dc.club/api/v1/address/#{uprn}")
       .to_return(body: response)
   end
 
@@ -31,9 +36,10 @@ class ElectoralLookUpTest < ActionDispatch::IntegrationTest
   end
 
   context "searching by postcode" do
-    context "when a valid postcode is entered which matches a single electoral service" do
+    context "when a valid postcode is entered which matches a single address" do
       should "display contact details, and upcoming elections if available" do
-        stub_democracy_club_api(api_response)
+        stub_api_postcode_lookup(api_response, "LS11UR")
+
         search_for(postcode: "LS11UR")
 
         assert page.has_selector?("h2", text: "Your local council")
@@ -47,7 +53,7 @@ class ElectoralLookUpTest < ActionDispatch::IntegrationTest
       should "display a helpful message if no contact details are present" do
         without_contact_information = JSON.parse(api_response)
         without_contact_information["registration"] = {}
-        stub_democracy_club_api(without_contact_information.to_json)
+        stub_api_postcode_lookup(without_contact_information.to_json, "LS11UR")
         search_for(postcode: "LS11UR")
 
         assert page.has_selector?("h2", text: "Get help with electoral registration")
@@ -57,11 +63,46 @@ class ElectoralLookUpTest < ActionDispatch::IntegrationTest
       should "inform user if there are no upcoming elections " do
         without_dates = JSON.parse(api_response)
         without_dates["dates"] = []
-        stub_democracy_club_api(without_dates.to_json)
+        stub_api_postcode_lookup(without_dates.to_json, "LS11UR")
         search_for(postcode: "LS11UR")
 
         assert page.has_selector?("h2", text: "Next elections")
         assert page.has_text?("There are no upcoming elections for your area")
+      end
+    end
+
+    context "when a valid postcode is entered which matches multiple addresses" do
+      should "display an address picker" do
+        @postcode = "IP224DN"
+        with_multiple_addresses = JSON.parse(api_response)
+        with_multiple_addresses["address_picker"] = true
+        with_multiple_addresses["addresses"] = [
+          {
+            "address" => "1 BUCKINGHAM PALACE",
+            "postcode" => @postcode,
+            "slug" => "1234",
+            "url" => "/foo",
+          },
+          {
+            "address" => "2 BUCKINGHAM PALACE",
+            "postcode" => @postcode,
+            "slug" => "5678",
+            "url" => "/bar",
+          },
+        ]
+        # Search for postcode
+        stub_api_postcode_lookup(with_multiple_addresses.to_json, @postcode)
+        search_for(postcode: @postcode)
+
+        # Multiple addresses are displayed
+        assert page.has_selector?("h1", text: "Choose your address")
+        assert page.has_selector?("p", text: "The #{@postcode} postcode could be in several council areas. Please choose your address from the list below.")
+        assert page.has_link?("1 BUCKINGHAM PALACE", href: "/find-electoral-things?uprn=1234")
+
+        # Click on one of the suggested addresses
+        stub_api_address_lookup(api_response, "1234")
+        click_link("1 BUCKINGHAM PALACE")
+        assert page.has_selector?("p", text: "We've matched the postcode to Cardiff Council")
       end
     end
   end

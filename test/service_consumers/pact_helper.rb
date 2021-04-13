0316d3473fe55be9c932f6313d1fe649f6e66d6f
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-04-13 13:22:17 +0100

    Remove unnecessary ProxyApp
    This is only required when the host forms part of the API
    being tested, which it doesn't in this case.

--- test/service_consumers/pact_helper.rb
+++ test/service_consumers/pact_helper.rb
@@ -12,23 +12,11 @@ end
 
 WebMock.allow_net_connect!
 
-class ProxyApp
-  def initialize(real_provider_app)
-    @real_provider_app = real_provider_app
-  end
-
-  def call(env)
-    env["HTTP_HOST"] = "localhost"
-    @real_provider_app.call(env)
-  end
-end
-
 def url_encode(str)
   ERB::Util.url_encode(str)
 end
 
 Pact.service_provider "Bank Holidays API" do
-  app { ProxyApp.new(Rails.application) }
   honours_pact_with "GDS API Adapters" do
     if ENV["PACT_URI"]
       pact_uri(ENV["PACT_URI"])

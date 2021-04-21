2f7ae36929f1bd2d74274fb41878d63be3d896a7
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-04-21 21:00:24 +0100

    Add instrumentation for API calls
    By raising errors, we'll get sentry to pick them up.
    
    We're also adding timing, and adding error codes to graphite to allow
    us to track them.

--- app/services/electoral_service.rb
+++ app/services/electoral_service.rb
@@ -10,8 +10,10 @@ class ElectoralService
 
   def make_request
     @body = begin
-              json = RestClient.get(request_url, headers)
-              JSON.parse(json)
+              with_caching do
+                json = RestClient.get(request_url, headers)
+                JSON.parse(json)
+              end
             rescue RestClient::Exception => e
               @error = assemble_error(e)
               {}
@@ -31,13 +33,33 @@ private
   attr_reader :postcode, :uprn
 
   def assemble_error(exception)
+    report_error(exception.http_code)
+
     if [400, 404].include? exception.http_code
       if postcode
         "validPostcodeNoElectionsMatch"
       else
         "validUprnNoElectionsMatch"
       end
+    else
+      raise
     end
+  end
+
+  def report_error(code)
+    GovukStatsd.increment("elections.errors.#{code}")
+  end
+
+  def with_caching
+    Rails.cache.fetch(request_url, expires_in: 1.minute) do
+      GovukStatsd.time("content_store.#{timing_endpoint}.request_time") do
+        yield
+      end
+    end
+  end
+
+  def timing_endpoint
+    postcode.present? ? "postcode" : "address"
   end
 
   def request_url

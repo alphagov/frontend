2e6c012ff3f1bb73a73cc3d345c0288fb5c30b55
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2021-05-26 16:54:08 +0100

    Add /account/saved-pages/add
    /account/saved-pages/new is an authenticated path, if the user attempts
    to save a page without an account_session_header then logged_in? will be
    false and we should redirect them to authenticate.
    
    The save page feature only requires level0 auth (No MFA needed!)
    We can pass on the state_id, and should redirect them back to the save a
    page path so they can try again after auth.
    
    Add /account/saved-pages/new - logged in behaviour
    
    If a user is authenticated then they can save a page!
    
    I have added a rescue in here, I will check in the review if it is
    really required. I was considering a scenario where the
    account_session_header is populated (perhaps with hacker junk) and the
    API returns an unauthorised.
    
    In that case seems best to clear the session header and attempt to
    re-authenticate from scratch
    
    ToSquash: Protect against redirect attacks
    
    Brakeman has rightly noted that we were offering a redirect based on an
    unverified parameter. This was a good catch.
    
    For the Happy path there is not really an issue here, the account-api
    will have gone and confirmed a path represents an actually existing bit
    of content on GOV.UK, meaning that the redirect path we send back is a
    valid path within GOV.UK.
    
    However, if we're getting back a 422 error then the user could have sent
    anything across, and in this case we have an obligation to ensure that
    there are no redirect attacks that would allow a user to be sent a
    seemingly valid GOV.UK domain link (with a sneaky param) that then
    redirects them back to a separate domain and evil site.
    
    ToSquash: Gate /account/saved-pages/new behind a feature flag

---
+++ test/functional/save_pages_controller_test.rb
@@ -0,0 +1,108 @@
+require "test_helper"
+require "gds_api/test_helpers/account_api"
+
+class SavePagesControllerTest < ActionController::TestCase
+  include GdsApi::TestHelpers::AccountApi
+  include SessionHelpers
+
+  context "when FEATURE_FLAG_SAVE_A_PAGE environment variable is not 'enabled'" do
+    context "GET /account/saved-pages/add" do
+      should "return 404" do
+        get :create, params: { page_path: ministry_of_magic_path }
+        assert_response :not_found
+      end
+    end
+  end
+
+  context "when FEATURE_FLAG_SAVE_A_PAGE environment variable is set to 'enabled'" do
+    context "GET /account/saved-pages/add" do
+      context "when logged out" do
+        setup do
+          stub_account_api_unauthorized_save_page(page_path: ministry_of_magic_path)
+          @stub = stub_account_api_get_sign_in_url(
+            level_of_authentication: "level0",
+            redirect_path: save_page_path(
+              page_path: ministry_of_magic_path,
+            ),
+          )
+        end
+
+        should "redirect the user to the Auth Provider" do
+          with_feature_flag_enabled do
+            get :create, params: { page_path: ministry_of_magic_path }
+
+            assert_response :redirect
+            assert_requested @stub
+          end
+        end
+      end
+
+      context "when logged in" do
+        setup do
+          mock_logged_in_session
+          stub_account_api_save_page(page_path: ministry_of_magic_path, new_govuk_account_session: "placeholder")
+        end
+
+        should "create a saved page and redirect the user to the saved page_path" do
+          with_feature_flag_enabled do
+            get :create, params: { page_path: ministry_of_magic_path }
+            assert_response :redirect
+
+            path = Addressable::URI.parse(@response.headers["Location"]).path
+            assert_equal path, ministry_of_magic_path
+          end
+        end
+
+        should "report a successful save to the requesting app with a query parameter" do
+          with_feature_flag_enabled do
+            get :create, params: { page_path: ministry_of_magic_path }
+            assert_response :redirect
+
+            query = Addressable::URI.parse(@response.headers["Location"]).query
+            assert_equal query, "personalisation=page_saved"
+          end
+        end
+
+        should "report an unsuccessful save to the requesting app with a query parameter" do
+          with_feature_flag_enabled do
+            stub_account_api_save_page_cannot_save_page(
+              page_path: ministry_of_magic_path,
+              new_govuk_account_session: "placeholder",
+            )
+
+            get :create, params: { page_path: ministry_of_magic_path }
+            assert_response :redirect
+
+            query = Addressable::URI.parse(@response.headers["Location"]).query
+            assert_equal query, "personalisation=page_not_saved"
+          end
+        end
+
+        should "protect against a redirect attack from a failed save page request" do
+          with_feature_flag_enabled do
+            stub_account_api_save_page_cannot_save_page(
+              page_path: ministry_of_magic_path,
+              new_govuk_account_session: "placeholder",
+            )
+
+            get :create, params: { page_path: "https://evil.g0v.uk#{ministry_of_magic_path}" }
+            assert_response :redirect
+
+            path = Addressable::URI.parse(@response.headers["Location"]).path
+            query = Addressable::URI.parse(@response.headers["Location"]).query
+            assert_equal query, "personalisation=page_not_saved"
+            assert_equal path, ministry_of_magic_path
+          end
+        end
+      end
+    end
+  end
+
+  def ministry_of_magic_path
+    "/government/organisations/ministry-of-magic"
+  end
+
+  def with_feature_flag_enabled(&block)
+    ClimateControl.modify(FEATURE_FLAG_SAVE_A_PAGE: "enabled", &block)
+  end
+end

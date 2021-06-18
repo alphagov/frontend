3d076052f5a49480fe006458d4cf76b0f2cd5031
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2021-06-18 13:09:44 +0100

    Determine devolved administration availability for a transaction
    This checks the devolved administration availability for a service based on the country name that is returned from the postcode lookup.
    
    This will be used by the controller to determine which results page to display to the user (in a following commit).
    
    It also sets the devolved_administration_service_alternative_url to return the alternative_url provided for a devolved_administration_service, which will be accessed from the relevant view in a future commit.
    
    Previously the availability and button_link text were set in the LocalTransactionService. This functionality has been moved to the presenter and the LocalTransactionService will be deleted in a following commit. As these methods determine how information is presented to a user on the view, a presenter seemed like a better place for this functionality.
    
    I have chosen a more traditional approach of setting the text in the views and rendering them separately, rather
    than using one view and displaying different text based on what the methods return (as was the case previously in LocalTransactionService). I felt this approach was easier to follow and didn’t really involve too much duplication.

--- test/unit/presenters/local_transaction_presenter_test.rb
+++ test/unit/presenters/local_transaction_presenter_test.rb
@@ -16,4 +16,74 @@ class LocalTransactionPresenterTest < ActiveSupport::TestCase
   test "#need_to_know" do
     assert_equal "foo", subject(details: { need_to_know: "foo" }).need_to_know
   end
+
+  test "#scotland_availability" do
+    devolved_administration = { "details" => { "scotland_availability" => { "type" => "devolved_administration_service", "alternative_url" => "https://gov.scot" } } }
+
+    assert_equal "devolved_administration_service", subject(devolved_administration).scotland_availability["type"]
+    assert_equal "https://gov.scot", subject(devolved_administration).scotland_availability["alternative_url"]
+  end
+
+  test "#wales_availability" do
+    devolved_administration = { "details" => { "wales_availability" => { "type" => "unavailable" } } }
+
+    assert_equal "unavailable", subject(devolved_administration).wales_availability["type"]
+    assert_equal nil, subject(devolved_administration).wales_availability["alternative_url"]
+  end
+
+  test "#northern_ireland_availability" do
+    devolved_administration = { "details" => { "northern_ireland_availability" => { "type" => "devolved_administration_service", "alternative_url" => "https://www.nidirect.gov.uk" } } }
+
+    assert_equal "devolved_administration_service", subject(devolved_administration).northern_ireland_availability["type"]
+    assert_equal "https://www.nidirect.gov.uk", subject(devolved_administration).northern_ireland_availability["alternative_url"]
+  end
+
+  test "#unavailable? returns true for a devolved administration marked as unavailable" do
+    devolved_administration = { "details" => { "wales_availability" => { "type" => "unavailable" } } }
+
+    assert_equal true, subject(devolved_administration).unavailable?("Wales")
+  end
+
+  test "#unavailable? returns false for a devolved administration marked as devolved_administration_service" do
+    devolved_administration = { "details" => { "scotland_availability" => { "type" => "devolved_administration_service", "alternative_url" => "https://gov.scot" } } }
+
+    assert_equal false, subject(devolved_administration).unavailable?("Scotland")
+  end
+
+  test "#unavailable? returns false for a non devolved administration" do
+    devolved_administration = { "details" => {} }
+
+    assert_equal false, subject(devolved_administration).unavailable?("England")
+  end
+
+  test "#devolved_administration_service? returns true for a devolved administration marked as devolved_administration_service" do
+    devolved_administration = { "details" => { "scotland_availability" => { "type" => "devolved_administration_service", "alternative_url" => "https://gov.scot" } } }
+    assert_equal true, subject(devolved_administration).devolved_administration_service?("Scotland")
+  end
+
+  test "#devolved_administration_service? returns false for a devolved administration marked as unavailable" do
+    devolved_administration = { "details" => { "wales_availability" => { "type" => "unavailable" } } }
+    assert_equal false, subject(devolved_administration).devolved_administration_service?("Wales")
+  end
+
+  test "#devolved_administration_service? returns false for a non devolved administration" do
+    devolved_administration = { "details" => {} }
+
+    assert_equal false, subject(devolved_administration).devolved_administration_service?("England")
+  end
+
+  test "#devolved_administration_service_alternative_url returns an alternative_url for a devolved_administration_service" do
+    devolved_administration = { "details" => { "scotland_availability" => { "type" => "devolved_administration_service", "alternative_url" => "https://gov.scot" } } }
+    assert_equal "https://gov.scot", subject(devolved_administration).devolved_administration_service_alternative_url("Scotland")
+  end
+
+  test "#devolved_administration_service_alternative_url does not return an alternative_url for an unavailable service" do
+    devolved_administration = { "details" => { "wales_availability" => { "type" => "unavailable" } } }
+    assert_equal nil, subject(devolved_administration).devolved_administration_service_alternative_url("Wales")
+  end
+
+  test "#devolved_administration_service_alternative_url does not return an alternative_url for a non devolved administration" do
+    devolved_administration = { "details" => {} }
+    assert_equal nil, subject(devolved_administration).devolved_administration_service_alternative_url("England")
+  end
 end

c87391913a3f5dc3f3aea7c4c6ae70997b7d7463
Michael Walker <mike@barrucadu.co.uk>
2021-06-25 11:28:08 +0100

    Remove redirect_path and level_of_authentication from sign in
    Originally we planned for this to be the only place which construct an
    OAuth auth URI, and other apps which need to sign the user in would
    redirect via https://www.gov.uk/sign-in?redirect_path=...
    
    We've since changed that approach, with other apps calling account-api
    to get a sign in URI directly.  So we can remove these parameters, as
    they're unused.

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -7,17 +7,17 @@ class SessionsControllerTest < ActionController::TestCase
 
   context "GET sign-in" do
     setup do
-      stub_account_api_get_sign_in_url(redirect_path: "/bank-holiday")
+      stub_account_api_get_sign_in_url
     end
 
     should "redirect the user to the GOV.UK Account service domain" do
-      get :create, params: { redirect_path: "/bank-holiday" }
+      get :create
       assert_response :redirect
       assert_equal @response.headers["Location"], "http://auth/provider"
     end
 
     should "preserve the _ga tracking parameter if provided" do
-      get :create, params: { redirect_path: "/bank-holiday", _ga: "ga123" }
+      get :create, params: { _ga: "ga123" }
       assert_equal @response.headers["Location"], "http://auth/provider?_ga=ga123"
     end
   end

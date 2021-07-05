eefcb02c9d2192e244d2686b5a158a05cbf87ac2
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-07-05 13:39:47 +0100

    SPIKE: Use OS APIs instead of Mapit
    This is a *really* hacky solution that proves we can use the OS Places
    and OS Names APIs instead of Mapit.
    
    To complete this work, we will need to:
    - migrate all the OAuth2 calls to postcode-lookup-api (via.
      gds-api-adapters)
    - merge changes to lcoal-links-manager and gds-api-adapters that will
      allow us to lookup a LA by GSS code (instead of slug)
    - add tests
    - deal with errors

--- app/controllers/local_transaction_controller.rb
+++ app/controllers/local_transaction_controller.rb
@@ -1,3 +1,8 @@
+API_KEY = "xx"
+API_SECRET = "xx"
+
+require "oauth2"
+
 class LocalTransactionController < ApplicationController
   include ActionView::Helpers::TextHelper
   include Cacheable
@@ -13,19 +18,16 @@ class LocalTransactionController < ApplicationController
 
   def search
     if request.post?
-      @location_error = location_error
+      ## TODO: deal with errors
+      local_authority = fetch_location(postcode)
 
-      if @location_error
-        @postcode = postcode
-      elsif mapit_response.location_found?
-        slug = if lgsl == 364 && country_name == "Northern Ireland"
-                 "electoral-office-for-northern-ireland"
-               else
-                 local_authority_slug
-               end
+      slug = if lgsl == 364 && country_name == "Northern Ireland"
+               "electoral-office-for-northern-ireland"
+             else
+               local_authority[:slug]
+             end
 
-        redirect_to local_transaction_results_path(local_authority_slug: slug)
-      end
+      redirect_to local_transaction_results_path(local_authority_slug: slug)
     end
   end
 
@@ -46,39 +48,12 @@ class LocalTransactionController < ApplicationController
 
 private
 
-  def local_authority_slug
-    @local_authority_slug ||= LocalTransactionLocationIdentifier.find_slug(mapit_response.location.areas, content_item)
-  end
-
   def country_name
-    @country_name ||= LocalTransactionLocationIdentifier.find_country(mapit_response.location.areas, content_item)
-  end
-
-  def location_error
-    return LocationError.new(INVALID_POSTCODE) if banned_postcode? || mapit_response.invalid_postcode? || mapit_response.blank_postcode?
-    return LocationError.new(NO_MAPIT_MATCH) if mapit_response.location_not_found?
-    return LocationError.new(NO_MATCHING_AUTHORITY) unless local_authority_slug
+    ## TODO: this can be inferred from the first letter of the GSS code
   end
 
   def banned_postcode?
     BANNED_POSTCODES.include? postcode
-  end
-
-  def mapit_response
-    @mapit_response ||= location_from_mapit
-  end
-
-  def location_from_mapit
-    if postcode.present?
-      begin
-        location = Frontend.mapit_api.location_for_postcode(postcode)
-      rescue GdsApi::HTTPNotFound
-        location = nil
-      rescue GdsApi::HTTPClientError => e
-        error = e
-      end
-    end
-    MapitPostcodeResponse.new(postcode, location, error)
   end
 
   def postcode
@@ -104,5 +79,58 @@ private
     else
       @_interaction ||= Frontend.local_links_manager_api.local_link(council, lgsl, lgil)
     end
+  end
+
+  def fetch_location(postcode)
+    if postcode.present?
+
+      ## TODO: all of these requests should be moved to postcode-lookup-api (via gds_api_adapters) and made into individual methods (plus we need to implement caching)
+
+      client = OAuth2::Client.new(API_KEY, API_SECRET, site: "https://api.os.uk", token_url: "/oauth2/token/v1")
+
+      token = client.client_credentials.get_token
+
+      response = token.get("/search/places/v1/postcode", params: {postcode: postcode, dataset: "DPA"}) # DPA = Delivery Point Addresses, i.e. postal addresses
+
+      results = JSON.parse(response.body)["results"]
+
+      if results.map { |address| address.dig("DPA", "LOCAL_CUSTODIAN_CODE") }.reject { |code| code == 7655 }.uniq.length == 1  ## 7655 = Ordnance Survey added data, so we can ignore this, needs a more elegant solution
+        ## Entire postcode maps to a single local authority (i.e. not a split postcode)
+        address = results.first
+
+        ## TODO: Can we get the GSS code without needing to make another 2 queries??
+        coordinates = [address.dig("DPA", "X_COORDINATE"), address.dig("DPA", "Y_COORDINATE")].join(",")
+
+        response = token.get("/search/names/v1/nearest", params: {point: coordinates})
+
+        utla_url = JSON.parse(response.body).dig("results", 0, "GAZETTEER_ENTRY", "COUNTY_UNITARY_URI")
+        ltla_url = JSON.parse(response.body).dig("results", 0, "GAZETTEER_ENTRY", "DISTRICT_BOROUGH_URI")
+
+        ## We need to return the lowest level of local authority, i.e. the LTLA, unless it is a Unitary Authority, then the UTLA
+        if ltla_url
+          gss_code = gss_code_from_authority_url(ltla_url)
+          slug = slug_from_gss_code(gss_code)
+          {gss_code: gss_code, slug: slug}
+        else
+          gss_code = gss_code_from_authority_url(utla_url)
+          slug = slug_from_gss_code(gss_code)
+          {gss_code: gss_code, slug: slug}
+        end
+      else
+        ## Postcode maps to more than one LA, so offer the user a choice of addresses, then use the UPRN of their choice to get the LA
+        ## TODO: deal with split postcodes
+        render plain: "Postcode split over multiple LAs"
+      end
+    end
+  end
+
+  def gss_code_from_authority_url(url)
+    uri = URI.parse("#{url.gsub('id', 'doc')}.json") # The gsub is a hack as Net::HTTP doesn't follow redirects
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body).dig(url, "http://data.ordnancesurvey.co.uk/ontology/admingeo/gssCode", 0, "value")
+  end
+
+  def slug_from_gss_code(gss_code)
+    Frontend.local_links_manager_api.local_authority_by_gss(gss_code).dig("local_authorities", 0, "slug")
   end
 end

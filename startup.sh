4c1cbda44bca3a4c496a029ea7fb680b11ae0104
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2021-07-14 16:05:51 +0100

    Only allow startup in live mode
    The startup script has been restricted to only operate in `live` mode as per
    our move to soley use GOVUK Docker for local development.
    
    Also does a `bundle check` first which ensures the app's gems
    are up to date before trying to install them.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bundle install
+bundle check || bundle install
 
 if [[ $1 == "--live" ]] ; then
   GOVUK_APP_DOMAIN=www.gov.uk \
@@ -10,5 +10,8 @@ if [[ $1 == "--live" ]] ; then
   PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-assets.publishing.service.gov.uk} \
   bundle exec rails s -p 3005
 else
-  bundle exec rails s -p 3005
+  echo "ERROR: other startup modes are not supported"
+  echo ""
+  echo "https://docs.publishing.service.gov.uk/manual/local-frontend-development.html"
+  exit 1
 fi

404bfbc3ec9a9ce889867546211696b8efe3bb74
Kevin Dew <kevindew@me.com>
2021-09-15 01:11:46 +0100

    Remove jQuery from save-location-spec.js
    I've done this because I felt that this test was conflated with rather a
    lot of wrapping and unwrapping of jQuery objects that I felt was a
    little confusing. It isn't essential to remove jQuery from jasmine tests
    as Jasmine provides it's own jQuery, however I feel that the use of
    jQuery in them can often make understanding JS patterns a bit harder.

--- spec/javascripts/unit/save-location.spec.js
+++ spec/javascripts/unit/save-location.spec.js
@@ -1,160 +1,156 @@
 describe('Saving bank holiday locations', function () {
-  var $locations
-  var html = '<section class="js-save-nation" data-module="save-bank-holiday-nation" data-nation="England_and_Wales" data-nation-matches=\'["England","Wales"]\' data-save-description="Do you want to save # as your location for bank holidays?" data-save-button-aria-label="Save # as your location for bank holidays" data-save-button-text="Save" data-undo-description="You\'ve saved # as your location for bank holidays" data-undo-button-aria-label="Remove # as your saved location for bank holidays" data-undo-button-text="Undo">' +
-    '<p class="js-nation-description" aria-live="polite">Do you want to save # as your location for bank holidays?</p>' +
-    '<button class="js-nation-button" type="submit" aria-label="Save # as your location for bank holidays">Save</button>' +
-    '<p class="js-nation-link">' +
-      '<a href="#" class="govuk-link">We\'ll add a cookie to your device</a>' +
-    '</p>' +
-  '</section>'
+  var element
 
   beforeEach(function () {
     window.GOVUK.setCookie('cookies_policy', '{"essential":true,"settings":true,"usage":true,"campaigns":true}')
+    var div = document.createElement('div')
+    div.innerHTML =
+      '<section class="js-save-nation" data-module="save-bank-holiday-nation" data-nation="England_and_Wales" data-nation-matches=\'["England","Wales"]\' data-save-description="Do you want to save # as your location for bank holidays?" data-save-button-aria-label="Save # as your location for bank holidays" data-save-button-text="Save" data-undo-description="You\'ve saved # as your location for bank holidays" data-undo-button-aria-label="Remove # as your saved location for bank holidays" data-undo-button-text="Undo">' +
+        '<p class="js-nation-description" aria-live="polite">Do you want to save # as your location for bank holidays?</p>' +
+        '<button class="js-nation-button" type="submit" aria-label="Save # as your location for bank holidays">Save</button>' +
+        '<p class="js-nation-link">' +
+          '<a href="#" class="govuk-link">We\'ll add a cookie to your device</a>' +
+        '</p>' +
+      '</section>'
+
+    element = div.querySelector('section')
   })
 
   afterEach(function () {
     window.GOVUK.deleteCookie('cookies_policy')
     window.GOVUK.deleteCookie('user_nation')
-    $locations.remove()
     window.location.hash = ''
   })
 
   describe('initial state', function () {
-    beforeEach(function () {
-      $locations = $(html)
-      $('body').append($locations)
-    })
-
     it('shows the bank holiday option if cookies are allowed', function () {
-      new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
-      expect($locations[0].style.display).toEqual('block')
+      new GOVUK.Modules.SaveBankHolidayNation(element).init()
+      expect(element.style.display).toEqual('block')
     })
 
     it('doesn\'t show the bank holiday option if cookies are not allowed', function () {
       window.GOVUK.setCookie('cookies_policy', '{"essential":false,"settings":false,"usage":false,"campaigns":false}')
-      new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
-      expect($locations[0].style.display).toEqual('')
+      new GOVUK.Modules.SaveBankHolidayNation(element).init()
+      expect(element.style.display).toEqual('')
     })
   })
 
   describe('interacting with the button', function () {
     beforeEach(function () {
-      $locations = $(html)
-      $('body').append($locations)
-      new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
+      new GOVUK.Modules.SaveBankHolidayNation(element).init()
     })
 
     it('saves and unsaves a nation', function () {
-      $('.js-nation-button').click()
+      var nationButton = element.querySelector('.js-nation-button')
+      nationButton.click()
 
       expect(window.GOVUK.getCookie('user_nation')).toEqual('England_and_Wales')
-      expect($('.js-nation-description').text()).toEqual('You\'ve saved England and Wales as your location for bank holidays')
-      expect($('.js-nation-button').text()).toEqual('Undo')
-      expect($('.js-nation-button').attr('aria-label')).toEqual('Remove England and Wales as your saved location for bank holidays')
-      expect($('.js-nation-link')[0].style.display).toEqual('none')
+      expect(element.querySelector('.js-nation-description').textContent)
+        .toEqual('You\'ve saved England and Wales as your location for bank holidays')
+      expect(nationButton.textContent).toEqual('Undo')
+      expect(nationButton.getAttribute('aria-label')).toEqual('Remove England and Wales as your saved location for bank holidays')
+      expect(element.querySelector('.js-nation-link').style.display).toEqual('none')
 
-      $('.js-nation-button').click()
+      nationButton.click()
 
       expect(window.GOVUK.getCookie('user_nation')).toEqual(null)
-      expect($('.js-nation-description').text()).toEqual('Do you want to save England and Wales as your location for bank holidays?')
-      expect($('.js-nation-button').text()).toEqual('Save')
-      expect($('.js-nation-button').attr('aria-label')).toEqual('Save England and Wales as your location for bank holidays')
-      expect($('.js-nation-link')[0].style.display).toEqual('block')
+      expect(element.querySelector('.js-nation-description').textContent)
+        .toEqual('Do you want to save England and Wales as your location for bank holidays?')
+      expect(nationButton.textContent).toEqual('Save')
+      expect(nationButton.getAttribute('aria-label')).toEqual('Save England and Wales as your location for bank holidays')
+      expect(element.querySelector('.js-nation-link').style.display).toEqual('block')
     })
   })
 
   describe('having set a cookie on a previous visit', function () {
     beforeEach(function () {
-      $locations = $(html)
-      $('body').append($locations)
       window.GOVUK.setCookie('user_nation', 'England_and_Wales')
-      new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
+      new GOVUK.Modules.SaveBankHolidayNation(element).init()
     })
 
     it('sets the state correctly', function () {
-      expect($('.js-nation-description').text()).toEqual('You\'ve saved England and Wales as your location for bank holidays')
-      expect($('.js-nation-button').text()).toEqual('Undo')
-      expect($('.js-nation-button').attr('aria-label')).toEqual('Remove England and Wales as your saved location for bank holidays')
-      expect($('.js-nation-link')[0].style.display).toEqual('none')
+      expect(element.querySelector('.js-nation-description').textContent)
+        .toEqual('You\'ve saved England and Wales as your location for bank holidays')
+      expect(element.querySelector('.js-nation-button').textContent).toEqual('Undo')
+      expect(element.querySelector('.js-nation-button').getAttribute('aria-label'))
+        .toEqual('Remove England and Wales as your saved location for bank holidays')
+      expect(element.querySelector('.js-nation-link').style.display).toEqual('none')
     })
   })
 
   describe('with multiple bank holiday locations', function () {
-    var englandAndWales
-    var northernIreland
+    var englandAndWalesElement
+    var northernIrelandElement
 
     beforeEach(function () {
-      var html = '<div>' +
-      '<section class="js-save-nation" data-module="save-bank-holiday-nation" data-nation="England_and_Wales" data-nation-matches=\'["England","Wales"]\' data-save-description="Do you want to save # as your location for bank holidays?" data-save-button-aria-label="Save # as your location for bank holidays" data-save-button-text="Save" data-undo-description="You\'ve saved # as your location for bank holidays" data-undo-button-aria-label="Remove # as your saved location for bank holidays" data-undo-button-text="Undo">' +
-        '<p class="js-nation-description" aria-live="polite">Do you want to save # as your location for bank holidays?</p>' +
-        '<button id="en" class="js-nation-button" type="submit" aria-label="Save # as your location for bank holidays">Save</button>' +
-        '<p class="js-nation-link">' +
-          '<a href="#" class="govuk-link">We\'ll add a cookie to your device</a>' +
-        '</p>' +
-      '</section>' +
-      '<section class="js-save-nation" data-module="save-bank-holiday-nation" data-nation="Northern_Ireland" data-nation-matches=\'["Northern Ireland"]\' data-save-description="Do you want to save # as your location for bank holidays?" data-save-button-aria-label="Save # as your location for bank holidays" data-save-button-text="Save" data-undo-description="You\'ve saved # as your location for bank holidays" data-undo-button-aria-label="Remove # as your saved location for bank holidays" data-undo-button-text="Undo">' +
-        '<p class="js-nation-description" aria-live="polite">Do you want to save # as your location for bank holidays?</p>' +
-        '<button id="ni" class="js-nation-button" type="submit" aria-label="Save # as your location for bank holidays">Save</button>' +
-        '<p class="js-nation-link">' +
-          '<a href="#" class="govuk-link">We\'ll add a cookie to your device</a>' +
-        '</p>' +
-      '</section>' +
-      '</div>'
-      $locations = $(html)
-
-      $('body').append($locations)
       window.GOVUK.setCookie('user_nation', 'Northern_Ireland')
-      $locations.find('.js-save-nation').each(function () {
-        new GOVUK.Modules.SaveBankHolidayNation(this).init()
-      })
-      englandAndWales = $('[data-nation=England_and_Wales]')
-      northernIreland = $('[data-nation=Northern_Ireland]')
+
+      englandAndWalesElement = element.cloneNode(true)
+
+      northernIrelandElement = element.cloneNode(true)
+      northernIrelandElement.setAttribute('data-nation', 'Northern_Ireland')
+      northernIrelandElement.setAttribute('data-nation-matches', '["Northern Ireland"]')
+
+      document.body.appendChild(englandAndWalesElement)
+      document.body.appendChild(northernIrelandElement)
+
+      new GOVUK.Modules.SaveBankHolidayNation(englandAndWalesElement).init()
+      new GOVUK.Modules.SaveBankHolidayNation(northernIrelandElement).init()
+    })
+
+    afterEach(function () {
+      document.body.removeChild(englandAndWalesElement)
+      document.body.removeChild(northernIrelandElement)
     })
 
     it('sets the state correctly', function () {
-      expect(englandAndWales.find('.js-nation-description').text()).toEqual('Do you want to save England and Wales as your location for bank holidays?')
-      expect(englandAndWales.find('.js-nation-button').text()).toEqual('Save')
-      expect(englandAndWales.find('.js-nation-button').attr('aria-label')).toEqual('Save England and Wales as your location for bank holidays')
-      expect(englandAndWales.find('.js-nation-link')[0].style.display).toEqual('block')
+      expect(englandAndWalesElement.querySelector('.js-nation-description').textContent)
+        .toEqual('Do you want to save England and Wales as your location for bank holidays?')
+      expect(englandAndWalesElement.querySelector('.js-nation-button').textContent).toEqual('Save')
+      expect(englandAndWalesElement.querySelector('.js-nation-button').getAttribute('aria-label'))
+        .toEqual('Save England and Wales as your location for bank holidays')
+      expect(englandAndWalesElement.querySelector('.js-nation-link').style.display).toEqual('block')
 
-      expect(northernIreland.find('.js-nation-description').text()).toEqual('You\'ve saved Northern Ireland as your location for bank holidays')
-      expect(northernIreland.find('.js-nation-button').text()).toEqual('Undo')
-      expect(northernIreland.find('.js-nation-button').attr('aria-label')).toEqual('Remove Northern Ireland as your saved location for bank holidays')
-      expect(northernIreland.find('.js-nation-link')[0].style.display).toEqual('none')
+      expect(northernIrelandElement.querySelector('.js-nation-description').textContent)
+        .toEqual('You\'ve saved Northern Ireland as your location for bank holidays')
+      expect(northernIrelandElement.querySelector('.js-nation-button').textContent).toEqual('Undo')
+      expect(northernIrelandElement.querySelector('.js-nation-button').getAttribute('aria-label'))
+        .toEqual('Remove Northern Ireland as your saved location for bank holidays')
+      expect(northernIrelandElement.querySelector('.js-nation-link').style.display).toEqual('none')
     })
 
     it('updates the state correctly after clicking', function () {
-      $locations.find('#en').click()
+      var enNationButton = englandAndWalesElement.querySelector('.js-nation-button')
+      enNationButton.click()
 
-      expect(englandAndWales.find('.js-nation-description').text()).toEqual('You\'ve saved England and Wales as your location for bank holidays')
-      expect(englandAndWales.find('.js-nation-button').text()).toEqual('Undo')
-      expect(englandAndWales.find('.js-nation-button').attr('aria-label')).toEqual('Remove England and Wales as your saved location for bank holidays')
-      expect(englandAndWales.find('.js-nation-link')[0].style.display).toEqual('none')
+      expect(englandAndWalesElement.querySelector('.js-nation-description').textContent).toEqual('You\'ve saved England and Wales as your location for bank holidays')
+      expect(enNationButton.textContent).toEqual('Undo')
+      expect(enNationButton.getAttribute('aria-label'))
+        .toEqual('Remove England and Wales as your saved location for bank holidays')
+      expect(englandAndWalesElement.querySelector('.js-nation-link').style.display).toEqual('none')
 
-      expect(northernIreland.find('.js-nation-description').text()).toEqual('Do you want to save Northern Ireland as your location for bank holidays?')
-      expect(northernIreland.find('.js-nation-button').text()).toEqual('Save')
-      expect(northernIreland.find('.js-nation-button').attr('aria-label')).toEqual('Save Northern Ireland as your location for bank holidays')
-      expect(northernIreland.find('.js-nation-link')[0].style.display).toEqual('block')
+      expect(northernIrelandElement.querySelector('.js-nation-description').textContent)
+        .toEqual('Do you want to save Northern Ireland as your location for bank holidays?')
+      expect(northernIrelandElement.querySelector('.js-nation-button').textContent).toEqual('Save')
+      expect(northernIrelandElement.querySelector('.js-nation-button').getAttribute('aria-label'))
+        .toEqual('Save Northern Ireland as your location for bank holidays')
+      expect(northernIrelandElement.querySelector('.js-nation-link').style.display).toEqual('block')
     })
   })
 
   describe('with URL fragments', function () {
     it('appends the right URL fragment if no fragment is present', function () {
       window.GOVUK.setCookie('user_nation', 'England_and_Wales')
-      $locations = $(html)
-      $('body').append($locations)
-      new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
+      new GOVUK.Modules.SaveBankHolidayNation(element).init()
 
       expect(window.location.hash).toEqual('#england-and-wales')
     })
 
     it('doesn\'t append a URL fragment if one is already present', function () {
       window.GOVUK.setCookie('user_nation', 'scotland')
-      $locations = $(html)
-      $('body').append($locations)
       window.location.hash = '#england-and-wales'
 
-      new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
+      new GOVUK.Modules.SaveBankHolidayNation(element).init()
 
       expect(window.location.hash).toEqual('#england-and-wales')
     })
@@ -162,23 +158,18 @@ describe('Saving bank holiday locations', function () {
 
   it('appends the correct URL fragment when the nation cookie partially matches one of the tabs', function () {
     window.GOVUK.setCookie('user_nation', 'England')
-    $locations = $(html)
-    $('body').append($locations)
 
-    new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
-
-    var englandAndWales = $('[data-nation=England_and_Wales]')
+    new GOVUK.Modules.SaveBankHolidayNation(element).init()
 
     expect(window.location.hash).toEqual('#england-and-wales')
-    expect(englandAndWales.find('.js-nation-description').text()).toEqual('Do you want to save England and Wales as your location for bank holidays?')
+    expect(element.querySelector('.js-nation-description').textContent)
+      .toEqual('Do you want to save England and Wales as your location for bank holidays?')
   })
 
   it('does not append a URL fragment when the cookie does not match one of the nations', function () {
     window.GOVUK.setCookie('user_nation', 'spooky cookie')
-    $locations = $(html)
-    $('body').append($locations)
 
-    new GOVUK.Modules.SaveBankHolidayNation($locations[0]).init()
+    new GOVUK.Modules.SaveBankHolidayNation(element).init()
 
     expect(window.location.hash).toEqual('')
   })

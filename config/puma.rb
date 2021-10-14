76511cc9ef0c709a5f6697db608710048b75f641
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2021-10-14 11:03:25 +0100

    Add Dockerfile specifically for Kubernetes
    This commit adds a new Dockerfile specifically targeted at Kubernetes, which switches from the use of Foreman for running Rails to instead running Rails directly. Foreman isn't necessary when running on Kubernetes, as the controller managers within Kubernetes will take care of ensuring that our app is running without the need of a separate process manager.
    
    The reason behind creating a separate Dockerfile in this commit is because the existing Dockerfile is used by the publishing end-to-end tests; once these tests are deprecated (as is the long-term plan) then `Dockerfile.k8s` can become `Dockerfile`.
    
    Co-authored-by: Chris Banks <chris.banks@digital.cabinet-office.gov.uk>

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+# TODO: Check usage of these env vars is consistent throughout apps / libraries.
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -24,6 +25,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 # pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+# TODO: Consider number of workers below.
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.

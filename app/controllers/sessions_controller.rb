alphagov/frontend

commit 59f09bf841541208531b9f81b475b221c7d294ad

Move first-time form to a separate page, and redirect to it
This commit moves the first-time form from the /sign-in/callback
endpoint to a new /sign-in/first-time endpoint, and redirects to it if
needed.

This has a couple of benefits:

1. The callback endpoint is now simpler: it either throws an error or
redirects.

2. Refreshing the form will no longer re-validate the code & state
parameters.

The downside is that we need a way of passing the session header value
from the callback endpoint to the form *without* logging the user in,
as they're not actually logged in until they submit it.  I've solved
this problem by storing the value in the header, but with a prefix,
which means account-api will be unable to decode it if the user
wanders off to another page and tries to use their account.

app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   include GovukPersonalisation::ControllerConcern
 
   before_action :set_no_cache_headers
+  before_action :set_up_first_time, only: %i[first_time first_time_post]
 
   def create
     redirect_path = http_referer_path
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
       state: params.require(:state),
     ).to_h
 
-    @cookie_consent =
+    cookie_consent =
       case params[:cookie_consent]
       when "accept"
         update_saved_cookie_consent = !callback["cookie_consent"]
@@ -32,15 +33,19 @@ class SessionsController < ApplicationController
       end
 
     if callback.key?("cookie_consent") && callback.key?("feedback_consent") && (callback["cookie_consent"].nil? || callback["feedback_consent"].nil?)
-      use_account_layout
+      # slightly hacky way of passing the session header along in the
+      # redirect without properly logging the user in: prefix it with
+      # "!" so the header isn't actually correct
+      set_account_session_header "!#{callback['govuk_account_session']}"
 
-      @redirect_path = callback["redirect_path"]
-      @govuk_account_session = callback["govuk_account_session"]
-      render :first_time
+      redirect_to GovukPersonalisation::Redirect.build_url(
+        new_govuk_session_first_time_path,
+        { _ga: params[:_ga], cookie_consent: cookie_consent, redirect_path: params[:redirect_path] }.compact,
+      )
     else
       do_login(
         redirect_path: callback["redirect_path"],
-        cookie_consent: @cookie_consent,
+        cookie_consent: cookie_consent,
         update_saved_cookie_consent: update_saved_cookie_consent,
         govuk_account_session: callback["govuk_account_session"],
       )
@@ -49,14 +54,9 @@ class SessionsController < ApplicationController
     head :bad_request
   end
 
-  def first_time
-    use_account_layout
+  def first_time; end
 
-    redirect_path = params[:redirect_path]
-    head :bad_request unless is_valid_redirect_path? redirect_path
-
-    govuk_account_session = params.fetch(:govuk_account_session)
-
+  def first_time_post
     @error_items = []
     unless %w[yes no].include? params[:cookie_consent]
       @cookie_consent_decision_error = I18n.t("sessions.first_time.cookie_consent.field.invalid")
@@ -76,15 +76,17 @@ class SessionsController < ApplicationController
           cookie_consent: cookie_consent_decision,
           feedback_consent: feedback_consent_decision,
         },
-        govuk_account_session: govuk_account_session,
+        govuk_account_session: @unprefixed_govuk_account_session,
       )
 
       do_login(
-        redirect_path: redirect_path,
+        redirect_path: params[:redirect_path],
         cookie_consent: cookie_consent_decision ? "accept" : "reject",
         update_saved_cookie_consent: false,
         govuk_account_session: response["govuk_account_session"],
       )
+    else
+      render :first_time
     end
   rescue GdsApi::HTTPUnauthorized
     head :bad_request
@@ -136,7 +138,13 @@ protected
     )
   end
 
-  def use_account_layout
+  def set_up_first_time
+    head :bad_request and return unless is_valid_redirect_path? params[:redirect_path]
+    head :bad_request and return unless account_session_header&.start_with? "!"
+
+    @prefixed_govuk_account_session = account_session_header
+    @unprefixed_govuk_account_session = @prefixed_govuk_account_session.delete_prefix("!")
+
     set_slimmer_headers(template: "gem_layout_account_manager_no_nav", remove_search: true, show_accounts: "signed-in")
   end
 end

test/functional/session_controller_test.rb
@@ -122,16 +122,17 @@ class SessionsControllerTest < ActionController::TestCase
           stub_account_api
         end
 
-        should "not sign the user in" do
+        should "not sign the user in properly" do
           get :callback, params: { code: "code123", state: "state123" }
 
-          assert_nil @response.headers["GOVUK-Account-Session"]
+          assert_equal @response.headers["GOVUK-Account-Session"], "!#{@govuk_account_session}"
         end
 
-        should "render the consent form" do
+        should "redirect to the consent form" do
           get :callback, params: { code: "code123", state: "state123" }
 
-          assert_includes @response.body, I18n.t("sessions.first_time.title")
+          assert_response :redirect
+          assert_includes @response.redirect_url, "/sign-in/first-time"
         end
       end
 
@@ -141,57 +142,67 @@ class SessionsControllerTest < ActionController::TestCase
           stub_account_api
         end
 
-        should "not sign the user in" do
+        should "not sign the user in properly" do
           get :callback, params: { code: "code123", state: "state123" }
 
-          assert_nil @response.headers["GOVUK-Account-Session"]
+          assert_equal @response.headers["GOVUK-Account-Session"], "!#{@govuk_account_session}"
         end
 
         should "render the consent form" do
           get :callback, params: { code: "code123", state: "state123" }
 
-          assert_includes @response.body, I18n.t("sessions.first_time.title")
+          assert_response :redirect
+          assert_includes @response.redirect_url, "/sign-in/first-time"
         end
       end
     end
   end
 
   context "POST /sign-in/first-time" do
-    should "save the consents, sign the user in, and send them to the redirect path" do
-      stub_account_api_set_attributes(
-        attributes: {
-          cookie_consent: true,
-          feedback_consent: true,
-        },
-        govuk_account_session: "foo",
-        new_govuk_account_session: "bar",
-      )
-
-      post :first_time, params: { govuk_account_session: "foo", redirect_path: "/account/home", cookie_consent: "yes", feedback_consent: "yes" }
-      assert_response :redirect
-      assert_includes @response.redirect_url, "/account/home?cookie_consent=accept"
-      assert_equal @response.headers["GOVUK-Account-Session"], "bar"
-    end
-
-    should "return a 400 error if the :redirect_path is invalid" do
-      post :first_time, params: { govuk_account_session: "foo", redirect_path: "https://www.example.com" }
+    should "return a 400 error if the :govuk_account_session is missing" do
+      post :first_time_post, params: { redirect_path: "https://www.example.com" }
       assert_response :bad_request
     end
 
-    should "raise an error if the :govuk_account_session is missing" do
-      assert_raises ActionController::ParameterMissing do
-        post :first_time, params: { redirect_path: "/account/home" }
+    should "return a 400 error if the :govuk_account_session does not start with a '!'" do
+      mock_logged_in_session "foo"
+      post :first_time_post, params: { redirect_path: "https://www.example.com" }
+      assert_response :bad_request
+    end
+
+    context "there is a valid :govuk_account_session" do
+      setup { mock_logged_in_session "!foo" }
+
+      should "save the consents, sign the user in, and send them to the redirect path" do
+        stub_account_api_set_attributes(
+          attributes: {
+            cookie_consent: true,
+            feedback_consent: true,
+          },
+          govuk_account_session: "foo",
+          new_govuk_account_session: "bar",
+        )
+
+        post :first_time_post, params: { redirect_path: "/account/home", cookie_consent: "yes", feedback_consent: "yes" }
+        assert_response :redirect
+        assert_includes @response.redirect_url, "/account/home?cookie_consent=accept"
+        assert_equal @response.headers["GOVUK-Account-Session"], "bar"
       end
-    end
 
-    should "display an error message if the cookie consent is invalid" do
-      post :first_time, params: { govuk_account_session: "foo", redirect_path: "/account/home", cookie_consent: "nonsense", feedback_consent: "yes" }
-      assert_includes @response.body, I18n.t("sessions.first_time.cookie_consent.field.invalid")
-    end
+      should "return a 400 error if the :redirect_path is invalid" do
+        post :first_time_post, params: { redirect_path: "https://www.example.com" }
+        assert_response :bad_request
+      end
 
-    should "display an error message if the feedback consent is invalid" do
-      post :first_time, params: { govuk_account_session: "foo", redirect_path: "/account/home", cookie_consent: "yes", feedback_consent: "nonsense" }
-      assert_includes @response.body, I18n.t("sessions.first_time.feedback_consent.field.invalid")
+      should "display an error message if the cookie consent is invalid" do
+        post :first_time_post, params: { redirect_path: "/account/home", cookie_consent: "nonsense" }
+        assert_includes @response.body, I18n.t("sessions.first_time.cookie_consent.field.invalid")
+      end
+
+      should "display an error message if the feedback consent is invalid" do
+        post :first_time_post, params: { redirect_path: "/account/home", cookie_consent: "yes", feedback_consent: "nonsense" }
+        assert_includes @response.body, I18n.t("sessions.first_time.feedback_consent.field.invalid")
+      end
     end
   end
 

app/views/sessions/first_time.html.erb
@@ -27,8 +27,8 @@ content_for :title, title
 
   <%= form_with(url: new_govuk_session_first_time_path, method: :post) do %>
     <%= hidden_field_tag :_ga, params[:_ga] %>
-    <%= hidden_field_tag :redirect_path, (@redirect_path || params[:redirect_path]) %>
-    <%= hidden_field_tag :govuk_account_session, (@govuk_account_session || params[:govuk_account_session]) %>
+    <%= hidden_field_tag :redirect_path, params[:redirect_path] %>
+    <%= hidden_field_tag :govuk_account_session, @prefixed_govuk_account_session %>
 
     <%= render "govuk_publishing_components/components/heading", {
       text: t("sessions.first_time.cookie_consent.heading"),
@@ -51,12 +51,12 @@ content_for :title, title
         {
           value: "yes",
           text: t("yes"),
-          checked: (params[:cookie_consent] == "yes") || (@cookie_consent == "accept"),
+          checked: params[:cookie_consent] == "yes",
         },
         {
           value: "no",
           text: t("no"),
-          checked: (params[:cookie_consent] == "no") || (@cookie_consent == "reject"),
+          checked: params[:cookie_consent] == "no",
         }
       ]
     } %>

config/routes.rb
@@ -27,7 +27,8 @@ Rails.application.routes.draw do
   get "/sign-in", to: "help#sign_in"
   get "/sign-in/redirect", to: "sessions#create", as: :new_govuk_session
   get "/sign-in/callback", to: "sessions#callback", as: :new_govuk_session_callback
-  post "/sign-in/first-time", to: "sessions#first_time", as: :new_govuk_session_first_time
+  get "/sign-in/first-time", to: "sessions#first_time", as: :new_govuk_session_first_time
+  post "/sign-in/first-time", to: "sessions#first_time_post"
   get "/sign-out", to: "sessions#delete", as: :end_govuk_session
 
   scope "/account" do

commit c7e30b59ccd321ddde5df2b25a885833309fafe6

Fix not passing redirect path to first_time form
This bug was introduced in 59f09bf.  If you look in that commit, you
can see that it was `callback["redirect_path"]` previously.  I've
added a test to check this case.

app/controllers/sessions_controller.rb
@@ -40,7 +40,7 @@ class SessionsController < ApplicationController
 
       redirect_to GovukPersonalisation::Redirect.build_url(
         new_govuk_session_first_time_path,
-        { _ga: params[:_ga], cookie_consent: cookie_consent, redirect_path: params[:redirect_path] }.compact,
+        { _ga: params[:_ga], cookie_consent: cookie_consent, redirect_path: callback["redirect_path"] }.compact,
       )
     else
       do_login(

test/functional/session_controller_test.rb
@@ -119,6 +119,7 @@ class SessionsControllerTest < ActionController::TestCase
       context "account-api returns a nil :cookie_consent" do
         setup do
           @cookie_consent = nil
+          @redirect_path = "/bank-holiday"
           stub_account_api
         end
 
@@ -128,17 +129,19 @@ class SessionsControllerTest < ActionController::TestCase
           assert_equal @response.headers["GOVUK-Account-Session"], "!#{@govuk_account_session}"
         end
 
-        should "redirect to the consent form" do
+        should "redirect to the consent form, passing along the original redirect_path" do
           get :callback, params: { code: "code123", state: "state123" }
 
           assert_response :redirect
           assert_includes @response.redirect_url, "/sign-in/first-time"
+          assert_includes @response.redirect_url, "redirect_path=#{@redirect_path}"
         end
       end
 
       context "account-api returns a nil :feedback_consent" do
         setup do
           @feedback_consent = nil
+          @redirect_path = "/bank-holiday"
           stub_account_api
         end
 
@@ -148,11 +151,12 @@ class SessionsControllerTest < ActionController::TestCase
           assert_equal @response.headers["GOVUK-Account-Session"], "!#{@govuk_account_session}"
         end
 
-        should "render the consent form" do
+        should "render the consent form, passing along the original redirect_path" do
           get :callback, params: { code: "code123", state: "state123" }
 
           assert_response :redirect
           assert_includes @response.redirect_url, "/sign-in/first-time"
+          assert_includes @response.redirect_url, "redirect_path=#{@redirect_path}"
         end
       end
     end
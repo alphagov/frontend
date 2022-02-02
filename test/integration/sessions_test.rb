f6c5acbb1eb4b5a058ce237e86da6a983e10121b
Michael Walker <mike@barrucadu.co.uk>
2022-02-02 14:21:29 +0000

    Move SessionsController HTTP referer tests to test/functional
    test/functional is for controller unit testing using `get` and
    test/integration is for capybara integration testing using `visit`.
    Since these tests are really unit tests, and we're not simulating a
    web browser, we should move them here.

--- test/integration/sessions_test.rb
+++ test/integration/sessions_test.rb
@@ -5,39 +5,6 @@ class SessionsTest < ActionDispatch::IntegrationTest
   include GdsApi::TestHelpers::AccountApi
   include GovukPersonalisation::TestHelpers::Features
 
-  context "HTTP Referer" do
-    should "add a redirect_path param to /account from the HTTP Referer header" do
-      stub = stub_account_api_get_sign_in_url(redirect_path: "/transition-check/results?c[]=import-wombats&c[]=practice-wizardry")
-
-      get "/account", headers: { "Referer" => "#{Plek.new.website_root}/transition-check/results?c[]=import-wombats&c[]=practice-wizardry" }
-
-      assert_response :redirect
-      assert_requested stub
-    end
-
-    should "not allow protocol-relative redirects" do
-      stub = stub_account_api_get_sign_in_url
-      stub_evil = stub_account_api_get_sign_in_url(redirect_path: "//evil")
-
-      get "/account", headers: { "Referer" => "//evil" }
-
-      assert_response :redirect
-      assert_requested stub
-      assert_not_requested stub_evil
-    end
-
-    should "not allow absolute redirects" do
-      stub = stub_account_api_get_sign_in_url
-      stub_evil = stub_account_api_get_sign_in_url(redirect_path: "http://www.evil.com")
-
-      get "/account", headers: { "Referer" => "http://www.evil.com" }
-
-      assert_response :redirect
-      assert_requested stub
-      assert_not_requested stub_evil
-    end
-  end
-
   context "First-time consent form" do
     setup do
       @govuk_account_session = "new-session-id"

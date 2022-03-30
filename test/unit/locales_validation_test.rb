37f0cdd4fdd06dd36d44aa4234381b5462378792
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-03-30 14:42:53 +0100

    Remove 'homepage' override in 'LocaleChecker'
    Previously, we ignored all locale properties beginning with the
    'homepage' prefix, for several reasons:
    
    1) The locale files lost the traditional key/value structure in
       51a5b885a21fa1e6da9c7d8092da9a814227058e, meaning Rails
       Translation Manager is unable to statically analyse which
       properties are used, and it complained of certain keys being
       unused.
    2) Said keys have now been removed.
    3) The English and Welsh keys weren't in sync, so the checker was
       complaining about that. That has now been fixed in the previous
       commits.

--- test/unit/locales_validation_test.rb
+++ test/unit/locales_validation_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class LocalesValidationTest < ActiveSupport::TestCase
   test "should validate all locale files" do
-    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*.yml", ["homepage", "formats.transaction"])
+    checker = RailsTranslationManager::LocaleChecker.new("config/locales/*.yml", ["formats.transaction"])
     assert checker.validate_locales
   end
 end

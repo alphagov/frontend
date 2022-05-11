43b70609c3456805911b185c5c46d00501c85873
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-05-11 10:41:29 +0100

    Remove first time sign in page
    When we first built the account home area, we were expecting it to be
    part of `www.gov.uk` with only the authentication pages on
    `account.gov.uk`. Since then, the team building the account area has
    moved to the Digital Identity directorate and we've decided that all of
    the central account pages should be on `account.gov.uk` and `www.gov.uk`
    should be treated as any other service that's using GOV.UK Sign In as
    an OIDC provider.
    
    The first time sign in page in this app currently has part of the
    account privacy policy and asks users for their consent to use cookies
    and send them emails to ask for feedback. These questions need to be
    moved along with the rest of the account home area to a new central
    app on `account.gov.uk` and so will not be stored in GOV.UK's account
    API.
    
    This app uses the presence of answers to these questions to decide if a
    user is signing in for the first time or not. If the questions are no
    longer present, then this will no longer be possible. The only other
    content on this page is privacy information which we are going to move
    to email alert frontend.
    
    Therefore we can remove the first time sign in page and logic in the
    callback endpoint to detect if a user is signing in for the first time.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   include GovukPersonalisation::ControllerConcern
 
   before_action :set_no_cache_headers
-  before_action :set_up_first_time, only: %i[first_time first_time_post]
 
   def create
     redirect_path = http_referer_path
@@ -19,75 +18,11 @@ class SessionsController < ApplicationController
       state: params.require(:state),
     ).to_h
 
-    cookie_consent =
-      case params[:cookie_consent]
-      when "accept"
-        update_saved_cookie_consent = !callback["cookie_consent"]
-        "accept"
-      when "reject"
-        update_saved_cookie_consent = callback["cookie_consent"]
-        "reject"
-      else
-        update_saved_cookie_consent = false
-        callback["cookie_consent"] ? "accept" : "reject"
-      end
-
-    if callback.key?("cookie_consent") && callback.key?("feedback_consent") && (callback["cookie_consent"].nil? || callback["feedback_consent"].nil?)
-      # slightly hacky way of passing the session header along in the
-      # redirect without properly logging the user in: prefix it with
-      # "!" so the header isn't actually correct
-      set_account_session_header "!#{callback['govuk_account_session']}"
-
-      redirect_to GovukPersonalisation::Redirect.build_url(
-        new_govuk_session_first_time_path,
-        { _ga: params[:_ga], cookie_consent: cookie_consent, redirect_path: callback["redirect_path"] ? CGI.escape(callback["redirect_path"]) : nil }.compact,
-      )
-    else
-      do_login(
-        redirect_path: callback["redirect_path"],
-        cookie_consent: cookie_consent,
-        update_saved_cookie_consent: update_saved_cookie_consent,
-        govuk_account_session: callback["govuk_account_session"],
-      )
-    end
-  rescue GdsApi::HTTPUnauthorized
-    head :bad_request
-  end
-
-  def first_time; end
-
-  def first_time_post
-    @error_items = []
-    unless %w[yes no].include? params[:cookie_consent]
-      @cookie_consent_decision_error = I18n.t("sessions.first_time.cookie_consent.field.invalid")
-      @error_items << { field: "cookie_consent", href: "#cookie_consent", text: @cookie_consent_decision_error }
-    end
-    unless %w[yes no].include? params[:feedback_consent]
-      @feedback_consent_decision_error = I18n.t("sessions.first_time.feedback_consent.field.invalid")
-      @error_items << { field: "feedback_consent", href: "#feedback_consent", text: @feedback_consent_decision_error }
-    end
-
-    if @error_items.empty?
-      cookie_consent_decision = params[:cookie_consent] == "yes"
-      feedback_consent_decision = params[:feedback_consent] == "yes"
-
-      response = GdsApi.account_api.set_attributes(
-        attributes: {
-          cookie_consent: cookie_consent_decision,
-          feedback_consent: feedback_consent_decision,
-        },
-        govuk_account_session: @unprefixed_govuk_account_session,
-      )
-
-      do_login(
-        redirect_path: params[:redirect_path],
-        cookie_consent: cookie_consent_decision ? "accept" : "reject",
-        update_saved_cookie_consent: false,
-        govuk_account_session: response["govuk_account_session"],
-      )
-    else
-      render :first_time
-    end
+    do_login(
+      redirect_path: callback["redirect_path"],
+      cookie_consent: params[:cookie_consent],
+      govuk_account_session: callback["govuk_account_session"],
+    )
   rescue GdsApi::HTTPUnauthorized
     head :bad_request
   end
@@ -119,16 +54,8 @@ protected
     false
   end
 
-  def do_login(redirect_path:, cookie_consent:, update_saved_cookie_consent:, govuk_account_session:)
+  def do_login(redirect_path:, cookie_consent:, govuk_account_session:)
     set_account_session_header(govuk_account_session)
-
-    if update_saved_cookie_consent
-      GdsApi.account_api.set_attributes(
-        attributes: { cookie_consent: cookie_consent == "accept" },
-        govuk_account_session: account_session_header,
-      )
-    end
-
     redirect_to GovukPersonalisation::Redirect.build_url(
       redirect_path.presence || account_home_path,
       {
@@ -136,15 +63,5 @@ protected
         cookie_consent: cookie_consent,
       }.compact,
     )
-  end
-
-  def set_up_first_time
-    head :bad_request and return unless is_valid_redirect_path? params[:redirect_path]
-    head :bad_request and return unless account_session_header&.start_with? "!"
-
-    @prefixed_govuk_account_session = account_session_header
-    @unprefixed_govuk_account_session = @prefixed_govuk_account_session.delete_prefix("!")
-
-    set_slimmer_headers(template: "gem_layout_account_manager_no_nav", remove_search: true, show_accounts: "signed-in")
   end
 end
--- test/integration/sessions_test.rb
+++ test/integration/sessions_test.rb
@@ -8,16 +8,9 @@ class SessionsTest < ActionDispatch::IntegrationTest
   include GovukPersonalisation::TestHelpers::Features
 
   context "Given a new user" do
-    should "Prompt for cookie and feedback consent, and then send them to the account dashboard" do
+    should "Log the user in and send them to the account dashboard" do
       given_a_successful_login_attempt(cookie_consent: nil, feedback_consent: nil)
-
       when_i_return_from_digital_identity
-      then_i_see_the_first_time_form
-
-      when_i_consent_to_cookies
-      when_i_consent_to_feedback
-      assert_this_redirects_me { and_i_click_continue }
-      and_my_consents_have_been_saved
       and_i_see_the_dashboard
     end
 
@@ -27,42 +20,8 @@ class SessionsTest < ActionDispatch::IntegrationTest
         given_a_successful_login_attempt(cookie_consent: nil, feedback_consent: nil)
       end
 
-      should "Prompt for cookie and feedback consent, and then send them to the redirect path" do
-        when_i_return_from_digital_identity
-        then_i_see_the_first_time_form
-
-        when_i_consent_to_cookies
-        when_i_consent_to_feedback
-        assert_this_redirects_me { and_i_click_continue }
-        and_my_consents_have_been_saved
-      end
-
-      should "Enforce that cookie consent is explicitly given or rejected" do
-        when_i_return_from_digital_identity
-        then_i_see_the_first_time_form
-
-        when_i_consent_to_feedback
-        and_i_click_continue
-        then_i_see_the_first_time_form
-        and_i_see_an_error_about_invalid_cookie_consent
-
-        when_i_consent_to_cookies
-        assert_this_redirects_me { and_i_click_continue }
-        and_my_consents_have_been_saved
-      end
-
-      should "Enforce that feedback consent is explicitly given or rejected" do
-        when_i_return_from_digital_identity
-        then_i_see_the_first_time_form
-
-        when_i_consent_to_cookies
-        and_i_click_continue
-        then_i_see_the_first_time_form
-        and_i_see_an_error_about_invalid_feedback_consent
-
-        when_i_consent_to_feedback
-        assert_this_redirects_me { and_i_click_continue }
-        and_my_consents_have_been_saved
+      should "Log the user in and send them to the redirect path" do
+        assert_this_redirects_me { when_i_return_from_digital_identity }
       end
     end
   end
@@ -87,73 +46,22 @@ class SessionsTest < ActionDispatch::IntegrationTest
   end
 
   def given_a_successful_login_attempt(cookie_consent:, feedback_consent:)
-    @govuk_account_session = "new-session-id"
+    govuk_account_session = "session-id"
 
     stub_account_api_validates_auth_response(
-      govuk_account_session: @govuk_account_session,
+      govuk_account_session: govuk_account_session,
       redirect_path: @redirect_path,
       cookie_consent: cookie_consent,
       feedback_consent: feedback_consent,
     )
 
-    if cookie_consent.nil? || feedback_consent.nil?
-      @stub_set_consents = stub_account_api_set_attributes(
-        attributes: {
-          cookie_consent: true,
-          feedback_consent: true,
-        },
-        govuk_account_session: @govuk_account_session,
-        new_govuk_account_session: @govuk_account_session,
-      )
-
-      # needed because we can't make capybara handle our special
-      # response header (ideally we'd have some code run between
-      # visiting the callback endpoint and making the request to the
-      # form, to pluck out the response header and fill in the request
-      # header)
-      mock_logged_in_session "!#{@govuk_account_session}"
-    end
-
-    @stub_user_info = stub_account_api_user_info
-    stub_email_alert_api_authenticate_subscriber_by_govuk_account("!#{@govuk_account_session}", "subscriber-id", "email@example.com")
-    stub_email_alert_api_authenticate_subscriber_by_govuk_account(@govuk_account_session, "subscriber-id", "email@example.com")
+    @stub_user_info = stub_account_api_user_info(new_govuk_account_session: govuk_account_session)
+    stub_email_alert_api_authenticate_subscriber_by_govuk_account(govuk_account_session, "subscriber-id", "email@example.com")
     stub_email_alert_api_has_subscriber_subscriptions("subscriber-id", "example@example.com")
   end
 
   def when_i_return_from_digital_identity
     visit new_govuk_session_callback_path(code: "code", state: "state")
-  end
-
-  def when_i_consent_to_cookies
-    within "#cookie_consent" do
-      choose "Yes"
-    end
-  end
-
-  def when_i_consent_to_feedback
-    within "#feedback_consent" do
-      choose "Yes"
-    end
-  end
-
-  def and_i_click_continue
-    click_on "Continue"
-  end
-
-  def then_i_see_the_first_time_form
-    assert page.has_content? "use cookies to collect anonymised analytics"
-  end
-
-  def and_i_see_an_error_about_invalid_cookie_consent
-    assert page.has_content? I18n.t("sessions.first_time.cookie_consent.field.invalid")
-  end
-
-  def and_i_see_an_error_about_invalid_feedback_consent
-    assert page.has_content? I18n.t("sessions.first_time.feedback_consent.field.invalid")
-  end
-
-  def and_my_consents_have_been_saved
-    assert_requested @stub_set_consents
   end
 
   def and_i_see_the_dashboard
@@ -164,8 +72,8 @@ class SessionsTest < ActionDispatch::IntegrationTest
   def assert_this_redirects_me
     # the /email/... redirect path isn't in this app so ignore a 404
     yield
-    assert_current_url "#{account_home_path}?cookie_consent=accept"
+    assert_current_url account_home_path
   rescue ActionController::RoutingError
-    assert_current_url "#{@redirect_path}&cookie_consent=accept"
+    assert_current_url @redirect_path
   end
 end

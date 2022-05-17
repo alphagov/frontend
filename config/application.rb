3b3c7992bcdc287190d51cab15796818dfabf2b0
Edward Kerry <edwardkerry@gmail.com>
2022-05-17 14:02:57 +0100

    Revert "Revert "Update Rails to 7.0.3""

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require "active_support/time"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "rails/test_unit/railtie"
-require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,7 +23,7 @@ module Frontend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     require "frontend"
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -89,5 +88,22 @@ module Frontend
     # to use CSS that has same function names as SCSS such as max.
     # https://github.com/alphagov/govuk-frontend/issues/1350
     config.assets.css_compressor = nil
+  end
+
+  # Rotate SHA1 cookies to SHA256 (the new Rails 7 default)
+  # TODO: Remove this after existing user sessions have been rotated
+  # https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256
+  Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
+    salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
+    secret_key_base = Rails.application.secrets.secret_key_base
+    next if secret_key_base.blank?
+
+    key_generator = ActiveSupport::KeyGenerator.new(
+      secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+    )
+    key_len = ActiveSupport::MessageEncryptor.key_len
+    secret = key_generator.generate_key(salt, key_len)
+
+    cookies.rotate :encrypted, secret
   end
 end

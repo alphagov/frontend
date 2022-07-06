8dbe2e4de266be77da511156f476fb52b2b09166
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-07-06 10:38:17 +0100

    Remove unused styles and JavaScript
    These stylesheets and JavaScript files were for components that aren't being
    used in frontend at the moment - so can be safely removed with no ill effect.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/character-count
 //= require govuk_publishing_components/components/details
-//= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/step-by-step-nav

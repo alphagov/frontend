9ed3653fd140d9efa8e994fe92281359aac848ce
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2022-08-04 16:36:25 +0100

    Make locations_api available to Frontend

--- lib/frontend.rb
+++ lib/frontend.rb
@@ -2,6 +2,7 @@ module Frontend
   mattr_accessor :organisations_search_client
   mattr_accessor :search_client
   mattr_accessor :mapit_api
+  mattr_accessor :locations_api
   mattr_accessor :imminence_api
   mattr_accessor :local_links_manager_api
   mattr_accessor :govuk_website_root

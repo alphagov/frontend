9ddea371c94f5e75a957376cf4648a110f40895d
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2022-09-06 14:11:33 +0100

    Fix Plek URL for Static in development
    This updates the URLs to use the asset domain and use HTTPS. This
    is required for the Static proxy to correctly forward request.

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ if [[ $1 == "--live" ]] ; then
   GOVUK_PROXY_STATIC_ENABLED=true \
   PLEK_SERVICE_LICENSIFY_URI=${PLEK_SERVICE_LICENSIFY_URI-https://licensify.publishing.service.gov.uk} \
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
-  PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-assets.publishing.service.gov.uk} \
+  PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-https://assets.publishing.service.gov.uk} \
   PLEK_SERVICE_IMMINENCE_URI=${PLEK_SERVICE_IMMINENCE_URI-https://imminence.publishing.service.gov.uk} \
   bundle exec rails s -p 3005
 else

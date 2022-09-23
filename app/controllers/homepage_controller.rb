76c308509f576481ddae4c1421582510f2f45440
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-09-23 18:35:09 +0100

    Spike sending header that decides whether or not to show mirror banner
    See https://github.com/alphagov/static/pull/2889 for details.
    
    Trello: https://trello.com/c/OuKvzs4L/2741-write-rfc-to-be-transparent-about-mirrored-content

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,6 +5,7 @@ class HomepageController < ContentItemsController
   def index
     set_slimmer_headers(
       template: "gem_layout_full_width",
+      show_mirror_banner: request.headers["X-Slimmer-Show-Mirror-Banner"],
       remove_search: true,
     )
   end

64afd61f4bd16b802fc9ff7d8907dbab6d0c79e9
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-11-24 17:43:44 +0000

    Creates new licence transaction endpoint
    This commit does the following:
    
    * adds routes for the new licence base_path (/find-licences/licence')
    
    * creates new controller and views for Specialist Publisher licence
      (Licence Transaction). This is broadly the same as the existing
      Licence controller / views but updated to respect the new `base_path`
      and data structure.
    
    * Adds controller test
    
    Common classes that don't need modifying are now shared between Licence
    and LicenceTransaction, e.g `LicenceDetailsPresenter`.
    
    We need the `content_item_slug` method in `LicenceTransactionController`
    to ensure the correct content item is returned due to [1], as
    `params[:slug]` was the content item base_path in old licence world but
    now it is simply the slug, e.g “tv-licence”, whereas the content item is
    located at `/find-licences/tv-licence`.  `content_item_slug` is
    customisable [2] and is by a few controllers.
    
    As a result of the above, we no longer use the presented slug, rather
    the presented base_path (for start view) or the licence slug from the
    request path which needs to be passed into the authority template to be
    used in the route helpers.
    
    [1]: https://github.com/alphagov/frontend/blob/be6d2fc2ee4d3def35e460397599d135c18fc517/app/controllers/content_items_controller.rb#L25
    [2]: https://github.com/alphagov/frontend/blob/main/app/controllers/content_items_controller.rb#L28-L30

---
+++ test/functional/licence_transaction_controller_test.rb
@@ -0,0 +1,100 @@
+require "test_helper"
+require "gds_api/test_helpers/locations_api"
+require "gds_api/test_helpers/local_links_manager"
+require "gds_api/test_helpers/licence_application"
+
+class LicenceTransactionControllerTest < ActionController::TestCase
+  include GdsApi::TestHelpers::LocationsApi
+  include GdsApi::TestHelpers::LocalLinksManager
+  include GdsApi::TestHelpers::LicenceApplication
+  include LocationHelpers
+
+  context "GET start" do
+    context "for live content" do
+      setup do
+        content_store_has_example_item(
+          "/find-licences/new-licence", schema: "specialist_document", example: "licence-transaction"
+        )
+      end
+
+      should "set the cache expiry headers" do
+        get :start, params: { slug: "new-licence" }
+
+        honours_content_store_ttl
+      end
+    end
+  end
+
+  context "POST to start" do
+    setup do
+      @payload = {
+        base_path: "/find-licences/new-licence",
+        document_type: "licence_transaction",
+        schema_name: "specialist_document",
+        title: "Licence to kill",
+        public_updated_at: "2012-10-02T12:30:33.483Z",
+        description: "Descriptive licence text.",
+        details: {
+          body: "You only live twice, Mr Bond.\n",
+          metadata: {
+            licence_transaction_licence_identifier: "1071-5-1",
+          },
+        },
+      }
+
+      stub_content_store_has_item("/find-licences/new-licence", @payload)
+    end
+
+    context "loading the licence edition when posting a location" do
+      setup do
+        stub_licence_exists(
+          "1071-5-1",
+          "isLocationSpecific" => true,
+          "isOfferedByCounty" => false,
+          "geographicalAvailability" => %w[England Wales],
+          "issuingAuthorities" => [],
+        )
+      end
+
+      context "for an English local authority" do
+        setup do
+          configure_locations_api_and_local_authority("ST10 4DB", %w[staffordshire staffordshire-moorlands], 3435)
+
+          post :start, params: { slug: "new-licence", postcode: "ST10 4DB" }
+        end
+
+        should "redirect to the slug for the lowest level authority" do
+          assert_redirected_to "/find-licences/new-licence/staffordshire-moorlands"
+        end
+      end
+
+      context "for a Northern Irish local authority" do
+        setup do
+          configure_locations_api_and_local_authority("BT1 5GS", %w[belfast], 8132)
+
+          post :start, params: { slug: "new-licence", postcode: "BT1 5GS" }
+        end
+
+        should "redirect to the slug for the lowest level authority" do
+          assert_redirected_to "/find-licences/new-licence/belfast"
+        end
+      end
+    end
+  end
+
+  context "GET authority" do
+    context "for live content" do
+      setup do
+        content_store_has_example_item(
+          "/find-licences/new-licence", schema: "specialist_document", example: "licence-transaction"
+        )
+      end
+
+      should "set the cache expiry headers" do
+        get :authority, params: { slug: "new-licence", authority_slug: "secret-service" }
+
+        honours_content_store_ttl
+      end
+    end
+  end
+end

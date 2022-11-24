854cdacd1a108319c39a5a595bad81a8929cf9c0
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-11-24 17:40:59 +0000

    Add LicenceTransactionPresenter
    New licence presenter which represents the specialist document data
    structure.

---
+++ app/presenters/licence_transaction_presenter.rb
@@ -0,0 +1,17 @@
+class LicenceTransactionPresenter < ContentItemPresenter
+  PASS_THROUGH_DETAILS_KEYS = %w[
+    licence_transaction_continuation_link
+    licence_transaction_licence_identifier
+    licence_transaction_will_continue_on
+  ].freeze
+
+  PASS_THROUGH_DETAILS_KEYS.each do |key|
+    define_method key do
+      details["metadata"][key] if details["metadata"]
+    end
+  end
+
+  def body
+    details["body"]
+  end
+end

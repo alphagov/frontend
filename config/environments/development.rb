ba70d86983c4ab2578ca8ea9aaad80c3765a4ebb
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-12-05 15:29:51 +0000

    add gitpod support for GOVUK frontend app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.hosts << "frontend.dev.gov.uk"
+  config.hosts << /[a-z0-9-.]+\.gitpod\.io/
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true

d3a1476a733aa64f72040dc051cd37ec5c8ea331
Patrick Cartlidge <patrickcartlidge@gmail.com>
2022-12-06 14:05:40 +0000

    testing w/ fake

--- app/presenters/electoral_presenter.rb
+++ app/presenters/electoral_presenter.rb
@@ -31,7 +31,9 @@ class ElectoralPresenter
   end
 
   def upcoming_elections
-    dates.flat_map { |date| present_ballots(date) } if dates.present?
+
+    fake_response["dates"].flat_map { |date| present_ballots(date) }
+    # dates.flat_map { |date| present_ballots(date) } if dates.present?
   end
 
   def use_electoral_services_contact_details?
@@ -64,11 +66,17 @@ private
 
   def present_ballots(date)
     if date["ballots"].present?
-      date["ballots"].map { |b| "#{b['poll_open_date']} - #{b['ballot_title']}" }
+      date["ballots"].map { |b| "#{b['poll_open_date']} - #{b['ballot_title']}" } +
+      date["ballots"].map { |b| "#{b['poll_open_date']} - #{b['ballot_title']}" } +
+      date["ballots"].map { |b| "#{b['poll_open_date']} - #{b['ballot_title']}" }      
     end
   end
 
   def no_contact_details?
     registration.nil? && electoral_services.nil?
+  end
+
+  def fake_response 
+    JSON.parse('{"dates":[{"date":"2017-05-04","polling_station":{"polling_station_known":true,"custom_finder":null,"report_problem_url":"http://wheredoivote.co.uk/report_problem/?source=testing&source_url=testing","station":{"id":"w06000015.QK","type":"Feature","geometry":{"type":"Point","coordinates":[-3.119229,51.510885]},"properties":{"postcode":"","address":"EarlswoodSocialClub,160-164GreenwayRoad,Rumney"}}},"advance_voting_station":null,"notifications":[{"title":"Someunexpectedeventishappening","type":"cancelled_election","detail":"Somemoredetails","url":"https://foo.bar/baz"}],"ballots":[{"ballot_paper_id":"local.cardiff.pontprennauold-st-mellons.2017-05-04","ballot_title":"CardifflocalelectionPontprennau/OldSt.Mellons","ballot_url":"https://developers.democracyclub.org.uk/api/v1/local.cardiff.pontprennauold-st-mellons.2017-05-04/","poll_open_date":"2017-05-04","elected_role":"LocalCouncillor","metadata":null,"cancelled":false,"replaced_by":null,"replaces":null,"election_id":"local.cardiff.2017-05-04","election_name":"Cardifflocalelection","post_name":"Pontprennau/OldSt.Mellons","candidates_verified":false,"voting_system":{"slug":"FPTP","name":"First-past-the-post","uses_party_lists":false},"seats_contested":1,"candidates":[{"list_position":null,"party":{"party_id":"party:90","party_name":"LiberalDemocrats"},"person":{"ynr_id":23417,"name":"DavidKeigwin","absolute_url":"https://whocanivotefor.co.uk/person/23417/david-keigwin","email":"dave@example.com","photo_url":null}},{"list_position":null,"party":{"party_id":"party:52","party_name":"ConservativeandUnionistParty"},"person":{"ynr_id":8071,"name":"JoelWilliams","absolute_url":"https://whocanivotefor.co.uk/person/8071/joel-williams","email":null,"photo_url":"https://static-candidates.democracyclub.org.uk/media/images/images/8071.png"}}],"wcivf_url":"https://whocanivotefor.co.uk/elections/local.cardiff.2017-05-04/post-UTE:W05000900/pontprennauold-st-mellons"}]}]}')
   end
 end

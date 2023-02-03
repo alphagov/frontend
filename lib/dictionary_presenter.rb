a8b65a843b32b05098c40709dfd04750dee28aca
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2023-02-03 09:28:25 +0000

    Autocorrect rubocop offences

--- lib/dictionary_presenter.rb
+++ lib/dictionary_presenter.rb
@@ -4,7 +4,7 @@ class DictionaryPresenter
   end
 
   def as_dictionary
-    dictionary = ("A".."Z").index_with { |_l| []; }
+    dictionary = ("A".."Z").index_with { |_l| [] }
 
     @collection.each do |p|
       dictionary[p.title[0]] << p

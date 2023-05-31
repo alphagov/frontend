cba14090ec6e95587009ba7e1befc6e252a03fc2
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-05-31 11:12:21 +0100

    Include attachment title on CSV preview
    This is retrieved from the content item for the parent page, as Asset
    Manager does not contain this information.
    
    Asset Manager returns the URL prefixed with `https://www.gov.uk`
    regardless of the environment, so we need to remove that before making
    the request to content store.

--- test/integration/csv_preview_test.rb
+++ test/integration/csv_preview_test.rb
@@ -1,13 +1,16 @@
 require "integration_test_helper"
 require "gds_api/test_helpers/asset_manager"
+require "gds_api/test_helpers/content_store"
 
 class CsvPreviewTest < ActionDispatch::IntegrationTest
   include GdsApi::TestHelpers::AssetManager
+  include GdsApi::TestHelpers::ContentStore
 
   attachment_id = 1234
   filename = "filename"
   legacy_url_path = "government/uploads/system/uploads/attachment_data/file/#{attachment_id}/#{filename}.csv"
-  parent_document_url = "https://www.gov.uk/government/important-guidance"
+  parent_document_base_path = "/government/important-guidance"
+  parent_document_url = "https://www.gov.uk#{parent_document_base_path}"
 
   setup do
     asset_manager_response = {
@@ -15,6 +18,23 @@ class CsvPreviewTest < ActionDispatch::IntegrationTest
       parent_document_url:,
     }
     stub_asset_manager_has_a_whitehall_asset(legacy_url_path, asset_manager_response)
+
+    content_item = {
+      base_path: parent_document_base_path,
+      details: {
+        attachments: [
+          {
+            title: "Attachment 1",
+            url: "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/5678/filename.csv",
+          },
+          {
+            title: "Attachment 2",
+            url: "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/#{attachment_id}/#{filename}.csv",
+          },
+        ],
+      },
+    }
+    stub_content_store_has_item(parent_document_base_path, content_item)
   end
 
   context "when visiting the preview" do
@@ -28,6 +48,10 @@ class CsvPreviewTest < ActionDispatch::IntegrationTest
 
     should "include a link to the parent document" do
       assert page.has_link?("See more information about this guidance", href: parent_document_url)
+    end
+
+    should "include the correct asset title from the content item" do
+      assert page.has_text?("Attachment 2")
     end
   end
 

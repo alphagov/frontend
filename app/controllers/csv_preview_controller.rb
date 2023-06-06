02954da1ea59626b10f3513ab85078dd0ae8f1b0
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-06-06 10:25:18 +0100

    Fix issue with CSV preview content item retrieval
    We previously assumed all `parent_document_url` values from Asset
    Manager were prefixed `www.gov.uk` regardless of environment.
    
    However we have realised this is an artefact of the data sync.
    Documents edited in integration (or another non-production environment
    will have the correct domain).
    
    Therefore updating the controller to only keep the path, regardless of
    the domain.

--- app/controllers/csv_preview_controller.rb
+++ app/controllers/csv_preview_controller.rb
@@ -3,7 +3,8 @@ class CsvPreviewController < ApplicationController
     legacy_url_path = request.path.sub("/preview", "").sub(/^\//, "")
     @asset = GdsApi.asset_manager.whitehall_asset(legacy_url_path).to_hash
 
-    @content_item = GdsApi.content_store.content_item(@asset["parent_document_url"].sub("https://www.gov.uk", "")).to_hash
+    parent_document_path = URI(@asset["parent_document_url"]).request_uri
+    @content_item = GdsApi.content_store.content_item(parent_document_path).to_hash
 
     @attachment_metadata = @content_item.dig("details", "attachments").select do |attachment|
       attachment["url"] =~ /#{legacy_url_path}$/

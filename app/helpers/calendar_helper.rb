6d14ad72b45a6893f3c22dd236923f4428b92842
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2023-07-05 15:11:06 +0100

    Add new helper method `format_date`
    This helper method is used to localize the time format - https://edgeguides.rubyonrails.org/i18n.html#adding-date-time-formats

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -8,4 +8,8 @@ module CalendarHelper
   def time_tag_safe(date)
     tag.time(datetime: date) { l(date, format: "%e %B") }.html_safe
   end
+
+  def format_date(date)
+    l date, format: "%e %B %Y"
+  end
 end

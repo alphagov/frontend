6375c4079a91dc09c839362ec726ce84c27f2605
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2023-09-11 15:31:48 +0100

    Set new instance variable

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,7 @@ class HomepageController < ContentItemsController
   slimmer_template "gem_layout_homepage"
 
   def index
+    @new_design = params[:new_design]
     set_slimmer_headers(
       template: "gem_layout_homepage",
     )

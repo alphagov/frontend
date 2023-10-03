380b9454569df0d950e545933087513f0dd2c2b5
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2023-10-03 14:04:05 +0100

    Update new_design query string matching to be more specific

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,9 +3,14 @@ class HomepageController < ContentItemsController
   slimmer_template "gem_layout_homepage"
 
   def index
-    @new_design = params[:new_design]
     set_slimmer_headers(
       template: "gem_layout_homepage",
     )
+  end
+
+  helper_method :new_design?
+
+  def new_design?
+    params[:new_design] == "impact"
   end
 end

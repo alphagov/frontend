ac1c9a48470287d42064ded512c6f479a21e5a66
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2023-10-11 14:30:20 +0100

    Configure builds
    - Create `dartsass.builds` initializer and add all Sass files to be compiled. See https://github.com/rails/dartsass-rails#configuring-builds
    - Create .keep
    - Update manifest file to use /builds directory
    - Ignore /builds
    
    Co-Authored-By: Martin Jones <28779939+MartinJJones@users.noreply.github.com>

---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,22 @@
+APP_STYLESHEETS = {
+  "application.scss" => "application.css",
+  "components/_calendar.scss" => "components/_calendar.css",
+  "components/_metadata.scss" => "components/_metadata.css",
+  "components/_subscribe.scss" => "components/_subscribe.css",
+  "views/_calendars.scss" => "views/_calendars.css",
+  "views/_cookie-settings.scss" => "views/_cookie-settings.css",
+  "views/_csv_preview.scss" => "views/_csv_preview.css",
+  "views/_homepage_header.scss" => "views/_homepage_header.css",
+  "views/_homepage_more_on_govuk.scss" => "views/_homepage_more_on_govuk.css",
+  "views/_homepage.scss" => "views/_homepage.css",
+  "views/_local-transaction.scss" => "views/_local-transaction.css",
+  "views/_location_form.scss" => "views/_location_form.css",
+  "views/_popular_links.scss" => "views/_popular_links.css",
+  "views/_report-child-abuse.scss" => "views/_report-child-abuse.css",
+  "views/_travel-advice.scss" => "views/_travel-advice.css",
+}.freeze
+
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+Rails.application.config.dartsass.builds = all_stylesheets
+
+Rails.application.config.dartsass.build_options << " --quiet-deps"

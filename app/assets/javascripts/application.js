f1677e6c8e4c76504474d5b223945c16e7e30eb7
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2024-03-14 16:29:15 +0000

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/image-card
 //= require govuk_publishing_components/components/intervention

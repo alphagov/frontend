31aa4337a023bceea8cdb5b19d3c758ff3f4b4c1
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-24 16:02:24 +0100

    Convert help integration tests
    SQUASH help

--- spec/system/help_spec.rb
+++ spec/system/help_spec.rb
@@ -1,8 +1,6 @@
-require "integration_test_helper"
-
-class HelpTest < ActionDispatch::IntegrationTest
+RSpec.describe "Help", type: :system do
   context "rendering the help index page" do
-    setup do
+    before do
       payload = {
         base_path: "/help",
         format: "special_route",
@@ -12,10 +10,10 @@ class HelpTest < ActionDispatch::IntegrationTest
       stub_content_store_has_item("/help", payload)
     end
 
-    should "render the help index page correctly" do
+    it "renders the help index page correctly" do
       visit "/help"
 
-      assert_has_component_title "Help using GOV.UK"
+      expect(page).to have_title("Help using GOV.UK")
     end
   end
 end

ac46aec09c700cee28e8405937ae86cc74dffaea
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-24 16:01:57 +0100

    Convert help_cookies integration tests

---
+++ spec/feature/help_cookies_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe "HelpCookies", type: :feature do
+  context "rendering the cookies setting page" do
+    before do
+      payload = { base_path: "/help/cookies", format: "special_route", title: "Cookies on GOV.UK", description: "You can choose which cookies you're happy for GOV.UK to use." }
+      stub_content_store_has_item("/help/cookies", payload)
+    end
+
+    it "renders the cookies setting page correctly" do
+      visit "/help/cookies"
+
+      within("#content") { assert_has_component_title("Cookies on GOV.UK") }
+    end
+
+    it "has radio buttons set to disable cookies by default" do
+      visit "/help/cookies"
+
+      within("#content") do
+        expect(page).to have_css("input[name=cookies-usage][value=off][checked]")
+        expect(page).to have_css("input[name=cookies-campaigns][value=off][checked]")
+        expect(page).to have_css("input[name=cookies-settings][value=off][checked]")
+      end
+    end
+  end
+end

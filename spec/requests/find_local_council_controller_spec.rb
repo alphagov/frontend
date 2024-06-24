047783866d11f861a3ac062b27b8dc3483bcd833
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-24 16:31:19 +0100

    Convert find_local_council_controller functional tests
    SQUASH find_local_council_controller

---
+++ spec/requests/find_local_council_controller_spec.rb
@@ -0,0 +1,20 @@
+require "gds_api/test_helpers/local_links_manager"
+
+RSpec.describe FindLocalCouncilController, type: :request do
+  include GdsApi::TestHelpers::LocalLinksManager
+
+  before { content_store_has_random_item(base_path: "/find-local-council") }
+
+  it "sets correct expiry headers" do
+    get "/find-local-council"
+
+    honours_content_store_ttl
+  end
+
+  it "returns a 404 if the local authority can't be found" do
+    stub_local_links_manager_does_not_have_an_authority("foo")
+    get "/find-local-council/foo"
+
+    expect(response).to have_http_status(:not_found)
+  end
+end

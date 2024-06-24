5d0da1fc36f6379f92d816b9b4c16afee4145352
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-24 16:37:00 +0100

    Convert simple_smart_answers routing tests
    - replace utility function from content_store_helper
      only called in one place

---
+++ spec/routing/simple_smart_answers_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe "Simple Smart Answers" do
+  include ContentStoreHelpers
+
+  before do
+    content_store_has_random_item(base_path: "/fooey", schema: "simple_smart_answer")
+  end
+
+  context "for the start page" do
+    it "routes the start page to the SimpleSmartAnswer controller" do
+      expect(get("/fooey")).to route_to(controller: "simple_smart_answers", action: "show", slug: "fooey")
+    end
+  end
+
+  context "routes in a flow" do
+    it "routes to the start of a flow" do
+      expect(get("/fooey/y")).to route_to(controller: "simple_smart_answers", action: "flow", slug: "fooey")
+    end
+
+    it "routes the the second question in a flow" do
+      expect(get("/fooey/y/answer-1")).to route_to(controller: "simple_smart_answers", action: "flow", slug: "fooey", responses: "answer-1")
+    end
+
+    it "routes the the a question deeper in a flow" do
+      expect(get("/fooey/y/answer-1/2013-06-26/no")).to route_to(controller: "simple_smart_answers", action: "flow", slug: "fooey", responses: "answer-1/2013-06-26/no")
+    end
+
+    it "has json variants of routes" do
+      expect(get("/fooey/y/answer-1/2013-06-26/no.json")).to route_to(controller: "simple_smart_answers", action: "flow", slug: "fooey", responses: "answer-1/2013-06-26/no", format: "json")
+    end
+  end
+end

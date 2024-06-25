30f79d041bb67810472d5d31f40b39b19562a917
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-25 15:10:29 +0100

    Convert account_home integration tests
    - Integration tests from here on become feature tests
    - We create a mostly empty content_store_helper, we'll
       add the methods we use as we come to them. For this
       test it's just there so that it can include the GDS
       content store helper methods.

---
+++ spec/feature/account_home_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe "AccountHome", type: :feature do
+  before do
+    stub_content_store_has_item("/")
+  end
+
+  context "/account/home" do
+    it "redirects users to One Login's Your Services page" do
+      visit(account_home_path)
+      expect(page.current_url).to eq("#{GovukPersonalisation::Urls.one_login_your_services}/")
+    end
+
+    context "when there are cookie consent and _ga url parameters" do
+      it "preserve them through the redirect" do
+        visit(account_home_path(cookie_consent: true, _ga: "abc123"))
+        expect(page.current_url).to include("cookie_consent=true")
+        expect(page.current_url).to include("_ga=abc123")
+      end
+    end
+  end
+end
---
+++ spec/support/content_store_helpers.rb
@@ -0,0 +1,5 @@
+require "gds_api/test_helpers/content_store"
+
+module ContentStoreHelpers
+  include GdsApi::TestHelpers::ContentStore
+end

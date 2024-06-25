990a9b37b9dbd2887da8b2ab16d3ffadbbe1cee5
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-25 16:02:41 +0100

    Convert error_handling integration tests
    - move content_store_throws_exception_for in content
      store helper (first use)

--- test/support/content_store_helpers.rb
+++ test/support/content_store_helpers.rb
@@ -59,12 +59,6 @@ module ContentStoreHelpers
     content_store_has_random_item(base_path: "/#{slug}")
   end
 
-  def content_store_throws_exception_for(path, exception)
-    content_store = stub
-    content_store.stubs(:content_item).with(path).raises(exception)
-    GdsApi.stubs(:content_store).returns(content_store)
-  end
-
   def content_store_has_archived_page(slug)
     stub_content_store_has_gone_item("/#{slug}")
   end

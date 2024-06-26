11b05be85adafc9ed7cfc0e638f0862eb8897201
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 11:52:56 +0100

    Convert local_authority presenter unit tests
    SQUASH local_authority presenter unit

---
+++ spec/unit/presenters/local_authority_presenter_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe LocalAuthorityPresenter, type: :model do
+  let(:local_authority_payload) do
+    {
+      "name" => "Westminster City Council",
+      "snac" => "00BK",
+      "gss" => "E060000034",
+      "tier" => "district",
+      "homepage_url" => "http://westminster.example.com/",
+    }
+  end
+  let(:local_authority_presenter) { LocalAuthorityPresenter.new(local_authority_payload) }
+
+  describe "#exposed_attribute" do
+    %i[name snac gss tier homepage_url].each do |exposed_attribute|
+      it "exposes value of #{exposed_attribute} from payload via a method" do
+        expect(local_authority_presenter).to respond_to(exposed_attribute)
+        expect(local_authority_presenter.send(exposed_attribute)).to eq(local_authority_payload[exposed_attribute.to_s])
+      end
+    end
+  end
+
+  describe "#url" do
+    context "when homepage_url is present" do
+      it "exposes the homepage_url as the url" do
+        expect(local_authority_presenter.url).to eq("http://westminster.example.com/")
+      end
+    end
+
+    context "when homepage_url is blank" do
+      let(:local_authority_payload) { { "name" => "Westminster City Council", "homepage_url" => "" } }
+
+      it "exposes no url" do
+        expect(local_authority_presenter.url).to be_nil
+      end
+    end
+  end
+end

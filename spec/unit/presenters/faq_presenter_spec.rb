15150f75a1850fc7090c3880907d00b19569905f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 11:50:08 +0100

    Convert faq presenter unit tests
    - Update to use CalendarHelpers rather
      than test code in Calendar class.
    
    SQUASH faq presenter unit

--- spec/unit/presenters/faq_presenter_spec.rb
+++ spec/unit/presenters/faq_presenter_spec.rb
@@ -1,41 +1,36 @@
-require "test_helper"
+RSpec.describe FaqPresenter, type: :model do
+  include CalendarHelpers
 
-class FaqPresenterTest < ActiveSupport::TestCase
-  def setup
+  before do
     @view_context = ApplicationController.new.view_context
+    mock_calendar_fixtures
   end
 
-  def test_uses_bank_holiday_body_for_bank_holidays
+  it "uses bank holiday body for bank holidays" do
     expected = [
       q_and_a("England and Wales", "The next bank holiday in England and Wales is Good Friday on the 6th of April"),
       q_and_a("Scotland", "The next bank holiday in Scotland is Good Friday on the 6th of April"),
       q_and_a("Northern Ireland", "The next bank holiday in Northern Ireland is Good Friday on the 6th of April"),
     ]
-
     Timecop.travel(Date.parse("2012-03-24")) do
       scope = "bank-holidays"
       calendar = Calendar.find(scope)
       content_item = CalendarContentItem.new(calendar).payload
-
       presenter = FaqPresenter.new(scope, calendar, content_item, @view_context)
 
-      assert_equal expected, presenter.metadata["mainEntity"]
+      expect(presenter.metadata["mainEntity"]).to eq(expected)
     end
   end
 
-  def test_uses_wdtcc_body_for_wdtcc
-    expected = [
-      q_and_a("When do the clocks change?", "The clocks advance on the 25th of March"),
-    ]
-
+  it "uses wdtcc body for wdtcc" do
+    expected = [q_and_a("When do the clocks change?", "The clocks advance on the 25th of March")]
     Timecop.travel(Date.parse("2012-03-24")) do
       scope = "when-do-the-clocks-change"
       calendar = Calendar.find(scope)
       content_item = CalendarContentItem.new(calendar).payload
-
       presenter = FaqPresenter.new(scope, calendar, content_item, @view_context)
 
-      assert_equal expected, presenter.metadata["mainEntity"]
+      expect(presenter.metadata["mainEntity"]).to eq(expected)
     end
   end
 
@@ -45,10 +40,7 @@ private
     {
       "@type" => "Question",
       "name" => question,
-      "acceptedAnswer" => {
-        "@type" => "Answer",
-        "text" => answer,
-      },
+      "acceptedAnswer" => { "@type" => "Answer", "text" => answer },
     }
   end
 end

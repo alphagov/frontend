5b28aa21b782cd3b31de3d6853076dc3a73a49da
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 10:11:58 +0100

    Convert find_local_council integration tests
    - local_links_manager_helpers only has one method
      only used in one place, so move it to there.
    - First appearance of content_store_has_random_item,
      so bring it into content_store_helper
    - bring in location_helpers (first use)
    - include component helpers in feature tests, this
      is the first time they're used but they're used a lot
      in subsequent feature tests.
    
    FIX: Find local council
    
    SQUASH find_local_council

--- spec/support/content_store_helpers.rb
+++ spec/support/content_store_helpers.rb
@@ -1,7 +1,32 @@
+require "govuk_schemas"
 require "gds_api/test_helpers/content_store"
 
 module ContentStoreHelpers
   include GdsApi::TestHelpers::ContentStore
+
+  def basic_taxon
+    {
+      "content_id" => "30c1b93d-2553-47c9-bc3c-fc5b513ecc32",
+      "locale" => "en",
+      "title" => "A Taxon",
+      "base_path" => "/a-taxon",
+    }
+  end
+
+  def content_store_has_random_item(base_path:, schema: "generic", is_tagged_to_taxon: false, details: {})
+    content_item = GovukSchemas::RandomExample.for_schema(frontend_schema: schema) do |item|
+      taxons = is_tagged_to_taxon ? [basic_taxon] : []
+      item.merge!(
+        "base_path" => base_path,
+        "links" => { "taxons" => taxons },
+      )
+      item["details"].merge!(details)
+      item
+    end
+
+    stub_content_store_has_item(content_item["base_path"], content_item)
+    content_item
+  end
 
   def content_store_throws_exception_for(path, exception)
     content_store = double

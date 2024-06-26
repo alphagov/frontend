ed3466aaef3a0f1ca3471561abd5454592a52164
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 11:50:39 +0100

    Convert licence_details presenter unit tests

--- spec/unit/presenters/licence_details_presenter_spec.rb
+++ spec/unit/presenters/licence_details_presenter_spec.rb
@@ -1,7 +1,5 @@
-require "test_helper"
-
-class LicenceDetailsPresenterTest < ActiveSupport::TestCase
-  setup do
+RSpec.describe LicenceDetailsPresenter do
+  before do
     @local_authority_licence = {
       "isLocationSpecific" => true,
       "isOfferedByCounty" => false,
@@ -32,7 +30,6 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
         },
       ],
     }
-
     @the_one_licence_authority = {
       "authorityName" => "The One Licence Authority",
       "authoritySlug" => "the-one-licence-authority",
@@ -46,7 +43,6 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
         ],
       },
     }
-
     @the_other_licence_authority = {
       "authorityName" => "The Other Licence Authority",
       "authoritySlug" => "the-other-licence-authority",
@@ -60,7 +56,6 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
         ],
       },
     }
-
     @licence_authority_not_using_licensify = {
       "authorityName" => "The Licence Authority Not Using Licensify",
       "authoritySlug" => "the-licence-authority-not-using-licensify",
@@ -74,7 +69,6 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
         ],
       },
     }
-
     @licence_authority_not_using_authority_url = {
       "authorityName" => "The Licence Authority Not Using Licensify",
       "authoritySlug" => "the-licence-authority-not-using-licensify",
@@ -88,48 +82,43 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
         ],
       },
     }
-
     @licence_authority_without_uses_licensify_param = {
       "authorityName" => "The Licence Authority Without UsesLicensify Param",
       "authoritySlug" => "the-licence-authority-without-uses-licensify-param",
       "authorityInteractions" => {
         "apply" => [
           {
-            "url" => "the-licence-authority-without-uses-licensify-param/apply-1",
+            "url" =>
+              "the-licence-authority-without-uses-licensify-param/apply-1",
             "usesAuthorityUrl" => "true",
           },
         ],
       },
     }
-
     @licence_authority_without_uses_authority_url_param = {
       "authorityName" => "The Licence Authority Without UsesLicensify Param",
       "authoritySlug" => "the-licence-authority-without-uses-licensify-param",
       "authorityInteractions" => {
         "apply" => [
           {
-            "url" => "the-licence-authority-without-uses-licensify-param/apply-1",
+            "url" =>
+              "the-licence-authority-without-uses-licensify-param/apply-1",
             "usesLicensify" => "true",
           },
         ],
       },
     }
-
     @licence_authority_with_no_actions = {
       "authorityName" => "The Licence Authority with no actions",
       "authoritySlug" => "the-licence-authority-with-no-actions",
       "authorityInteractions" => {},
     }
-
     @licence_authority_licence = {
       "isLocationSpecific" => false,
       "isOfferedByCounty" => false,
       "geographicalAvailability" => %w[England Wales],
-      "issuingAuthorities" => [
-        @the_one_licence_authority,
-      ],
+      "issuingAuthorities" => [@the_one_licence_authority],
     }
-
     @multiple_authorities_and_location_specific_licence = {
       "isLocationSpecific" => true,
       "isOfferedByCounty" => false,
@@ -141,32 +130,32 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
     }
   end
 
-  context "#single_licence_authority_present?" do
+  describe "#single_licence_authority_present?" do
     context "when authority present" do
-      should "return true for licence authority specific licence" do
-        subject = LicenceDetailsPresenter.new(@licence_authority_licence)
+      it "returns true for licence authority specific licence" do
+        subject = described_class.new(@licence_authority_licence)
 
-        assert subject.single_licence_authority_present?
+        expect(subject.single_licence_authority_present?).to be true
       end
 
-      should "return false for local authority specific licence" do
-        subject = LicenceDetailsPresenter.new(@local_authority_licence)
+      it "returns false for local authority specific licence" do
+        subject = described_class.new(@local_authority_licence)
 
-        assert_not subject.single_licence_authority_present?
+        expect(subject.single_licence_authority_present?).to be false
       end
     end
 
     context "when authority non-present" do
-      should "return false" do
-        subject = LicenceDetailsPresenter.new(@licence_authority_licence.merge("issuingAuthorities" => []))
+      it "returns false" do
+        subject = described_class.new(@licence_authority_licence.merge("issuingAuthorities" => []))
 
-        assert_not subject.single_licence_authority_present?
+        expect(subject.single_licence_authority_present?).to be false
       end
     end
   end
 
   context "authority look up methods" do
-    setup do
+    before do
       @presented_the_one_licence_authority = {
         "name" => "The One Licence Authority",
         "slug" => "the-one-licence-authority",
@@ -184,7 +173,6 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
           ],
         },
       }
-
       @presented_the_other_licence_authority = {
         "name" => "The Other Licence Authority",
         "slug" => "the-other-licence-authority",
@@ -204,124 +192,118 @@ class LicenceDetailsPresenterTest < ActiveSupport::TestCase
       }
     end
 
-    context "#authority" do
+    describe "#authority" do
       context "when one authority is present" do
-        should "return the authority" do
-          subject = LicenceDetailsPresenter.new(@licence_authority_licence)
+        it "returns the authority" do
+          subject = described_class.new(@licence_authority_licence)
 
-          assert_equal @presented_the_one_licence_authority, subject.authority
+          expect(subject.authority).to eq(@presented_the_one_licence_authority)
         end
       end
 
       context "when more than one authority are present" do
-        should "return the matched authority if authority slug provided" do
-          subject = LicenceDetailsPresenter.new(@multiple_authorities_and_location_specific_licence, "the-other-licence-authority")
+        it "returns the matched authority if authority slug provided" do
+          subject = described_class.new(@multiple_authorities_and_location_specific_licence, "the-other-licence-authority")
 
-          assert_equal @presented_the_other_licence_authority, subject.authority
+          expect(subject.authority).to eq(@presented_the_other_licence_authority)
         end
 
-        should "return nil if no matched authority found" do
-          subject = LicenceDetailsPresenter.new(@multiple_authorities_and_location_specific_licence, "a-funky-licence-authority")
+        it "returns nil if no matched authority found" do
+          subject = described_class.new(@multiple_authorities_and_location_specific_licence, "a-funky-licence-authority")
 
-          assert_nil subject.authority
+          expect(subject.authority).to be_nil
         end
 
         context "no authority_slug is provided" do
-          should "return the first authority" do
-            subject = LicenceDetailsPresenter.new(@multiple_authorities_and_location_specific_licence)
+          it "returns the first authority" do
+            subject = described_class.new(@multiple_authorities_and_location_specific_licence)
 
-            assert_equal @presented_the_other_licence_authority, subject.authority
+            expect(subject.authority).to eq(@presented_the_other_licence_authority)
           end
         end
       end
 
       context "when no authority is present" do
-        should "return nil" do
-          subject = LicenceDetailsPresenter.new(@licence_authority_licence.merge("issuingAuthorities" => []))
+        it "returns nil" do
+          subject = described_class.new(@licence_authority_licence.merge("issuingAuthorities" => []))
 
-          assert_nil subject.authority
+          expect(subject.authority).to be_nil
         end
       end
     end
   end
 
-  context "#uses_licensify" do
-    should "return true if the action has a field to confirm that it uses licensify" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_licence, "the-one-licence-authority")
+  describe "#uses_licensify" do
+    it "is true if the action has a field to confirm that it uses licensify" do
+      subject = described_class.new(@licence_authority_licence, "the-one-licence-authority")
 
-      assert_equal subject.uses_licensify("apply"), true
+      expect(subject.uses_licensify("apply")).to be true
     end
 
-    should "return true if the default action has uses_licensify set to true" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_licence, "the-one-licence-authority", "apply")
-
-      assert_equal subject.uses_licensify, true
+    it "is true if the default action has uses_licensify set to true" do
+      subject = described_class.new(@licence_authority_licence, "the-one-licence-authority", "apply")
+      expect(subject.uses_licensify("apply")).to be true
     end
 
-    should "return false if the action has the uses_licensify field set to false" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_not_using_licensify)
-
-      assert_equal subject.uses_licensify("apply"), false
+    it "is false if the action has the uses_licensify field set to false" do
+      subject = described_class.new(@licence_authority_not_using_licensify)
+      expect(subject.uses_licensify("apply")).to be false
     end
 
-    should "return false if the action does not have a usesLicensify field" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_without_uses_licensify_param)
-
-      assert_equal subject.uses_licensify("apply"), false
+    it "is false if the action does not have a usesLicensify field" do
+      subject = described_class.new(@licence_authority_without_uses_licensify_param)
+      expect(subject.uses_licensify("apply")).to be false
     end
 
-    should "return false if authority is nil" do
-      subject = LicenceDetailsPresenter.new(@the_one_licence_authority)
-
-      assert_equal subject.uses_licensify("apply"), false
+    it "is false if authority is nil" do
+      subject = described_class.new(@the_one_licence_authority)
+      expect(subject.uses_licensify("apply")).to be false
     end
   end
 
-  context "#uses_authority_url" do
-    should "return true if the action has a field to confirm that it uses authority url" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_licence, "the-one-licence-authority")
+  describe "#uses_authority_url" do
+    it "is true if the action has a field to confirm that it uses authority url" do
+      subject = described_class.new(@licence_authority_licence, "the-one-licence-authority")
 
-      assert_equal subject.uses_authority_url("apply"), true
+      expect(subject.uses_authority_url("apply")).to be true
     end
 
-    should "return true if the default action has uses_authority_url set to true" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_licence, "the-one-licence-authority", "apply")
+    it "is true if the default action has uses_authority_url set to true" do
+      subject = described_class.new(@licence_authority_licence, "the-one-licence-authority", "apply")
 
-      assert_equal subject.uses_authority_url, true
+      expect(subject.uses_authority_url("apply")).to be true
     end
 
-    should "return false if the action has the uses_authority_url field set to false" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_not_using_authority_url)
+    it "is false if the action has the uses_authority_url field set to false" do
+      subject = described_class.new(@licence_authority_not_using_authority_url)
 
-      assert_equal subject.uses_authority_url("apply"), false
+      expect(subject.uses_authority_url("apply")).to be false
     end
 
-    should "return false if the action does not have a usesAuthorityUrl field" do
-      subject = LicenceDetailsPresenter.new(@licence_authority_without_uses_authority_url_param)
+    it "is false if the action does not have a usesAuthorityUrl field" do
+      subject = described_class.new(@licence_authority_without_uses_authority_url_param)
 
-      assert_equal subject.uses_authority_url("apply"), false
+      expect(subject.uses_authority_url("apply")).to be false
     end
 
-    should "return false if authority is nil" do
-      subject = LicenceDetailsPresenter.new(@the_one_licence_authority)
+    it "is false if authority is nil" do
+      subject = described_class.new(@the_one_licence_authority)
 
-      assert_equal subject.uses_authority_url("apply"), false
+      expect(subject.uses_authority_url("apply")).to be false
     end
   end
 
-  context "#action" do
-    should "return action name if available" do
-      subject = LicenceDetailsPresenter.new(@local_authority_licence, nil, "apply")
+  describe "#action" do
+    it "returns action name if available" do
+      subject = described_class.new(@local_authority_licence, nil, "apply")
 
-      assert_equal "apply", subject.action
+      expect(subject.action).to eq("apply")
     end
 
-    should "raise RecordNotFound if not available" do
-      subject = LicenceDetailsPresenter.new(@local_authority_licence, nil, "complain")
+    it "raises RecordNotFound if not available" do
+      subject = described_class.new(@local_authority_licence, nil, "complain")
 
-      assert_raises RecordNotFound do
-        subject.action
-      end
+      expect { subject.action }.to raise_error(RecordNotFound)
     end
   end
 end

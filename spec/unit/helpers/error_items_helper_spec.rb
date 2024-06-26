59dfbde4d5eedc0769d93ecde0deb49ad01d3b69
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 11:16:29 +0100

    Convert error_items_helper unit tests

---
+++ spec/unit/helpers/error_items_helper_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe ErrorItemsHelper, type: :view do
+  include ErrorItemsHelper
+
+  before do
+    flash[:validation] = [{ field: "full_name", text: "Enter full name" }, { field: "job_title", text: "Enter job title" }]
+  end
+
+  describe "#error_items" do
+    it "contains error items" do
+      expect(error_items("job_title")).to eq("Enter job title")
+      expect(error_items("email_address")).to(be_nil)
+    end
+  end
+end

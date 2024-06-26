e43a10f9a42ac00ac2f80e3704f8f0935b89af4b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 11:54:07 +0100

    Convert simple_smart_answer presenter unit tests

---
+++ spec/unit/presenters/simple_smart_answer_presenter_spec.rb
@@ -0,0 +1,34 @@
+RSpec.describe SimpleSmartAnswerPresenter do
+  def subject(content_item)
+    described_class.new(content_item.deep_stringify_keys!)
+  end
+
+  context "locale is 'cy'" do
+    before { I18n.locale = :cy }
+    after { I18n.locale = :en }
+
+    context "start_button_text is 'Start now'" do
+      it "returns Welsh translation 'Dechrau nawr'" do
+        @item = { details: { start_button_text: "Start now" } }
+
+        expect(subject(@item).start_button_text).to eq("Dechrau nawr")
+      end
+    end
+
+    context "start_button_text is 'Continue'" do
+      it "returns Welsh translation 'Parhau'" do
+        @item = { details: { start_button_text: "Continue" } }
+
+        expect(subject(@item).start_button_text).to eq("Parhau")
+      end
+    end
+
+    context "start_button_text is explicitly set" do
+      it "returns the set value" do
+        @item = { details: { start_button_text: "Go for it" } }
+
+        expect(subject(@item).start_button_text).to eq("Go for it")
+      end
+    end
+  end
+end

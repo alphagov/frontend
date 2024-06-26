5b07f634793dbdec8bbf88a94419ab2d07a665d1
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 13:47:30 +0100

    Convert api_error_routing_constraint unit tests

---
+++ spec/unit/api_error_routing_constraint_spec.rb
@@ -0,0 +1,21 @@
+require "api_error_routing_constraint"
+
+RSpec.describe ApiErrorRoutingConstraint do
+  it "returns true if there's a cached error" do
+    request = double(env: { content_item_error: StandardError.new })
+
+    expect(subject.matches?(request)).to be true
+  end
+
+  it "returns false if there was no error in API calls" do
+    request = double(env: {})
+
+    expect(subject.matches?(request)).to be false
+  end
+
+private
+
+  def subject
+    ApiErrorRoutingConstraint.new
+  end
+end

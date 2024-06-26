3f5169fc5e72b2351393eaa8b9dbaf90fdd0ff0c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 13:48:56 +0100

    Convert format_routing_constraint unit tests
    SQUASH format routing constraint unit

--- spec/unit/format_routing_constraint_spec.rb
+++ spec/unit/format_routing_constraint_spec.rb
@@ -1,56 +1,58 @@
-require "test_helper"
+RSpec.describe FormatRoutingConstraint, type: :model do
+  include ContentStoreHelpers
 
-class FormatRoutingConstraintTest < ActiveSupport::TestCase
-  context "#matches?" do
+  describe "#matches?" do
     context "when the content_store returns a document" do
-      setup do
+      before do
         @format = "foo"
         stub_content_store_has_item("/#{slug}", schema_name: @format)
         @request = request
       end
 
-      should "return true if format matches" do
-        assert subject(@format).matches?(@request)
+      it "returns true if format matches" do
+        expect(subject(@format).matches?(@request)).to be true
       end
 
-      should "return false if format doesn't match" do
-        assert_not subject("not_the_format").matches?(@request)
+      it "returns false if format doesn't match" do
+        expect(subject("not_the_format").matches?(@request)).to be false
       end
 
-      should "set the content item on the request object" do
+      it "sets the content item on the request object" do
         subject(@format).matches?(@request)
-        assert @request.env[:content_item].present?
+
+        expect(@request.env[:content_item].present?).to be true
       end
     end
 
     context "when the content_store API call throws an error" do
-      setup do
+      before do
         stub_content_store_does_not_have_item("/#{slug}")
         @request = request
       end
 
-      should "return false" do
-        assert_not subject("any_format").matches?(@request)
+      it "returns false" do
+        expect(subject("any_format").matches?(@request)).to be false
       end
 
-      should "set an error on the request object" do
+      it "sets an error on the request object" do
         subject("any_format").matches?(@request)
-        assert @request.env[:content_item_error].present?
+        expect(@request.env[:content_item_error].present?).to be true
       end
     end
   end
 
   context "content items are memoized and used across multiple requests" do
-    setup do
+    before do
       @stub = stub_content_store_has_item("/#{slug}", schema_name: "foo")
       @subject = subject("foo")
       @request = request
     end
 
-    should "only make one API call" do
+    it "only makes one API call" do
       @subject.matches?(@request)
       @subject.matches?(@request)
-      assert_requested @stub, times: 1
+
+      assert_requested(@stub, times: 1)
     end
   end
 
@@ -59,7 +61,7 @@ class FormatRoutingConstraintTest < ActiveSupport::TestCase
   end
 
   def request
-    stub(params: { slug: }, env: {})
+    double(params: { slug: }, env: {})
   end
 
   def subject(format)

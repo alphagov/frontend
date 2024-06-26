224d9734e5d9952b000a75f386190a14e560f298
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-06-26 13:44:09 +0100

    Convert recruitment_banner_helper unit tests
    - Move the fixture file used by this test

---
+++ spec/unit/helpers/recruitment_banner_helper_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe RecruitmentBannerHelper, type: :view do
+  include RecruitmentBannerHelper
+
+  before do
+    @recruitment_banners_data = YAML.load_file(Rails.root.join("spec/fixtures/recruitment_banners.yml"))
+  end
+
+  def request
+    OpenStruct.new(path: "/")
+  end
+
+  def recruitment_banners
+    @recruitment_banners_data["banners"]
+  end
+
+  it "recruitment_banner returns banners that include the current url" do
+    actual_banners = recruitment_banner
+    expected_banners = { "name" => "Banner 1", "suggestion_text" => "Help improve GOV.UK", "suggestion_link_text" => "Take part in user research", "survey_url" => "https://google.com", "page_paths" => ["/"] }
+    expect(actual_banners).to eq(expected_banners)
+  end
+
+  it "recruitment_banners yaml structure is valid" do
+    @recruitment_banners_data = YAML.load_file(Rails.root.join("lib/data/recruitment_banners.yml"))
+
+    if @recruitment_banners_data["banners"].present?
+      recruitment_banners.each do |banner|
+        expect(banner.key?("suggestion_text")).to be true
+        expect(banner["suggestion_text"].blank?).to be true
+        expect(banner.key?("suggestion_link_text")).to be true
+        expect(banner["suggestion_link_text"].blank?).to be true
+        expect(banner.key?("survey_url")).to be true
+        expect(banner["survey_url"].blank?).to be true
+        expect(banner.key?("page_paths")).to be true
+        expect(banner["page_paths"].blank?).to be true
+      end
+    end
+  end
+end

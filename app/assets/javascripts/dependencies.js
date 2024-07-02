81157d57e861f7e4be22bdd02ce73beddb5ba669
Mateusz Grotek <mateusz.grotek@digital.cabinet-office.gov.uk>
2024-07-02 11:01:23 +0100

    Restructure JavaScript files

---
+++ app/assets/javascripts/dependencies.js
@@ -0,0 +1,7 @@
+// This file contains the dependencies required by the application.
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/image-card
+//= require govuk_publishing_components/components/intervention
+//= require govuk_publishing_components/components/step-by-step-nav
+//= require govuk_publishing_components/components/table
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,5 @@
+// This file should only contain the code being a part
+// of this application, not its dependencies.
+// It will be checked with respect to test coverage.
+//= require support
+//= require_tree ./modules

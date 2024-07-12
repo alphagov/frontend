2a0c0b876353698b24aa26c427ffbf9a34876f03
Mateusz Grotek <mateusz.grotek@digital.cabinet-office.gov.uk>
2024-07-12 16:39:07 +0100

    If available, use popular links from content item.

--- test/functional/homepage_controller_test.rb
+++ test/functional/homepage_controller_test.rb
@@ -5,7 +5,7 @@ class HomepageControllerTest < ActionController::TestCase
 
   context "loading the homepage" do
     setup do
-      stub_content_store_has_item("/", schema: "special_route")
+      stub_content_store_has_item("/", schema: "special_route", links: {})
     end
 
     should "respond with success" do
@@ -16,6 +16,40 @@ class HomepageControllerTest < ActionController::TestCase
     should "set correct expiry headers" do
       get :index
       honours_content_store_ttl
+    end
+
+    context "with popular links in the content item" do
+      setup do
+        links = {
+          "popular_links" => [
+            {
+              "details" => {
+                "link_items" => [
+                  {
+                    "title" => "Some popular links title",
+                    "url" => "/some/path",
+                  },
+                ],
+              },
+            },
+          ],
+        }
+        stub_content_store_has_item("/", schema: "special_route", links:)
+      end
+
+      should "show popular links" do
+        get :index
+        assert_match "Some popular links title", @response.body
+      end
+    end
+
+    context "with popular links not in the content item" do
+      should "show popular links" do
+        get :index
+        I18n.t("homepage.index.popular_links").each do |item|
+          assert_match item[:title], @response.body
+        end
+      end
     end
   end
 end

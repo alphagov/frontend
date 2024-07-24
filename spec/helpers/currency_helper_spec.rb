ec0d35729482b06304c562ced10a7477610afe2a
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-07-24 15:21:00 +0100

    Convert currency_helper unit tests

---
+++ spec/helpers/currency_helper_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe CurrencyHelper do
+  include CurrencyHelper
+
+  let(:sample_number) { "12000" }
+
+  describe "#format_amount" do
+    it "returns a formatted number" do
+      expect(format_amount(sample_number)).to eq("12,000 euros")
+    end
+
+    it "returns nil if passed nil" do
+      expect(format_amount(nil)).to be_nil
+    end
+  end
+end

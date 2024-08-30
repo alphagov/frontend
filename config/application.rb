ce18dea488d48de67404f3147f0598898837b809
Mateusz Grotek <mateusz.grotek@digital.cabinet-office.gov.uk>
2024-08-30 17:00:59 +0100

    Enable new Rails 7.2 configuration

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Frontend
     include GovukPublishingComponents::AppHelpers::AssetHelper
     # Initialize configuration defaults for originally generated Rails version.
     require "frontend"
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.

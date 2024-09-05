d2ce1436e223c68610ff181f40ac7d54a7f6c7c0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-09-05 14:02:46 +0100

    Add latest update to travel advice presenter
    This was in the travel advice presenter in government-frontend. However
    as it is a purely presentation concern, it did not feel appropriate to
    add it to the model.
    
    It'll be needed by some new presenter methods.

--- app/presenters/travel_advice_presenter.rb
+++ app/presenters/travel_advice_presenter.rb
@@ -8,4 +8,17 @@ class TravelAdvicePresenter < ContentItemModelPresenter
       "#{content_item.current_part_title} - #{super}"
     end
   end
+
+  # FIXME: Update publishing app UI and remove from content
+  # Change description is used as "Latest update" but isn't labelled that way
+  # in the publisher. The frontend didn't add this label before.
+  # This led to users appending (in a variety of formats)
+  # "Latest update:" to the start of the change description. The frontend now
+  # has a latest update label, so we can strip this out.
+  # Avoids: "Latest update: Latest update - ..."
+  def latest_update
+    content_item.change_description.sub(/^Latest update:?\s-?\s?/i, "").tap do |latest|
+      latest[0] = latest[0].capitalize if latest.present?
+    end
+  end
 end

34ee53640c519e9aa5afacd9c309985665d37edb
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-09-26 16:00:48 +0100

    DELETE machine_readable_metadata_helper

--- app/helpers/machine_readable_metadata_helper.rb
+++
@@ -1,6 +0,0 @@
-module MachineReadableMetadataHelper
-  def machine_readable_metadata(content_item, args)
-    locals = { content_item: content_item.to_h }.merge(args)
-    render("govuk_publishing_components/components/machine_readable_metadata", locals)
-  end
-end

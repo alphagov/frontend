dcd191394c7f5ae998d4581e03fff0c3323283f0
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-03 11:27:13 +0100

    Update columns to add class size based on column count

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -8,4 +8,17 @@ module BlockHelper
       }
     end
   end
+
+  def column_class_for_n_columns(number_of_columns)
+    case number_of_columns
+    when 1
+      "govuk-grid-column"
+    when 2
+      "govuk-grid-column-one-half"
+    when 3
+      "govuk-grid-column-one-third"
+    else
+      "govuk-grid-column-one-quarter"
+    end
+  end
 end

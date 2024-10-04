6892afc4b29b785de89aeec27323c09e141c9ed5
George Schena <george.schena1@digital.cabinet-office.gov.uk>
2024-10-04 07:37:52 +0100

    Add DateHelper and add new method in LocaleHelper
    - Add new DateHelper to deal with the last updated date on help pages
    - Add page_text_direction to LocaleHelper
    
    Commit audit trail:
    - app/helpers/locale_helper.rb https://github.com/alphagov/government-frontend/blob/af54031948650448b965ac274c3248d23658c4f6/app/helpers/application_helper.rb
    - app/helpers/date_helper.rb https://github.com/alphagov/government-frontend/blob/af54031948650448b965ac274c3248d23658c4f6/app/presenters/content_item_presenter.rb

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -2,4 +2,8 @@ module LocaleHelper
   def lang_attribute(locale)
     "lang=#{locale}" unless I18n.default_locale.to_s == locale.to_s
   end
+
+  def page_text_direction
+    I18n.t("i18n.direction", locale: I18n.locale, default: "ltr")
+  end
 end

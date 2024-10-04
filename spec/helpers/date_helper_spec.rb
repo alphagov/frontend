5e571f7f2b9615315cc7b2656a6ef005d42c8544
George Schena <george.schena1@digital.cabinet-office.gov.uk>
2024-10-04 07:37:52 +0100

    Add DateHelper and add new method in LocaleHelper
    - Add new DateHelper to deal with the last updated date on help pages
    - Add page_text_direction to LocaleHelper
    
    Commit audit trail:
    - app/helpers/locale_helper.rb https://github.com/alphagov/government-frontend/blob/af54031948650448b965ac274c3248d23658c4f6/app/helpers/application_helper.rb
    - app/helpers/date_helper.rb https://github.com/alphagov/government-frontend/blob/af54031948650448b965ac274c3248d23658c4f6/app/presenters/content_item_presenter.rb

---
+++ spec/helpers/date_helper_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe DateHelper do
+  include DateHelper
+
+  let(:timestamp) { "2024-10-03 19:30:22 +0100" }
+
+  describe "#display_date" do
+    it "returns a formatted date" do
+      expect(display_date(timestamp)).to eq("3 October 2024")
+    end
+
+    it "returns nil if passed nil" do
+      expect(display_date(nil)).to be_nil
+    end
+  end
+end

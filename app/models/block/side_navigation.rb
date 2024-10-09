0525e4784500770b79fc11d00a913aaa1a37f18e
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-10-09 16:27:34 +0100

    Add a model for the side navigation block
    This model reads in the same list of links for each landing page.
    This model will need to be modified to read side navigation links from
    the links hash in the content item, assuming that's where they will end
    up, when they become available.

---
+++ app/models/block/side_navigation.rb
@@ -0,0 +1,10 @@
+module Block
+  class SideNavigation < Block::Base
+    LINKS_FILE_PATH = "lib/data/landing_page_content_items/links/side_navigation.yaml".freeze
+
+    def links
+      file_path = Rails.root.join(LINKS_FILE_PATH)
+      YAML.load(File.read(file_path))
+    end
+  end
+end

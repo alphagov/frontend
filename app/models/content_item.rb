fb7a3e5a2f25178591d6ccdffc16d79fe08a1edb
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-16 16:09:39 +0100

    Expose schema on ContentItem

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -1,5 +1,6 @@
 class ContentItem
-  attr_reader :content_store_response, :content_store_hash, :body, :image, :description, :document_type, :title, :base_path, :locale
+  attr_reader :content_store_response, :content_store_hash, :body, :image, :description,
+              :document_type, :schema, :title, :base_path, :locale
 
   # SCAFFOLDING: remove the override_content_store_hash parameter when full landing page
   # content items including block details are available from content-store
@@ -11,6 +12,7 @@ class ContentItem
     @image = content_store_hash.dig("details", "image")
     @description = content_store_hash["description"]
     @document_type = content_store_hash["document_type"]
+    @schema = content_store_hash["schema"]
     @title = content_store_hash["title"]
     @base_path = content_store_hash["base_path"]
     @locale = content_store_hash["locale"]

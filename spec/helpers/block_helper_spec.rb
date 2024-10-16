33d20363dbb133e903aa119b739212bca6293b0b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-10-16 15:59:26 +0100

    Add a render_block helper
    The render_block method fails silently if a block doesn't exist.
    This means that the page won't fail if a block is added to the YAML
    before the block has been built, or if the block name has a typo in it.
    The page won't fail, the block just won't display.

--- spec/helpers/block_helper_spec.rb
+++ spec/helpers/block_helper_spec.rb
@@ -60,4 +60,11 @@ RSpec.describe BlockHelper do
       end
     end
   end
+
+  describe "#render_block" do
+    it "returns an empty string when a partial template doesn't exist" do
+      block = double(type: "not_a_block")
+      expect(render_block(block)).to be_empty
+    end
+  end
 end

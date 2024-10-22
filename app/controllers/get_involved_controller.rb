ebdca3c8f240a96355693e8d6378c2a19788d6a4
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2024-10-22 13:18:42 +0100

    updated code to separate consultation count calculation logic

--- app/controllers/get_involved_controller.rb
+++ app/controllers/get_involved_controller.rb
@@ -2,4 +2,8 @@ class GetInvolvedController < ContentItemsController
   def content_item_slug
     request.path
   end
+
+  def show
+    @presenter = GetInvolvedPresenter.new(@content_item)
+  end
 end

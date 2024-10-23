be0bfb88da2770cb97a4e05af5009054254d398b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-10-23 13:53:54 +0100

    Allow blocks to control subblock options
    - create a new thing for blocks, options
    - allow blocks to define options, that then get passed to subblocks
    - this means blocks can directly control things like inverse and margin, based on the context
    - so we know that any blocks within a hero text block are going to need to be inverse, so now the hero block sets that once, rather than having to set it in the YAML for each individual block

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -1,6 +1,6 @@
 module BlockHelper
-  def render_block(block)
-    render("landing_page/blocks/#{block.type}", block:)
+  def render_block(block, options: {})
+    render("landing_page/blocks/#{block.type}", block:, options:)
   rescue ActionView::MissingTemplate
     Rails.logger.warn("Missing template for block #{block.type}")
     ""

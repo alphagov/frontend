c28c3c0e4e45a17840a7d800f04b32753d33b292
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-10-25 16:32:27 +0100

    Refactor card block
    - assuming that a card always has a heading link
    - so easier to split it out as specific attributes than treat it as a subblock, because then we can modify it
    - introduce the link_cover_block option, which sets the link class to include a pseudo element that covers the whole card in a single link, removing the need to make minimal charts a link and avoiding having duplicate links
    - update the YAML and tests accordingly
    
    Co-authored-by: Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,9 +1,10 @@
 module LinkHelper
-  def govuk_styled_link(text, path: nil, inverse: false)
+  def govuk_styled_link(text, path: nil, inverse: false, card_link: false)
     return text if path.blank?
 
     classes = "govuk-link"
     classes << " govuk-link--inverse" if inverse
+    classes << " app-b-card__textbox-link" if card_link
 
     "<a href='#{path}' class='#{classes}'>#{text}</a>".html_safe
   end

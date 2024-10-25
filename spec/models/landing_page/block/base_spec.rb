ca56df015873c4d468c60ef49da31cae8900f6ae
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-10-25 12:02:39 +0100

    Namespace Block model under Landing Page
    At the moment blocks are only used by Landing Page documents.
    The block views are namespaced under LandingPage and the models should
    be too to keep consistency, and so that it's clearer where blocks are
    supposed to be used.

--- spec/models/landing_page/block/base_spec.rb
+++ spec/models/landing_page/block/base_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Block::Base do
+RSpec.describe LandingPage::Block::Base do
   describe "#full_width?" do
     it "is false by default" do
       expect(described_class.new({}).full_width?).to eq(false)

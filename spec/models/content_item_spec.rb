9762c0dc1f48749a634c185ad86b17c215b4097b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-28 16:14:34 +0000

    Add attachments as an accessible thing in ContentItems
    - this is general enough that it makes sense to add it at the ContentItem level rather than to LandingPage.

--- spec/models/content_item_spec.rb
+++ spec/models/content_item_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe ContentItem do
-  describe "#is_a_xxxx?" do
-    let(:subject) { described_class.new({ "schema_name" => "landing_page" }) }
+  let(:subject) { build(:content_item_with_data_attachments, schema_name: "fancy_page_type") }
 
+  describe "#is_a_xxxx?" do
     it "returns true when called with the schema name of the object" do
-      expect(subject.is_a_landing_page?).to be true
+      expect(subject.is_a_fancy_page_type?).to be true
     end
 
     it "returns false when called with a mismatching schema name" do
@@ -15,13 +15,20 @@ RSpec.describe ContentItem do
     end
 
     it "still passes other missing methods to parent" do
-      expect { subject.was_a_landing_page? }.to raise_error(NoMethodError)
+      expect { subject.was_a_fancy_page_type? }.to raise_error(NoMethodError)
     end
 
     it "responds to the various methods" do
-      expect(subject.respond_to?(:is_a_landing_page?)).to be true
+      expect(subject.respond_to?(:is_a_fancy_page_type?)).to be true
       expect(subject.respond_to?(:is_an_organisation?)).to be true
       expect(subject.respond_to?(:was_a_landing_page?)).to be false
+    end
+  end
+
+  describe "#attachments" do
+    it "loads the attachment data from the content item" do
+      expect(subject.attachments.count).to eq(2)
+      expect(subject.attachments[0].title).to eq("Data One")
     end
   end
 end

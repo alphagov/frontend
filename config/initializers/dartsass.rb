1bfaed71d406b5d3a52f21917c84796aa187722b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-30 09:58:37 +0000

    Add Figure component
    - Used in Take Part, among others
    - Includes spec file, documentation
    
    Commit audit trail:
    - https://github.com/alphagov/government-frontend/blob/6523741691ddfb9551967311a3f79775e9403ecf/app/assets/stylesheets/components/_figure.scss
    - https://github.com/alphagov/government-frontend/blob/6523741691ddfb9551967311a3f79775e9403ecf/app/views/components/_figure.html.erb
    - https://github.com/alphagov/government-frontend/blob/6523741691ddfb9551967311a3f79775e9403ecf/app/views/components/docs/figure.yml
    - https://github.com/alphagov/government-frontend/blob/6523741691ddfb9551967311a3f79775e9403ecf/test/components/figure_test.rb

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,7 @@
 APP_STYLESHEETS = {
   "application.scss" => "application.css",
   "components/_calendar.scss" => "components/_calendar.css",
+  "components/_figure.scss" => "components/_figure.css",
   "components/_subscribe.scss" => "components/_subscribe.css",
   "components/_published-dates.scss" => "components/_published-dates.css",
   "views/_calendars.scss" => "views/_calendars.css",

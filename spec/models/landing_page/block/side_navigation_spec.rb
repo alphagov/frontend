7aff887de65738f51feb41af7109c3f0cb797d7c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-31 14:15:16 +0000

    Use landing page navigation_groups for side navigation

--- spec/models/landing_page/block/side_navigation_spec.rb
+++ spec/models/landing_page/block/side_navigation_spec.rb
@@ -1,24 +1,17 @@
 RSpec.describe LandingPage::Block::SideNavigation do
   let(:blocks_hash) do
-    { "type" => "side_navigation" }
+    {
+      "type" => "side_navigation",
+      "navigation_group_id" => "Submenu",
+    }
   end
-  let(:subject) { described_class.new(blocks_hash, build(:landing_page)) }
-
-  before do
-    LandingPage::Block::SideNavigation.send(:remove_const, "LINKS_FILE_PATH")
-    LandingPage::Block::SideNavigation.const_set("LINKS_FILE_PATH", "spec/fixtures/landing_page/links/side_navigation.yaml")
-  end
-
-  after do
-    LandingPage::Block::SideNavigation.send(:remove_const, "LINKS_FILE_PATH")
-    LandingPage::Block::SideNavigation.const_set("LINKS_FILE_PATH", "lib/data/landing_page_content_items/links/side_navigation.yaml")
-  end
+  let(:subject) { described_class.new(blocks_hash, build(:landing_page_with_navigation_groups)) }
 
   describe "#links" do
     it "returns all of the navigation links from the specified navigation group" do
       expect(subject.links.count).to eq(2)
-      expect(subject.links.first["text"]).to eq("Landing Page")
-      expect(subject.links.first["href"]).to eq("/landing-page")
+      expect(subject.links.first["text"]).to eq("Child a")
+      expect(subject.links.first["href"]).to eq("/a")
     end
   end
 end

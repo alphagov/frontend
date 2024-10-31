64e1b6c4b61b67b7876d6ac795139d1cfff815af
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-31 12:59:57 +0000

    Simplify session_spec test

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -7,17 +7,9 @@ RSpec.describe "Sessions" do
   include GovukPersonalisation::TestHelpers::Features
 
   before do
+    # We add a fake content item pointed at homepage, which allows us to check we've
+    # been redirected
     stub_content_store_has_item("/", schema: "special_route", links: {})
-
-    # The redirect test needs a route to actually exist to redirect to
-    # (in reality this route is handled by another app), so we add a fake
-    # route and just point it at homepage. This allows us to check we've
-    # been redirected, then when the test is done we remove the test route.
-    Rails.application.routes.disable_clear_and_finalize = true
-
-    Rails.application.routes.prepend do
-      get "/email/subscriptions/account/confirm", to: "homepage#index"
-    end
   end
 
   after { Rails.application.reload_routes! }
@@ -31,7 +23,7 @@ RSpec.describe "Sessions" do
     end
 
     it "Logs the user in and send them to a redirect path if supplied" do
-      redirect_path = "/email/subscriptions/account/confirm?frequency=immediately&return_to_url=true&topic_id=some-page-with-notifications"
+      redirect_path = "/?frequency=immediately&return_to_url=true&topic_id=some-page-with-notifications"
       given_a_successful_login_attempt(redirect_path:)
       visit new_govuk_session_callback_path(code: "code", state: "state")
 

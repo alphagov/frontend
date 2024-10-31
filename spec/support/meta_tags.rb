75b6e31a56ec3bd62508d58a4147ec11d81346e6
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-31 12:58:19 +0000

    Shared tests for meta_tags with images
    - Needed for the tests added in the next commit.
    - Separate from the no-image tags test already here,
      as the shared examples for images break if run on a
      non-image-bearing document type like help pages
    
    Commit audit trail:
    - https://github.com/alphagov/government-frontend/blob/6523741691ddfb9551967311a3f79775e9403ecf/test/integration/meta_tags_test.rb

--- spec/support/meta_tags.rb
+++ spec/support/meta_tags.rb
@@ -18,3 +18,28 @@ RSpec.shared_examples "it has meta tags" do |schema, base_path|
     expect(page).to have_css("meta[property='og:title'][content='Zhe title']", visible: false)
   end
 end
+
+RSpec.shared_examples "it has meta tags for images" do |schema, base_path|
+  before do
+    example_doc = GovukSchemas::RandomExample.for_schema(frontend_schema: schema) do |random|
+      random["details"].merge!(
+        "image" => {
+          "url" => "https://example.org/image.jpg",
+          "alt_text" => "An accessible alt text",
+        },
+      )
+      random["links"]["available_translations"] = []
+      random
+    end
+
+    stub_content_store_has_item("#{base_path}/some-page", example_doc.to_json)
+  end
+
+  it "renders image tags" do
+    visit "#{base_path}/some-page"
+
+    expect(page).to have_css("meta[name='twitter:card'][content='summary_large_image']", visible: false)
+    expect(page).to have_css("meta[name='twitter:image'][content='https://example.org/image.jpg']", visible: false)
+    expect(page).to have_css("meta[property='og:image'][content='https://example.org/image.jpg']", visible: false)
+  end
+end

2dec2f8e7dd8b4aa02813a5d434c02323ce452f8
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-11-01 11:42:43 +0000

    Add alert statuses to TravelAdvice model
    Moves the alert_status method from the travel_advice_presenter in
    goverment-frontend.
    
    The method has been slightly refactored and also removes the
    presentation elements, specifically the translation lookup.
    
    Audit_trail:
    travel_advice_presenter.rb: https://github.com/alphagov/government-frontend/blob/c4bea9e232e929995c4fb5539951cadae8c740c8/app/presenters/travel_advice_presenter.rb#L77-L90
    travel_advice_presenter_test.rb: https://github.com/alphagov/government-frontend/blob/c4bea9e232e929995c4fb5539951cadae8c740c8/test/presenters/travel_advice_presenter_test.rb#L189-L197

--- app/models/travel_advice.rb
+++ app/models/travel_advice.rb
@@ -1,4 +1,11 @@
 class TravelAdvice < ContentItem
+  ALERT_STATUSES = %w[
+    avoid_all_but_essential_travel_to_parts
+    avoid_all_travel_to_parts
+    avoid_all_but_essential_travel_to_whole_country
+    avoid_all_travel_to_whole_country
+  ].freeze
+
   def country_name
     content_store_response["details"]["country"]["name"]
   end
@@ -14,4 +21,15 @@ class TravelAdvice < ContentItem
   def email_signup_link
     content_store_response["details"]["email_signup_link"]
   end
-end
+
+  # Deprecated feature
+  # Exists in travel advice publisher but isn't used by FCDO
+  # Feature included as it _could_ still be used
+  # Remove when alert status boxes no longer in travel advice publisher
+  def alert_status
+    alert_statuses = content_store_response["details"]["alert_status"]
+    return [] if alert_statuses.blank?
+
+    alert_statuses.filter_map { |alert| alert if ALERT_STATUSES.include?(alert) }
+  end
+

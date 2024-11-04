9aefe4657cda12bce8625fcad19fd655feadb827
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-04 13:03:07 +0000

    Update routing constraints to use ContentItemLoader
    - Now that ContentItemLoader is handling the caching, we can remove that layer of code / responsibility from the constraints.
    - ContentItemLoader either returns the adapter response or the error that it caught, so querying for those classes gives us a "was it or wasn't it an error" check.
    - We also simplify the spec tests slightly to use more idiomatic RSpec.

--- spec/unit/full_path_format_routing_constraint_spec.rb
+++ spec/unit/full_path_format_routing_constraint_spec.rb
@@ -2,43 +2,29 @@ RSpec.describe FullPathFormatRoutingConstraint do
   include ContentStoreHelpers
 
   describe "#matches?" do
-    context "when the content_store returns a document" do
-      let(:format) { "foo" }
-      let(:request) { double(path: "/format/routing/test", env: {}) }
+    let(:request) { double(path: "/format/routing/test", env: {}) }
 
+    context "when the content_store returns a document" do
       before do
-        stub_content_store_has_item("/format/routing/test", schema_name: format)
+        stub_content_store_has_item("/format/routing/test", schema_name: "foo")
       end
 
       it "returns true if format matches" do
-        expect(described_class.new(format).matches?(request)).to be true
+        expect(described_class.new("foo").matches?(request)).to be true
       end
 
       it "returns false if format doesn't match" do
         expect(described_class.new("not_the_format").matches?(request)).to be false
       end
-
-      it "sets the content item on the request object" do
-        described_class.new(format).matches?(request)
-
-        expect(request.env[:content_item].present?).to be true
-      end
     end
 
     context "when the content_store API call throws an error" do
-      let(:request) { double(path: "/format/routing/test", env: {}) }
-
       before do
         stub_content_store_does_not_have_item("/format/routing/test")
       end
 
       it "returns false" do
         expect(described_class.new("any_format").matches?(request)).to be false
-      end
-
-      it "sets an error on the request object" do
-        described_class.new("any_format").matches?(request)
-        expect(request.env[:content_item_error].present?).to be true
       end
     end
   end

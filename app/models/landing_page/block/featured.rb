a322ce63d5c97d05e9ca506bca95ca0cb5f85afc
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-05 15:31:48 +0000

    Use HasImageSet concern for featured block

--- app/models/landing_page/block/featured.rb
+++ app/models/landing_page/block/featured.rb
@@ -1,16 +1,12 @@
 module LandingPage::Block
-  FeaturedImageSources = Data.define(:desktop, :desktop_2x, :tablet, :tablet_2x, :mobile, :mobile_2x)
-  FeaturedImage = Data.define(:alt, :sources)
-
   class Featured < Base
-    attr_reader :image, :featured_content
+    include LandingPage::Block::Concerns::HasImageSet
+
+    attr_reader :featured_content
 
     def initialize(block_hash, landing_page)
       super
 
-      alt, sources = data.fetch("image").values_at("alt", "sources")
-      sources = FeaturedImageSources.new(**sources)
-      @image = FeaturedImage.new(alt:, sources:)
       @featured_content = data.dig("featured_content", "blocks")&.map { |subblock_hash| LandingPage::BlockFactory.build(subblock_hash, landing_page) }
     end
 

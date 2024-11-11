44bc3f69150836eac512e8607495759096b82290
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-11-11 15:45:28 +0000

    Add email_subscribe_unsubscribe_flash shared partial
    It is also used on consultation, detailed guide, document collection
    and publication pages.
    
    Commit audit trail:
    https://github.com/alphagov/government-frontend/blob/05d961f98ae8fec9ca0c37200aec96ee7bd1d590/app/views/shared/_email_subscribe_unsubscribe_flash.html.erb

--- spec/helpers/govuk_personalisation_helper_spec.rb
+++ spec/helpers/govuk_personalisation_helper_spec.rb
@@ -3,44 +3,44 @@ RSpec.describe GovukPersonalisationHelper do
 
   describe "#email_subscription_success_banner_heading" do
     it "displays the subscription success banner when the 'email-subscription-success' flash is present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-success])
-      expect(email_subscription_success_banner_heading(account_flash)).to eq("You’ve subscribed to emails about this page")
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-success])
+      expect(email_subscription_success_banner_heading(account_flash)).to eq(t("email.subscribe_title"))
     end
 
     it "displays the unsubscription success banner when the 'email-unsubscribe-success' flash is present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", %w[email-unsubscribe-success])
-      expect(email_subscription_success_banner_heading(account_flash)).to eq("You’ve unsubscribed from emails about this page")
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", %w[email-unsubscribe-success])
+      expect(email_subscription_success_banner_heading(account_flash)).to eq(t("email.unsubscribe_title"))
     end
 
     it "displays the subscription already subscribed banner when the 'email-subscription-already-subscribed' flash is present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-already-subscribed])
-      expect(email_subscription_success_banner_heading(account_flash)).to eq("You’re already getting emails about this page")
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-already-subscribed])
+      expect(email_subscription_success_banner_heading(account_flash)).to eq(t("email.already_subscribed_title"))
     end
 
     it "does not display a banner when the flash is not present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", [])
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", [])
       expect(email_subscription_success_banner_heading(account_flash)).to be_nil
     end
   end
 
   describe "#show_email_subscription_success_banner?" do
     it "returns true when the 'email-subscription-success' flash is present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-success])
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-success])
       expect(show_email_subscription_success_banner?(account_flash)).to be(true)
     end
 
     it "returns true when the 'email-unsubscribe-success' flash is present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", %w[email-unsubscribe-success])
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", %w[email-unsubscribe-success])
       expect(show_email_subscription_success_banner?(account_flash)).to be(true)
     end
 
     it "returns true when the 'email-subscription-already-subscribed' flash is present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-already-subscribed])
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", %w[email-subscription-already-subscribed])
       expect(show_email_subscription_success_banner?(account_flash)).to be(true)
     end
 
     it "returns false when a flash is not present" do
-      account_flash = request.headers["GOVUK-Account-Session"] = GovukPersonalisation::Flash.encode_session("session-id", [])
+      account_flash = GovukPersonalisation::Flash.encode_session("session-id", [])
       expect(show_email_subscription_success_banner?(account_flash)).to be(false)
     end
   end

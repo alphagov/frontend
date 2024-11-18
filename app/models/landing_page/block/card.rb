736a3729f7da34913c9bae7b6c3ef0106fbfef0b
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-11-18 18:48:48 +0000

    Add border top card variant
    For the 3 boxes that appear, I've assumed that only 3 would be displayed and therefore I've used the nth-child() selector to apply the necessary colours. I'm not sure if this is too brittle an approach, however, so happy to discuss.

--- app/models/landing_page/block/card.rb
+++ app/models/landing_page/block/card.rb
@@ -2,7 +2,7 @@ module LandingPage::Block
   CardImage = Data.define(:alt, :source)
 
   class Card < Base
-    attr_reader :image, :card_content, :href, :content, :link_cover_block
+    attr_reader :image, :card_content, :href, :content, :link_cover_block, :border_top
 
     def initialize(block_hash, landing_page)
       super
@@ -10,6 +10,7 @@ module LandingPage::Block
       @href = data["href"] || ""
       @content = data["content"] || ""
       @link_cover_block = data["link_cover_block"] || false
+      @border_top = data["border_top"] || false
 
       if data["image"].present?
         alt, source = data.fetch("image").values_at("alt", "source")

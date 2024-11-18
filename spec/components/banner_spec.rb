662a6e1636750379cfb95b95d3de9c4ad4cdd33d
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-11-18 12:21:15 +0000

    Add Banner component
    This has been directly copied over from government-frontend and
    the tests have been updated to use RSpec. It is also used on
    consultation pages.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/app/assets/stylesheets/components/_banner.scss > app/assets/stylesheets/components/_banner.scss
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/app/views/components/_banner.html.erb > app/views/components/_banner.html.erb
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/app/views/components/docs/banner.yml > app/views/components/docs/banner.yml
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/config/initializers/dartsass.rb#L4 > config/initializers/dartsass.rb
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/test/components/banner_test.rb > spec/components/banner_spec.rb

---
+++ spec/components/banner_spec.rb
@@ -0,0 +1,54 @@
+RSpec.describe "BannerComponent", type: :view do
+  def component_name
+    "banner"
+  end
+
+  it "fails to render a banner when no text is given" do
+    expect { render_component({}) }.to raise_error(ActionView::Template::Error)
+  end
+
+  it "renders a banner with text correctly" do
+    render_component(title: "Summary", text: "This was published under the 2010 to 2015 Conservative government")
+
+    expect(rendered).not_to have_css(".app-c-banner--aside")
+    expect(rendered).to have_css(".app-c-banner__desc", text: "This was published under the 2010 to 2015 Conservative government")
+  end
+
+  it "renders a banner with an aria label" do
+    render_component(title: "Summary", text: "Text")
+    expect(rendered).to have_css("section[aria-label]")
+  end
+
+  it "renders a banner with title and text correctly" do
+    render_component(title: "Summary", text: "This was published under the 2010 to 2015 Conservative government")
+
+    expect(rendered).not_to have_css(".app-c-banner--aside")
+    expect(rendered).to have_css(".app-c-banner__title", text: "Summary")
+    expect(rendered).to have_css(".app-c-banner__desc", text: "This was published under the 2010 to 2015 Conservative government")
+  end
+
+  it "renders a banner with title, text and aside correctly" do
+    render_component(
+      title: "Summary",
+      text: "This was published under the 2010 to 2015 Conservative government",
+      aside: "This consultation ran from 9:30am on 30 January 2017 to 5pm on 28 February 2017",
+    )
+
+    expect(rendered).to have_css(".app-c-banner--aside")
+    expect(rendered).to have_css(".app-c-banner__title", text: "Summary")
+    expect(rendered).to have_css(".app-c-banner__desc", text: "This was published under the 2010 to 2015 Conservative government")
+    expect(rendered).to have_css(".app-c-banner__desc", text: "This consultation ran from 9:30am on 30 January 2017 to 5pm on 28 February 2017")
+  end
+
+  it "renders a banner with GA4 tracking" do
+    render_component(
+      title: "Summary",
+      text: "This was published under the 2010 to 2015 Conservative government",
+      aside: "This consultation ran from 9:30am on 30 January 2017 to 5pm on 28 February 2017",
+    )
+
+    expect(rendered).to have_css(".app-c-banner--aside[data-module=ga4-link-tracker]")
+    expect(rendered).to have_css(".app-c-banner--aside[data-ga4-track-links-only]")
+    expect(rendered).to have_css(".app-c-banner--aside[data-ga4-link='{\"event_name\":\"navigation\",\"type\":\"callout\"}']")
+  end
+end

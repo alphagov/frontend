53b4a5cddadb64acf1db23672a0488b8aaa7e861
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-11-18 12:21:15 +0000

    Add Banner component
    This has been directly copied over from government-frontend and
    the tests have been updated to use RSpec. It is also used on
    consultation pages.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/app/assets/stylesheets/components/_banner.scss > app/assets/stylesheets/components/_banner.scss
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/app/views/components/_banner.html.erb > app/views/components/_banner.html.erb
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/app/views/components/docs/banner.yml > app/views/components/docs/banner.yml
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/config/initializers/dartsass.rb#L4 > config/initializers/dartsass.rb
    - https://github.com/alphagov/government-frontend/blob/842a887dc0c83311a9ead2daa08bd430ad0ac347/test/components/banner_test.rb > spec/components/banner_spec.rb

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,7 @@
 APP_STYLESHEETS = {
   "application.scss" => "application.css",
   "static-error-pages.scss" => "static-error-pages.css",
+  "components/_banner.scss" => "components/_banner.css",
   "components/_calendar.scss" => "components/_calendar.css",
   "components/_download-link.scss" => "components/_download-link.css",
   "components/_figure.scss" => "components/_figure.css",

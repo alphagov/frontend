182a8a466fe74bc139b8a6affedaf64f6d980aef
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-11-19 17:29:00 +0000

    (drop) Add published_dates_with_notification_button partial
    The Updatable concern that has been moved across from government-frontend
    no longer as a method called `published` available. The content_item
    model now has the `first_public_at` attribute directly available. See https://github.com/alphagov/frontend/pull/4372/commits/c5aa5099fae4448ca75b36bcfca32bc1ce9f7066
    
    Audit commit trail:
    - https://github.com/alphagov/government-frontend/blob/c229e1c71ffb8424c1e072fa99db33cecf13e199/app/views/shared/_published_dates_with_notification_button.html.erb > app/views/shared/_published_dates_with_notification_button.html.erb
    - https://github.com/alphagov/government-frontend/blob/c229e1c71ffb8424c1e072fa99db33cecf13e199/app/assets/stylesheets/views/_published-dates-button-group.scss > app/assets/stylesheets/views/_published-dates-button-group.scss

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -17,6 +17,7 @@ APP_STYLESHEETS = {
   "views/_popular_links.scss" => "views/_popular_links.css",
   "views/_publisher_metadata.scss" => "views/_publisher_metadata.css",
   "views/_sidebar-navigation.scss" => "views/_sidebar-navigation.css",
+  "views/_published-dates-button-group.scss" => "views/_published-dates-button-group.css",
   "views/_travel-advice.scss" => "views/_travel-advice.css",
   "views/_landing_page.scss" => "views/_landing_page.css",
   "views/_landing_page/block-error.scss" => "views/_landing_page/block-error.css",

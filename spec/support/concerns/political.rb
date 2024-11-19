fdacbe2c282e4ac7f44d1b18b1a276a0d5f57255
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-11-19 17:24:18 +0000

    Add Political concern and tests (to refactor)
    These methods are used in the `history_notice` partial, which is
    also included in a number of other different document types:
    - consultation
    - detailed_guide
    - html_publication
    - document_collection
    - news_article
    - publication
    - speech
    - statistical_data_set
    
    It might make more sense to move some of these methods into the
    content item model, and get the publishing_government information
    directly in the partial (_history_notice.html.erb)
    
    There were no existing tests and have now been added.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/dfecd13f3c24edd0ba24eef87b0d7fc149031ab1/app/presenters/content_item/political.rb > app/models/concerns/political.rb

---
+++ spec/support/concerns/political.rb
@@ -0,0 +1,20 @@
+RSpec.shared_examples "it can have historical government information" do |document_type, example_name|
+  before do
+    @content_store_response = GovukSchemas::Example.find(document_type, example_name:)
+  end
+
+  it "knows it is part of historical government if it is not the current government" do
+    @content_store_response["links"]["government"][0]["details"]["current"] = false
+    expect(described_class.new(@content_store_response).historically_political?).to eq true
+  end
+
+  it "knows it is not part of historical government if it is the current government" do
+    @content_store_response["links"]["government"][0]["details"]["current"] = true
+    expect(described_class.new(@content_store_response).historically_political?).to eq false
+  end
+
+  it "knows it's publishing government" do
+    expected_publishing_government = @content_store_response.dig("links", "government", 0, "title")
+    expect(described_class.new(@content_store_response).publishing_government).to eq(expected_publishing_government)
+  end
+end

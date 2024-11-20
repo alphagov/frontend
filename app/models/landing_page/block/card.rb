0f5e27fc6854d7e743a40cd7195fc461b70aac5c
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-11-20 10:34:30 +0000

    Add top borders

--- app/models/landing_page/block/card.rb
+++ app/models/landing_page/block/card.rb
@@ -2,7 +2,7 @@ module LandingPage::Block
   CardImage = Data.define(:alt, :source)
 
   class Card < Base
-    attr_reader :image, :card_content, :href, :content, :body_content, :link_cover_block, :border_top, :big_number
+    attr_reader :image, :card_content, :href, :content, :body_content, :link_cover_block, :big_number
 
     def initialize(block_hash, landing_page)
       super
@@ -11,7 +11,6 @@ module LandingPage::Block
       @content = data["content"] || ""
       @body_content = data["body_content"] || ""
       @link_cover_block = data["link_cover_block"] || false
-      @border_top = data["border_top"] || false
       @big_number = data["big_number"] || {}
       @big_number_preface = data.dig("big_number", "preface") || ""
       @big_number_number = data.dig("big_number", "number") || ""

a79454e011c09d3764735f19a856244ece65b3c9
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-11-20 18:00:22 +0000

    Populate document list block from taxon
    Takes the base path to a taxon, and populates the list with the most
    recent content tagged to the content. At the moment 5 content items are
    being returned, but this value can be updated.
    
    The hard-coded items have been left just in case, and to aid with
    testing.

--- spec/models/landing_page/block/document_list_spec.rb
+++ spec/models/landing_page/block/document_list_spec.rb
@@ -1,4 +1,9 @@
+require "gds_api/test_helpers/search"
+
 RSpec.describe LandingPage::Block::DocumentList do
+  include GdsApi::TestHelpers::Search
+  include ContentStoreHelpers
+
   describe "#items" do
     context "when the list is hard-coded" do
       let(:blocks_hash) do
@@ -16,5 +21,122 @@ RSpec.describe LandingPage::Block::DocumentList do
         expect(result.second).to eq(link: { text: "link 2", path: "/another-link" }, metadata: { document_type: "Press release", public_updated_at: "2023-01-01 10:24:00" })
       end
     end
+
+    context "when a taxon path is provided" do
+      let(:blocks_hash) do
+        { "type" => "document_list",
+          "taxon_base_path" => basic_taxon["base_path"] }
+      end
+
+      it "returns an array of items tagged to the taxon" do
+        stub_content_store_has_item(basic_taxon["base_path"], basic_taxon)
+        stub_taxon_search_results
+
+        expected_result = {
+          link: {
+            text: "Doc one",
+            path: "/doc-one",
+          },
+          metadata: {
+            document_type: "Press release",
+            public_updated_at: "2024-01-01 10:24:00",
+          },
+        }
+
+        result = described_class.new(blocks_hash, build(:landing_page)).items
+        expect(result.size).to eq 1
+        expect(result.first).to eq(expected_result)
+      end
+    end
+
+    context "when both the taxon base path and a hard-coded list is provided" do
+      let(:blocks_hash) do
+        { "type" => "document_list",
+          "taxon_base_path" => basic_taxon["base_path"],
+          "items" => [
+            { "text" => "link 1", "path" => "/a-link", "document_type" => "News article", "public_updated_at" => "2024-01-01 10:24:00" },
+            { "text" => "link 2", "path" => "/another-link", "document_type" => "Press release", "public_updated_at" => "2023-01-01 10:24:00" },
+          ] }
+      end
+
+      it "only returns an array of items tagged to the taxon" do
+        stub_content_store_has_item(basic_taxon["base_path"], basic_taxon)
+        stub_taxon_search_results
+
+        expected_result = {
+          link: {
+            text: "Doc one",
+            path: "/doc-one",
+          },
+          metadata: {
+            document_type: "Press release",
+            public_updated_at: "2024-01-01 10:24:00",
+          },
+        }
+
+        result = described_class.new(blocks_hash, build(:landing_page)).items
+        expect(result.size).to eq 1
+        expect(result.first).to eq(expected_result)
+      end
+
+      it "returns the hard-coded list if the taxon doesn't exist" do
+        stub_content_store_does_not_have_item(basic_taxon["base_path"])
+
+        expected_first_result = {
+          link: {
+            text: "link 1",
+            path: "/a-link",
+          },
+          metadata: {
+            document_type: "News article",
+            public_updated_at: "2024-01-01 10:24:00",
+          },
+        }
+
+        result = described_class.new(blocks_hash, build(:landing_page)).items
+        expect(result.size).to eq 2
+        expect(result.first).to eq(expected_first_result)
+      end
+
+      it "returns the hard-coded list if nothing is tagged to the taxon" do
+        stub_content_store_has_item(basic_taxon["base_path"], basic_taxon)
+        stub_any_search_to_return_no_results
+
+        expected_first_result = {
+          link: {
+            text: "link 1",
+            path: "/a-link",
+          },
+          metadata: {
+            document_type: "News article",
+            public_updated_at: "2024-01-01 10:24:00",
+          },
+        }
+
+        result = described_class.new(blocks_hash, build(:landing_page)).items
+        expect(result.size).to eq 2
+        expect(result.first).to eq(expected_first_result)
+      end
+    end
+  end
+
+  def stub_taxon_search_results
+    results = [
+      {
+        "title" => "Doc one",
+        "link" => "/doc-one",
+        "description" => "Doc one description",
+        "format" => "press_release",
+        "public_timestamp" => Time.zone.local(2024, 1, 1, 10, 24, 0),
+      },
+    ]
+
+    body = {
+      "results" => results,
+      "start" => "0",
+      "total" => results.size,
+    }
+
+    stub_any_search.to_return("body" => body.to_json)
   end
 end

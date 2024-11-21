7a26ff081337f6b07e0ed2d9afd885eb115cfa7c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-21 14:01:57 +0000

    Use the ContentItemLoader.for_request call in the routing constraints.
    - Because api_error_routing_constraint's test uses a double for the request, we add an empty env to it to allow the test to pass

--- lib/format_routing_constraint.rb
+++ lib/format_routing_constraint.rb
@@ -4,7 +4,7 @@ class FormatRoutingConstraint
   end
 
   def matches?(request)
-    content_item = ContentItemLoader.load(key(request))
+    content_item = ContentItemLoader.for_request(request).load(key(request))
     content_item.is_a?(GdsApi::Response) && content_item["schema_name"] == @format
   end
 

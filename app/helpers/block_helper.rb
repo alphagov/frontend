873c8a9e9aa5c33af8798d63698d3b2334d5283c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-02 11:52:25 +0000

    tidy: remove unused tabs block and supporting method

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -5,14 +5,4 @@ module BlockHelper
     Rails.logger.warn("Missing template for block #{block.type}")
     ""
   end
-
-  def tab_items_to_component_format(tab_items)
-    tab_items.map do |ti|
-      {
-        id: ti["id"],
-        label: ti["label"],
-        content: sanitize("<p class=\"govuk-body-m\">#{ti['content']}</p>"),
-      }
-    end
-  end
 end

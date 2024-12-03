593642ffae9d6d0ff24dbcdea23888e916ec6b5e
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-03 11:44:16 +0000

    Add individual error page templates
    Audit trail:
    
    - https://github.com/alphagov/static/tree/bb53325994ebb23476f577f8dbb8bc78d04ceac7/app/views/root

--- app/controllers/static_error_pages_controller.rb
+++ app/controllers/static_error_pages_controller.rb
@@ -3,7 +3,22 @@ class StaticErrorPagesController < ApplicationController
     response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
   end
 
-  ERROR_CODES = %w[].freeze
+  ERROR_CODES = %w[
+    400
+    401
+    403
+    404
+    405
+    406
+    410
+    422
+    429
+    500
+    501
+    502
+    503
+    504
+  ].freeze
 
   def show
     if ERROR_CODES.include?(params[:error_code])

137637f3e5967385681295c8184c299875f77718
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-12-12 17:56:32 +0000

    Update SimpleSmartAnswerPresenter to accept a content item object
    Also removes the modelling code from the presenter, and updates the
    `start_button_text` method to use attributes from the model.

--- spec/unit/presenters/simple_smart_answer_presenter_spec.rb
+++ spec/unit/presenters/simple_smart_answer_presenter_spec.rb
@@ -3,31 +3,38 @@ RSpec.describe SimpleSmartAnswerPresenter do
     described_class.new(content_item.deep_stringify_keys!)
   end
 
+  before do
+    @content_store_response = GovukSchemas::Example.find("simple_smart_answer", example_name: "simple-smart-answer")
+  end
+
   context "locale is 'cy'" do
     before { I18n.locale = :cy }
     after { I18n.locale = :en }
 
     context "start_button_text is 'Start now'" do
       it "returns Welsh translation 'Dechrau nawr'" do
-        @item = { details: { start_button_text: "Start now" } }
+        @content_store_response["details"]["start_button_text"] = "Start now"
+        content_item = SimpleSmartAnswer.new(@content_store_response)
 
-        expect(subject(@item).start_button_text).to eq("Dechrau nawr")
+        expect(described_class.new(content_item).start_button_text).to eq("Dechrau nawr")
       end
     end
 
     context "start_button_text is 'Continue'" do
       it "returns Welsh translation 'Parhau'" do
-        @item = { details: { start_button_text: "Continue" } }
+        @content_store_response["details"]["start_button_text"] = "Continue"
+        content_item = SimpleSmartAnswer.new(@content_store_response)
 
-        expect(subject(@item).start_button_text).to eq("Parhau")
+        expect(described_class.new(content_item).start_button_text).to eq("Parhau")
       end
     end
 
     context "start_button_text is explicitly set" do
       it "returns the set value" do
-        @item = { details: { start_button_text: "Go for it" } }
+        @content_store_response["details"]["start_button_text"] = "Go for it"
+        content_item = SimpleSmartAnswer.new(@content_store_response)
 
-        expect(subject(@item).start_button_text).to eq("Go for it")
+        expect(described_class.new(content_item).start_button_text).to eq("Go for it")
       end
     end
   end

2ac0c68ba7ee6362be51d576aad90aa0b770619c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-12 16:18:14 +0000

    lint: fix spec/helpers
    - Here the ?query=true parameter was added because otherwise
      there wasn't actually a query string to strip, and rubocop identified
      it as being the identical to the test on line 39.

--- spec/helpers/block_helper_spec.rb
+++ spec/helpers/block_helper_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe BlockHelper do
 
   describe "#render_block" do
     it "returns an empty string when a partial template doesn't exist" do
-      block = double(type: "not_a_block")
+      block = instance_double(LandingPage::Block::Base, type: "not_a_block")
       expect(render_block(block)).to be_empty
     end
   end

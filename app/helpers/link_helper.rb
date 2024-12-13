3b460d8eff6097bf91790eef71a1ece2a28e1772
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2024-12-13 17:12:22 +0000

    Consolidates the links and link helpers
    These helpers were created at similar times in different PRs but should
    be one file.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,4 +1,12 @@
 module LinkHelper
+  def feed_link(base_path)
+    "#{base_path}.atom"
+  end
+
+  def print_link(base_path)
+    "#{base_path}/print"
+  end
+
   def govuk_styled_link(text, path: nil, inverse: false)
     return text if path.blank?
 
--- app/helpers/links_helper.rb
+++
@@ -1,9 +0,0 @@
-module LinksHelper
-  def feed_link(base_path)
-    "#{base_path}.atom"
-  end
-
-  def print_link(base_path)
-    "#{base_path}/print"
-  end
-end

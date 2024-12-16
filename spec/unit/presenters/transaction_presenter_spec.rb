b6a04e26e3286c3d15c6a1b7264adafa4450c76b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-16 16:26:11 +0000

    lint: fix spec/unit/presenters

--- spec/unit/presenters/transaction_presenter_spec.rb
+++ spec/unit/presenters/transaction_presenter_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe TransactionPresenter do
 
   let(:content_store_response) { GovukSchemas::Example.find("transaction", example_name: "transaction") }
 
-  context "start_button_text is 'Start now'" do
+  context "when start_button_text is 'Start now'" do
     describe "#start_button_text" do
       it "shows the start_button_text" do
         content_store_response["details"]["start_button_text"] = "Start now"
@@ -15,7 +15,7 @@ RSpec.describe TransactionPresenter do
     end
   end
 
-  context "start_button_text is 'Sign in'" do
+  context "when start_button_text is 'Sign in'" do
     describe "#start_button_text" do
       it "shows the custom start button text" do
         content_store_response["details"]["start_button_text"] = "Sign in"
@@ -25,11 +25,11 @@ RSpec.describe TransactionPresenter do
     end
   end
 
-  context "locale is 'cy'" do
+  context "when locale is 'cy'" do
     before { I18n.locale = :cy }
     after { I18n.locale = :en }
 
-    context "start_button_text is 'Start now'" do
+    context "and start_button_text is 'Start now'" do
       it "returns Welsh translation 'Dechrau nawr'" do
         content_store_response["details"]["start_button_text"] = "Start now"
         content_item = Transaction.new(content_store_response)
@@ -38,7 +38,7 @@ RSpec.describe TransactionPresenter do
       end
     end
 
-    context "start_button_text is 'Sign in'" do
+    context "and start_button_text is 'Sign in'" do
       it "returns Welsh translation 'Mewngofnodi'" do
         content_store_response["details"]["start_button_text"] = "Sign in"
         content_item = Transaction.new(content_store_response)

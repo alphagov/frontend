5cace6cd4732b17856be4503f2f081b3aedb28d4
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-16 10:46:23 +0000

    lint: fix spec/support

--- spec/support/concerns/withdrawal.rb
+++ spec/support/concerns/withdrawal.rb
@@ -1,9 +1,7 @@
 RSpec.shared_examples "it can be withdrawn" do |document_type, example_name|
-  before do
-    @content_store_response = GovukSchemas::Example.find(document_type, example_name:)
-  end
+  let(:content_store_response) { GovukSchemas::Example.find(document_type, example_name:) }
 
   it "knows it's withdrawn" do
-    expect(described_class.new(@content_store_response).withdrawn?).to be true
+    expect(described_class.new(content_store_response).withdrawn?).to be true
   end
 end

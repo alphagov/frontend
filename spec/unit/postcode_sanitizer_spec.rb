eee825171a5934fee42e7806b6da4939c2b26432
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-16 14:18:30 +0000

    lint: fix spec/unit
    - One lint (RSpec/SubjectStub) is disabled in two classes rather than being fixed,
      because fixing it really requires a rethink about the actual classes, and
      this set of commits is scoped to just making the lint pass. We should
      revisit how these two classes work later (especially the call out to mtime
      in ics_renderer)

--- spec/unit/postcode_sanitizer_spec.rb
+++ spec/unit/postcode_sanitizer_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe PostcodeSanitizer do
-  context "postcodes are sanitized before being sent to LocationsApi" do
+  describe "#sanitize" do
     it "strips trailing spaces from entered postcodes" do
       expect(described_class.sanitize("WC2B 6NH ")).to eq("WC2B 6NH")
     end
@@ -11,11 +11,11 @@ RSpec.describe PostcodeSanitizer do
     it "transposes O/0 and I/1 if necessary" do
       expect(described_class.sanitize("WIA OAA")).to eq("W1A 0AA")
     end
-  end
 
-  context "if the postcode parameter is nil or an empty string" do
-    it "returns nil and does not try to perform sanitization" do
-      expect(described_class.sanitize("")).to be_nil
+    context "when the postcode parameter is nil or an empty string" do
+      it "returns nil and does not try to perform sanitization" do
+        expect(described_class.sanitize("")).to be_nil
+      end
     end
   end
 end

00692de35c81c10f263b1fd5976d6511fb83f112
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-16 11:59:15 +0000

    lint: fix spec/unit/simple_smart_answers

--- spec/unit/simple_smart_answers/flow_spec.rb
+++ spec/unit/simple_smart_answers/flow_spec.rb
@@ -1,141 +1,125 @@
-module SimpleSmartAnswers
-  RSpec.describe Flow do
-    context "finding nodes" do
-      before do
-        @nodes = [
-          {
-            "kind" => "question",
-            "slug" => "question-1",
-            "title" => "Question 1",
-            "body" => "<p>This is question 1</p>",
-            "options" => [],
-          },
-          {
-            "kind" => "question",
-            "slug" => "question-2",
-            "title" => "Question 2",
-            "body" => "<p>This is question 2</p>",
-            "options" => [],
-          },
-          {
-            "kind" => "outcome",
-            "slug" => "outcome-1",
-            "title" => "Outcome 1",
-            "body" => "<p>This is outcome 1</p>",
-          },
-          {
-            "kind" => "outcome",
-            "slug" => "outcome-1",
-            "title" => "Outcome 1",
-            "body" => "<p>This is outcome 1</p>",
-          },
-        ]
-        @flow = described_class.new(@nodes)
-      end
+RSpec.describe SimpleSmartAnswers::Flow do
+  subject(:flow) { described_class.new(nodes) }
 
-      it "returns the node matching the slug" do
-        node = @flow.node_for_slug("question-2")
-
-        expect(node).to be_a(Node)
-        expect(node.title).to eq("Question 2")
-      end
-
-      it "returns nil if none match" do
-        expect(@flow.node_for_slug("question-3")).to be_nil
-      end
-
-      it "returns the first node as the start_node" do
-        node = @flow.start_node
-
-        expect(node).to be_a(Node)
-        expect(node.title).to eq("Question 1")
-      end
+  context "when finding nodes" do
+    let(:nodes) do
+      [
+        {
+          "kind" => "question",
+          "slug" => "question-1",
+          "title" => "Question 1",
+          "body" => "<p>This is question 1</p>",
+          "options" => [],
+        },
+        {
+          "kind" => "question",
+          "slug" => "question-2",
+          "title" => "Question 2",
+          "body" => "<p>This is question 2</p>",
+          "options" => [],
+        },
+      ]
     end
 
-    describe "#state_for_responses" do
-      before do
-        @flow =
-          described_class.new(
-            [
-              {
-                "kind" => "question",
-                "slug" => "question-1",
-                "title" => "Question 1",
-                "body" => "<p>This is question 1</p>",
-                "options" => [
-                  {
-                    "label" => "Option 1",
-                    "slug" => "option-1",
-                    "next_node" => "question-2",
-                  },
-                  {
-                    "label" => "Option 2",
-                    "slug" => "option-2",
-                    "next_node" => "outcome-1",
-                  },
-                  {
-                    "label" => "Option 3",
-                    "slug" => "option-3",
-                    "next_node" => "outcome-2",
-                  },
-                ],
-              },
-              {
-                "kind" => "question",
-                "slug" => "question-2",
-                "title" => "Question 2",
-                "body" => "<p>This is question 2</p>",
-                "options" => [
-                  {
-                    "label" => "Option 1",
-                    "slug" => "option-1",
-                    "next_node" => "outcome-1",
-                  },
-                  {
-                    "label" => "Option 2",
-                    "slug" => "option-2",
-                    "next_node" => "outcome-2",
-                  },
-                ],
-              },
-              {
-                "kind" => "outcome",
-                "slug" => "outcome-1",
-                "title" => "Outcome 1",
-                "body" => "<p>This is outcome 1</p>",
-              },
-              {
-                "kind" => "outcome",
-                "slug" => "outcome-2",
-                "title" => "Outcome 2",
-                "body" => "<p>This is outcome 2</p>",
-              },
-            ],
-          )
-      end
+    it "returns the node matching the slug" do
+      node = flow.node_for_slug("question-2")
 
-      it "returns a state for the given responses" do
-        state = @flow.state_for_responses(%w[option-1])
+      expect(node).to be_a(SimpleSmartAnswers::Node)
+      expect(node.title).to eq("Question 2")
+    end
 
-        expect(state.current_node.slug).to eq("question-2")
-        expect(state.current_question_number).to eq(2)
-        expect(state.completed_questions.size).to eq(1)
+    it "returns nil if none match" do
+      expect(flow.node_for_slug("question-3")).to be_nil
+    end
 
-        answer1 = state.completed_questions.first
-        expect(answer1.label).to eq("Option 1")
-        expect(answer1.slug).to eq("option-1")
-        expect(answer1.question.title).to eq("Question 1")
-      end
+    it "returns the first node as the start_node" do
+      node = flow.start_node
 
-      it "stops processing and set error flag on invalid response" do
-        state = @flow.state_for_responses(%w[option-1 fooey option-2])
+      expect(node).to be_a(SimpleSmartAnswers::Node)
+      expect(node.title).to eq("Question 1")
+    end
+  end
 
-        expect(state.error?).to be true
-        expect(state.current_node.slug).to eq("question-2")
-        expect(state.current_question_number).to eq(2)
-        expect(state.completed_questions.size).to eq(1)
-        expect(state.completed_questions.first.slug).to eq("option-1")
-      end
+  describe "#state_for_responses" do
+    let(:nodes) do
+      [
+        {
+          "kind" => "question",
+          "slug" => "question-1",
+          "title" => "Question 1",
+          "body" => "<p>This is question 1</p>",
+          "options" => [
+            {
+              "label" => "Option 1",
+              "slug" => "option-1",
+              "next_node" => "question-2",
+            },
+            {
+              "label" => "Option 2",
+              "slug" => "option-2",
+              "next_node" => "outcome-1",
+            },
+            {
+              "label" => "Option 3",
+              "slug" => "option-3",
+              "next_node" => "outcome-2",
+            },
+          ],
+        },
+        {
+          "kind" => "question",
+          "slug" => "question-2",
+          "title" => "Question 2",
+          "body" => "<p>This is question 2</p>",
+          "options" => [
+            {
+              "label" => "Option 1",
+              "slug" => "option-1",
+              "next_node" => "outcome-1",
+            },
+            {
+              "label" => "Option 2",
+              "slug" => "option-2",
+              "next_node" => "outcome-2",
+            },
+          ],
+        },
+        {
+          "kind" => "outcome",
+          "slug" => "outcome-1",
+          "title" => "Outcome 1",
+          "body" => "<p>This is outcome 1</p>",
+        },
+        {
+          "kind" => "outcome",
+          "slug" => "outcome-2",
+          "title" => "Outcome 2",
+          "body" => "<p>This is outcome 2</p>",
+        },
+      ]
+    end
+
+    it "returns a state for the given responses" do
+      state = flow.state_for_responses(%w[option-1])
+
+      expect(state.current_node.slug).to eq("question-2")
+      expect(state.current_question_number).to eq(2)
+      expect(state.completed_questions.size).to eq(1)
+
+      answer1 = state.completed_questions.first
+      expect(answer1.label).to eq("Option 1")
+      expect(answer1.slug).to eq("option-1")
+      expect(answer1.question.title).to eq("Question 1")
+    end
+
+    it "stops processing and set error flag on invalid response" do
+      state = flow.state_for_responses(%w[option-1 fooey option-2])
+
+      expect(state.error?).to be true
+      expect(state.current_node.slug).to eq("question-2")
+      expect(state.current_question_number).to eq(2)
+      expect(state.completed_questions.size).to eq(1)
+      expect(state.completed_questions.first.slug).to eq("option-1")
     end
   end
 end
--- spec/unit/simple_smart_answers/node_spec.rb
+++ spec/unit/simple_smart_answers/node_spec.rb
@@ -1,114 +1,110 @@
-module SimpleSmartAnswers
-  RSpec.describe Node do
-    it "has attribute accessors for basic fields" do
-      node =
-        described_class.new(
-          :a_flow,
-          "kind" => "question",
-          "slug" => "question-1",
-          "title" => "Question 1",
-          "body" => "<p>This is question 1</p>",
-          "options" => [],
-        )
-      expect(node.kind).to eq("question")
-      expect(node.title).to eq("Question 1")
-      expect(node.slug).to eq("question-1")
-      expect(node.body).to eq("<p>This is question 1</p>")
+RSpec.describe SimpleSmartAnswers::Node do
+  let(:flow) { instance_double(SimpleSmartAnswers::Flow, node_for_slug: :a_node) }
+
+  it "has attribute accessors for basic fields" do
+    node =
+      described_class.new(
+        :a_flow,
+        "kind" => "question",
+        "slug" => "question-1",
+        "title" => "Question 1",
+        "body" => "<p>This is question 1</p>",
+        "options" => [],
+      )
+    expect(node.kind).to eq("question")
+    expect(node.title).to eq("Question 1")
+    expect(node.slug).to eq("question-1")
+    expect(node.body).to eq("<p>This is question 1</p>")
+  end
+
+  describe "#outcome?" do
+    it "is true for an outcome node" do
+      expect(described_class.new(:a_flow, "kind" => "outcome").outcome?).to be true
     end
 
-    describe "#outcome?" do
-      it "is true for an outcome node" do
-        expect(described_class.new(:a_flow, "kind" => "outcome").outcome?).to be true
-      end
+    it "is false for a question node" do
+      expect(described_class.new(:a_flow, "kind" => "question").outcome?).to be false
+    end
+  end
 
-      it "is false for a question node" do
-        expect(described_class.new(:a_flow, "kind" => "question").outcome?).to be false
-      end
+  describe "#options" do
+    let(:node) do
+      described_class.new(
+        flow,
+        "kind" => "question",
+        "slug" => "question-1",
+        "options" => [
+          {
+            "label" => "Option 1",
+            "slug" => "option-1",
+            "next_node" => "question-2",
+          },
+          {
+            "label" => "Option 3",
+            "slug" => "option-3",
+            "next_node" => "question-3",
+          },
+          {
+            "label" => "Option 2",
+            "slug" => "option-2",
+            "next_node" => "question-2",
+          },
+        ],
+      )
     end
 
-    describe "#options" do
-      before do
-        @flow = instance_double(Flow, node_for_slug: :a_node)
-        @node =
-          described_class.new(
-            @flow,
-            "kind" => "question",
-            "slug" => "question-1",
-            "options" => [
-              {
-                "label" => "Option 1",
-                "slug" => "option-1",
-                "next_node" => "question-2",
-              },
-              {
-                "label" => "Option 3",
-                "slug" => "option-3",
-                "next_node" => "question-3",
-              },
-              {
-                "label" => "Option 2",
-                "slug" => "option-2",
-                "next_node" => "question-2",
-              },
-            ],
-          )
-      end
+    it "constructs options" do
+      expect(node.options.map(&:label)).to eq(["Option 1", "Option 3", "Option 2"])
+      expect(node.options.map(&:slug)).to eq(%w[option-1 option-3 option-2])
+      expect(node.options.map(&:next_node_slug)).to eq(%w[question-2 question-3 question-2])
+      expect(node.options.first.question).to eq(node)
+    end
+  end
 
-      it "constructs options" do
-        expect(@node.options.map(&:label)).to eq(["Option 1", "Option 3", "Option 2"])
-        expect(@node.options.map(&:slug)).to eq(%w[option-1 option-3 option-2])
-        expect(@node.options.map(&:next_node_slug)).to eq(%w[question-2 question-3 question-2])
-        expect(@node.options.first.question).to eq(@node)
-      end
+  describe "#process_response" do
+    let(:node) do
+      described_class.new(
+        flow,
+        "kind" => "question",
+        "slug" => "question-1",
+        "options" => [
+          {
+            "label" => "Option 1",
+            "slug" => "option-1",
+            "next_node" => "question-2",
+          },
+          {
+            "label" => "Option 3",
+            "slug" => "option-3",
+            "next_node" => "question-3",
+          },
+          {
+            "label" => "Option 2",
+            "slug" => "option-2",
+            "next_node" => "question-2",
+          },
+        ],
+      )
     end
 
-    describe "#process_response" do
-      before do
-        @flow = instance_double(Flow, node_for_slug: :a_node)
-        @node =
-          described_class.new(
-            @flow,
-            "kind" => "question",
-            "slug" => "question-1",
-            "options" => [
-              {
-                "label" => "Option 1",
-                "slug" => "option-1",
-                "next_node" => "question-2",
-              },
-              {
-                "label" => "Option 3",
-                "slug" => "option-3",
-                "next_node" => "question-3",
-              },
-              {
-                "label" => "Option 2",
-                "slug" => "option-2",
-                "next_node" => "question-2",
-              },
-            ],
-          )
-      end
+    it "returns the matching option instance" do
+      expect(node.process_response("option-2").label).to eq("Option 2")
+    end
 
-      it "returns the matching option instance" do
-        expect(@node.process_response("option-2").label).to eq("Option 2")
-      end
+    it "populates the next_node on the returned option" do
+      allow(flow).to receive(:node_for_slug).with("question-2").and_return(:question_2_node)
 
-      it "populates the next_node on the returned option" do
-        allow(@flow).to receive(:node_for_slug).with("question-2").and_return(:question_2_node)
+      expect(node.process_response("option-2").next_node).to eq(:question_2_node)
+    end
 
-        expect(@node.process_response("option-2").next_node).to eq(:question_2_node)
-      end
+    it "raises InvalidResponse if no option matches" do
+      expect { node.process_response("option-4") }.to raise_error(SimpleSmartAnswers::InvalidResponse)
+    end
 
-      it "raises InvalidResponse if no option matches" do
-        expect { @node.process_response("option-4") }.to raise_error(InvalidResponse)
-      end
+    it "raises InvalidResponse if option points to a non-existent node" do
+      allow(flow).to receive(:node_for_slug).with("question-2").and_return(nil)
 
-      it "raises InvalidResponse if option points to a non-existent node" do
-        allow(@flow).to receive(:node_for_slug).with("question-2").and_return(nil)
-
-        expect { @node.process_response("option-2") }.to raise_error(InvalidResponse)
-      end
+      expect { node.process_response("option-2") }.to raise_error(SimpleSmartAnswers::InvalidResponse)
     end
   end
 end

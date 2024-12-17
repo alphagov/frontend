393e8069815e25370780037eabe068ce57c87f5b
Matt Hill <matt.hill@digital.cabinet-office.gov.uk>
2024-12-17 10:42:16 +0000

    Remove app-b prefixes from blocks

---
+++ app/assets/javascripts/modules/main-navigation.js
@@ -0,0 +1,27 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function (Modules) {
+  'use strict'
+
+  function MainNavigation (module) {
+    this.module = module
+    this.module.button = this.module.querySelector('button')
+    this.module.navContainer = this.module.querySelector('.js-main-nav__nav-container')
+    this.module.buttonContainer = this.module.querySelector('.js-main-nav__button-container')
+    this.module.button.classList.remove('main-nav__button--no-js')
+  }
+
+  MainNavigation.prototype.init = function () {
+    this.module.button.addEventListener('click', this.toggleMenu.bind(this))
+  }
+
+  MainNavigation.prototype.toggleMenu = function () {
+    var ariaExpanded = this.module.button.getAttribute('aria-expanded') === 'true'
+    this.module.navContainer.classList.toggle('main-nav__nav-container--js-hidden')
+    this.module.button.setAttribute('aria-expanded', `${!ariaExpanded}`)
+    this.module.buttonContainer.classList.toggle('main-nav__button-container--collapsed')
+  }
+
+  Modules.MainNavigation = MainNavigation
+})(window.GOVUK.Modules)

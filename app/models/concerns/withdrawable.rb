914bbccaaea662f63371b4d9478a87ebb96b12b0
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2024-12-20 14:33:18 +0000

    Update withdrawable concern

--- app/models/concerns/withdrawable.rb
+++ app/models/concerns/withdrawable.rb
@@ -1,46 +1,23 @@
 module Withdrawable
-    extend ActiveSupport::Concern
-  
-    included do
-      def withdrawn?
-        withdrawal_notice.present?
-      end
-  
-      def withdrawal_notice
-        content_store_hash["withdrawn_notice"]
-      end
+  extend ActiveSupport::Concern
 
-      def withdrawal_notice_component
-        if withdrawn?
-          {
-            title: withdrawal_notice_title,
-            description_govspeak: withdrawal_notice["explanation"]&.html_safe,
-            time: withdrawal_notice_time,
-            lang: I18n.locale.to_s == "en" ? false : "en",
-          }
-        end
-      end
-    end
-  end  
-
-  private  
-
-    def withdrawal_notice_title
-      "This #{withdrawal_notice_context.downcase} was withdrawn on #{withdrawal_notice_time}".html_safe
+  included do
+    def withdrawn?
+      withdrawal_notice.present?
     end
 
-    def withdrawal_notice_context
-      I18n.t("content_item.schema_name.#{schema_name}", count: 1, locale: :en)
+    def withdrawn_at
+      withdrawal_notice["withdrawn_at"]
     end
 
-    def withdrawal_notice_time
-      view_context.tag.time(
-        english_display_date(withdrawal_notice["withdrawn_at"]),
-        datetime: withdrawal_notice["withdrawn_at"],
-      )
-    end
-
-    def english_display_date(timestamp, format = "%-d %B %Y")
-      I18n.l(Time.zone.parse(timestamp), format:, locale: :en) if timestamp
+    def withdrawn_explanation
+      withdrawal_notice["explanation"]
     end
   end
+
+private
+
+  def withdrawal_notice
+    content_store_response["withdrawn_notice"]
+  end
+end

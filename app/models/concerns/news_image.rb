f74911abfd4b6205f16ceff1882686cf77ee5402
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2024-12-20 10:30:50 +0000

    Update news image concern

--- app/models/concerns/news_image.rb
+++ app/models/concerns/news_image.rb
@@ -8,13 +8,13 @@ module NewsImage
 private
 
   def default_news_image
-    organisation = content_item.dig("links", "primary_publishing_organisation")
+    organisation = content_store_hash.dig("links", "primary_publishing_organisation")
     organisation[0].dig("details", "default_news_image") if organisation.present?
   end
 
   def placeholder_image
     # this image has been uploaded to asset-manager
-    if content_item["document_type"] == "world_news_story"
+    if content_store_hash["document_type"] == "world_news_story"
       { "url" => "https://assets.publishing.service.gov.uk/media/5e985599d3bf7f3fc943bbd8/UK_government_logo.jpg" }
     else
       { "url" => "https://assets.publishing.service.gov.uk/media/5e59279b86650c53b2cefbfe/placeholder.jpg" }

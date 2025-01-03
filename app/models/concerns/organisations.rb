3b7e6028730582e9a49cbf1e2b8364e7d01a9fd8
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-01-03 10:45:23 +0000

    Modify Views and  Speech model for rendering the publisher metadata and its related concerns
    - Refactor to handle the speaker link along with the organisations for speeches in Speech model
    - Modify Views with publisher_metadata content
      Reference commit: 338bfe1
    - Add meta description in headers

---
+++ app/models/concerns/organisations.rb
@@ -0,0 +1,27 @@
+module Organisations
+  extend ActiveSupport::Concern
+
+  included do
+    def linkable_organisations
+      organisation_links_with_emphasis
+    end
+  end
+
+private
+
+  def organisation_links_with_emphasis
+    expanded_organisations.sort_by { |link| emphasised?(link) ? -1 : 1 }
+  end
+
+  def expanded_organisations
+    content_store_hash.dig("links", "organisations") || []
+  end
+
+  def emphasised?(link)
+    link["content_id"].in?(emphasised_organisations)
+  end
+
+  def emphasised_organisations
+    content_store_hash.dig("details", "emphasised_organisations") || []
+  end
+end

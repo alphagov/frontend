d1da4fb27544aacccd4cfba7ad87f86b10c4937b
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-01-03 10:45:23 +0000

    Modify Views and  Speech model for rendering the publisher metadata and its related concerns
    - Refactor to handle the speaker link along with the organisations for speeches in Speech model
    - Modify Views with publisher_metadata content
      Reference commit: 338bfe1

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -1,7 +1,20 @@
-class Speech < ContentItem  
+class Speech < ContentItem
   include Organisations
   include Political
   include Updatable
-  include Metadata
   include NewsImage
+
+  def related_entities
+    entities = linkable_organisations + (speaker || [])
+    entities += [speaker_without_profile] if speaker_without_profile
+    entities.uniq
+  end
+
+  def speaker
+    content_store_hash.dig("links", "speaker")
+  end
+
+  def speaker_without_profile
+    content_store_hash.dig("details", "speaker_without_profile")
+  end
 end

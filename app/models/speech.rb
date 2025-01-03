42ebd5652ac1a8deda52d00039ec514ffe0ede29
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-01-03 10:45:23 +0000

    Modify Views and  Speech model for rendering the publisher metadata and its related concerns

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -2,6 +2,14 @@ class Speech < ContentItem
   include Organisations
   include Political
   include Updatable
-  include Metadata
-  include NewsImage
+  include NewsImage  
+
+  def related_entities
+    linkable_organisations + speaker
+  end
+
+  def speaker
+    content_store_hash.dig("links", "speaker")
+  end
+
 end

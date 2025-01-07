ddea85e214600dc03c0c9cdf6a6b22b2e91c21cc
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-01-07 15:17:36 +0000

    Add Speech model tests
     - Add tests for speech model and its related concerns

---
+++ spec/models/speech_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Speech do
+  let(:content_store_response) { GovukSchemas::Example.find("speech", example_name: "speech") }
+
+  it_behaves_like "it has news image", "speech"
+  it_behaves_like "it has historical government information", "speech", "speech"
+  it_behaves_like "it has updates", "speech", "speech"
+  it_behaves_like "it can be withdrawn", "speech", "withdrawn-speech"
+
+  describe "#related_entities" do
+    it "returns the linkable organisations as well as speaker" do
+      organisations = content_store_response["links"]["organisations"].first
+      expect(described_class.new(content_store_response).speaker).to eq(content_store_response["links"]["speaker"])
+      expect(described_class.new(content_store_response).related_entities).to include(organisations)
+    end
+
+    it "includes speaker without profile" do
+      content_store_response = GovukSchemas::Example.find("speech", example_name: "speech-speaker-without-profile")
+      expect(described_class.new(content_store_response).related_entities).to include(content_store_response["details"]["speaker_without_profile"])
+    end
+  end
+end

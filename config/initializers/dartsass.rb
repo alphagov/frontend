6aeba9ad2007cf691c65c8915b09ed839961caf9
Matt Hill <matt.hill@digital.cabinet-office.gov.uk>
2025-01-08 16:23:01 +0000

    Add block level styles for images and remove govuk overrides

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -23,6 +23,7 @@ APP_STYLESHEETS = {
   "views/_landing_page/card.scss" => "views/_landing_page/card.css",
   "views/_landing_page/columns_layout.scss" => "views/_landing_page/columns_layout.css",
   "views/_landing_page/hero.scss" => "views/_landing_page/hero.css",
+  "views/_landing_page/image.scss" => "views/_landing_page/image.css",
   "views/_landing_page/featured.scss" => "views/_landing_page/featured.css",
   "views/_landing_page/main-navigation.scss" => "views/_landing_page/main-navigation.css",
   "views/_landing_page/quote.scss" => "views/_landing_page/quote.css",

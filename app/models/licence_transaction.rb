c83096459bbab65d8792f135fc7b060df191edab
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-01-08 11:39:29 +0000

    Add LicenceTransaction model
    We currently have two ways to model content items and two content item
    presenters.
    
    * ContentItem models the response from content store
    * ContentItemModelPresenter takes a ContentItem object and adds a
    presentation layer to it.
    * ContentItemPresenter does both, it models the response from content
    store and adds presentation to it.
    
    Ideally there would be content item models that inherit from ContentItem
    and content item presenters that inherit from ContentItemModelPresenter.
    And when the original ContentItemPresenter is removed, to rename
    ContentItemModelPresenter to ContentItemPresenter.
    
    To achieve this we need to look at the existing presenters one by one
    and move the logic to the appropriate place.
    
    All of the methods in the LicenceTransactionPresenter can be moved to a
    model.
    
    The "slug" method is different from the "slug" method in the
    SimpleSmartAnswer model, so they will kept separate.

---
+++ app/models/licence_transaction.rb
@@ -0,0 +1,19 @@
+class LicenceTransaction < ContentItem
+  attr_reader :body,
+              :licence_transaction_continuation_link,
+              :licence_transaction_licence_identifier,
+              :licence_transaction_will_continue_on
+
+  def initialize(content_store_response)
+    super(content_store_response)
+
+    @body = content_store_response.dig("details", "body")
+    @licence_transaction_continuation_link = content_store_response.dig("details", "metadata", "licence_transaction_continuation_link")
+    @licence_transaction_licence_identifier = content_store_response.dig("details", "metadata", "licence_transaction_licence_identifier")
+    @licence_transaction_will_continue_on = content_store_response.dig("details", "metadata", "licence_transaction_will_continue_on")
+  end
+
+  def slug
+    URI.parse(base_path).path.split("/").last
+  end
+end

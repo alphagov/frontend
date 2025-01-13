d4a0932424c1d16d61ee5665617b7fc6c2173697
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-01-13 17:48:16 +0000

    Update presenter and model to include ContentsList concern
    It is possible to have extra H2 headings in the contents list
    to anchor to other sections that are included at the bottom
    of the page. Overriding the `contents_items` method allows
    us to include the "Corporate information" heading if there
    are `corporate_information_groups` available in the `details`
    hash.
    
    As the corporate information groups methods now live in the
    CorporateInformationPagePresenter it means the methods
    `extra_headings` use is no longer accessible within the model.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/90cf5386b165b3a342689744feece7c83449b153/app/presenters/corporate_information_page_presenter.rb#L21-L31
    - https://github.com/alphagov/government-frontend/blob/90cf5386b165b3a342689744feece7c83449b153/app/presenters/content_item/corporate_information_groups.rb#L50-L57
    
    contentlist concern presenter tests (squash)

--- app/models/corporate_information_page.rb
+++ app/models/corporate_information_page.rb
@@ -1,2 +1,28 @@
 class CorporateInformationPage < ContentItem
+  include ContentsList
+
+  def contents_items
+    extract_headings_with_ids + extra_headings
+  end
+
+private
+
+  def extra_headings
+    extra_headings = []
+    extra_headings << corporate_information_heading if corporate_information_groups.any?
+    extra_headings
+  end
+
+  def corporate_information_heading
+    heading_text = I18n.t("formats.corporate_information_page.corporate_information")
+
+    {
+      text: heading_text,
+      id: heading_text.tr(" ", "-").downcase,
+    }
+  end
+
+  def corporate_information_groups
+    content_store_response.dig("details", "corporate_information_groups") || []
+  end
 end

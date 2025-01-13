a8315c07d04d6c69a094a97853d30894335032ff
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-01-13 12:05:29 +0000

    Add default_organisation method to CorporateInformationPage model
    This method gets the organisation based on the organisation ID from
    the details hash. We can easily access the logo/brand information
    now that we have modelled the Organisation and Logo, so we can pass
    this directly to the `organisation_logo` publishing component in the view.
    
    - We no longer need the hack to overide the executive order crest,
    as this is already dealt with in Whitehall
    - We not longer need to specify the brand class, as the `organisation_logo`
    component takes care of this
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/4c38f1d3d8600373fe49ab506c758ee05e4909a3/app/presenters/corporate_information_page_presenter.rb#L33-L42
    - https://github.com/alphagov/frontend/blob/1fe9d3e15cabe1f373314088449e63a2d35a02a3/app/models/organisation.rb
    - https://github.com/alphagov/government-frontend/blob/4c38f1d3d8600373fe49ab506c758ee05e4909a3/app/presenters/content_item/organisation_branding.rb
    - https://github.com/alphagov/government-frontend/blob/4c38f1d3d8600373fe49ab506c758ee05e4909a3/test/presenters/content_item/organisation_branding_test.rb

--- app/models/corporate_information_page.rb
+++ app/models/corporate_information_page.rb
@@ -1,2 +1,7 @@
 class CorporateInformationPage < ContentItem
+  def default_organisation
+    organisation_content_id = content_store_response.dig("details", "organisation")
+
+    @default_organisation ||= organisations.detect { |org| org.content_id == organisation_content_id }
+  end
 end

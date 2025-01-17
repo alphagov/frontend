c63d6ad2daf212c15818d147124d6622b7b2daec
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-01-17 15:35:01 +0000

    Add headers to SpecialistDocument model
    The list of headers is used to generate the contents list if there are
    more than 3 headers in the list.
    
    Confusingly this list of headers is generated in multiple places.
    
    [specialist-publisher] uses the govspeak gem to parse the body content
    and look for `h2` and `h3` tags and generate the `headers` list in
    `details` from them. If there are no `h2` or `h3` tags the entire
    `headers` element is removed from `details.
    
    government-frontend deals with `headers` in two different ways. It uses
    the `headers` element in details to [create the contents list], but then
    somewhat replicates the code from the govspeak gem by [parsing the `h2`
    tags] in the body content to determine whether or not so show the
    contents list.
    
    Those it's not being used to render the contents list in
    government-frontend the `headers` element does have a `level` attribute
    that indicates whether the header is a `h2` or `h3`. Therefore it should
    not be possible to greatly simplify the code from its current form in
    government-frontend.
    
    The first step is to just extract the headers list in the
    SpecialistPublisher model. The code to determine whether or not to
    display the contents list will be added to a presenter in a later
    commit.
    
    An example from content schemas is being used to initialise the
    SpecialistDocument model object, however  the ["details"]["headers"]
    have been copied from the example into the test as unlike other model
    tests the values in the array are being modified and the test is easier
    to follow if you can see exactly what's changed.
    
    Audit trail:
    specialist_document_presenter: https://github.com/alphagov/government-frontend/blob/c6eead39b29192e692c802c9b5e8bc234e16692c/app/presenters/specialist_document_presenter.rb#L86-L100
    
    [specialist-publisher](https://github.com/alphagov/specialist-publisher/blob/efadf3c4df33d045c0067e095f958d4b12638f29/app/presenters/document_presenter.rb#L50)
    [create the contents list]: https://github.com/alphagov/government-frontend/blob/c6eead39b29192e692c802c9b5e8bc234e16692c/app/presenters/specialist_document_presenter.rb#L86-L100
    [parsing the `h2` tags]: https://github.com/alphagov/government-frontend/blob/main/app/presenters/content_item/contents_list.rb#L43

--- spec/models/specialist_document_spec.rb
+++ spec/models/specialist_document_spec.rb
@@ -1,4 +1,54 @@
 RSpec.describe SpecialistDocument do
+  describe "#headers" do
+    let(:content_store_response) { GovukSchemas::Example.find("specialist_document", example_name: "aaib-reports") }
+    let(:details_headers) do
+      [
+        {
+          "text" => "Summary:",
+          "level" => 2,
+          "id" => "summary",
+          "headers" => [
+            {
+              "text" => "Download report:",
+              "level" => 3,
+              "id" => "download-report",
+            },
+            {
+              "text" => "Download glossary of abbreviations:",
+              "level" => 3,
+              "id" => "download-glossary-of-abbreviations",
+            },
+          ],
+        },
+      ]
+    end
+
+    it "gets a list of headers" do
+      content_store_response["details"]["headers"] = details_headers
+      expected_headers = [
+        {
+          href: "#summary",
+          text: "Summary",
+          level: 2,
+          items: [
+            {
+              href: "#download-report",
+              text: "Download report",
+              level: 3,
+            },
+            {
+              href: "#download-glossary-of-abbreviations",
+              text: "Download glossary of abbreviations",
+              level: 3,
+            },
+          ],
+        },
+      ]
+
+      expect(described_class.new(content_store_response).headers).to eq(expected_headers)
+    end
+  end
+
   context "with links to an external site" do
     let(:content_store_response) { GovukSchemas::Example.find("specialist_document", example_name: "business-finance-support-scheme") }
 

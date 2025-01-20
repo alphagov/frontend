1a7dbc9d947554a4db00da71fef2a3a1ddf293f8
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-01-20 18:03:10 +0000

    Add show_protection_type_image? to specialist document presenter

--- app/presenters/specialist_document_presenter.rb
+++ app/presenters/specialist_document_presenter.rb
@@ -5,6 +5,10 @@ class SpecialistDocumentPresenter < ContentItemPresenter
     content_item.headers
   end
 
+  def show_protection_type_image?
+    protected_food_drink_name? && content_item.protection_type_image.present?
+  end
+
   def show_finder_link?
     content_item.finder.present? && statutory_instrument?
   end
@@ -19,6 +23,10 @@ private
 
   def level_two_headings?
     content_item.headers.any? { |header| header[:level] == 2 }
+  end
+
+  def protected_food_drink_name?
+    content_item.document_type == "protected_food_drink_name"
   end
 
   def statutory_instrument?

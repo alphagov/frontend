6755462f65a0f2eb2accff0a47d2a506b6f35d6b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-02-04 15:22:24 +0000

    Remove Slimmer Gem and config
    - Layouts will be updated in a later commit (one each for application,
      homepage, and landing_page).
    - Remove the specific setting of gem_layout for travel_advice_controller
      and related test - this is the default anyway, so it's not clear
      that this controller needs special treatment.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,5 @@
 class HomepageController < ContentItemsController
   include Cacheable
 
-  slimmer_template "gem_layout_homepage"
-
-  def index
-    set_slimmer_headers(template: "gem_layout_homepage_new")
-  end
+  def index;  end
 end

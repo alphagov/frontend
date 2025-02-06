02f2dd33c787e5577a3c53b271484e2f1bfb2a67
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-02-06 15:31:38 +0000

    Add contents-list-with-body component,stylesheet,yaml and test files
     Contents-list-with-body component is applicable for other document types such as corporate_information_page,document_collection,specialist document
    
     Audit trail: https://github.com/alphagov/government-frontend/blob/85ede5293306d70f701749822a2ad0c75f201c5a/app/views/components/_contents_list_with_body.html.erb
    https://github.com/alphagov/government-frontend/blob/85ede5293306d70f701749822a2ad0c75f201c5a/app/assets/stylesheets/components/_contents-list-with-body.scss
    https://github.com/alphagov/government-frontend/blob/85ede5293306d70f701749822a2ad0c75f201c5a/app/views/components/docs/contents_list_with_body.yml
    Audit trail for tests: https://github.com/alphagov/government-frontend/blob/a718ddf50ab81320b3586bfb31acf0a9ea7ef82e/test/components/contents_list_with_body_test.rb

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -3,6 +3,7 @@ APP_STYLESHEETS = {
   "static-error-pages.scss" => "static-error-pages.css",
   "components/_back-to-top.scss" => "components/_back-to-top.css",
   "components/_calendar.scss" => "components/_calendar.css",
+  "components/_contents-list-with-body.scss" => "components/_contents-list-with-body.css",
   "components/_download-link.scss" => "components/_download-link.css",
   "views/_calendars.scss" => "views/_calendars.css",
   "views/_cookie-settings.scss" => "views/_cookie-settings.css",

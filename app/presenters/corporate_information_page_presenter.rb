61692ead4ba6666d7ba067deb298f8a50c4c5b08
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-02-10 12:15:22 +0000

    Add corporate information group methods to model
    Similar to the organisation branding methods, these methods are in
    a separate module in government-frontend but is not used by any
    other document types.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/a91838affca0c907ff94beba7c3bd021ddbd1809/app/presenters/content_item/corporate_information_groups.rb
    - https://github.com/alphagov/government-frontend/blob/90cf5386b165b3a342689744feece7c83449b153/test/presenters/corporate_information_page_presenter_test.rb#L42-L79

--- app/presenters/corporate_information_page_presenter.rb
+++ app/presenters/corporate_information_page_presenter.rb
@@ -1,2 +1,38 @@
 class CorporateInformationPagePresenter < ContentItemPresenter
+  def initialize(content_item, view_context)
+    super(content_item)
+    @view_context = view_context
+  end
+
+  def corporate_information_heading
+    I18n.t("formats.corporate_information_page.corporate_information")
+  end
+
+  def corporate_information_heading_id
+    corporate_information_heading.tr(" ", "-").downcase
+  end
+
+  def further_information
+    [
+      further_information_about("publication_scheme"),
+      further_information_about("welsh_language_scheme"),
+      further_information_about("personal_information_charter"),
+      further_information_about("social_media_use"),
+      further_information_about("about_our_services"),
+    ].join(" ").html_safe
+  end
+
+private
+
+  def further_information_about(type)
+    link = further_information_link(type)
+    I18n.t("formats.corporate_information_page.#{type}_html", link:) if link
+  end
+
+  def further_information_link(type)
+    links = content_item.content_store_response.dig("links", "corporate_information_pages") || []
+
+    link = links.find { |l| l["document_type"] == type }
+    @view_context.link_to(link["title"], link["base_path"]) if link
+  end
 end

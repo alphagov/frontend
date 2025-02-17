85dfdd6cc68db2ee453aa8fd819d039b16dc714d
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-02-17 15:40:07 +0000

    Add Speech model and presenter
    - Added Speech model that includes the related concerns for Speech along with the tests
    - Modified Speech model to include the methods to retrieve the important-metadata information that were copied over from Speech Presenter in government-frontend
    - Added Speech Presenter and tests to handle the delivery_type of the speech
    Commit audit trail - https://github.com/alphagov/government-frontend/blob/122ae292fa540059ee165a94f8129d873c5205d5/app/presenters/speech_presenter.rb
    Audit trail for presenter tests - https://github.com/alphagov/government-frontend/blob/122ae292fa540059ee165a94f8129d873c5205d5/test/presenters/speech_presenter_test.rb

---
+++ spec/models/speech_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe Speech do
+  let(:content_store_response) { GovukSchemas::Example.find("speech", example_name: "speech") }
+
+  it_behaves_like "it has updates", "speech", "speech-with-updates"
+  it_behaves_like "it has no updates", "speech", "speech"
+  it_behaves_like "it can be withdrawn", "speech", "withdrawn-speech"
+
+  describe "#contributors" do
+    it "returns the organisations as well as speaker" do
+      organisations = content_store_response["links"]["organisations"]
+      expect(described_class.new(content_store_response).speaker).to eq(content_store_response["links"]["speaker"])
+      expected_contributors =
+        { "title" => organisations[0]["title"], "base_path" => organisations[0]["base_path"], "content_id" => organisations[0]["content_id"] }
+
+      expect(described_class.new(content_store_response).contributors).to include(expected_contributors)
+    end
+
+    it "returns speaker without profile" do
+      content_store_response = GovukSchemas::Example.find("speech", example_name: "speech-speaker-without-profile")
+      expect(described_class.new(content_store_response).contributors).to include(content_store_response["details"]["speaker_without_profile"])
+    end
+  end
+
+  describe "#location" do
+    it "returns location for important-metadata" do
+      expect(described_class.new(content_store_response).location).to eq(content_store_response["details"]["location"])
+    end
+  end
+
+  describe "#delivered_on_date" do
+    it "returns delivered on date for important-metadata" do
+      expect(described_class.new(content_store_response).delivered_on_date).to eq(content_store_response["details"]["delivered_on"])
+    end
+  end
+
+  describe "#speech_type_explanation" do
+    it "returns speech type explanation for important-metadata" do
+      expect(described_class.new(content_store_response).speech_type_explanation).to eq(" (#{content_store_response['details']['speech_type_explanation']})")
+    end
+
+    it "returns nil when explanation does not exist" do
+      content_store_response = GovukSchemas::Example.find("speech", example_name: "speech-written-statement-parliament")
+      expect(described_class.new(content_store_response).speech_type_explanation).to be_nil
+    end
+  end
+end

9d5971b345694ad5643b5c122a05f8aa9212215f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-02-19 17:20:08 +0000

    Use the ContentItemHelpers methods to simplify model tests

--- spec/models/landing_page_spec.rb
+++ spec/models/landing_page_spec.rb
@@ -1,74 +1,40 @@
 RSpec.describe LandingPage do
-  let(:content_item) do
-    http_response = instance_double(RestClient::Response)
-    allow(http_response).to receive(:body).and_return(
-      {
-        "base_path" => "/landing-page",
-        "title" => "Landing Page",
-        "description" => "A landing page example",
-        "locale" => "en",
-        "document_type" => "landing_page",
-        "schema_name" => "landing_page",
-        "publishing_app" => "whitehall",
-        "rendering_app" => "frontend",
-        "update_type" => "major",
-        "details" => {},
-        "routes" => [
-          {
-            "type" => "exact",
-            "path" => "/landing-page",
-          },
-        ],
-      }.to_json,
-    )
-    GdsApi::Response.new(http_response)
-  end
+  include ContentItemHelpers
 
-  before do
-    stub_const("LandingPage::ADDITIONAL_CONTENT_PATH", "spec/fixtures")
-  end
+  subject(:landing_page) { content_item_from_factory(content_item_hash:) }
+
+  let(:content_item_hash) { GovukSchemas::Example.find(:landing_page, example_name: :landing_page) }
 
   describe "#blocks" do
-    let(:blocks_content) { YAML.load_file(Rails.root.join("spec/fixtures/landing_page.yaml")) }
-
     it "builds all of the blocks" do
-      expected_size = blocks_content["blocks"].count
-
-      expect(described_class.new(content_item).blocks.count).to eq(expected_size)
+      expect(landing_page.blocks.count).to eq(content_item_hash.dig("details", "blocks").count)
     end
 
     it "builds blocks of the correct type" do
-      expected_type = blocks_content["blocks"].first["type"]
-
-      expect(described_class.new(content_item).blocks.first.type).to eq(expected_type)
-    end
-
-    it "loads blocks from the content item if present instead of using hardcoded data" do
-      expect(build(:landing_page_with_one_block).blocks.count).to eq(1)
+      expect(landing_page.blocks.first.type).to eq(content_item_hash.dig("details", "blocks", 0, "type"))
     end
   end
 
   describe "#navigation_groups" do
     it "returns a map of navigation_groups" do
-      expect(described_class.new(content_item).navigation_groups["Top Menu"]["id"]).to eq("Top Menu")
-      expect(described_class.new(content_item).navigation_groups["Top Menu"]["links"].count).to eq(2)
-      expect(described_class.new(content_item).navigation_groups["Top Menu"]["links"][0]["links"].count).to eq(2)
+      expect(landing_page.navigation_groups["Top Menu"]["id"]).to eq("Top Menu")
+      expect(landing_page.navigation_groups["Top Menu"]["links"].count).to eq(2)
+      expect(landing_page.navigation_groups["Top Menu"]["links"][0]["links"].count).to eq(2)
     end
   end
 
   describe "#breadcrumbs" do
     it "returns nil if there are no breadcrumbs" do
-      # Setting this prevents the YAML file from overloading the content
-      content_item["details"]["blocks"] = []
+      content_item_hash["details"].delete("breadcrumbs")
 
-      content_item["details"]["breadcrumbs"] = nil
-      expect(described_class.new(content_item).breadcrumbs).to be_nil
+      expect(landing_page.breadcrumbs).to be_nil
     end
 
     it "returns breadcrumbs in the structure expected by govuk_publishing_components" do
-      # Note the config in the YAML file is { "title" => ..., "href" => ... } rather than { title: ..., url: ...}
-      # this is for consistency with the blocks, which tend to use href for their URLs.
-      expect(described_class.new(content_item).breadcrumbs).to eq([
+      # Note the config in the content item details is { "title" => ..., "href" => ... } rather than
+      # { title: ..., url: ...} this is for consistency with the blocks, which tend to use href for
+      # their URLs.
+      expect(landing_page.breadcrumbs).to eq([
         { title: "Some breadcrumb", url: "/some-breadcrumb" },
         { title: "Some other breadcrumb", url: "/some-other-breadcrumb" },
       ])
@@ -76,28 +42,26 @@ RSpec.describe LandingPage do
   end
 
   describe "#theme" do
-    it "returns the default theme if theme is not specified" do
-      content_item["details"]["theme"] = nil
-
-      expect(described_class.new(content_item).theme).to eq("default")
+    it "returns the default theme if theme is default" do
+      expect(landing_page.theme).to eq("default")
     end
 
-    it "returns the default theme if theme is default" do
-      content_item["details"]["theme"] = "default"
+    it "returns the default theme if theme is not specified" do
+      content_item_hash["details"]["theme"] = nil
 
-      expect(described_class.new(content_item).theme).to eq("default")
+      expect(landing_page.theme).to eq("default")
     end
 
     it "returns the default theme if theme is not in the list of accepted themes" do
-      content_item["details"]["theme"] = "missing-theme"
+      content_item_hash["details"]["theme"] = "missing-theme"
 
-      expect(described_class.new(content_item).theme).to eq("default")
+      expect(landing_page.theme).to eq("default")
     end
 
     it "returns the specified theme if theme is in the list of accepted themes" do
-      content_item["details"]["theme"] = "prime-ministers-office-10-downing-street"
+      content_item_hash["details"]["theme"] = "prime-ministers-office-10-downing-street"
 
-      expect(described_class.new(content_item).theme).to eq("prime-ministers-office-10-downing-street")
+      expect(landing_page.theme).to eq("prime-ministers-office-10-downing-street")
     end
   end
 end

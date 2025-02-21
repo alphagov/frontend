0424e2113eb1f5efa700f89bcb43100b0032827c
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-02-21 15:57:07 +0000

    Add important_metadata presenter method
    The view_context is being passed to the presenter from controller.
    An array of selected facet values is returned from the model. Rather
    than looping through the model in the view to apply the helper methods
    it was was felt better to move the complexity to a presenter which is
    easier to test than a view. The compromise is that rather than
    recreating the date and link helper methods, they are instead being
    accessed in presenter via the view_context. There's no need to define
    a new view_context as the controller already has access to it.
    
    Audit trail:
    
    specialist_document_presenter.rb: https://github.com/alphagov/government-frontend/blob/main/app/presenters/specialist_document_presenter.rb#L27
    specialist_document_presenter_test.rb: https://github.com/alphagov/government-frontend/blob/868603abc5c2e3a50cd78dbb505e56ed3102f5a1/test/presenters/specialist_document_presenter_test.rb

--- app/controllers/specialist_document_controller.rb
+++ app/controllers/specialist_document_controller.rb
@@ -2,6 +2,6 @@ class SpecialistDocumentController < ContentItemsController
   include Cacheable
 
   def show
-    @specialist_document_presenter = SpecialistDocumentPresenter.new(content_item)
+    @specialist_document_presenter = SpecialistDocumentPresenter.new(content_item, view_context)
   end
 end

14f4bc9be404d38ea5628ccfd0f23b20976a3810
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-02-26 15:30:41 +0000

    Add logo image to logo model

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,8 +1,18 @@
 class Logo
-  attr_reader :crest, :formatted_title
+  attr_reader :crest, :formatted_title, :image
 
   def initialize(logo)
+    @image = get_image(logo["image"])
     @crest = logo["crest"]
     @formatted_title = logo["formatted_title"]
+  end
+
+  def get_image(logo_image)
+    return unless logo_image
+
+    OpenStruct.new(
+      alt_text: logo_image["alt_text"],
+      url: logo_image["url"],
+    )
   end
 end
--- spec/models/logo_spec.rb
+++ spec/models/logo_spec.rb
@@ -1,20 +1,50 @@
 RSpec.describe Logo do
   let(:logo) do
     {
-      "crest": "dbt",
-      "formatted_title": "Department for<br/>Business &amp; Trade",
+      "crest" => "dbt",
+      "formatted_title" => "Department for<br/>Business &amp; Trade",
     }
   end
 
-  describe "logo" do
+  describe "#crest" do
     it "gets the crest" do
       expect(described_class.new(logo).crest)
         .to eq(logo["crest"])
     end
+  end
 
+  describe "#formatted_title" do
     it "gets the formatted title" do
       expect(described_class.new(logo).formatted_title)
         .to eq(logo["formatted_title"])
+    end
+  end
+
+  describe "#image" do
+    context "when there is an image" do
+      let(:logo) do
+        {
+          "formatted_title" => "Forensic Science <br/>Regulator",
+          "image" => {
+            "alt_text" => "Forensic Science Regulator",
+            "url" => "https://example.com/sample.png",
+          },
+        }
+      end
+
+      it "gets the image alt_text" do
+        expect(described_class.new(logo).image.alt_text).to eq(logo["image"]["alt_text"])
+      end
+
+      it "gets the image url" do
+        expect(described_class.new(logo).image.url).to eq(logo["image"]["url"])
+      end
+    end
+
+    context "when there is no image" do
+      it "returns nil" do
+        expect(described_class.new(logo).image).to be_nil
+      end
     end
   end
 end

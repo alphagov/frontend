f23ecba790d818c81c540dd19632ad576fda8436
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-04 16:12:11 +0000

    Add request tests

---
+++ spec/requests/news_article_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe "News Article" do
+  describe "GET show" do
+    let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "news_article") }
+    let(:base_path) { content_item.fetch("base_path") }
+
+    before do
+      stub_content_store_has_item(base_path, content_item)
+    end
+
+    it "succeeds" do
+      get base_path
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the show template" do
+      get base_path
+
+      expect(response).to render_template(:show)
+    end
+
+    it "sets cache-control headers" do
+      get base_path
+
+      expect(response).to honour_content_store_ttl
+    end
+  end
+end

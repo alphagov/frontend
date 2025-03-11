50da66b90370240a4331b3c98591ea878431b39f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-11 16:37:04 +0000

    Use political concern
    - update call to shared/history_notice

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,6 +1,7 @@
 class NewsArticle < ContentItem
   include EmphasisedOrganisations
   include People
+  include Political
   include Updatable
   include Withdrawable
   include WorldwideOrganisations

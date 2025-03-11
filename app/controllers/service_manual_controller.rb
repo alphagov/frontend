441c19a81bf497b66a29dae2288775de04e74091
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-03-11 14:43:52 +0000

    Use cards component for homepage topics
    - add a presenter to sort the homepage topics to pass to the cards component, moving the code out of the view

--- app/controllers/service_manual_controller.rb
+++ app/controllers/service_manual_controller.rb
@@ -2,5 +2,7 @@ class ServiceManualController < ContentItemsController
   include Cacheable
   slimmer_template "gem_layout_full_width"
 
-  def index; end
+  def index
+    @presenter = ServiceManualHomepagePresenter.new(content_item)
+  end
 end

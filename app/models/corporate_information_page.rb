0a979a39990aa56e6d1bf658d848ba9f69c806ec
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-03-11 11:06:10 +0000

    Add corporate information groups methods to CorporateInformationPage model
    Similar to the organisation branding methods, these methods are in
    a separate module in government-frontend but is not used by any
    other document types.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/a91838affca0c907ff94beba7c3bd021ddbd1809/app/presenters/content_item/corporate_information_groups.rb
    - https://github.com/alphagov/government-frontend/blob/90cf5386b165b3a342689744feece7c83449b153/test/presenters/corporate_information_page_presenter_test.rb#L42-L79

--- app/models/corporate_information_page.rb
+++ app/models/corporate_information_page.rb
@@ -4,4 +4,50 @@ class CorporateInformationPage < ContentItem
 
     @default_organisation ||= organisations.detect { |org| org.content_id == organisation_content_id }
   end
+
+  def corporate_information?
+    corporate_information_groups.any?
+  end
+
+  def corporate_information
+    corporate_information_groups.map do |group|
+      {
+        title: group["name"],
+        id: group["name"].tr(" ", "-").downcase,
+        links: normalised_group_links(group),
+      }
+    end
+  end
+
+  def corporate_information_pages
+    linked("corporate_information_pages")
+  end
+
+private
+
+  def normalised_group_links(group)
+    group["contents"].map { |group_item| normalised_group_item_link(group_item) }.compact
+  end
+
+  def normalised_group_item_link(group_item)
+    if group_item.is_a?(String)
+      group_item_link = corporate_information_pages.find { |l| l.content_id == group_item }
+      # it's possible for corporation_information_groups in details and links hashes to be out of sync.
+      if group_item_link
+        {
+          title: group_item_link.title,
+          path: group_item_link.base_path,
+        }
+      end
+    else
+      {
+        title: group_item["title"],
+        path: group_item["path"] || group_item["url"],
+      }
+    end
+  end
+
+  def corporate_information_groups
+    content_store_response.dig("details", "corporate_information_groups") || []
+  end
 end

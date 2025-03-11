dfcbec5a42b3c6ef67eacf05ab4fa509bdb1a049
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-03-11 11:06:10 +0000

    Add corporate information groups methods to CorporateInformationPage model
    Similar to the organisation branding methods, these methods are in
    a separate module in government-frontend but is not used by any
    other document types.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/a91838affca0c907ff94beba7c3bd021ddbd1809/app/presenters/content_item/corporate_information_groups.rb
    - https://github.com/alphagov/government-frontend/blob/90cf5386b165b3a342689744feece7c83449b153/test/presenters/corporate_information_page_presenter_test.rb#L42-L79

--- spec/models/corporate_information_page_spec.rb
+++ spec/models/corporate_information_page_spec.rb
@@ -17,4 +17,64 @@ RSpec.describe CorporateInformationPage do
       expect(described_class.new(content_store_response).default_organisation).to be_nil
     end
   end
+
+  describe "#corporate_information?" do
+    it "presents corporate information groups on about pages" do
+      expect(described_class.new(content_store_response).corporate_information?).to be(true)
+    end
+
+    it "does not present corporate information groups on about pages if information isn't available" do
+      content_store_response["details"]["corporate_information_groups"] = nil
+
+      expect(described_class.new(content_store_response).corporate_information?).to be(false)
+    end
+  end
+
+  describe "#corporate_information" do
+    subject(:corporate_information) { described_class.new(content_store_response).corporate_information }
+
+    it "includes group headings" do
+      expect(corporate_information.first[:title]).to eq("Access our information")
+      expect(corporate_information.first[:id]).to eq("access-our-information")
+    end
+
+    context "with 'Access our information' section" do
+      it "includes the organisation chart as the first group link if it is available" do
+        expect(corporate_information.first[:links].first).to eq({ title: "Our organisation chart", path: "https://data.gov.uk/dataset/04427362-663e-49e0-9103-8bc01dcaa2c7/organogram-of-staff-roles-and-salaries" })
+      end
+
+      it "includes group links and titles that have matching guids" do
+        expect(corporate_information.first[:links].last).to eq({ title: "Statistics at DHSC", path: "/government/organisations/department-of-health-and-social-care/about/statistics" })
+      end
+
+      it "excludes group links that do not have matching guids" do
+        groups = content_store_response.dig("details", "corporate_information_groups")
+        groups.first["contents"] << "123"
+
+        corporate_information = described_class.new(content_store_response).corporate_information
+
+        expect(corporate_information.first[:links].count).not_to eq(groups.count)
+      end
+    end
+
+    context "with 'Jobs and contracts' section" do
+      it "includes group links that are external" do
+        expect(corporate_information.last[:links].last).to eq({ title: "Jobs", path: "https://www.civilservicejobs.service.gov.uk/csr" })
+      end
+    end
+  end
+
+  describe "#corporate_information_pages" do
+    subject(:corporate_information_pages) { described_class.new(content_store_response).corporate_information_pages }
+
+    it "models the corporate information pages" do
+      expect(corporate_information_pages.first).to be_instance_of(described_class)
+    end
+
+    it "returns all the avalilable corporate information pages" do
+      expected_pages = content_store_response["links"]["corporate_information_pages"]
+
+      expect(corporate_information_pages.count).to eq(expected_pages.count)
+    end
+  end
 end

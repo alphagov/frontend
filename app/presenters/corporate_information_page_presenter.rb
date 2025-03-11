d9319f43567ce0c2a5c01a702fe65ab2a6a49de5
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-03-11 11:04:38 +0000

    Add corporate information group methods to CorporateInformationPage presenter
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/f25bb54b3ce6fbe2a61e185c85e6137e808b33c4/app/presenters/content_item/corporate_information_groups.rb

--- app/presenters/corporate_information_page_presenter.rb
+++ app/presenters/corporate_information_page_presenter.rb
@@ -1,2 +1,46 @@
 class CorporateInformationPagePresenter < ContentItemPresenter
+  include LinkHelper
+
+  def corporate_information_heading
+    I18n.t("formats.corporate_information_page.corporate_information")
+  end
+
+  def corporate_information_heading_id
+    corporate_information_heading.tr(" ", "-").downcase
+  end
+
+  def further_information
+    return unless corporate_information_pages?
+
+    further_information_links
+  end
+
+private
+
+  def corporate_information_pages?
+    content_item.corporate_information_pages.any?
+  end
+
+  def further_information_links
+    links = further_information_types.map do |further_information_type|
+      page = content_item.corporate_information_pages.find { |content_item| content_item.document_type == further_information_type }
+      next unless page
+
+      link = govuk_styled_link(page.title, path: page.base_path)
+
+      I18n.t("formats.corporate_information_page.#{further_information_type}_html", link:)
+    end
+
+    links.compact.join(" ").html_safe
+  end
+
+  def further_information_types
+    %w[
+      publication_scheme
+      welsh_language_scheme
+      personal_information_charter
+      social_media_use
+      about_our_services
+    ]
+  end
 end
---
+++ spec/presenter/corporate_information_page_presenter_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe CorporateInformationPagePresenter do
+  subject(:presenter) { described_class.new(content_item) }
+
+  let(:content_store_response) { GovukSchemas::Example.find("corporate_information_page", example_name: "corporate_information_page") }
+  let(:content_item) { CorporateInformationPage.new(content_store_response) }
+
+  describe "#corporate_information_heading" do
+    it "returns the heading title" do
+      expect(presenter.corporate_information_heading).to eq("Corporate information")
+    end
+  end
+
+  describe "#corporate_information_heading_id" do
+    it "returns the title id" do
+      expect(presenter.corporate_information_heading_id).to eq("corporate-information")
+    end
+  end
+
+  describe "#further_information" do
+    context "when there are corporate information pages" do
+      it "presents further information based on corporate information page links" do
+        expect(presenter.further_information).to include("Publication scheme")
+        expect(presenter.further_information).to include("/government/organisations/department-of-health/about/publication-scheme")
+        expect(presenter.further_information).to include("Personal information charter")
+        expect(presenter.further_information).to include("/government/organisations/department-of-health/about/personal-information-charter")
+      end
+    end
+
+    context "when there are no corporate information pages" do
+      let(:content_store_response) { GovukSchemas::Example.find("corporate_information_page", example_name: "corporate_information_page_without_description") }
+
+      it "returns nil" do
+        expect(presenter.further_information).to be_nil
+      end
+    end
+  end
+end

40aa417b11c4eee7b0f4baf2fdc32b0a639418e9
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-03-12 14:54:34 +0000

    Replace contents list with body component with gem version

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -2,7 +2,6 @@ APP_STYLESHEETS = {
   "application.scss" => "application.css",
   "static-error-pages.scss" => "static-error-pages.css",
   "components/_calendar.scss" => "components/_calendar.css",
-  "components/_contents-list-with-body.scss" => "components/_contents-list-with-body.css",
   "components/_download-link.scss" => "components/_download-link.css",
   "components/_figure.scss" => "components/_figure.css",
   "components/_published-dates.scss" => "components/_published-dates.css",

9fdbc35d22da77f0c503aa3025c8caa44627495d
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-13 08:04:40 +0000

    Add option to load from graphql
    - The use_graphql? method replicates government-frontend current behaviour - if
      the graphql param is set to true, we always try to load from the graphql endpoint.
      if the param is false, we never try to load from the graphql endpoint. If the param
      is absent, we load if the environment flag GRAPHQL_FEATURE_FLAG is true. This
      allows us to default to graphql (but turn it off per request), or default to no graphql
      (but turn it on per request).
    - In load_from_sources tries to get the data from the publishing api graphql endpoint. If it succeeds, it
      then checks against GRAPHQL_ALLOWED_SCHEMAS. If it's allowed, the response
      is returned. If it's not, we fall back to loading the content item the traditional way.
      This is how it's currently done in government-frontend.
    - As per government-frontend, the only allowed schema at the moment is news_article.
    
    Audit trail:
    - load_content_item: https://github.com/alphagov/government-frontend/blob/0ff5edca387d1b9288445016a6eab82451088072/app/controllers/content_items_controller.rb#L139-L157
    - use_graphql?: https://github.com/alphagov/government-frontend/blob/0ff5edca387d1b9288445016a6eab82451088072/app/controllers/content_items_controller.rb#L171-L179

--- lib/content_item_loader.rb
+++ lib/content_item_loader.rb
@@ -2,6 +2,7 @@ require "ostruct"
 
 class ContentItemLoader
   LOCAL_ITEMS_PATH = "lib/data/local-content-items".freeze
+  GRAPHQL_ALLOWED_SCHEMAS = %w[news_article].freeze
 
   def self.for_request(request)
     request.env[:loader] ||= ContentItemLoader.new(request:)
@@ -27,11 +28,25 @@ private
     elsif use_local_file? && File.exist?(json_filename(base_path))
       Rails.logger.debug("Loading content item #{base_path} from #{json_filename(base_path)}")
       load_json_file(base_path)
+    elsif use_graphql?
+      graphql_response = GdsApi.publishing_api.graphql_content_item(Graphql::EditionQuery.new(base_path).query)
+      if GRAPHQL_ALLOWED_SCHEMAS.include?(graphql_response["schema"])
+        graphql_response
+      else
+        GdsApi.content_store.content_item(base_path)
+      end
     else
       GdsApi.content_store.content_item(base_path)
     end
   rescue GdsApi::HTTPErrorResponse, GdsApi::InvalidUrl => e
     e
+  end
+
+  def use_graphql?
+    return true if request && request.params["graphql"] == "true"
+    return false if request && request.params["graphql"] == "false"
+
+    ENV["GRAPHQL_FEATURE_FLAG"] == "true"
   end
 
   def use_local_file?

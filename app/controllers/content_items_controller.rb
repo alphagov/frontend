713f93ef9cbbfbbe2d4525f07d5a43e5588bda87
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-13 10:48:22 +0000

    Pass query classes in from controllers

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -7,7 +7,7 @@ class ContentItemsController < ApplicationController
 private
 
   def set_content_item_and_cache_control
-    loader_response = ContentItemLoader.for_request(request).load(content_item_path)
+    loader_response = ContentItemLoader.for_request(request).load(content_item_path, graphql_query_class:)
     raise loader_response if loader_response.is_a?(StandardError)
 
     @content_item = ContentItemFactory.build(loader_response.to_hash)
@@ -16,6 +16,10 @@ private
 
   def content_item_path
     request.path
+  end
+
+  def graphql_query_class
+    Graphql::EditionQuery
   end
 
   # NOTE: Frontend honours the max-age directive  provided

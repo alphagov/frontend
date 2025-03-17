1e7ca3683d78cc44b2818958f94d42594929431b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-17 16:38:54 +0000

    refactor: Allow postcode lookup to fetch addresses
    - Since we do a bunch of checking in the initializer, and we can't
      call addresses without an object, it's safe to skip a bunch of
      error checking here (we know we're going to get _something_).
    - We don't strictly need to make the postcode attribute readable
      outside the model, but it seems a reasonable thing to do.

--- app/models/postcode_lookup.rb
+++ app/models/postcode_lookup.rb
@@ -1,7 +1,10 @@
+require "ostruct"
+
 class PostcodeLookup
-  attr_reader :local_custodian_codes
+  attr_reader :local_custodian_codes, :postcode
 
   def initialize(postcode)
+    @postcode = postcode
     raise LocationError, "invalidPostcodeFormat" if postcode.blank?
 
     @local_custodian_codes = Frontend.locations_api.local_custodian_code_for_postcode(postcode)
@@ -10,5 +13,17 @@ class PostcodeLookup
     raise LocationError, "noLaMatch"
   rescue GdsApi::HTTPClientError
     raise LocationError, "invalidPostcodeFormat"
+  end
+
+  def addresses
+    @addresses ||= begin
+      response = Frontend.locations_api.results_for_postcode(postcode)
+      response["results"].map do |result|
+        OpenStruct.new(
+          address: result["address"],
+          local_custodian_code: result["local_custodian_code"],
+        )
+      end
+    end
   end
 end

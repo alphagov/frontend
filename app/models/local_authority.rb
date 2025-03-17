4cb02f10dd9296a0070fd1237a7348d9619ef899
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-17 16:10:56 +0000

    perf: Cache LCC -> Authority mappings for 5 minutes
    - These actually change _very_ infrequently, but 5 minutes is a common
      caching time for gov.uk, so we shouldn't introduce a surprise longer
      cache without very good reason.

--- app/models/local_authority.rb
+++ app/models/local_authority.rb
@@ -2,11 +2,13 @@ class LocalAuthority
   attr_reader :name, :homepage_url, :country_name, :tier, :slug, :gss, :parent
 
   def self.from_local_custodian_code(local_custodian_code)
-    authority_results = Frontend.local_links_manager_api.local_authority_by_custodian_code(local_custodian_code)
-    if authority_results["local_authorities"].count == 2
-      parent = LocalAuthority.new(authority_results["local_authorities"].last)
+    Rails.cache.fetch(local_custodian_code, expires_in: 5.minutes) do
+      authority_results = Frontend.local_links_manager_api.local_authority_by_custodian_code(local_custodian_code)
+      if authority_results["local_authorities"].count == 2
+        parent = LocalAuthority.new(authority_results["local_authorities"].last)
+      end
+      LocalAuthority.new(authority_results["local_authorities"].first, parent:)
     end
-    LocalAuthority.new(authority_results["local_authorities"].first, parent:)
   end
 
   def initialize(map, parent: nil)

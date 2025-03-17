88423f5cc2713aaa9ad2f0103adae9ea976c0f2f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-17 15:17:41 +0000

    refactor: Model LocalAuthority
    - although it might normally be a factory or service item, this class is relatively simple so
      we also add a method to build from a local custodian code.
    - Local links manager always returns 2-tier councils with the district first, then county, so
      it's safe to rely on that order. It only ever returns 1 or 2, not found will throw a GdsApi
      error which a controller can handle reasonably.

---
+++ app/models/local_authority.rb
@@ -0,0 +1,21 @@
+class LocalAuthority
+  attr_reader :name, :homepage_url, :country_name, :tier, :slug, :gss, :parent
+
+  def self.from_local_custodian_code(local_custodian_code)
+    authority_results = Frontend.local_links_manager_api.local_authority_by_custodian_code(local_custodian_code)
+    if authority_results["local_authorities"].count == 2
+      parent = LocalAuthority.new(authority_results["local_authorities"].last)
+    end
+    LocalAuthority.new(authority_results["local_authorities"].first, parent:)
+  end
+
+  def initialize(map, parent: nil)
+    @name = map["name"]
+    @homepage_url = map["homepage_url"]
+    @country_name = map["country_name"]
+    @tier = map["tier"]
+    @slug = map["slug"]
+    @gss = map["gss"]
+    @parent = parent
+  end
+end

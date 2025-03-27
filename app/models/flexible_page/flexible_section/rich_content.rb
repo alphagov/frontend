4b17e6343d82dd2d4bb1b6989662a05062764f89
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-03-27 14:37:24 +0000

    Rich text content flexible section frontend
    - adds the model and frontend code to display rich text with contents lists flexible sections
    - again preliminary as we want to auto generate contents lists, but a decent starting point

--- app/models/flexible_page/flexible_section/rich_content.rb
+++ app/models/flexible_page/flexible_section/rich_content.rb
@@ -1,13 +1,23 @@
 class FlexiblePage
   module FlexibleSection
     class RichContent < Base
-      attr_reader :content_list, :govspeak
+      attr_reader :content_list, :govspeak, :image
 
       def initialize(flexible_section_hash, flexible_page)
         super
 
-        @content_list = data["content_list"]
+        @content_list = contents_list(data["content_list"])
         @govspeak = data["govspeak"]
+        @image = data["image"]
+      end
+
+      def contents_list(contents)
+        contents.map do |item|
+          {
+            href: item['href'],
+            text: item['text'],
+          }
+        end
       end
     end
   end

627d7079ff98f1727c43bad3f32d6c2a878eb4f9
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-04-04 16:10:08 +0100

    Add presenter to controller

--- app/controllers/field_of_operation_controller.rb
+++ app/controllers/field_of_operation_controller.rb
@@ -1,4 +1,6 @@
 class FieldOfOperationController < ContentItemsController
   include Cacheable
-  def show; end
+  def show
+    @presenter = FieldOfOperationPresenter.new(@content_item)
+  end
 end

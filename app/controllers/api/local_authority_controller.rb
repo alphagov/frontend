1ce1a8d300dd4415e04bc301fab6e044195abb89
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-04-07 20:22:06 +0100

    Add fake index and LA queries in production only (suitable for preview apps)

--- app/controllers/api/local_authority_controller.rb
+++ app/controllers/api/local_authority_controller.rb
@@ -1,6 +1,8 @@
 module Api
   class LocalAuthorityController < Api::BaseController
     def index
+      return fake_index_query if Rails.env.production?
+
       postcode_lookup = PostcodeLookup.new(postcode)
 
       if postcode_lookup.local_custodian_codes.count == 1
@@ -16,7 +18,7 @@ module Api
     end
 
     def show
-      @local_authority = LocalAuthority.from_slug(params[:authority_slug])
+      @local_authority = Rails.env.production? ? fake_local_authority : LocalAuthority.from_slug(params[:authority_slug])
 
       render json: { local_authority: @local_authority.to_h }
     rescue GdsApi::HTTPNotFound
@@ -24,6 +26,38 @@ module Api
     end
 
   private
+
+    FAKE_ADDRESS_LIST = [
+      { address: "PEARDOWN HOUSE, FENAPPLE CLOSE, BREAM ROAD, WEST THYME, APPLETON, SW1A 2BB", slug: "dorset", name: "Dorset Council" },
+      { address: "STRAWBERRY COTTAGE, FENAPPLE CLOSE, BREAM ROAD, WEST THYME, APPLETON, SW1A 2BB", slug: "westminster", name: "City of Westminster" },
+    ].freeze
+
+    def fake_index_query
+      raise LocationError, "invalidPostcodeFormat" if postcode.blank?
+
+      case postcode
+      when "BAD"
+        raise LocationError, "invalidPostcodeFormat"
+      when "SW1A 2AA"
+        redirect_to "/api/local-authority/westminster"
+      when "SW1A 2BB"
+        render json: { addresses: FAKE_ADDRESS_LIST }
+      else
+        raise LocationError, "noLaMatch"
+      end
+    end
+
+    def fake_local_authority
+      case params[:authority_slug]
+      when "missing"
+        raise GdsApi::HTTPNotFound, 404
+      when "derbyshire-dales"
+        parent = LocalAuthority.new({ "slug" => "derbyshire", "name" => "Derbyshire County Council", "tier" => "county", "homepage_url" => "https://www.gov.uk" })
+        LocalAuthority.new({ "slug" => "derbyshire-dales", "name" => "Derbyshire Dales District Council", "tier" => "district", "homepage_url" => "https://www.gov.uk" }, parent:)
+      else
+        LocalAuthority.new({ "slug" => params[:authority_slug], "name" => params[:authority_slug].gsub("-", " ").titleize, "tier" => "unitary", "homepage_url" => "https://www.gov.uk" })
+      end
+    end
 
     def postcode
       @postcode ||= PostcodeSanitizer.sanitize(params[:postcode])

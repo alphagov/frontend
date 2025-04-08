3ca326acb9ae6be172f8ffef843b4ec524300fdf
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-04-08 09:45:56 +0100

    Rewrite JavaScript
    - remove fetches, as data is local
    - remove omnivore, as CSV data has been converted to geojson (and omnivore was causing an error with our CSP)
    - remove Promise, as fetches no longer required
    - reindent

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,9 +5,11 @@
 //= link test-dependencies.js
 //= link views/travel-advice.js
 //= link views/missions/os-api-branding.js
-//= link views/missions/leaflet.js
-//= link views/missions/leaflet-omnivore.min.js
 //= link views/missions/main.js
+//= link views/missions/data/lookup.json.js
+//= link views/missions/data/icb.geojson.js
+//= link views/missions/data/cdc.geojson.js
+//= link views/missions/data/hub.geojson.js
 
 //= link static-error-pages.js
 
--- app/assets/javascripts/views/missions/main.js
+++ app/assets/javascripts/views/missions/main.js
@@ -1,32 +1,36 @@
-const apiKey = '';
+//= require leaflet
+//= require views/missions/data/cdc.geojson.js
+//= require views/missions/data/hub.geojson.js
+//= require views/missions/data/icb.geojson.js
+//= require views/missions/data/lookup.json.js
 
-// Initialize the map.
-const mapOptions = {
+window.addEventListener("DOMContentLoaded", function () {
+  const apiKey = ''
+
+  // Initialize the map.
+  const mapOptions = {
     minZoom: 7,
     maxZoom: 16,
     center: [ 51.063, -1.319 ],
     zoom: 7,
     maxBounds: [
-        [ 49.528423, -10.76418 ],
-        [ 61.331151, 1.9134116 ]
+      [ 49.528423, -10.76418 ],
+      [ 61.331151, 1.9134116 ]
     ],
     attributionControl: false
-};
+  }
 
-const map = L.map('map', mapOptions);
+  const map = L.map('map', mapOptions)
 
-// Load and display ZXY tile layer on the map.
-const basemap = L.tileLayer('https://api.os.uk/maps/raster/v1/zxy/Light_3857/{z}/{x}/{y}.png?key=' + apiKey, {
+  // Load and display ZXY tile layer on the map.
+  const basemap = L.tileLayer('https://api.os.uk/maps/raster/v1/zxy/Light_3857/{z}/{x}/{y}.png?key=' + apiKey, {
     maxZoom: 20
-}).addTo(map);
+  }).addTo(map)
 
-let icbLookup = {};
-let nhsLookup = {};
+  let icbLookup = {}
+  let nhsLookup = {}
 
-// const icbNameTemplate = "NHS {name} Integrated Care Board";
-// const nhsNameTemplate = "{name} NHS Region";
-
-const colorLookup = {
+  const colorLookup = {
     E40000003: "#FF1F5B",
     E40000005: "#00CD6C",
     E40000006: "#009ADE",
@@ -34,141 +38,124 @@ const colorLookup = {
     E40000010: "#FFC61E",
     E40000011: "#F28522",
     E40000012: "#A0B1BA"
-};
+  }
 
-const baseMaps = {};
-const overlayMaps = {};
-const icbOverlays = {};
+  const baseMaps = {}
+  const overlayMaps = {}
+  const icbOverlays = {}
 
-const lkupPromise = fetch('../lookup.json').then((response) => response.json());
-const icbPromise = fetch('../data/icb.geojson').then((response) => response.json());
-const cdcPromise = fetch('../data/cdc.csv').then((response) => response.text());
-const hubPromise = fetch('../data/hub.csv').then((response) => response.text());
+  // lookupjson and similar are declared in data files in views/missions/data
+  icbLookup = lookupjson.icb
+  nhsLookup = lookupjson.nhs
 
-Promise.all([ lkupPromise, icbPromise, cdcPromise, hubPromise ]).then((values) => {
-    const lkupData = values[0];
-    const icbData = values[1];
-    const cdcData = values[2];
-    const hubData = values[3];
+  //Add the DHSC Community Diagnostic Centre (CDC) locations.
+  map.createPane('cdc')
+  map.getPane('cdc').style.zIndex = 650
 
-    icbLookup = lkupData.icb;
-    nhsLookup = lkupData.nhs;
-
-    //Add the DHSC Community Diagnostic Centre (CDC) locations.
-    map.createPane('cdc');
-    map.getPane('cdc').style.zIndex = 650;
-
-    const cdcCustomOverlay = L.geoJson(null, {
-        onEachFeature: bindPopup,
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng);
-        },
-        style: function(feature) {
-            return {
-                color: '#fff',
-                fillColor: colorLookup[ feature.properties.REGION ],
-                fillOpacity: 1,
-                radius: 5,
-                weight: 2,
-                pane: 'cdc'
-            }
-        }
-    }).addTo(map);
-
-    const cdcOverlay = omnivore.csv.parse(cdcData, null, cdcCustomOverlay);
-
-    //Add the DHSC surgical hub locations.
-    map.createPane('hub');
-    map.getPane('hub').style.zIndex = 651;
-
-    const hubCustomOverlay = L.geoJson(null, {
-        onEachFeature: bindPopup,
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng);
-        },
-        style: function(feature) {
-            return {
-                color: '#000',
-                fillColor: '#fff',
-                fillOpacity: 1,
-                radius: 4,
-                weight: 1,
-                pane: 'hub'
-            }
-        }
-    }).addTo(map);
-
-    const hubOverlay = omnivore.csv.parse(hubData, null, hubCustomOverlay);
-
-    // Generate CDC and surgical hub counts per ICB boundaries.
-    let icb_cdcCount = cdcOverlay.toGeoJSON().features.reduce(function(obj, v) {
-        obj[ v.properties.ICS_ICB ] = (obj[ v.properties.ICS_ICB ] || 0) + 1;
-        return obj;
-    }, {});
-
-    let icb_hubCount = hubOverlay.toGeoJSON().features.reduce(function(obj, v) {
-        obj[ v.properties.ICS_ICB ] = (obj[ v.properties.ICS_ICB ] || 0) + 1;
-        return obj;
-    }, {});
-
-    icbData.features.forEach((element) => {
-        element.properties.CDC_COUNT = icb_cdcCount[element.properties.ICS_ICB] || 0;
-        element.properties.HUB_COUNT = icb_hubCount[element.properties.ICS_ICB] || 0;
-    });
-
-    // Add the Integrated Care Board (IBC) boundaries.
-    for (const [ key, value ] of Object.entries(nhsLookup)) {
-        icbOverlays[key] = L.geoJson(icbData, {
-            onEachFeature: bindPopup,
-            filter: function(feature) {
-                if( feature.properties.REGION === key ) return true;
-            },
-            style: function(feature) {
-                return {
-                    color: '#fff',
-                    fillColor: colorLookup[ feature.properties.REGION ],
-                    fillOpacity: 0.5,
-                    weight: 0.5
-                }
-            }
-        }).addTo(map);
-
-        overlayMaps[`${value} NHS Region`] = icbOverlays[key];
+  const cdcCustomOverlay = L.geoJSON(cdcGeojson, {
+    onEachFeature: bindPopup,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng)
+    },
+    style: function(feature) {
+      return {
+        color: '#fff',
+        fillColor: colorLookup[ feature.properties.REGION ],
+        fillOpacity: 1,
+        radius: 5,
+        weight: 2,
+        pane: 'cdc'
+      }
     }
+  }).addTo(map)
+  const cdcOverlay = cdcCustomOverlay
 
-    overlayMaps['Community Diagnostic Centres (All)'] = cdcCustomOverlay;
-    overlayMaps['Surgical Hubs (All)'] = hubCustomOverlay;
+  //Add the DHSC surgical hub locations.
+  map.createPane('hub')
+  map.getPane('hub').style.zIndex = 651
 
-    const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-});
+  const hubCustomOverlay = L.geoJSON(hubGeojson, {
+    onEachFeature: bindPopup,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng)
+    },
+    style: function(feature) {
+      return {
+        color: '#000',
+        fillColor: '#fff',
+        fillOpacity: 1,
+        radius: 4,
+        weight: 1,
+        pane: 'hub'
+      }
+    }
+  }).addTo(map)
+  const hubOverlay = hubCustomOverlay
 
-/**
- * Binds a popup to the layer with the passed content and sets up the necessary event listeners.
- */
-function bindPopup(feature, layer) {
-    let properties = layer.feature.properties;
-    // properties = (({ column_name, ...o }) => o)(properties);
+  // Generate CDC and surgical hub counts per ICB boundaries.
+  let icb_cdcCount = cdcOverlay.toGeoJSON().features.reduce(function(obj, v) {
+    obj[ v.properties.ICS_ICB ] = (obj[ v.properties.ICS_ICB ] || 0) + 1
+    return obj
+  }, {})
 
-    const codeRegex = /E[\d]+/;
+  let icb_hubCount = hubOverlay.toGeoJSON().features.reduce(function(obj, v) {
+    obj[ v.properties.ICS_ICB ] = (obj[ v.properties.ICS_ICB ] || 0) + 1
+    return obj
+  }, {})
 
-    let content = '<table id="popup-table">';
+  icbGeojson.features.forEach((element) => {
+    element.properties.CDC_COUNT = icb_cdcCount[element.properties.ICS_ICB] || 0
+    element.properties.HUB_COUNT = icb_hubCount[element.properties.ICS_ICB] || 0
+  })
+
+  // Add the Integrated Care Board (IBC) boundaries.
+  for (const [ key, value ] of Object.entries(nhsLookup)) {
+    icbOverlays[key] = L.geoJson(icbGeojson, {
+      onEachFeature: bindPopup,
+      filter: function(feature) {
+        if( feature.properties.REGION === key ) return true
+      },
+      style: function(feature) {
+        return {
+          color: '#fff',
+          fillColor: colorLookup[ feature.properties.REGION ],
+          fillOpacity: 0.5,
+          weight: 0.5
+        }
+      }
+    }).addTo(map)
+
+    overlayMaps[`${value} NHS Region`] = icbOverlays[key]
+  }
+
+  overlayMaps['Community Diagnostic Centres (All)'] = cdcCustomOverlay
+  overlayMaps['Surgical Hubs (All)'] = hubCustomOverlay
+  L.control.layers(baseMaps, overlayMaps).addTo(map)
+
+  // Binds a popup to the layer with the passed content and sets up the necessary event listeners.
+  function bindPopup(feature, layer) {
+    let properties = layer.feature.properties
+    const codeRegex = /E[\d]+/
+
+    let content = '<table id="popup-table">'
     for( let i in properties ) {
-        if( i === 'REGION' && codeRegex.test(properties[i]) )
-            content += `<tr><td>${i}</td><td style="font-weight:600;">${nhsLookup[properties[i]]}</td></tr>`;
-        else if( i === 'ICS_ICB' && codeRegex.test(properties[i]) )
-            content += `<tr><td>${i}</td><td style="font-weight:600;">${icbLookup[properties[i]]}</td></tr>`;
-        else
-            content += `<tr><td>${i}</td><td style="font-weight:600;">${properties[i]}</td></tr>`;
+      if( i === 'REGION' && codeRegex.test(properties[i]) )
+        content += `<tr><td>${i}</td><td style="font-weight:600">${nhsLookup[properties[i]]}</td></tr>`
+      else if( i === 'ICS_ICB' && codeRegex.test(properties[i]) )
+        content += `<tr><td>${i}</td><td style="font-weight:600">${icbLookup[properties[i]]}</td></tr>`
+      else
+        content += `<tr><td>${i}</td><td style="font-weight:600">${properties[i]}</td></tr>`
     }
-    content += '<table>';
+    content += '<table>'
 
-    let popup = layer.bindPopup(content);
+    let popup = layer.bindPopup(content)
     if( layer.feature.geometry.type !== 'Point' ) {
-        popup.on('popupopen', function(e) {
-            e.target.setStyle({ fillOpacity: 0.8 });
-        });
-        popup.on('popupclose', function(e) {
-            e.target.setStyle({ fillOpacity: 0.5 });
-        });
+      popup.on('popupopen', function(e) {
+        e.target.setStyle({ fillOpacity: 0.8 })
+      })
+      popup.on('popupclose', function(e) {
+        e.target.setStyle({ fillOpacity: 0.5 })
+      })
     }
-}
+  }
+})

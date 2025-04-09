0a5c330ec537a5f45150227e928b3bc926f1645a
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-04-09 11:30:21 +0100

    Make content security policy specific to MissionsController, add extra rules
    Fixes data:image CSP error

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,10 @@
 class MissionsController < ApplicationController
   include Cacheable
 
+  content_security_policy do |policy|
+    # The map on this page makes use of the OS api and inline styles
+    policy.img_src(*policy.img_src, "https://api.os.uk", :data)
+  end
+
   def index; end
 end

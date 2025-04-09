b736d917bb945ccc840da4afd40efa655bf4f488
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-04-09 15:31:49 +0100

    refactor: Remove contents-related code from specialist document presenter
    - This code won't be necessary when we convert specialist documents to use a contents outline
      (in next commit)

--- spec/presenter/specialist_document_presenter_spec.rb
+++ spec/presenter/specialist_document_presenter_spec.rb
@@ -1,40 +1,4 @@
 RSpec.describe SpecialistDocumentPresenter do
-  describe "#contents" do
-    it "returns an empty array when there are no headers" do
-      content_store_response = GovukSchemas::Example.find("specialist_document", example_name: "business-finance-support-scheme")
-      content_item = SpecialistDocument.new(content_store_response)
-
-      expect(described_class.new(content_item).contents).to eq([])
-    end
-
-    it "returns a contents list when there are level two headers" do
-      content_store_response = GovukSchemas::Example.find("specialist_document", example_name: "aaib-reports")
-      content_item = SpecialistDocument.new(content_store_response)
-
-      expected_contents = [
-        {
-          href: "#summary",
-          items: [
-            {
-              href: "#download-report",
-              level: 3,
-              text: "Download report",
-            },
-            {
-              href: "#download-glossary-of-abbreviations",
-              level: 3,
-              text: "Download glossary of abbreviations",
-            },
-          ],
-          level: 2,
-          text: "Summary",
-        },
-      ]
-
-      expect(described_class.new(content_item).contents).to eq(expected_contents)
-    end
-  end
-
   describe "#show_finder_link?" do
     it "returns true when document type is statutory instrument" do
       content_store_response = GovukSchemas::Example.find("specialist_document", example_name: "eu-withdrawal-act-2018-statutory-instruments")

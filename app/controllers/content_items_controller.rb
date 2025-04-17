c89f08a6a26159de681f9edf3642adc5efda5f5a
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-04-17 16:49:14 +0100

    Add set_account_vary_header method
     - 'set_account_vary_header' method overrides 'set_account_vary_header' method defined in GovukPersonalisation::ControllerConcern https://github.com/alphagov/govuk_personalisation/blob/90da0152c328b9a1b99aa05528c29f2a32fed9ae/lib/govuk_personalisation/controller_concern.rb#L54 which is called as part of before_action
    
     - This caches pages based on three flash message variations instead of caching pages per user. The Vary header appends "GOVUK-Account-Session-Exists" and "GOVUK-Account-Session-Flash" to the existing Vary header values as explained here https://github.com/alphagov/government-frontend/commit/8e27396ecfdb6d2ea99cd3abf795402593c8d872
    
    - Audit trail for the method: https://github.com/alphagov/government-frontend/blob/41f451f0bfe1552998ab62ab1ed374f284737f63/app/controllers/content_items_controller.rb#L283
    - Audit trail for the test: https://github.com/alphagov/government-frontend/blob/41f451f0bfe1552998ab62ab1ed374f284737f63/test/controllers/content_items_controller_test.rb#L310

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -46,4 +46,10 @@ private
   def deny_framing
     response.headers["X-Frame-Options"] = "DENY"
   end
+
+  def set_account_vary_header
+    # Override the default from GovukPersonalisation::ControllerConcern so pages are cached on each flash message
+    # variation, rather than caching pages per user
+    response.headers["Vary"] = [response.headers["Vary"], "GOVUK-Account-Session-Exists", "GOVUK-Account-Session-Flash"].compact.join(", ")
+  end
 end

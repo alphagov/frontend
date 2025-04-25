fa9ad4f17204079b910b19728a3c15a1466b3cc2
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-04-25 12:36:00 +0100

    Add ContentsOutline to presenter

--- app/controllers/corporate_information_page_controller.rb
+++ app/controllers/corporate_information_page_controller.rb
@@ -1,5 +1,6 @@
 class CorporateInformationPageController < ContentItemsController
   def show
     @presenter = CorporateInformationPagePresenter.new(@content_item)
+    @contents_outline_presenter = ContentsOutlinePresenter.new(@presenter.contents_list_headings)
   end
 end

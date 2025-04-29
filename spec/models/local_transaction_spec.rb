db67026ba28bbf9f34878423007dc03f92f5a7c4
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-04-29 14:13:41 +0100

    refactor: Improve local transaction tests with subject/let

--- spec/models/local_transaction_spec.rb
+++ spec/models/local_transaction_spec.rb
@@ -1,26 +1,25 @@
 RSpec.describe LocalTransaction do
+  subject(:local_transaction) { described_class.new(content_store_response) }
+
   let(:content_store_response) do
     GovukSchemas::Example.find("local_transaction", example_name: "local_transaction_with_devolved_administration_availability")
   end
 
   describe "#introduction" do
     it "gets the introduction from the content store response" do
-      content_item = described_class.new(content_store_response)
-      expect(content_item.introduction).to eq(content_store_response["details"]["introduction"])
+      expect(local_transaction.introduction).to eq(content_store_response["details"]["introduction"])
     end
   end
 
   describe "#more_information" do
     it "gets more_information from the content store response" do
-      content_item = described_class.new(content_store_response)
-      expect(content_item.more_information).to eq(content_store_response["details"]["more_information"])
+      expect(local_transaction.more_information).to eq(content_store_response["details"]["more_information"])
     end
   end
 
   describe "#need_to_know" do
     it "gets need_to_know from the content store response" do
-      content_item = described_class.new(content_store_response)
-      expect(content_item.need_to_know).to eq(content_store_response["details"]["need_to_know"])
+      expect(local_transaction.need_to_know).to eq(content_store_response["details"]["need_to_know"])
     end
   end
 
@@ -35,17 +34,15 @@ RSpec.describe LocalTransaction do
       content_store_response["details"]["northern_ireland_availability"] = {
         "type" => "unavailable",
       }
-      content_item = described_class.new(content_store_response)
 
       ["Scotland", "Northern Ireland", "Wales"].each do |country_name|
-        content_item.set_country(country_name)
-        expect(content_item.unavailable?).to be true
+        local_transaction.set_country(country_name)
+        expect(local_transaction.unavailable?).to be true
       end
     end
 
     it "returns false when the country name hasn't been set" do
-      content_item = described_class.new(content_store_response)
-      expect(content_item.unavailable?).to be false
+      expect(local_transaction.unavailable?).to be false
     end
   end
 
@@ -60,17 +57,15 @@ RSpec.describe LocalTransaction do
       content_store_response["details"]["northern_ireland_availability"] = {
         "type" => "devolved_administration_service",
       }
-      content_item = described_class.new(content_store_response)
 
       ["Scotland", "Northern Ireland", "Wales"].each do |country_name|
-        content_item.set_country(country_name)
-        expect(content_item.devolved_administration_service?).to be true
+        local_transaction.set_country(country_name)
+        expect(local_transaction.devolved_administration_service?).to be true
       end
     end
 
     it "returns false when the country name hasn't been set" do
-      content_item = described_class.new(content_store_response)
-      expect(content_item.devolved_administration_service?).to be false
+      expect(local_transaction.devolved_administration_service?).to be false
     end
   end
 
@@ -81,10 +76,9 @@ RSpec.describe LocalTransaction do
         "alternative_url" => "https://www.gov.scot/stray-dog",
       }
 
-      content_item = described_class.new(content_store_response)
-      content_item.set_country("Scotland")
+      local_transaction.set_country("Scotland")
 
-      expect(content_item.devolved_administration_service_alternative_url)
+      expect(local_transaction.devolved_administration_service_alternative_url)
         .to eq("https://www.gov.scot/stray-dog")
     end
 
@@ -93,27 +87,27 @@ RSpec.describe LocalTransaction do
         "type" => "unavailable",
       }
 
-      content_item = described_class.new(content_store_response)
-      content_item.set_country("Scotland")
+      local_transaction.set_country("Scotland")
 
-      expect(content_item.devolved_administration_service_alternative_url).to be_nil
+      expect(local_transaction.devolved_administration_service_alternative_url).to be_nil
     end
 
     it "does not return an alternative_url for a non devolved administration" do
       content_store_response["details"]["scotland_availability"] = {}
       content_store_response["details"]["wales_availability"] = {}
       content_store_response["details"]["northern_ireland_availability"] = {}
-      content_item = described_class.new(content_store_response)
 
-      expect(content_item.devolved_administration_service_alternative_url).to be_nil
+      expect(local_transaction.devolved_administration_service_alternative_url).to be_nil
     end
   end
 
   describe "#slug" do
+    let(:content_store_response) do
+      GovukSchemas::Example.find("local_transaction", example_name: "local_transaction").merge("base_path" => "/foo/bar")
+    end
+
     it "returns the subject slug" do
-      content_store_response = GovukSchemas::Example.find("local_transaction", example_name: "local_transaction")
-      content_store_response["base_path"] = "/foo/bar"
-      expect(described_class.new(content_store_response).slug).to eq("foo/bar")
+      expect(local_transaction.slug).to eq("foo/bar")
     end
   end
 end

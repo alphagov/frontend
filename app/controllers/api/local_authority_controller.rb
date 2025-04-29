29af52edc1e365454d2c803080e5f1d853750b46
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-04-29 13:35:12 +0100

    feature: Add index action with query to local authority controller
    - Trying to stay as close to a standard RESTful interface as we can, we note that
      the resource is the local authority, and a common pattern for search is to put the
      search on the index of the resource via a query parameter.
    
    - We don't want at the moment to provide a traditional paged index of all local
      authorities, so the index exists just as the query point, and in the golden path
      we can just redirect directly to the show path introduced in the previous commit.
    
    - Since we've simplified the code in the existing find_local_council controller,
      (https://github.com/alphagov/frontend/pull/4705) the index action can be close to
      a direct copy of the web interface's find action.
    
    - the query is a GET - in the web interface it's a POST, but that's largely
      for privacy. The lookup is extremely cacheable at the CDN level - it might
      change less than one time a year - so we might as well take advantage
      of that.
    
    - status codes are 301 (redirect straight to a local authority url to be added
      in a subsequent commit), 200 (a list of addresses to choose from if the postcode
      crosses boundaries), 404 (postcode is structurally valid but not found in the
      database), or 400 (postcode query parameter is missing or not valid).

--- app/controllers/api/local_authority_controller.rb
+++ app/controllers/api/local_authority_controller.rb
@@ -1,12 +1,33 @@
 # rubocop:disable Rails/ApplicationController
 module Api
   class LocalAuthorityController < ActionController::Base
+    def index
+      postcode_lookup = PostcodeLookup.new(postcode)
+
+      if postcode_lookup.local_custodian_codes.count == 1
+        local_authority = LocalAuthority.from_local_custodian_code(postcode_lookup.local_custodian_codes.first)
+        redirect_to "/api/local-authority/#{local_authority.slug}"
+      else
+        @address_list_presenter = AddressListPresenter.new(postcode_lookup.addresses)
+        render json: { addresses: @address_list_presenter.addresses_with_authority_data }
+      end
+    rescue LocationError => e
+      status = e.postcode_error == "invalidPostcodeFormat" ? :bad_request : :not_found
+      render json: { errors: { postcode: [I18n.t(e.message)] } }, status:
+    end
+
     def show
       @local_authority = LocalAuthority.from_slug(params[:authority_slug])
 
       render json: { local_authority: @local_authority.to_h }
     rescue GdsApi::HTTPNotFound
       render json: { errors: { local_authority_slug: ["Not found"] } }, status: :not_found
+    end
+
+  private
+
+    def postcode
+      @postcode ||= PostcodeSanitizer.sanitize(params[:postcode])
     end
   end
 end
--- spec/requests/local_authority_api_spec.rb
+++ spec/requests/local_authority_api_spec.rb
@@ -2,8 +2,76 @@ require "gds_api/test_helpers/local_links_manager"
 
 RSpec.describe "Local Authority API" do
   include GdsApi::TestHelpers::LocalLinksManager
+  include LocationHelpers
 
   before { content_store_has_random_item(base_path: "/api/local-authority") }
+
+  describe "querying" do
+    it "returns a 400 if there is no query parameter" do
+      get "/api/local-authority"
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "returns a 400 if the postcode is empty" do
+      get "/api/local-authority?postcode="
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "returns a 400 if the postcode is invalid" do
+      stub_locations_api_does_not_have_a_bad_postcode("ZZZ")
+      get "/api/local-authority?postcode=ZZZ"
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "returns a 404 if the postcode is not found" do
+      stub_locations_api_has_no_location("SW12 1ZZ")
+      get "/api/local-authority?postcode=SW121ZZ"
+
+      expect(response).to have_http_status(:not_found)
+    end
+
+    context "when a postcode is matched to a single authority" do
+      before { configure_locations_api_and_local_authority("SW1A 1AA", %w[westminster], 5990) }
+
+      it "redirects to the local authority" do
+        get "/api/local-authority?postcode=SW1A1AA"
+
+        expect(response).to redirect_to("/api/local-authority/westminster")
+      end
+    end
+
+    context "when a postcode spans multiple authorities" do
+      before do
+        stub_locations_api_has_location(
+          "CH25 9BJ",
+          [
+            { "address" => "House 1", "local_custodian_code" => "1" },
+            { "address" => "House 2", "local_custodian_code" => "2" },
+            { "address" => "House 3", "local_custodian_code" => "3" },
+          ],
+        )
+        stub_local_links_manager_has_a_local_authority("achester", local_custodian_code: 1)
+        stub_local_links_manager_has_a_local_authority("beechester", local_custodian_code: 2)
+        stub_local_links_manager_has_a_local_authority("ceechester", local_custodian_code: 3)
+      end
+
+      it "returns a list of addresses to choose from" do
+        get "/api/local-authority?postcode=CH259BJ"
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to eq({
+          "addresses" => [
+            { "address" => "House 1", "local_authority_slug" => "achester", "local_authority_name" => "Achester" },
+            { "address" => "House 2", "local_authority_slug" => "beechester", "local_authority_name" => "Beechester" },
+            { "address" => "House 3", "local_authority_slug" => "ceechester", "local_authority_name" => "Ceechester" },
+          ],
+        })
+      end
+    end
+  end
 
   describe "local authority slugs" do
     context "when the slug is not found"

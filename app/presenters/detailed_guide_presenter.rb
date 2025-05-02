bd4ec9e7707f51923bec666737dd1a2ab47a5f03
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-05-02 11:43:11 +0100

    Update Detailed guide presenter
    - As implemented in corporate information pages- https://github.com/alphagov/frontend/blob/main/app/presenters/corporate_information_page_presenter.rb, the contents list are rendered by retrieving the headers from the contend_item and rendering them accordingly

--- app/presenters/detailed_guide_presenter.rb
+++ app/presenters/detailed_guide_presenter.rb
@@ -1,2 +1,24 @@
 class DetailedGuidePresenter < ContentItemPresenter
+  def headers_for_contents_list_component
+    @headers = contents_list_headings
+    return [] unless content_item.headers
+
+    ContentsOutlinePresenter.new(@headers).for_contents_list_component
+  end
+
+private
+
+  def contents_list_headings
+    content_item.headers
+
+    exclude_nested_headings
+
+    ContentsOutline.new(content_item.headers) if content_item.headers.present?
+  end
+
+  def exclude_nested_headings
+    content_item.headers.each do |header|
+      header.delete("headers") unless header["headers"].nil?
+    end
+  end
 end

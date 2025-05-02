6d78e9ff6b8dd017689c29a5b26bdd9965cdd0e2
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-05-02 11:43:11 +0100

    [WIP] Add ContentsList

--- app/presenters/detailed_guide_presenter.rb
+++ app/presenters/detailed_guide_presenter.rb
@@ -1,2 +1,9 @@
 class DetailedGuidePresenter < ContentItemPresenter
+  def headers_for_content_list_component
+    return [] unless content_item.headers
+
+    ContentsOutlinePresenter.new(contents_list_headings).for_contents_list_component
+  end
+
+  def contents_list_headings; end
 end

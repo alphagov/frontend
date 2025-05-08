854e5e827d0c1c1869fe9bdef26c25aa16b11eb6
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-05-08 11:33:03 +0100

    Copy service_manual_service_standard presenter from government-frontend
    Removes the 'poster' functionality, as that's no longer used.
    
    Audit trail: https://github.com/alphagov/government-frontend/blob/94416eb1636589415edd208ec67ea3494a2e3e24/app/presenters/service_manual_service_standard_presenter.rb

--- app/controllers/service_manual_service_standard_controller.rb
+++ app/controllers/service_manual_service_standard_controller.rb
@@ -1,5 +1,7 @@
 class ServiceManualServiceStandardController < ContentItemsController
   include Cacheable
 
-  def index; end
+  def index
+    @presenter = ServiceManualServiceStandardPresenter.new(@content_item)
+  end
 end

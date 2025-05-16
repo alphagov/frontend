2401bf0766bd21ccd54af6ddf6c6342039412e0b
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-05-16 09:29:07 +0100

    Modify DetailedGuide model - Retrieve headers

--- app/models/detailed_guide.rb
+++ app/models/detailed_guide.rb
@@ -5,6 +5,14 @@ class DetailedGuide < ContentItem
   include SinglePageNotificationButton
   include Updatable
 
+  attr_reader :headers
+
+  def initialize(content_store_response)
+    super(content_store_response)
+
+    @headers = content_store_response.dig("details", "headers") || []
+  end
+
   def contributors
     organisations_ordered_by_emphasis.uniq(&:content_id)
   end

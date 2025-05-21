7b0093a5fdc7e35e4cc3ae22eceb2d35b13f609b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-05-21 17:55:20 +0100

    Convert map code into a GOV.UK JS module
    - remove old map code and add new module and test file
    - add delayed check for cookie consent with analytics
    - add styling to make map error state stand out a little more

--- app/assets/javascripts/test-dependencies.js
+++ app/assets/javascripts/test-dependencies.js
@@ -2,3 +2,4 @@
 // environment. In the dev and live environment these are provided by static.
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib/cookie-functions
+//= require leaflet

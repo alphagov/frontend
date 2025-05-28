9a4792d900e32888b13611f06654bc5cde27b7fd
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-05-28 16:45:44 +0100

    Add Attachments concern and tests
    These methods are used in the `attachments_list` partial, which will
    also be included on consultation and publication pages.
    
    There were no existing tests and have now been added.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/0adbc42e5591f9ad914791306b1ae2558f9d3cfc/app/presenters/content_item/attachments.rb

---
+++ app/models/concerns/attachments.rb
@@ -0,0 +1,28 @@
+module Attachments
+  extend ActiveSupport::Concern
+
+  included do
+    def attachments
+      return [] unless document_attachments
+
+      docs = document_attachments.select { |a| !a.key?("locale") || a["locale"] == locale }
+
+      docs.each do |doc|
+        doc["type"] = "html" unless doc["content_type"]
+        doc["type"] = "external" if doc["attachment_type"] == "external"
+        doc["preview_url"] = "#{doc['url']}/preview" if doc["preview_url"]
+        doc["alternative_format_contact_email"] = nil if doc["accessible"] == true
+      end
+    end
+
+    def attachments_from(attachment_id_list)
+      attachments.select { |doc| (attachment_id_list || []).include? doc["id"] }
+    end
+  end
+
+private
+
+  def document_attachments
+    @document_attachments ||= content_store_response.dig("details", "attachments")
+  end
+end

822376a5c241734222584fd851d372e01294df05
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-05-29 17:31:33 +0100

    Add CallForEvidence presenter
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/app/presenters/call_for_evidence_presenter.rb#L11-L29
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/test/presenters/call_for_evidence_presenter_test.rb#L26-L61

---
+++ app/presenters/call_for_evidence_presenter.rb
@@ -0,0 +1,29 @@
+class CallForEvidencePresenter < ContentItemPresenter
+  def opening_date
+    display_date_and_time(content_item.opening_date_time)
+  end
+
+  def opening_date_midnight?
+    Time.zone.parse(content_item.opening_date_time).strftime("%l:%M%P") == "12:00am"
+  end
+
+  def closing_date
+    display_date_and_time(content_item.closing_date_time, rollback_midnight: true)
+  end
+
+private
+
+  def display_date_and_time(date, rollback_midnight: false)
+    time = Time.zone.parse(date)
+    date_format = "%-e %B %Y"
+    time_format = "%l:%M%P"
+
+    if rollback_midnight && (time.strftime(time_format) == "12:00am")
+      # 12am, 12:00am and "midnight on" can all be misinterpreted
+      # Use 11:59pm on the day before to remove ambiguity
+      # 12am on 10 January becomes 11:59pm on 9 January
+      time -= 1.second
+    end
+    I18n.l(time, format: "#{time_format} on #{date_format}").gsub(":00", "").gsub("12pm", "midday").gsub("12am on ", "").strip
+  end
+end
---
+++ spec/presenter/call_for_evidence_presenter_spec.rb
@@ -0,0 +1,53 @@
+RSpec.describe CallForEvidencePresenter do
+  let(:content_store_response) { GovukSchemas::Example.find("call_for_evidence", example_name: "open_call_for_evidence") }
+  let(:content_item) { CallForEvidence.new(content_store_response) }
+  let(:presenter) { described_class.new(content_item) }
+
+  describe "#opening_date" do
+    it "presents a friendly opening date and time" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-12-16T16:00:00+00:00"
+
+      expect(presenter.opening_date).to eq("4pm on 16 December 2016")
+    end
+
+    it "presents an opening date without a time if the time is 12am" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-11-03T00:00:00+00:00"
+
+      expect(presenter.opening_date).to eq("3 November 2016")
+    end
+
+    it "presents 12pm as midday" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-11-04T12:00:00+00:00"
+
+      expect(presenter.opening_date).to eq("midday on 4 November 2016")
+    end
+  end
+
+  describe "#closing_date" do
+    it "presents a friendly closing date and time" do
+      content_item.content_store_response["details"]["closing_date"] = "2016-12-16T16:00:00+00:00"
+
+      expect(presenter.closing_date).to eq("4pm on 16 December 2016")
+    end
+
+    it "presents a closing date and time as 11:59am on the day before if the time is 12am" do
+      content_item.content_store_response["details"]["closing_date"] = "2016-11-04T00:00:00+00:00"
+
+      expect(presenter.closing_date).to eq("11:59pm on 3 November 2016")
+    end
+  end
+
+  describe "#opening_date_midnight?" do
+    it "returns true if the opening time is 12am" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-11-04T00:00:00+00:00"
+
+      expect(presenter.opening_date_midnight?).to be(true)
+    end
+
+    it "returns false if the opening time is not 12am" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-04-16T13:01:57.000+00:00"
+
+      expect(presenter.opening_date_midnight?).to be(false)
+    end
+  end
+end

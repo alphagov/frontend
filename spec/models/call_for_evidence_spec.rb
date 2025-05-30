4f23f964d91894d2a0e7b379a9bbff5a56a2d840
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-05-30 13:47:40 +0100

    Update model to return document dates
    There were no existing tests and have now been added.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/app/presenters/call_for_evidence_presenter.rb#L11-L17

--- spec/models/call_for_evidence_spec.rb
+++ spec/models/call_for_evidence_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe CallForEvidence do
   let(:open_call_for_evidence) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "open_call_for_evidence")) }
   let(:call_for_evidence_outcome_with_featured_attachments) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "call_for_evidence_outcome_with_featured_attachments")) }
+  let(:closed_call_for_evidence) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "closed_call_for_evidence")) }
 
   it_behaves_like "it can have attachments", "call_for_evidence", "call_for_evidence_outcome_with_featured_attachments"
   it_behaves_like "it can have national applicability", "call_for_evidence", "call_for_evidence_outcome_with_featured_attachments"
@@ -19,6 +20,18 @@ RSpec.describe CallForEvidence do
       it "returns a list of organisations followed by people" do
         expect(open_call_for_evidence.contributors[1].title).to eq("The Rt Hon Baroness Smith of Malvern")
         expect(open_call_for_evidence.contributors[1].base_path).to eq("/government/people/jacqui-smith")
+      end
+    end
+
+    describe "#opening_date_time" do
+      it "returns the opening date and time" do
+        expect(closed_call_for_evidence.opening_date_time).to eq("2022-09-29T14:00:00+01:00")
+      end
+    end
+
+    describe "#closing_date_time" do
+      it "returns the closing date and time" do
+        expect(closed_call_for_evidence.closing_date_time).to eq("2022-10-27T17:00:00+01:00")
       end
     end
   end

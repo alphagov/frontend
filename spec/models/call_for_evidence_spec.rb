6611f5653551991a335a7f1b5b3097febd66c71f
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-05-30 13:55:55 +0100

    Update model to return document types
    Call for evidence documents have different stages (by document type).
    They are used in the view to determine which sections of information
    are shown.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/app/presenters/call_for_evidence_presenter.rb#L31-L45
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/test/presenters/call_for_evidence_presenter_test.rb#L63-L75

--- spec/models/call_for_evidence_spec.rb
+++ spec/models/call_for_evidence_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe CallForEvidence do
   let(:open_call_for_evidence) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "open_call_for_evidence")) }
   let(:call_for_evidence_outcome_with_featured_attachments) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "call_for_evidence_outcome_with_featured_attachments")) }
+  let(:unopened_call_for_evidence) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "unopened_call_for_evidence")) }
   let(:closed_call_for_evidence) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "closed_call_for_evidence")) }
+  let(:call_for_evidence_outcome) { described_class.new(GovukSchemas::Example.find("call_for_evidence", example_name: "call_for_evidence_outcome")) }
 
   it_behaves_like "it can have attachments", "call_for_evidence", "call_for_evidence_outcome_with_featured_attachments"
   it_behaves_like "it can have national applicability", "call_for_evidence", "open_call_for_evidence"
@@ -32,6 +34,50 @@ RSpec.describe CallForEvidence do
     describe "#closing_date_time" do
       it "returns the closing date and time" do
         expect(closed_call_for_evidence.closing_date_time).to eq("2022-10-27T17:00:00+01:00")
+      end
+    end
+
+    describe "#open?" do
+      it "returns true for an open_call_for_evidence document type" do
+        expect(open_call_for_evidence.open?).to be(true)
+      end
+
+      it "returns false if it is not an open_call_for_evidence document type" do
+        expect(unopened_call_for_evidence.open?).to be(false)
+        expect(closed_call_for_evidence.open?).to be(false)
+        expect(call_for_evidence_outcome.open?).to be(false)
+      end
+    end
+
+    describe "#closed?" do
+      it "returns true for closed_call_for_evidence or call_for_evidence_outcome document types" do
+        expect(unopened_call_for_evidence.closed?).to be(false)
+        expect(open_call_for_evidence.closed?).to be(false)
+
+        expect(closed_call_for_evidence.closed?).to be(true)
+        expect(call_for_evidence_outcome.closed?).to be(true)
+      end
+    end
+
+    describe "#unopened?" do
+      it "returns true if it has the unopened_call_for_evidence document type" do
+        expect(unopened_call_for_evidence.unopened?).to be(true)
+
+        expect(open_call_for_evidence.unopened?).to be(false)
+        expect(closed_call_for_evidence.unopened?).to be(false)
+        expect(call_for_evidence_outcome.unopened?).to be(false)
+      end
+    end
+
+    describe "#outcome?" do
+      it "returns true for an the call_for_evidence_outcome document type" do
+        expect(call_for_evidence_outcome.outcome?).to be(true)
+      end
+
+      it "returns false if it does not have the call_for_evidence_outcome document type" do
+        expect(unopened_call_for_evidence.outcome?).to be(false)
+        expect(open_call_for_evidence.outcome?).to be(false)
+        expect(closed_call_for_evidence.outcome?).to be(false)
       end
     end
   end

f3ba1690fd5eec0af537e27b5f824ef99b56520b
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-05-30 13:47:40 +0100

    Update model to return document dates
    There were no existing tests and have now been added.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/app/presenters/call_for_evidence_presenter.rb#L11-L17

--- app/models/call_for_evidence.rb
+++ app/models/call_for_evidence.rb
@@ -9,4 +9,12 @@ class CallForEvidence < ContentItem
   def contributors
     organisations + people
   end
+
+  def opening_date_time
+    content_store_response.dig("details", "opening_date")
+  end
+
+  def closing_date_time
+    content_store_response.dig("details", "closing_date")
+  end
 end

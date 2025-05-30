ec50f5b478aedcfb4319ada1c8f430a0c24e307f
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-05-30 16:38:46 +0100

    Update model to handle document contact details
    The methods `ways_to_respond?` and `response_form?` are called in
    the view to determine whether certain sections are shown on the
    page. They were originally returning the first contact details
    available in their check, but we only care about whether they are
    present or not.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/app/presenters/call_for_evidence_presenter.rb#L75-L97
    - https://github.com/alphagov/government-frontend/blob/ef31d532d50a1ae80e8831f2f6eac60f54e06ec9/test/presenters/call_for_evidence_presenter_test.rb#L107-L148

--- app/models/call_for_evidence.rb
+++ app/models/call_for_evidence.rb
@@ -61,4 +61,34 @@ class CallForEvidence < ContentItem
   def held_on_another_website_url
     content_store_response.dig("details", "held_on_another_website_url")
   end
+
+  def ways_to_respond?
+    open? && (respond_online_url.present? || email.present? || postal_address.present?)
+  end
+
+  def email
+    ways_to_respond["email"] if ways_to_respond
+  end
+
+  def postal_address
+    ways_to_respond["postal_address"] if ways_to_respond
+  end
+
+  def respond_online_url
+    ways_to_respond["link_url"] if ways_to_respond
+  end
+
+  def attachment_url
+    ways_to_respond["attachment_url"] if ways_to_respond
+  end
+
+  def response_form?
+    attachment_url.present? && (email.present? || postal_address.present?)
+  end
+
+private
+
+  def ways_to_respond
+    @ways_to_respond ||= content_store_response.dig("details", "ways_to_respond")
+  end
 end

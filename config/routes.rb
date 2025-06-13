4f5480a82c6f433e84875caeda162b8dc609b295
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-06-13 09:41:00 +0100

    Add placeholder controller/view/route for service_manual_guide

--- config/routes.rb
+++ config/routes.rb
@@ -111,6 +111,7 @@ Rails.application.routes.draw do
   scope "/service-manual" do
     get "/", to: "service_manual#index"
     get "/service-standard", to: "service_manual#service_standard"
+    get "/:slug/:document", to: "service_manual#service_manual_guide"
   end
 
   # Service toolkit page

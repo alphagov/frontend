002391e7cc1ee738bf2f5e2d899b16ce948c302c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-06-17 11:30:17 +0100

    Add updated-at to document list metadata
    - Copied from the group_document_link_public_updated_at method used in government-frontend
    
    Audit Trail:
    https://github.com/alphagov/government-frontend/blob/ef6b603fbe4a3cc4fe355854d00d00634039c1b8/app/presenters/document_collection_presenter.rb#L60C1-L73C6

--- app/presenters/document_collection_presenter.rb
+++ app/presenters/document_collection_presenter.rb
@@ -15,7 +15,27 @@ class DocumentCollectionPresenter < ContentItemPresenter
           text: document.title,
           path: document.base_path,
         },
+        metadata: {
+          public_updated_at: sanitised_updated_at(document),
+        },
       }
     end
+  end
+
+private
+
+  def sanitised_updated_at(document)
+    disallowed_document_types = %w[answer
+                                   completed_transaction
+                                   guide
+                                   local_transaction
+                                   place
+                                   simple_smart_answer
+                                   transaction
+                                   smart_answer]
+
+    return nil if disallowed_document_types.include?(document.document_type)
+
+    document.public_updated_at&.then { |time| Time.zone.parse(time) }
   end
 end

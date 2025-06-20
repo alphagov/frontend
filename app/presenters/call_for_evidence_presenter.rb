e14edcf870c7f8a9048bac004fc9a828920157d3
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-06-20 15:33:18 +0100

    Move logic out of view into presenter
    The view renders different text depending on the document type
    (unopened, open, closed, outcome). This is an attempt at making the
    view less confusing, and taking care of some of the logic in the
    presenter.
    
    We will dry up the code in subsequent commits.

--- app/presenters/call_for_evidence_presenter.rb
+++ app/presenters/call_for_evidence_presenter.rb
@@ -3,15 +3,29 @@ class CallForEvidencePresenter < ContentItemPresenter
     display_date_and_time(content_item.opening_date_time)
   end
 
-  def opening_date_midnight?
-    Time.zone.parse(content_item.opening_date_time).strftime("%l:%M%P") == "12:00am"
-  end
-
   def closing_date
     display_date_and_time(content_item.closing_date_time, rollback_midnight: true)
   end
 
+  def on_or_at
+    opening_date_midnight? ? I18n.t("formats.call_for_evidence.on") : I18n.t("formats.call_for_evidence.at")
+  end
+
+  def opens_closes_or_ran
+    if content_item.closed?
+      I18n.t("formats.call_for_evidence.ran_from")
+    elsif content_item.open?
+      I18n.t("formats.call_for_evidence.closes_at")
+    else
+      "#{I18n.t('formats.call_for_evidence.opens')} #{I18n.t('formats.call_for_evidence.at')}"
+    end
+  end
+
 private
+
+  def opening_date_midnight?
+    Time.zone.parse(content_item.opening_date_time).strftime("%l:%M%P") == "12:00am"
+  end
 
   def display_date_and_time(date, rollback_midnight: false)
     time = Time.zone.parse(date)

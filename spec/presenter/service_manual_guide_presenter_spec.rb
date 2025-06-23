17faa2a42b271c47403bec80f018213fe9b88873
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-06-23 12:25:29 +0100

    Convert presenter tests to rspec

--- spec/presenter/service_manual_guide_presenter_spec.rb
+++ spec/presenter/service_manual_guide_presenter_spec.rb
@@ -1,64 +1,75 @@
-require "presenter_test_helper"
+RSpec.describe ServiceManualGuidePresenter do
+  let(:content_store_response) { GovukSchemas::Example.find("service_manual_guide", example_name: "service_manual_guide") }
 
-class ServiceManualGuidePresenterTest < PresenterTestCase
-  def schema_name
-    "service_manual_guide"
+  let(:content_item) do
+    ServiceManualGuide.new(content_store_response)
   end
 
-  test "presents the basic details required to display a Service Manual Guide" do
-    assert_equal "Agile Delivery", presented_item.title
-    assert_equal "service_manual_guide", presented_item.document_type
-    assert presented_item.body.size > 10
-    assert presented_item.header_links.size >= 1
-
-    content_owner = presented_item.content_owners.first
-    assert content_owner.title.present?
-    assert content_owner.href.present?
+  let(:presenter) do
+    described_class.new(content_item)
   end
 
-  test "breadcrumbs have a root and a topic link" do
-    guide = presented_item
-    assert_equal [
+  it "presents the basic details required to display a Service Manual Guide" do
+    expect(presenter.header_links.size >= 1).to be(true)
+
+    content_owner = presenter.content_owners.first
+    expect(content_owner.title.present?).to be(true)
+    expect(content_owner.href.present?).to be(true)
+  end
+
+  it "breadcrumbs have a root and a topic link" do
+    expect(presenter.breadcrumbs).to eq([
       { title: "Service manual", url: "/service-manual" },
       { title: "Agile", url: "/service-manual/agile" },
-    ],
-                 guide.breadcrumbs
+    ])
   end
 
-  test "breadcrumbs gracefully omit topic if it's not present" do
-    presented_item = presented_item(schema_name, "links" => {})
-    assert_equal [
+  it "breadcrumbs gracefully omit topic if it's not present" do
+    content_store_response["links"]["service_manual_topics"] = {}
+    content_store_response["links"]["parent"] = {}
+    content_item = ServiceManualGuide.new(content_store_response)
+    presenter = described_class.new(content_item)
+
+    expect(presenter.breadcrumbs).to eq([
       { title: "Service manual", url: "/service-manual" },
-    ],
-                 presented_item.breadcrumbs
+    ])
   end
 
-  test "#category_title is the title of the category" do
-    guide = presented_item
-    assert_equal "Agile", guide.category_title
+  it "#category_title is the title of the category" do
+    expect(presenter.category_title).to eq("Agile")
   end
 
-  test "#category_title is the title of the parent for a point" do
-    content_item = GovukSchemas::Example.find("service_manual_guide", example_name: "point_page")
+  describe "#category_title" do
+    context "when there are no links" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("service_manual_guide", example_name: "service_manual_guide").tap { |i| i["links"] = {} }
+      end
 
-    presenter = present_example(content_item)
+      it "returns nil" do
+        expect(presenter.category_title).to be_nil
+      end
+    end
 
-    assert presenter.category_title, "The Service Standard"
+    context "when looking at a point page under the service standard" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("service_manual_guide", example_name: "point_page")
+      end
+
+      it "#category_title is the title of the parent for a point" do
+        expect(presenter.category_title).to eq("The Service Standard")
+      end
+    end
   end
 
-  test "#category_title can be empty" do
-    guide = presented_item(schema_name, "links" => {})
-    assert_nil guide.category_title
-  end
-
-  test "#content_owners when stored in the links" do
-    guide = presented_item(schema_name,
-                           "details" => { "content_owner" => nil },
-                           "links" => { "content_owners" => [{
-                             "content_id" => "e5f09422-bf55-417c-b520-8a42cb409814",
-                             "title" => "Agile delivery community",
-                             "base_path" => "/service-manual/communities/agile-delivery-community",
-                           }] })
+  it "#content_owners when stored in the links" do
+    content_store_response["details"] = { "content_owner" => nil }
+    content_store_response["links"] = { "content_owners" => [{
+      "content_id" => "e5f09422-bf55-417c-b520-8a42cb409814",
+      "title" => "Agile delivery community",
+      "base_path" => "/service-manual/communities/agile-delivery-community",
+    }] }
+    content_item = ServiceManualGuide.new(content_store_response)
+    presenter = described_class.new(content_item)
 
     expected = [
       ServiceManualGuidePresenter::ContentOwner.new(
@@ -66,65 +77,75 @@ class ServiceManualGuidePresenterTest < PresenterTestCase
         "/service-manual/communities/agile-delivery-community",
       ),
     ]
-    assert_equal expected, guide.content_owners
+
+    expect(presenter.content_owners).to eq(expected)
   end
 
-  test "#show_description? is false if not set" do
-    assert_not presented_item(schema_name, {}).show_description?
+  it "#show_description? is false if not set" do
+    content_store_response["details"]["show_description"] = nil
+    content_item = ServiceManualGuide.new(content_store_response)
+    presenter = described_class.new(content_item)
+
+    expect(presenter.show_description?).to be(false)
   end
 
-  test "#public_updated_at returns a time" do
-    assert_kind_of Time, presented_item.public_updated_at
+  it "#public_updated_at returns a time" do
+    expect(presenter.public_updated_at).to be_a(Time)
   end
 
-  test "#public_updated_at returns nil if not available" do
-    example = govuk_content_schema_example("service_manual_guide", "service_manual_guide")
-    content_item = example.merge("public_updated_at" => nil)
-    guide = present_example(content_item)
+  it "#public_updated_at returns nil if not available" do
+    content_store_response["public_updated_at"] = nil
+    content_item = ServiceManualGuide.new(content_store_response)
+    presenter = described_class.new(content_item)
 
-    assert_nil guide.public_updated_at
+    expect(presenter.public_updated_at).to be_nil
   end
 
-  test "#visible_updated_at returns the public_updated_at" do
+  it "#visible_updated_at returns the public_updated_at" do
     timestamp = "2015-10-10T09:00:00+00:00"
-    guide = presented_item(schema_name, "public_updated_at" => timestamp)
+    content_store_response["public_updated_at"] = timestamp
+    content_item = ServiceManualGuide.new(content_store_response)
+    presenter = described_class.new(content_item)
 
-    assert_equal guide.visible_updated_at, Time.zone.parse(timestamp)
+    expect(presenter.visible_updated_at).to eq(Time.zone.parse(timestamp))
   end
 
-  test "#visible_updated_at returns the updated_at time if the public_updated_at hasn't yet been set" do
+  it "#visible_updated_at returns the updated_at time if the public_updated_at hasn't yet been set" do
     timestamp = "2015-10-10T09:00:00+00:00"
-    example = schema_item("service_manual_guide", "service_manual_guide")
-    content_item = example.merge("updated_at" => timestamp, "public_updated_at" => nil)
-    guide = present_example(content_item)
+    content_store_response["updated_at"] = timestamp
+    content_store_response["public_updated_at"] = nil
+    content_item = ServiceManualGuide.new(content_store_response)
+    presenter = described_class.new(content_item)
 
-    assert_equal guide.visible_updated_at, Time.zone.parse(timestamp)
+    expect(presenter.visible_updated_at).to eq(Time.zone.parse(timestamp))
   end
 
-  test "#latest_change returns the details for the most recent change" do
+  it "#latest_change returns the details for the most recent change" do
     expected_history = ServiceManualGuidePresenter::Change.new(
       Time.zone.parse("2015-10-09T08:17:10+00:00"),
       "This is our latest change",
     )
 
-    assert_equal expected_history, presented_item.latest_change
+    expect(presenter.latest_change).to eq(expected_history)
   end
 
-  test "#latest_change timestamp is the updated_at time if public_updated_at hasn't been set" do
+  it "#latest_change timestamp is the updated_at time if public_updated_at hasn't been set" do
     timestamp = "2015-10-07T09:00:00+00:00"
-    example = schema_item("service_manual_guide", "service_manual_guide")
-    content_item = example.merge("updated_at" => timestamp, "public_updated_at" => nil)
-    guide = present_example(content_item)
+    content_store_response["updated_at"] = timestamp
+    content_store_response["public_updated_at"] = nil
+
+    content_item = ContentItem.new(content_store_response)
+    presenter = described_class.new(content_item)
 
     expected_history = ServiceManualGuidePresenter::Change.new(
       Time.zone.parse(timestamp),
       "This is our latest change",
     )
 
-    assert_equal expected_history, guide.latest_change
+    expect(presenter.latest_change).to eq(expected_history)
   end
 
-  test "#previous_changes returns the change history for the guide" do
+  it "#previous_changes returns the change history for the guide" do
     expected_history = [
       ServiceManualGuidePresenter::Change.new(
         Time.zone.parse("2015-09-09T08:17:10+00:00"),
@@ -136,10 +157,10 @@ class ServiceManualGuidePresenterTest < PresenterTestCase
       ),
     ]
 
-    assert_equal expected_history, presented_item.previous_changes
+    expect(presenter.previous_changes).to eq(expected_history)
   end
 
-  test "#header_links returns a list of hrefs and text" do
-    assert_equal [{ "text" => "What is it, why it works and how to do it", "href" => "#what-is-it-why-it-works-and-how-to-do-it" }], presented_item.header_links
+  it "#header_links returns a list of hrefs and text" do
+    expect(presenter.header_links).to eq([{ "text" => "What is it, why it works and how to do it", "href" => "#what-is-it-why-it-works-and-how-to-do-it" }])
   end
 end

ffb1e541ee7898936f019997b52954e79343ff6e
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-06-23 16:26:11 +0100

    Add accordion sections to service manual topic presenter
    - add sections method to output data formatted for the accordion component
    - includes list of links as a chunk of html for the accordion section content, requires ActionView
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/eff89790a0a122b1fc944bff6641ed41e665efa1/app/presenters/service_manual_topic_presenter.rb

--- app/presenters/service_manual_topic_presenter.rb
+++ app/presenters/service_manual_topic_presenter.rb
@@ -1,7 +1,36 @@
 class ServiceManualTopicPresenter < ContentItemPresenter
+  include ActionView::Helpers
+  include ActionView::Context
+
   def breadcrumbs
     [
       { title: "Service manual", url: "/service-manual" },
     ]
+  end
+
+  def sections
+    @content_item.groups_with_links.map do |group|
+      {
+        heading: {
+          text: group[:name],
+        },
+        summary: {
+          text: group[:description],
+        },
+        content: {
+          html: list_of_links(group[:items]),
+        },
+      }
+    end
+  end
+
+private
+
+  def list_of_links(items)
+    content_tag(:ul, class: "govuk-list") do
+      items.each do |i|
+        concat content_tag(:li, link_to(i["title"], i["base_path"], class: "govuk-link"))
+      end
+    end
   end
 end

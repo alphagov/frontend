6c4d0a98e684440299a0ded338fb42de35f3f246
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-06-24 15:24:29 +0100

    tests: convert title/description test

--- spec/system/document_collection_spec.rb
+++ spec/system/document_collection_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe "Document Collection" do
-  # test "document collection with no body" do
-  #   setup_and_visit_content_item("document_collection")
-  #   assert_has_component_title(@content_item["title"])
-  #   assert page.has_text?(@content_item["description"])
-  # end
+  context "when visiting a document collection" do
+    let(:base_path) { "/government/collections/national-driving-and-riding-standards" }
+
+    before { content_store_has_example_item(base_path, schema: :document_collection, example: "document_collection") }
+
+    it "displays the title" do
+      visit base_path
+
+      expect(page).to have_title("National standards for driving and riding")
+    end
+
+    it "includes the description" do
+      visit base_path
+
+      expect(page).to have_text("The standards set out what it takes to be a safe and responsible driver and rider and provide training to drivers and riders.")
+    end
+  end
 
   # test "renders metadata and document footer" do
   #   setup_and_visit_content_item("document_collection")

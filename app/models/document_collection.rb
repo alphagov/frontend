700f62c96fb6a5650d48a7e86c0c489deaeb83d3
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-02 17:04:20 +0100

    feat: add #taxonomy_topic_email_override_base_path method
    - this is a special links element found in these document types that allows a topic-based email setup.
    - it's an array, but government-frontend only ever uses the first element, if one is present.
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/04f02c31e99b20732a5265b0a3744825a7dd9308/app/presenters/document_collection/signup_link.rb#L7-L9

--- app/models/document_collection.rb
+++ app/models/document_collection.rb
@@ -1,4 +1,8 @@
 class DocumentCollection < ContentItem
   include Political
   include Updatable
+
+  def taxonomy_topic_email_override_base_path
+    linked("taxonomy_topic_email_override").first&.base_path
+  end
 end

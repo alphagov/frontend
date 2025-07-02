cf10c5e85779152eece71696c12e09a3203c9be8
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-02 17:04:20 +0100

    feat: add #taxonomy_topic_email_override_base_path method
    - this is a special links element found in these document types that allows a topic-based email setup.
    - it's an array, but government-frontend only ever uses the first element, if one is present.
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/04f02c31e99b20732a5265b0a3744825a7dd9308/app/presenters/document_collection/signup_link.rb#L7-L9

--- spec/models/document_collection_spec.rb
+++ spec/models/document_collection_spec.rb
@@ -1,6 +1,30 @@
 RSpec.describe DocumentCollection do
+  subject(:document_collection) { described_class.new(content_store_response) }
+
+  let(:content_store_response) { GovukSchemas::Example.find("document_collection", example_name: "document_collection") }
+
   it_behaves_like "it has historical government information", "document_collection", "document_collection_political"
   it_behaves_like "it has updates", "document_collection", "document_collection_with_history"
   it_behaves_like "it has no updates", "document_collection", "document_collection"
   it_behaves_like "it can be withdrawn", "document_collection", "document_collection_withdrawn"
+
+  describe "taxonomy_topic_email_override_base_path" do
+    it "returns nil" do
+      expect(document_collection.taxonomy_topic_email_override_base_path).to be_nil
+    end
+
+    context "when the taxonomy_topic_email_override_base_path is set in the content item" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("document_collection", example_name: "document_collection").tap do |item|
+          item["links"]["taxonomy_topic_email_override"] = [{
+            "base_path" => "/money/paying-hmrc",
+          }]
+        end
+      end
+
+      it "returns the path" do
+        expect(document_collection.taxonomy_topic_email_override_base_path).to eq("/money/paying-hmrc")
+      end
+    end
+  end
 end

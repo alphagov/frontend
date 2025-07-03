8d01b26bebe4f01426fdf4b69ef24c1e066f7620
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-03 13:54:06 +0100

    feat: make document_collection political
    - add Political concern and tests, this will be used by the shared/historical
      partial

--- app/models/document_collection.rb
+++ app/models/document_collection.rb
@@ -1,2 +1,3 @@
 class DocumentCollection < ContentItem
+  include Political
 end
--- spec/models/document_collection_spec.rb
+++ spec/models/document_collection_spec.rb
@@ -1,3 +1,4 @@
 RSpec.describe DocumentCollection do
+  it_behaves_like "it has historical government information", "document_collection", "document_collection_political"
   it_behaves_like "it can be withdrawn", "document_collection", "document_collection_withdrawn"
 end

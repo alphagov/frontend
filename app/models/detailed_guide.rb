e38828f74f425298d7e4183c19bb0fd2907082b4
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-07-03 09:56:52 +0100

    Add related concerns to Detailed guide model
    - Added Emphasised organisations concern,contributors method and tests as handled in the migration of routes for case studies and speeches for rendering organisations as part of publisher-metadata values in the page.
    Audit trail: https://github.com/alphagov/government-frontend/blob/a361915b42cad2326fd6aac2531744429aa1b895/app/presenters/detailed_guide_presenter.rb#L4
    Ref. for the change in the organisation concern: https://github.com/alphagov/frontend/pull/4653/commits/95c0852a6c1047e7bc827c2a8c0ff416bc3c15fe
    
    - Added Updatable concern to retrieve published_dates values from  shared partial - published_dates_with_notification_button
    - Added Political concern as in government-frontend to handle historically_political values in view
    Audit trail: https://github.com/alphagov/government-frontend/blob/a361915b42cad2326fd6aac2531744429aa1b895/app/presenters/detailed_guide_presenter.rb
    
    - Added withdrawn tests as the Withdrawn concern is already added in the ContentItem model - https://github.com/alphagov/frontend/blob/8f4326c056248a0f6a1b90b69c61ce3ff9636a03/app/models/content_item.rb#L4

--- app/models/detailed_guide.rb
+++ app/models/detailed_guide.rb
@@ -1,4 +1,11 @@
 class DetailedGuide < ContentItem
+  include EmphasisedOrganisations
   include NationalApplicability
+  include Political
   include SinglePageNotificationButton
+  include Updatable
+
+  def contributors
+    organisations_ordered_by_emphasis.uniq(&:content_id)
+  end
 end
--- spec/models/detailed_guide_spec.rb
+++ spec/models/detailed_guide_spec.rb
@@ -3,4 +3,24 @@ RSpec.describe DetailedGuide do
 
   it_behaves_like "it can have national applicability", "detailed_guide", "national_applicability_detailed_guide"
   it_behaves_like "it can have single page notifications", "detailed_guide", "detailed_guide"
+  it_behaves_like "it has historical government information", "detailed_guide", "political_detailed_guide"
+  it_behaves_like "it has updates", "detailed_guide", "detailed_guide"
+  it_behaves_like "it has no updates", "detailed_guide", "national_applicability_detailed_guide"
+  it_behaves_like "it can be withdrawn", "detailed_guide", "withdrawn_detailed_guide"
+
+  describe "#contributors" do
+    context "when emphasised organisation is present" do
+      it "returns the emphasised organisation first" do
+        content_store_response = GovukSchemas::Example.find("detailed_guide", example_name: "england-2014-to-2020-european-structural-and-investment-funds")
+        content_item = described_class.new(content_store_response)
+
+        organisations = content_store_response["links"]["organisations"]
+        expect(content_item.contributors.count).to eq(4)
+        expect(content_item.contributors[0].title).to eq(organisations[0]["title"])
+        expect(content_item.contributors[1].title).to eq(organisations[1]["title"])
+        expect(content_item.contributors[2].title).to eq(organisations[2]["title"])
+        expect(content_item.contributors[3].title).to eq(organisations[3]["title"])
+      end
+    end
+  end
 end

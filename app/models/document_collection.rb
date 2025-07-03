1464e66ae7275c6d50256f7825254ad3aaaec666
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-03 16:12:05 +0100

    feat: Add collection_groups to document_collection model
    - Add a datatype to represent items from the details/collection_groups element of
      the content item. Each CollectionGroup has a body, id and title, and a list of
      documents, mapped from the documents listed in links. This seems like a natural
      thing to map this way (not complex or shared enough for its own model, but useful
      to be able to manipulate like a model rather than as a big hash).
    - NB: Wrapping the details/collection_groups dig with a fallback to an empty array
      is important, because sometimes document_collections are presented as document
      links (for instance, at the time of commit...
      https://www.gov.uk/api/content/government/collections/self-employment-detailed-information
      ...points to another document collection. When document collections appear in
      links, they don't contain their collection_groups, so we can't rely on this
      not returning `nil`.

--- app/models/document_collection.rb
+++ app/models/document_collection.rb
@@ -3,10 +3,21 @@ class DocumentCollection < ContentItem
   include SinglePageNotificationButton
   include Updatable
 
-  attr_reader :headers
+  attr_reader :collection_groups, :headers
+
+  CollectionGroup = Data.define(:body, :documents, :id, :title)
 
   def initialize(content_store_response)
     super
+
+    @collection_groups = (content_store_response.dig("details", "collection_groups") || []).map do |group_details|
+      CollectionGroup.new(
+        body: group_details["body"],
+        documents: group_details["documents"].map { |id| linked("documents").find { |d| d.content_id == id } }.compact,
+        id: group_details["title"].tr(" ", "-").downcase,
+        title: group_details["title"],
+      )
+    end
 
     @headers = content_store_response.dig("details", "headers") || []
   end

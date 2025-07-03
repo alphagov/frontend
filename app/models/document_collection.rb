8a090b1df5b0696f67b486d152cd0c5f3820191a
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-03 15:41:42 +0100

    feat: add headers to document_collections model
    - The contents list in a document_collection is a mix of h2 headings from the body
      (extracted at publish time and supplied as a headings element), and the group
      headings of non-empty collection groups. First we add the headers.

--- app/models/document_collection.rb
+++ app/models/document_collection.rb
@@ -4,6 +4,14 @@ class DocumentCollection < ContentItem
   include Updatable
   include Withdrawable
 
+  attr_reader :headers
+
+  def initialize(content_store_response)
+    super
+
+    @headers = content_store_response.dig("details", "headers") || []
+  end
+
   def taxonomy_topic_email_override_base_path
     linked("taxonomy_topic_email_override").first&.base_path
   end

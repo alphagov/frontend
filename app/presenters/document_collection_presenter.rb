9e361938498b343a040fafd5562496f8185118cd
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-03 16:04:08 +0100

    feat: add headers_for_contents_list_component to presenter
    - Following the pattern from existing migrations. (there is a future
      task to refactor/DRY up this code, but for the moment this
      matches what we're doing elsewhere).

--- app/presenters/document_collection_presenter.rb
+++ app/presenters/document_collection_presenter.rb
@@ -1,5 +1,21 @@
 class DocumentCollectionPresenter < ContentItemPresenter
+  def headers_for_contents_list_component
+    return [] unless contents_outline.level_two_headers?
+
+    ContentsOutlinePresenter.new(contents_outline).for_contents_list_component
+  end
+
   def show_email_signup_link?
     content_item.taxonomy_topic_email_override_base_path.present? && I18n.locale == :en
+  end
+
+private
+
+  def contents_outline
+    @contents_outline ||= ContentsOutline.new(valid_outline_headers)
+  end
+
+  def valid_outline_headers
+    content_item.headers.map { |header| header.except("headers") }
   end
 end

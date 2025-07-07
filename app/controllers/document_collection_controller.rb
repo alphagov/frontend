1017da31d5e4d853555ff67a401bdc114bbfe4e3
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-07 13:47:32 +0100

    feat: add logged_in? as helper method
    - This was a method from the content items controller (which set @has_govuk_account), but there's
      an existing method available from the personalisation concern, which we add as a helper so that the
      view can access it.
    
    Audit Trail:
    https://github.com/alphagov/government-frontend/blob/1d6be124a3274ae38b97a337ca388b8065c1c9ef/app/controllers/content_items_controller.rb#L183

--- app/controllers/document_collection_controller.rb
+++ app/controllers/document_collection_controller.rb
@@ -1,6 +1,8 @@
 class DocumentCollectionController < ContentItemsController
   include Cacheable
 
+  helper_method :logged_in?
+
   def show
     @presenter = DocumentCollectionPresenter.new(content_item)
   end

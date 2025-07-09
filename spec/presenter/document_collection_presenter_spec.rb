009f39fe8b2d96a81883cbd3e82c357ee4e3e4cf
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-09 09:58:46 +0100

    feat: add headers_for_contents_list_component to presenter
    - Mostly Following the pattern from existing migrations. (there is a future
      task to refactor/DRY up this code, but for the moment this matches - with the exception
      of the method below - what we're doing elsewhere).
    - Note that valid_outline_headers is doing the same job as exclude_nested_headings
      in other presenters, but doesn't delete information (changing the model)
    
    Audit trail:
    https://github.com/alphagov/frontend/blob/main/app/presenters/corporate_information_page_presenter.rb#L43-L47

--- spec/presenter/document_collection_presenter_spec.rb
+++ spec/presenter/document_collection_presenter_spec.rb
@@ -4,6 +4,29 @@ RSpec.describe DocumentCollectionPresenter do
   let(:content_item) { DocumentCollection.new(content_store_response) }
   let(:content_store_response) { GovukSchemas::Example.find("document_collection", example_name: "document_collection") }
 
+  describe "#headers_for_contents_list_component" do
+    context "with no headers present in the body" do
+      it "returns an empty array" do
+        expect(presenter.headers_for_contents_list_component).to eq([])
+      end
+    end
+
+    context "with a body with h2 headers present" do
+      let(:content_store_response) { GovukSchemas::Example.find("document_collection", example_name: "document_collection_with_body") }
+
+      it "returns the h2 headers (without nested headers) in a format suitable for a Contents List component" do
+        expect(presenter.headers_for_contents_list_component.count).to eq(1)
+
+        expect(presenter.headers_for_contents_list_component[0][:href]).to eq("#consolidated-list")
+        expect(presenter.headers_for_contents_list_component[0][:text]).to eq("Consolidated list")
+      end
+
+      it "strips the nested headers from the headers for the contents list" do
+        expect(presenter.headers_for_contents_list_component[0][:items]).to be_empty
+      end
+    end
+  end
+
   describe "#show_email_signup_link?" do
     context "with no taxonomy_topic_email_override present" do
       it "returns false" do

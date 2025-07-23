ae90361db6d4144bfd2d3cd8d61f07621f258b2d
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-07-23 14:01:28 +0100

    feat: add headers_for_contents_list_component to presenter
    - Mostly Following the pattern from existing migrations. (there is a future
      task to refactor/DRY up this code, but for the moment this matches - with the exception
      of the method below - what we're doing elsewhere).
    - Note that valid_outline_headers is doing the same job as exclude_nested_headings
      in other presenters, but doesn't delete information (changing the model)
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/dc254aa0b1ced9686be9765efd6ac0c6ca6b2739/app/presenters/statistical_data_set_presenter.rb#L3
    https://github.com/alphagov/government-frontend/blob/dc254aa0b1ced9686be9765efd6ac0c6ca6b2739/app/presenters/content_item/contents_list.rb

--- app/presenters/statistical_data_set_presenter.rb
+++ app/presenters/statistical_data_set_presenter.rb
@@ -1,2 +1,17 @@
 class StatisticalDataSetPresenter < ContentItemPresenter
+  def headers_for_contents_list_component
+    return [] unless contents_outline.level_two_headers?
+
+    ContentsOutlinePresenter.new(contents_outline).for_contents_list_component
+  end
+
+private
+
+  def contents_outline
+    @contents_outline ||= ContentsOutline.new(valid_outline_headers)
+  end
+
+  def valid_outline_headers
+    content_item.headers.map { |header| header.except("headers") }
+  end
 end
--- spec/presenter/statistical_data_set_presenter_spec.rb
+++ spec/presenter/statistical_data_set_presenter_spec.rb
@@ -1,2 +1,29 @@
 RSpec.describe StatisticalDataSetPresenter do
+  subject(:statistical_data_set_presenter) { described_class.new(content_item) }
+
+  let(:content_item) { StatisticalDataSet.new(content_store_response) }
+  let(:content_store_response) { GovukSchemas::Example.find("statistical_data_set", example_name: "statistical_data_set") }
+
+  describe "#headers_for_contents_list_component" do
+    context "with no headers present in the body" do
+      let(:content_store_response) { GovukSchemas::Example.find("statistical_data_set", example_name: "statistical_data_set_with_block_attachments") }
+
+      it "returns an empty array" do
+        expect(statistical_data_set_presenter.headers_for_contents_list_component).to eq([])
+      end
+    end
+
+    context "with a body with h2 headers present" do
+      it "returns the h2 headers in a format suitable for a Contents List component" do
+        expect(statistical_data_set_presenter.headers_for_contents_list_component.count).to eq(6)
+
+        expect(statistical_data_set_presenter.headers_for_contents_list_component[0][:href]).to eq("#olympics")
+        expect(statistical_data_set_presenter.headers_for_contents_list_component[0][:text]).to eq("Olympics")
+      end
+
+      it "strips the nested headers from the headers for the contents list" do
+        expect(statistical_data_set_presenter.headers_for_contents_list_component[0][:items]).to be_empty
+      end
+    end
+  end
 end

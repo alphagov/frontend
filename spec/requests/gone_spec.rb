989a22f42fe719c9cb71d5fd37c1c2fe4c87ce9f
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-07-29 16:04:47 +0100

    Add gone route request tests

--- spec/requests/gone_spec.rb
+++ spec/requests/gone_spec.rb
@@ -1,5 +1,5 @@
-RSpec.describe "Gone" do
-  describe "GET index" do
+RSpec.describe "Gone page" do
+  describe "Pages routed to gone internally" do
     before do
       stub_content_store_has_item("/foreign-travel-advice/grand-fenwick", schema_name: "gone")
     end
@@ -8,6 +8,33 @@ RSpec.describe "Gone" do
       get "/foreign-travel-advice/grand-fenwick"
 
       expect(response).to have_http_status(:gone)
+    end
+  end
+
+  describe "GET gone page" do
+    let(:content_item) { GovukSchemas::Example.find("gone", example_name: "gone") }
+    let(:base_path) { content_item.fetch("base_path") }
+
+    before do
+      stub_content_store_has_item(base_path, content_item)
+    end
+
+    it "succeeds" do
+      get base_path
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the show template" do
+      get base_path
+
+      expect(response).to render_template(:show)
+    end
+
+    it "sets cache-control headers" do
+      get base_path
+
+      expect(response).to honour_content_store_ttl
     end
   end
 end

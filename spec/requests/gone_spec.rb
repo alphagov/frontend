88c290b928c9fd3cb50d48ad2d532735f1661ef5
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-07-29 16:04:47 +0100

    Add gone route request tests

--- spec/requests/gone_spec.rb
+++ spec/requests/gone_spec.rb
@@ -1,13 +1,27 @@
 RSpec.describe "Gone" do
   describe "GET index" do
+    let(:base_path) { "/foreign-travel-advice/grand-fenwick" }
+
     before do
-      stub_content_store_has_item("/foreign-travel-advice/grand-fenwick", schema_name: "gone")
+      stub_content_store_has_item(base_path, schema_name: "gone")
     end
 
     it "redirects the gone item to the gone controller" do
       get "/foreign-travel-advice/grand-fenwick"
 
-      expect(response).to have_http_status(:gone)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the show template" do
+      get base_path
+
+      expect(response).to render_template(:show)
+    end
+
+    it "sets cache-control headers" do
+      get base_path
+
+      expect(response).to honour_content_store_ttl
     end
   end
 end

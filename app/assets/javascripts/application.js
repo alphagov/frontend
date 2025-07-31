33b3296a14e4f4fc22b6c1b2cbc3cd7cdcb1e432
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-07-31 14:38:13 +0100

    Add basic elements bundle
    - As done in email-alert-frontend - https://github.com/alphagov/email-alert-frontend/pull/1890, bundled everything used in all pages of the app here
    - Added basic component stylesheets in application.scss
    - anything in application.scss added to the custom exclude list so that it isn't included twice by the components gem.
    - Added the JS files needed for those components (previously supplied by static)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,10 @@
 // This file is linked to in the application.
 //= require dependencies
 //= require main
+//= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/layout-super-navigation-header
+//= require govuk_publishing_components/components/search-with-autocomplete
+//= require govuk_publishing_components/components/skip-link

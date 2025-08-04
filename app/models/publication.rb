83bc6b48cff7d1312667f5dafe563c0814071e0c
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-04 13:38:25 +0100

    feat: add #datasets? model to publication
    - this will be needed by the header data for the view
    - add tests, as none existed in government-frontend
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/8cb50cfaf715da5b5a99a4fd28db7f26b869e9c5/app/presenters/publication_presenter.rb#L32-L34

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,2 +1,5 @@
 class Publication < ContentItem
+  def dataset?
+    %w[national_statistics official_statistics transparency].include? document_type
+  end
 end
--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -1,2 +1,19 @@
 RSpec.describe Publication do
+  subject(:publication) { described_class.new(content_store_response) }
+
+  let(:content_store_response) { GovukSchemas::Example.find("publication", example_name: "publication") }
+
+  describe "#dataset?" do
+    it "returns false" do
+      expect(publication.dataset?).to be false
+    end
+
+    context "when the document_type is in the datasets list" do
+      let(:content_store_response) { GovukSchemas::Example.find("publication", example_name: "best-practice-official-statistics") }
+
+      it "returns true" do
+        expect(publication.dataset?).to be true
+      end
+    end
+  end
 end

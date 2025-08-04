2313e72ab1f1c23ea5a8e382121d37c9c51f0999
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-04 13:49:31 +0100

    feat: add #hide_from_search_engines? method to publication presenter
    - this will be needed by the next view update.
    - have checked with the app team and Jess which paths are still required, and have removed
      the others.
    - simplified slightly by moving the exception into the constant array
    - tests slightly reduced. Added risk is that someone updates the array in the presenter without
      updating the test, but this method is likely not to be valid for long, and since there are only
      two items,
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/main/app/presenters/publication_presenter.rb#L9-L14
    https://github.com/alphagov/government-frontend/blob/8cb50cfaf715da5b5a99a4fd28db7f26b869e9c5/app/presenters/publication_presenter.rb#L36-L41
    https://github.com/alphagov/government-frontend/blob/8cb50cfaf715da5b5a99a4fd28db7f26b869e9c5/test/presenters/publication_presenter_test.rb#L113-L140

--- spec/presenter/publication_presenter_spec.rb
+++ spec/presenter/publication_presenter_spec.rb
@@ -1,2 +1,23 @@
 RSpec.describe PublicationPresenter do
+  subject(:publication_presenter) { described_class.new(Publication.new(content_store_response)) }
+
+  let(:content_store_response) { GovukSchemas::Example.find("publication", example_name: "publication") }
+
+  describe "#hide_from_search_engines?" do
+    it "returns false" do
+      expect(publication_presenter.hide_from_search_engines?).to be false
+    end
+
+    context "when the page is in the hide list" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("publication", example_name: "publication").merge({
+          "base_path" => "/government/publications/govuk-app-testing-privacy-notice-how-we-use-your-data",
+        })
+      end
+
+      it "returns true" do
+        expect(publication_presenter.hide_from_search_engines?).to be true
+      end
+    end
+  end
 end

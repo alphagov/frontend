18d72b5b2af2d9e6c23b51eac26ffad2333c5ac1
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-05 09:35:50 +0100

    feat: add people support to publication model
    - Will be needed in the metadata, as per:
      https://github.com/alphagov/government-frontend/blob/f42d3c0775896474fae4584067382a88c2faa226/test/integration/publication_test.rb#L22-L28

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe Publication do
 
   let(:content_store_response) { GovukSchemas::Example.find("publication", example_name: "publication") }
 
+  it_behaves_like "it can have people", "publication", "publication"
   it_behaves_like "it has updates", "publication", "withdrawn_publication"
   it_behaves_like "it has no updates", "publication", "publication"
 

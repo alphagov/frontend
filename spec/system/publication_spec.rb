0d5fb7c7f10d116fd86b9a961e8bde1c2f9d0522
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-06 11:14:40 +0100

    test: does not render the single page notification button when the publication is in Welsh

--- spec/system/publication_spec.rb
+++ spec/system/publication_spec.rb
@@ -259,12 +259,21 @@ RSpec.describe "Publication" do
 
       expect(actual_tracking_bottom).to eq(expected_tracking_bottom)
     end
-  end
 
-  # test "does not render the single page notification button on foreign language pages" do
-  #   setup_and_visit_content_item("publication", "locale" => "cy")
-  #   assert_not page.has_css?(".gem-c-single-page-notification-button")
-  # end
+    context "when the publication is in Welsh" do
+      let(:content_item) do
+        GovukSchemas::Example.find(:publication, example_name: "publication").tap do |example|
+          example["locale"] = "cy"
+        end
+      end
+
+      it "does not render the single page notification button" do
+        visit base_path
+
+        expect(page).not_to have_selector(".gem-c-single-page-notification-button")
+      end
+    end
+  end
 
   # test "adds the noindex meta tag to '/government/publications/pension-credit-claim-form--2'" do
   #   overrides = { "base_path" => "/government/publications/pension-credit-claim-form--2" }

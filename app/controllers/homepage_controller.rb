c6aa9f2b0b0d81e07aec009d1284965f8b9873a8
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-08-12 16:24:13 +0100

    Add full width layout for specific pages
    - Added 'full_width' layout for below pages as the layout for these pages vary from the default 'application' layout:
      Homepage
      Landing page
      Service manual index
      Service toolkit
    
    - Added homepage flag to be true as the margins for homepage in mobile and tablets are wider compared to rest of the pages.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,3 +1,12 @@
 class HomepageController < ContentItemsController
   include Cacheable
+
+  layout "full_width"
+  before_action :set_homepage_layout
+
+private
+
+  def set_homepage_layout
+    @homepage_layout = true
+  end
 end

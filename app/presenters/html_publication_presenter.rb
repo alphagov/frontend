ee25b8060ae6ab718f6c8e5c863a5f5ba80abd08
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-08-12 15:11:23 +0100

    Add hide_from_search_engines method
    - we have to add a robots noindex tag to pages that are children of a list of specific pages in the publication presenter
    - extends model to include parent

--- app/presenters/html_publication_presenter.rb
+++ app/presenters/html_publication_presenter.rb
@@ -1,2 +1,7 @@
 class HtmlPublicationPresenter < ContentItemPresenter
+  def hide_from_search_engines?
+    return false unless content_item.parent
+
+    PublicationPresenter::PATHS_TO_HIDE.include? content_item.parent.base_path
+  end
 end

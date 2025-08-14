c7d3b6341897a4207896c9b6598872db0dbec29c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-08-14 12:37:00 +0100

    Add organisation logos
    - can have multiple organisations, see https://www.gov.uk/government/publications/lowland-grazing-offer-countryside-stewardship/lowland-grazing-offer-what-you-need-to-know
    - but if more than one organisation, remove images from being displayed in organisation logo components
    - government-frontend had a whole stack of logic for handling organisations and branding, see https://github.com/alphagov/government-frontend/blob/main/app/presenters/content_item/organisation_branding.rb
    - but this wasn't needed in this context, a simpler solution was possible

--- spec/models/html_publication_spec.rb
+++ spec/models/html_publication_spec.rb
@@ -6,4 +6,123 @@ RSpec.describe HtmlPublication do
   it "returns a parent" do
     expect(html_publication.parent.content_id).to eq("8b19c238-54e3-4e27-b0d7-60f8e2a677c9")
   end
+
+  it "returns public timestamp" do
+    expect(html_publication.public_timestamp).to eq("2016-01-17T14:19:42.460Z")
+  end
+
+  it "returns first published version" do
+    expect(html_publication.first_published_version).to be(true)
+  end
+
+  context "when content is not historically political" do
+    let(:content_store_response) do
+      GovukSchemas::Example.find(:html_publication, example_name: "published_with_history_mode").tap do |example|
+        example["details"]["political"] = false
+      end
+    end
+
+    it "political is false" do
+      expect(html_publication.historically_political?).to be(false)
+    end
+  end
+
+  context "when content is historically political" do
+    let(:content_store_response) do
+      GovukSchemas::Example.find(:html_publication, example_name: "published_with_history_mode").tap do |example|
+        example["details"]["political"] = true
+        example["links"]["government"][0]["details"]["current"] = false
+      end
+    end
+
+    it "political is true" do
+      expect(html_publication.historically_political?).to be(true)
+      expect(html_publication.publishing_government).to eq("2010 to 2015 Conservative and Liberal Democrat coalition government")
+    end
+  end
+
+  it "is not withdrawn" do
+    expect(html_publication.withdrawn?).to be(false)
+  end
+
+  context "when content has been withdrawn" do
+    let(:content_store_response) do
+      GovukSchemas::Example.find("html_publication", example_name: "published").tap do |example|
+        example["withdrawn_notice"] = {
+          withdrawn_at: "2020",
+          explanation: "because of a reason",
+        }.deep_stringify_keys
+      end
+    end
+
+    it "returns the appropriate information" do
+      expect(html_publication.withdrawn?).to be(true)
+      expect(html_publication.withdrawn_at).to eq("2020")
+      expect(html_publication.withdrawn_explanation).to eq("because of a reason")
+    end
+  end
+
+  it "when content has no national applicability it doesn't return anything" do
+    expect(html_publication.national_applicability).to be_nil
+  end
+
+  context "when content has national applicability" do
+    let(:content_store_response) { GovukSchemas::Example.find("html_publication", example_name: "national_applicability_alternative_url_html_publication") }
+
+    it "returns applicability information" do
+      expected = {
+        england: {
+          applicable: true,
+          label: "England",
+        },
+        northern_ireland: {
+          alternative_url: "http://www.dardni.gov.uk/news-dard-pa022-a-13-new-procedure-for",
+          applicable: false,
+          label: "Northern Ireland",
+        },
+        scotland: {
+          applicable: true,
+          label: "Scotland",
+        },
+        wales: {
+          applicable: true,
+          label: "Wales",
+        },
+      }
+      expect(html_publication.national_applicability).to eq(expected)
+    end
+  end
+
+  context "when there are no headers" do
+    let(:content_store_response) { GovukSchemas::Example.find("html_publication", example_name: "prime_ministers_office") }
+
+    it "return an empty array" do
+      expect(described_class.new(content_store_response).headers.count).to eq(0)
+    end
+  end
+
+  context "when the page contains headers" do
+    let(:content_store_response) { GovukSchemas::Example.find("html_publication", example_name: "multiple_organisations") }
+
+    it "returns an array of headers" do
+      expect(described_class.new(content_store_response).headers.count).to eq(4)
+      expect(described_class.new(content_store_response).headers[3]["headers"].count).to eq(1)
+    end
+  end
+
+  it "returns copyright year" do
+    expect(described_class.new(content_store_response).copyright_year).to eq(2016)
+  end
+
+  context "when there is an isbn" do
+    let(:content_store_response) { GovukSchemas::Example.find("html_publication", example_name: "print_with_meta_data") }
+
+    it "returns isbn" do
+      expect(described_class.new(content_store_response).isbn).to eq("978-1-4098-4066-4")
+    end
+  end
+
+  it "returns the govspeak content" do
+    expect(described_class.new(content_store_response).govspeak_body).to include("The Environment Agency has received a new bespoke application for an environmental permit under the Environmental Permitting (England and Wales) Regulations 2010 from Mr Derek Mears.")
+  end
 end

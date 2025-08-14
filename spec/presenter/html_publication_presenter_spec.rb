e2bc0385cad130c1325f6023279fb31140e219b9
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-08-14 12:37:00 +0100

    Add organisation logos
    - can have multiple organisations, see https://www.gov.uk/government/publications/lowland-grazing-offer-countryside-stewardship/lowland-grazing-offer-what-you-need-to-know
    - but if more than one organisation, remove images from being displayed in organisation logo components
    - government-frontend had a whole stack of logic for handling organisations and branding, see https://github.com/alphagov/government-frontend/blob/main/app/presenters/content_item/organisation_branding.rb
    - but this wasn't needed in this context, a simpler solution was possible

--- spec/presenter/html_publication_presenter_spec.rb
+++ spec/presenter/html_publication_presenter_spec.rb
@@ -44,4 +44,51 @@ RSpec.describe HtmlPublicationPresenter do
       end
     end
   end
+
+  describe "#organisation_logos" do
+    context "when there is one organisation" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find(:html_publication, example_name: "arabic_translation")
+      end
+
+      it "returns the organisation details" do
+        expected = [
+          {
+            brand: "uk-trade-investment",
+            crest: "single-identity",
+            image: nil,
+            name: "UK Trade<br>&amp; Investment",
+            url: "/government/organisations/uk-trade-investment",
+          },
+        ]
+        expect(html_publication_presenter.organisation_logos).to eq(expected)
+      end
+    end
+
+    context "when there are multiple organisations" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find(:html_publication, example_name: "long_form_and_automatically_numbered_headings").tap do |example|
+          example["links"]["organisations"] << {
+            schema_name: "organisation",
+            document_type: "organisation",
+            title: "Environment Agency",
+            details: {
+              logo: {
+                formatted_title: "Environment Agency",
+                image: {
+                  url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/organisation/logo/199/EAlogo.png",
+                },
+              },
+            },
+            base_path: "/government/organisations/environment-agency",
+          }.deep_stringify_keys
+        end
+      end
+
+      it "returns the organisation details minus any images" do
+        expect(html_publication_presenter.organisation_logos[1][:name]).to eq("Environment Agency")
+        expect(html_publication_presenter.organisation_logos[1][:image]).to be_nil
+      end
+    end
+  end
 end

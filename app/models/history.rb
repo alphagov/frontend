8c660303e970bace750fe8cd60dce53f17454c8a
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-21 14:19:12 +0100

    TEMP fake lead paragraph for mockup

--- app/models/history.rb
+++ app/models/history.rb
@@ -14,10 +14,11 @@ private
         type: "page_title",
         context: "History",
         heading_text: title,
+        # lead_paragraph: fake_lp,
       },
       {
         type: "rich_content",
-        contents_list: content_store_response["details"]["headers"].map { |header| header.except("headers") },
+        contents_list: (content_store_response.dig("details", "headers") || []).map { |header| header.except("headers") },
         govspeak: body,
         image: {
           alt: content_store_response.dig("details", "sidebar_image", "caption"),
@@ -26,4 +27,8 @@ private
       },
     ]
   end
+
+  # def fake_lp
+  #   'In this section you can read short biographies of <a href="#notable-people" class="govuk-link">notable people</a> and explore the history of <a href="#government-buildings" class="govuk-link">government buildings</a>. You can also search our <a href="#documents-records" class="govuk-link">online records</a> and read <a href="#articles-and-blog-posts" class="govuk-link">articles and blog posts</a> by historians.'.html_safe
+  # end
 end

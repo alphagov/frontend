c97be6c46a27bc8007173593c86dc0f508c9f2ad
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-27 13:51:11 +0100

    test: add test for no image in rich content flexible section

--- spec/models/flexible_page/flexible_section/rich_content_spec.rb
+++ spec/models/flexible_page/flexible_section/rich_content_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe FlexiblePage::FlexibleSection::RichContent do
-  subject(:rich_content) do
-    described_class.new({
+  subject(:content_hash) do
+    {
       "contents_list" => [
         { "id" => "introduction", "text" => "Introduction" },
         { "id" => "take_tour", "text" => "Take the tour" },
@@ -10,18 +10,38 @@ RSpec.describe FlexiblePage::FlexibleSection::RichContent do
         "alt" => "Picture of No. 10",
         "src" => "https://example.gov.uk/no10.jpg",
       },
-    }, FlexiblePage.new({}))
+    }
   end
+
+  let(:rich_content) { described_class.new(content_hash, FlexiblePage.new({})) }
 
   describe "#initialize" do
     it "sets the attributes from the contents hash" do
       expect(rich_content.contents_list).to be_instance_of(ContentsOutline)
       expect(rich_content.govspeak).to eq("<h2>Hello!</h2>")
     end
+  end
 
-    it "creates an image record" do
+  describe "image attribute" do
+    it "returns an image record" do
       expect(rich_content.image.alt).to eq("Picture of No. 10")
       expect(rich_content.image.src).to eq("https://example.gov.uk/no10.jpg")
+    end
+
+    context "with no image supplied" do
+      let(:content_hash) do
+        {
+          "contents_list" => [
+            { "id" => "introduction", "text" => "Introduction" },
+            { "id" => "take_tour", "text" => "Take the tour" },
+          ],
+          "govspeak" => "<h2>Hello!</h2>",
+        }
+      end
+
+      it "returns nil" do
+        expect(rich_content.image).to be_nil
+      end
     end
   end
 end

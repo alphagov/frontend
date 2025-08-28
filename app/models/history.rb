e8fdf9c6fee9ba112294e4f56bef7fa43931a534
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-28 13:17:16 +0100

    feat: Add flexible page rendering for /government/history files
    - Adds route for /government/history/:slug pointing to the flexible page controller
    - Adds a History model, derived from FlexiblePage, which will supply a default
      flexible-pages section config of a page_title derived from the content item's title
      and optionally lead_paragraph, and a rich_content derived from the content items's
      body, headings, and optionally sidebar_image.
    - This corresponds to the output from the history_page configurable document type
      https://github.com/alphagov/whitehall/blob/3ca07751d7fe1cab5aadd9ddbb777ed1dde00e36/app/models/configurable_document_types/history_page.json
    - ..and the history content schema
      https://github.com/alphagov/publishing-api/blob/97b26a381048ed3a526f97f75f1e1206b210955e/content_schemas/formats/history.jsonnet

---
+++ app/models/history.rb
@@ -0,0 +1,30 @@
+class History < FlexiblePage
+private
+
+  def default_flexible_sections
+    [
+      {
+        type: "page_title",
+        context: "History",
+        heading_text: title,
+        lead_paragraph: content_store_response.dig("details", "lead_paragraph"),
+      },
+      {
+        type: "rich_content",
+        contents_list: (content_store_response.dig("details", "headers") || []).map { |header| header.except("headers") },
+        govspeak: body,
+        image: image_hash,
+      },
+    ]
+  end
+
+  def image_hash
+    sidebar_image = content_store_response.dig("details", "sidebar_image")
+    return nil unless sidebar_image
+
+    {
+      alt: sidebar_image["caption"],
+      src: sidebar_image["url"],
+    }
+  end
+end
---
+++ spec/models/history_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe History do
+  subject(:history) { described_class.new(content_store_response) }
+
+  let(:content_store_response) do
+    {
+      "title" => "History Page",
+      "details" => {
+        "body" => "Hello",
+        "headers" => [],
+        "sidebar_image" => {
+          "caption" => "Here's an image!",
+          "url" => "https://assets.publishing.service.gov.uk/image.png",
+        },
+      },
+    }
+  end
+
+  describe "flexible_sections attribute" do
+    it "generates flexible sections if not supplied" do
+      expect(history.flexible_sections.count).to eq(2)
+      expect(history.flexible_sections.first).to be_instance_of(FlexiblePage::FlexibleSection::PageTitle)
+      expect(history.flexible_sections.second).to be_instance_of(FlexiblePage::FlexibleSection::RichContent)
+    end
+
+    context "when no sidebar image is provided" do
+      let(:content_store_response) do
+        {
+          "title" => "History Page",
+          "details" => {
+            "body" => "Hello",
+            "headers" => [],
+          },
+        }
+      end
+
+      it "generates a rich content element without an image" do
+        expect(history.flexible_sections.second).to be_instance_of(FlexiblePage::FlexibleSection::RichContent)
+        expect(history.flexible_sections.second.image).to be_nil
+      end
+    end
+  end
+end

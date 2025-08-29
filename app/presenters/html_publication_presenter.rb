251415e08435c1244cbf0121627650c1c5181ade
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-08-29 16:05:44 +0100

    Add HTML publication header
    - adds blue box containing document type, title and updated/published date

--- app/presenters/html_publication_presenter.rb
+++ app/presenters/html_publication_presenter.rb
@@ -1,4 +1,6 @@
 class HtmlPublicationPresenter < ContentItemPresenter
+  include DateHelper
+
   def hide_from_search_engines?
     return false unless content_item.parent
 
@@ -14,6 +16,25 @@ class HtmlPublicationPresenter < ContentItemPresenter
         crest: organisation.logo.crest,
         image: content_item.organisations.count == 1 ? organisation.logo.image : nil,
       }
+    end
+  end
+
+  def last_changed
+    timestamp = display_date(content_item.public_timestamp)
+
+    # This assumes that a translation doesn't need the date to come beforehand.
+    if content_item.first_published_version
+      "#{I18n.t('common.metadata.published')} #{timestamp}"
+    else
+      "#{I18n.t('common.metadata.updated')} #{timestamp}"
+    end
+  end
+
+  def format_sub_type
+    if content_item.parent && content_item.parent.document_type.present?
+      content_item.parent.document_type
+    else
+      "publication"
     end
   end
 end

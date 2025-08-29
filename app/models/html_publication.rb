b83a0fdbbe753c71ff2364e622e40f66cdfea09b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-08-29 16:05:44 +0100

    Add HTML publication header
    - adds blue box containing document type, title and updated/published date

--- app/models/html_publication.rb
+++ app/models/html_publication.rb
@@ -6,4 +6,12 @@ class HtmlPublication < ContentItem
   def organisations
     linked("organisations")
   end
+
+  def public_timestamp
+    content_store_response["details"]["public_timestamp"]
+  end
+
+  def first_published_version
+    content_store_response.dig("details", "first_published_version")
+  end
 end

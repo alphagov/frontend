98da32a86015253491d3cee892db5ccae8f0fbb8
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-03 15:41:34 +0100

    Update ContentsList to accept additional headers
    Some document types such as DocumentCollection have
    additional headers that need to be included in the
    contents list. They can be passed as a keyword
    argument, which is by default set as an empty array.
    They are always added to the end of the list after
    the H2 headers.
    
    Reference:
    - https://github.com/alphagov/frontend/blob/3e0970653ce22888e3c20836430cf4c4a94330a9/app/presenters/document_collection_presenter.rb#L45-L55

--- app/presenters/concerns/contents_list.rb
+++ app/presenters/concerns/contents_list.rb
@@ -1,15 +1,13 @@
 module ContentsList
-  def headers_for_contents_list_component
+  def headers_for_contents_list_component(additional_headers: [])
+    contents_outline = ContentsOutline.new(only_level_two_headers + additional_headers)
+
     return [] unless contents_outline.level_two_headers?
 
     ContentsOutlinePresenter.new(contents_outline).for_contents_list_component
   end
 
 private
-
-  def contents_outline
-    @contents_outline ||= ContentsOutline.new(only_level_two_headers)
-  end
 
   def only_level_two_headers
     content_item.headers.map { |header| header.except("headers") }
--- spec/support/concerns/contents_list.rb
+++ spec/support/concerns/contents_list.rb
@@ -3,6 +3,13 @@ RSpec.shared_examples "it can have a contents list" do |document_type, example_n
 
   let(:content_store_response) { GovukSchemas::Example.find(document_type, example_name:) }
 
+  let(:additional_headers) do
+    [
+      { "id" => "consolidated-list", "level" => 2, "text" => "Consolidated list" },
+      { "id" => "further-information", "level" => 2, "text" => "Further information" },
+    ]
+  end
+
   context "when there are no headers present in the body" do
     before do
       content_store_response["details"]["headers"] = nil
@@ -11,13 +18,29 @@ RSpec.shared_examples "it can have a contents list" do |document_type, example_n
     it "returns an empty array" do
       expect(presenter.headers_for_contents_list_component).to eq([])
     end
+
+    context "when there are additional headers" do
+      it "includes the h2 headers" do
+        headers = presenter.headers_for_contents_list_component(additional_headers:)
+
+        expect(headers.count).to eq(2)
+        expect(headers[0][:href]).to include(additional_headers[0]["id"])
+        expect(headers[0][:text]).to eq(additional_headers[0]["text"])
+      end
+    end
   end
 
   context "when there are h2s headers present in the body" do
-    it "returns the h2 headers in a format suitable for a Contents List component" do
+    it "includes the content_item headers" do
       expected_headers = content_item.headers
 
+      expect(presenter.headers_for_contents_list_component.count).to be > 0
+
       expect(presenter.headers_for_contents_list_component.count).to eq(expected_headers.count)
+    end
+
+    it "returns the h2 headers in a format suitable for a Contents List component" do
+      expected_headers = content_item.headers
 
       expect(presenter.headers_for_contents_list_component[0][:href]).to include(expected_headers[0]["id"])
       expect(presenter.headers_for_contents_list_component[0][:text]).to eq(expected_headers[0]["text"])
@@ -40,6 +63,18 @@ RSpec.shared_examples "it can have a contents list" do |document_type, example_n
 
       it "by default strips the nested headers from the headers for the contents list" do
         expect(presenter.headers_for_contents_list_component[0][:items]).to be_empty
+      end
+    end
+
+    context "when there are additional headers" do
+      it "includes the h2 headers" do
+        expected_headers_count = content_item.headers.count + additional_headers.count
+
+        headers = presenter.headers_for_contents_list_component(additional_headers:)
+
+        expect(headers.count).to eq(expected_headers_count)
+        expect(headers.last[:href]).to include(additional_headers.last["id"])
+        expect(headers.last[:text]).to eq(additional_headers.last["text"])
       end
     end
   end

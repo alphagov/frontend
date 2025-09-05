4ca234f0b3c912b7eb77495e58bace31ae1fb6ec
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-05 12:19:00 +0100

    Add Attachments concern to model
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ec1b3e365bfa9b769259bd453bf83eee69d7167f/app/presenters/consultation_presenter.rb#L2

--- spec/models/consultation_spec.rb
+++ spec/models/consultation_spec.rb
@@ -1,3 +1,4 @@
 RSpec.describe Consultation do
   it_behaves_like "it can be withdrawn", "consultation", "consultation_withdrawn"
+  it_behaves_like "it can have attachments", "consultation", "consultation_outcome_with_featured_attachments"
 end

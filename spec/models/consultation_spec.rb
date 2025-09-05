52857c8e20c68cc8e077ebb8ea3e9bc5166af78b
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-05 14:04:09 +0100

    Add People concern to model
    We also show the organisations as well as people
    in the metadata.
    
    Reference:
    - https://github.com/alphagov/government-frontend/blob/6285583f2385567f785992fd7901209d8ddb7980/test/integration/consultation_test.rb#L90

--- spec/models/consultation_spec.rb
+++ spec/models/consultation_spec.rb
@@ -1,5 +1,22 @@
 RSpec.describe Consultation do
+  let(:withdrawn_consultation) { described_class.new(GovukSchemas::Example.find("consultation", example_name: "consultation_withdrawn")) }
+
   it_behaves_like "it can be withdrawn", "consultation", "consultation_withdrawn"
   it_behaves_like "it can have attachments", "consultation", "consultation_outcome_with_featured_attachments"
   it_behaves_like "it can have national applicability", "consultation", "consultation_outcome_with_featured_attachments"
+  it_behaves_like "it can have people", "consultation", "consultation_withdrawn"
+
+  describe "#contributors" do
+    it "returns a list of organisations" do
+      expect(withdrawn_consultation.contributors[0].title).to eq("Department for Education")
+      expect(withdrawn_consultation.contributors[0].base_path).to eq("/government/organisations/department-for-education")
+    end
+
+    context "when there are people present" do
+      it "returns a list of organisations followed by people" do
+        expect(withdrawn_consultation.contributors[1].title).to eq("The Rt Hon Baroness Smith of Malvern")
+        expect(withdrawn_consultation.contributors[1].base_path).to eq("/government/people/jacqui-smith")
+      end
+    end
+  end
 end

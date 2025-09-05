5164b92e4e9b0c3c5373caede6f95f0554ac2511
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-05 12:41:39 +0100

    Add NationalApplicability concern to model
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/ec1b3e365bfa9b769259bd453bf83eee69d7167f/app/presenters/consultation_presenter.rb#L5

--- spec/models/consultation_spec.rb
+++ spec/models/consultation_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe Consultation do
   it_behaves_like "it can be withdrawn", "consultation", "consultation_withdrawn"
   it_behaves_like "it can have attachments", "consultation", "consultation_outcome_with_featured_attachments"
+  it_behaves_like "it can have national applicability", "consultation", "consultation_outcome_with_featured_attachments"
 end

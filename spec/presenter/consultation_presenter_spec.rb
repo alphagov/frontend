07c50e1bacceb473e2a7b3dbacf69413c8180da4
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-08 15:34:02 +0100

    Add #opening_date to presenter
    Consultation is the only document type using the
    DateTimeHelper, so moved over as a private method
    for now.
    
    CallForEvidence (already migrated to Frontend)
    also has a similar method which we can look to
    refactor in future.
    
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/app/presenters/consultation_presenter.rb#L19-L21
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/app/helpers/date_time_helper.rb#L6-L18
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/test/presenters/consultation_presenter_test.rb#L42
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/test/presenters/consultation_presenter_test.rb#L48C44-L48C69
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/test/presenters/consultation_presenter_test.rb#L60

--- spec/presenter/consultation_presenter_spec.rb
+++ spec/presenter/consultation_presenter_spec.rb
@@ -1,2 +1,26 @@
 RSpec.describe ConsultationPresenter do
+  subject(:presenter) { described_class.new(content_item) }
+
+  let(:content_store_response) { GovukSchemas::Example.find("consultation", example_name: "open_consultation") }
+  let(:content_item) { Consultation.new(content_store_response) }
+
+  describe "#opening_date" do
+    it "presents a friendly opening date and time" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-11-04T10:00:00+00:00"
+
+      expect(presenter.opening_date).to eq("10am on 4 November 2016")
+    end
+
+    it "presents an opening date without a time if the time is 12am" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-11-03T00:00:00+00:00"
+
+      expect(presenter.opening_date).to eq("3 November 2016")
+    end
+
+    it "presents 12pm as midday" do
+      content_item.content_store_response["details"]["opening_date"] = "2016-11-04T12:00:00+00:00"
+
+      expect(presenter.opening_date).to eq("midday on 4 November 2016")
+    end
+  end
 end

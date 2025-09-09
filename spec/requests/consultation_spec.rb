6606e17e0861a63bb352a695f09a54b34c45ad43
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-09 14:09:36 +0100

    Add request tests

---
+++ spec/requests/consultation_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe "Consultation" do
+  let(:content_item) { GovukSchemas::Example.find("consultation", example_name: "open_consultation") }
+  let(:base_path) { content_item.fetch("base_path") }
+
+  before do
+    stub_content_store_has_item(base_path, content_item)
+  end
+
+  describe "GET show" do
+    context "when visiting a Consultation page" do
+      it "returns 200" do
+        get base_path
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the show template" do
+        get base_path
+
+        expect(response).to render_template("show")
+      end
+    end
+  end
+end

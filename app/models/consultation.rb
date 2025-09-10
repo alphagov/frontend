0ddf2b692689f9330f3f4a5d54e6b3a5dee14e8a
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-09-10 09:43:24 +0100

    Add #held_on_another_website? to model
    Audit trail:
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/app/presenters/consultation_presenter.rb#L81-L83
    - https://github.com/alphagov/government-frontend/blob/922537bb02ee6fa507fae8578d3009ebdbdfb517/test/presenters/consultation_presenter_test.rb#L108-L109

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -71,4 +71,8 @@ class Consultation < ContentItem
   def held_on_another_website_url
     content_store_response.dig("details", "held_on_another_website_url")
   end
+
+  def held_on_another_website?
+    held_on_another_website_url.present?
+  end
 end

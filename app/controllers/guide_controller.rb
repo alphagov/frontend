071a2845a842b00c74e8d9b307adcbf9c4ff4468
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-09-29 21:16:59 +0100

    Set current part in controller
    - match the pattern set in travel advice pages

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -2,6 +2,8 @@ class GuideController < ContentItemsController
   include Cacheable
 
   def show
+    content_item.set_current_part(params[:part])
+
     @presenter = GuidePresenter.new(content_item)
   end
 

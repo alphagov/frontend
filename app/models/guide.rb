c4092b1e4061749d735cd9459b31d06e68a55851
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-09-29 16:02:41 +0100

    Add #hide_chapter_navigation? method to model
    - this is a model method, surfacing whether the flag is set or not. It's optional in the content
      item, so we have to allow for it being nil. It'll be used in presenter methods to be added in
      the subsequent commits.

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,6 +1,10 @@
 class Guide < ContentItem
   include Parts
 
+  def hide_chapter_navigation?
+    content_store_response["details"]["hide_chapter_navigation"].presence || false
+  end
+
   def is_evisa?
     base_path == "/evisa"
   end
--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -5,6 +5,20 @@ RSpec.describe Guide do
 
   it_behaves_like "it has parts", "guide", "guide"
 
+  describe "#hide_chapter_navigation?" do
+    it "returns false" do
+      expect(guide.hide_chapter_navigation?).to be false
+    end
+
+    context "when details/hide_chapter_navigation is true" do
+      let(:content_store_response) { GovukSchemas::Example.find("guide", example_name: "guide-with-hide-navigation") }
+
+      it "returns true" do
+        expect(guide.hide_chapter_navigation?).to be true
+      end
+    end
+  end
+
   describe "#is_evisa?" do
     it "returns false" do
       expect(guide.is_evisa?).to be false

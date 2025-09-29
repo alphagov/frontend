e2b8f1f3db6e78418a5bcdad3c54e055169e5078
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-09-29 15:47:52 +0100

    Add #is_evisa? method
    - This is needed for the next part of the view, where we add a scroll-tracking meta tag only to
      pages/parts of /evisa

--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -1,5 +1,23 @@
 RSpec.describe Guide do
+  subject(:guide) { described_class.new(content_store_response) }
+
   let(:content_store_response) { GovukSchemas::Example.find("guide", example_name: "guide") }
 
   it_behaves_like "it has parts", "guide", "guide"
+
+  describe "#is_evisa?" do
+    it "returns false" do
+      expect(guide.is_evisa?).to be false
+    end
+
+    context "when the base_path is /evisa" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("guide", example_name: "guide").tap { |i| i.merge!("base_path" => "/evisa") }
+      end
+
+      it "returns true" do
+        expect(guide.is_evisa?).to be true
+      end
+    end
+  end
 end

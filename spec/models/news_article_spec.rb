4b51a7bf0d009816ee6e080401dbb57848dd7890
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-10-06 18:19:44 +0100

    Introduce complete fallback behaviour for lead image across all types
    The existing code did not actually get triggered (at least from whitehall)* because we have the same duplicate behaviour publisher-side. Whitehall used this fallback logic and produced a lead image based on the organisation default news image, meaning it got to this bit code in FE with a lead image already set.
    
    We are keen to remove that logic from whitehall, since it already exists here. Nonetheless, the true functionality across types that use a lead image is sort of spread between WH and FE:
    
    - NewsArticles: image defaults to primary organisation's image, then to the first organisation's image if primary is not set, and eventually to worldwide organisation's image if it's available. The latter is only the case for WorldNewsStory. These additional fallbacks where happening in WH code, and are now here. Test coverage was added to capture behaviour.
    - Speeches - WH was unaware of any lead image fallback - this was only happening FE side here. There is some additional "fall back" to speaker image, but that's not in the same concern as the lead image. The current changes did not introduce anything new for speeches, though I added test coverage to make the behaviour explicit. Speeches have the placeholder behaviour.
    - Case studies - WH falls back to primary organisation image. This was not in FE before, but it behaves the same as speeches so it can be here. Currently speeches to not render the placeholder image; that has been kept the same. Test coverage was added to capture behaviour.
    
    The context in which this work is being done is that we are reimplementing some WH functionality as "config-driven" blocks, and reevaluating business logic. There will be some UR around this that might mean we can remove this behaviour altogether. We are also exploring what the similitudes are between the default news image and crest usage, since they seem to largely serve the same purpose of departmental "branding".
    
    The code changes should not actually change any behaviour FE-side.
    
    *It was probably being triggered for content publisher news articles.

--- spec/models/news_article_spec.rb
+++ spec/models/news_article_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe NewsArticle do
+  subject(:news_article) { described_class.new(content_item) }
+
   it_behaves_like "it has news image", "news_article", "news_article"
   it_behaves_like "it has updates", "news_article", "best-practice-event"
   it_behaves_like "it has no updates", "news_article", "news_article"
@@ -9,8 +11,6 @@ RSpec.describe NewsArticle do
   it_behaves_like "it can be withdrawn", "news_article", "news_article_withdrawn"
 
   describe "#contributors" do
-    subject(:news_article) { described_class.new(content_item) }
-
     let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "best-practice-government-response") }
 
     it "returns the organisations ordered by emphasis" do
@@ -20,6 +20,77 @@ RSpec.describe NewsArticle do
       expect(news_article.contributors[0].title).to eq(organisations[0]["title"])
       expect(news_article.contributors[1].title).to eq(organisations[1]["title"])
       expect(news_article.contributors[2].title).to eq(organisations[2]["title"])
+    end
+  end
+
+  describe "#image" do
+    describe "document is a news article with a custom lead image" do
+      let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "news_article") }
+
+      it "fetches lead image from the details" do
+        expect(news_article.image).to eq(content_item.dig("details", "image"))
+      end
+    end
+
+    describe "document is a news article without a custom lead image" do
+      let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "best-practice-news-story") }
+
+      it "fetches lead image from primary publishing organisation" do
+        content_item["details"] = content_item["details"].delete("image") # drop the custom image so we can test the fallback
+        content_item["links"]["primary_publishing_organisation"] = [
+          "details" => {
+            "default_news_image" => {
+              "high_resolution_url" => "https://assets.publishing.service.gov.uk/media/621e4de4e90e0710be0354d7/s960_fcdo-main-building.jpg",
+              "url" => "https://assets.publishing.service.gov.uk/media/621e4de48fa8f5490aff83b4/s300_fcdo-main-building.jpg",
+            },
+          },
+        ]
+        expect(news_article.image).to eq(content_item.dig("links", "primary_publishing_organisation")[0].dig("details", "default_news_image"))
+      end
+    end
+
+    describe "document is a news article without a custom lead image, nor default news image on the primary organisation" do
+      let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "best-practice-news-story") }
+
+      it "fetches lead image from the first organisation" do
+        content_item["details"] = content_item["details"].delete("image") # drop the custom image so we can test the fallback
+        content_item["links"]["primary_publishing_organisation"][0]["default_news_image"] = nil # drop the default news image so we can test the fallback
+        content_item["links"]["organisations"] = [
+          { "details" =>
+            {
+              "default_news_image" => {
+                "high_resolution_url" => "https://assets.publishing.service.gov.uk/media/621e4de4e90e0710be0354d7/s960_fcdo-main-building.jpg",
+                "url" => "https://assets.publishing.service.gov.uk/media/621e4de48fa8f5490aff83b4/s300_fcdo-main-building.jpg",
+              },
+            } },
+        ]
+        expect(news_article.image).to eq(content_item.dig("links", "organisations")[0].dig("details", "default_news_image"))
+      end
+
+      it "falls back to placeholder image" do
+        content_item["details"] = content_item["details"].delete("image") # drop the custom image so we can test the fallback
+        content_item["links"]["primary_publishing_organisation"][0]["details"]["default_news_image"] = nil # drop the default news image so we can test the fallback
+        content_item["links"]["organisations"][0]["details"]["default_news_image"] = nil # drop the default news image so we can test the fallback
+
+        expect(news_article.image).to include({ "url" => "https://assets.publishing.service.gov.uk/media/5e59279b86650c53b2cefbfe/placeholder.jpg" })
+      end
+    end
+
+    describe "document is a world news story without a custom lead image" do
+      let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "world_news_story_news_article") }
+
+      it "fetches lead image from worldwide organisation" do
+        content_item["details"] = content_item["details"].delete("image") # drop the custom image so we can test the fallback
+        content_item["links"]["worldwide_organisations"] = [
+          { "details" => {
+            "default_news_image" => {
+              "high_resolution_url" => "https://assets.publishing.service.gov.uk/media/621e4de4e90e0710be0354d7/s960_fcdo-main-building.jpg",
+              "url" => "https://assets.publishing.service.gov.uk/media/621e4de48fa8f5490aff83b4/s300_fcdo-main-building.jpg",
+            },
+          } },
+        ]
+        expect(news_article.image).to eq(content_item.dig("links", "worldwide_organisations")[0].dig("details", "default_news_image"))
+      end
     end
   end
 end

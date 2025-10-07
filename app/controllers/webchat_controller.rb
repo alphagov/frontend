1c3b72fb5f2a9cdf6fbd2dc9a69b3d4b16c4735c
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-07 16:17:54 +0100

    Set content_security_policy in controller
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/2a1927a3a8100374593aa708525d928a7e3c51a6/app/controllers/webchat_controller.rb#L7-L9

--- app/controllers/webchat_controller.rb
+++ app/controllers/webchat_controller.rb
@@ -2,4 +2,8 @@ class WebchatController < ContentItemsController
   include Cacheable
 
   def show; end
+
+  content_security_policy do |p|
+    p.connect_src(*p.connect_src, -> { content_item.csp_connect_src })
+  end
 end

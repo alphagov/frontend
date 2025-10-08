272451e4a95ad87b620de3f09c399dd299a75109
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-08 13:14:36 +0100

    Add ga4_link_data attribute
    - this is hardcoded in the presenter. It's somewhat presentational, but since it's essentially a
      hardcoded attribute it doesn't seem too awful to put it here.
    
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/2a1927a3a8100374593aa708525d928a7e3c51a6/app/presenters/webchat_presenter.rb#L54-L60

--- app/models/webchat.rb
+++ app/models/webchat.rb
@@ -1,10 +1,15 @@
 class Webchat < ContentItem
-  attr_reader :availability_url, :open_url, :redirect_attribute
+  attr_reader :availability_url, :ga4_link_data, :open_url, :redirect_attribute
 
   def initialize(content_store_response)
     super
 
     @availability_url = "https://d1y02qp19gjy8q.cloudfront.net/availability/18555309"
+    @ga4_link_data = {
+      event_name: "navigation",
+      type: "webchat",
+      text: I18n.t("webchat.speak_to_adviser", locale: :en),
+    }.to_json
     @open_url = "https://d1y02qp19gjy8q.cloudfront.net/open/index.html"
     @redirect_attribute = "false"
   end
--- spec/models/webchat_spec.rb
+++ spec/models/webchat_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe Webchat do
       expect(webchat.availability_url).not_to be_nil
     end
 
+    it "has a ga4_link_data" do
+      expect(webchat.ga4_link_data).not_to be_nil
+    end
+
     it "has an open_url" do
       expect(webchat.open_url).not_to be_nil
     end

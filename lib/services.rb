1210fb596d388c203b698576414b33cc2e6b9b7b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-10-08 14:49:18 +0100

    Restore breadcrumbs
    - breadcrumbs relies upon services.rb, which is a bit odd and will be refactored in a following PR
    - fix a related spacing issue where the gap between breadcrumbs and org logos was too big
    - this has also revealed that on pages with the prime ministers office org logo the current site doesn't show the left hand border on the org logo, because the component is being passed the wrong brand value (executive office instead of prime ministers office) so while this is a visual change it appears to be correcting a bug

---
+++ lib/services.rb
@@ -0,0 +1,13 @@
+require "gds_api/content_store"
+
+module Services
+  def self.content_store
+    @content_store ||= GdsApi::ContentStore.new(
+      Plek.new.find("content-store"),
+      # Disable caching to avoid caching a stale max-age in the cache control
+      # headers, which would cause this app to set the wrong max-age on its
+      # own responses
+      disable_cache: true,
+    )
+  end
+end

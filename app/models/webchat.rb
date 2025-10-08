cd6238ce771e37bb55403f5a20a0b69559a871df
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-08 13:13:15 +0100

    Add availability_url attribute
    Audit trail:
    https://github.com/alphagov/government-frontend/blob/2a1927a3a8100374593aa708525d928a7e3c51a6/lib/webchat.yaml#L6C21-L6C80

--- app/models/webchat.rb
+++ app/models/webchat.rb
@@ -1,2 +1,9 @@
 class Webchat < ContentItem
+  attr_reader :availability_url
+
+  def initialize(content_store_response)
+    super
+
+    @availability_url = "https://d1y02qp19gjy8q.cloudfront.net/availability/18555309"
+  end
 end
---
+++ spec/models/webchat_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe Webchat do
+  subject(:webchat) { described_class.new(content_store_response) }
+
+  let(:content_store_response) do
+    {
+      "base_path" => "/government/organisations/hm-passport-office/contact/hm-passport-office-webchat",
+      "description" => "Handles the webchat view for HM Passport Office",
+      "title" => "HM Passport Office webchat",
+      "schema_name" => "special_route",
+    }
+  end
+
+  describe "webchat attributes" do
+    it "has an availability_url" do
+      expect(webchat.availability_url).not_to be_nil
+    end
+  end
+end

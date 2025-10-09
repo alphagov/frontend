13dd2db06770b51f20b15f9ca603621ad131d91c
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-10-09 16:27:47 +0100

    Add #notice_description to call_for_evidence presenter
    We want to move to using a shared notice partial with
    consultations. Consultations have more variations for
    the notice description, so we also need the method here.
    
    Ref:
    - https://github.com/alphagov/frontend/blob/dd33f58a3842656d41c0ef884a9055ff4cd6b92f/app/presenters/consultation_presenter.rb#L36-L44
    - https://github.com/alphagov/frontend/blob/dd33f58a3842656d41c0ef884a9055ff4cd6b92f/app/views/consultation/_notice.html.erb#L3

--- spec/presenter/call_for_evidence_presenter_spec.rb
+++ spec/presenter/call_for_evidence_presenter_spec.rb
@@ -118,4 +118,38 @@ RSpec.describe CallForEvidencePresenter do
       end
     end
   end
+
+  describe "#notice_description" do
+    context "when document type is open_call_for_evidence" do
+      let(:content_store_response) { GovukSchemas::Example.find("call_for_evidence", example_name: "open_call_for_evidence") }
+
+      it "returns empty text" do
+        expect(presenter.notice_description).to eq("")
+      end
+    end
+
+    context "when document type is unopened_call_for_evidence" do
+      let(:content_store_response) { GovukSchemas::Example.find("call_for_evidence", example_name: "unopened_call_for_evidence") }
+
+      it "returns not_open_yet text" do
+        expect(presenter.notice_description).to eq("This call for evidence opens")
+      end
+    end
+
+    context "when document type is closed_call_for_evidence" do
+      let(:content_store_response) { GovukSchemas::Example.find("call_for_evidence", example_name: "closed_call_for_evidence") }
+
+      it "returns empty text" do
+        expect(presenter.notice_description).to eq("")
+      end
+    end
+
+    context "when document type is call_for_evidence_outcome" do
+      let(:content_store_response) { GovukSchemas::Example.find("call_for_evidence", example_name: "call_for_evidence_outcome") }
+
+      it "returns empty text" do
+        expect(presenter.notice_description).to eq("")
+      end
+    end
+  end
 end

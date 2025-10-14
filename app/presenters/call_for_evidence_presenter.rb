1b2f087ecd109a8a003b1c4c9af3a937699a67a9
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-10-14 14:02:48 +0100

    Update CallForEvidencecPresenter to use new `on` locale key location

--- app/presenters/call_for_evidence_presenter.rb
+++ app/presenters/call_for_evidence_presenter.rb
@@ -8,7 +8,7 @@ class CallForEvidencePresenter < ContentItemPresenter
   end
 
   def on_or_at
-    opening_date_midnight? ? I18n.t("formats.call_for_evidence.on") : I18n.t("formats.call_for_evidence.at")
+    opening_date_midnight? ? I18n.t("common.on") : I18n.t("formats.call_for_evidence.at")
   end
 
   def opens_closes_or_ran
@@ -52,6 +52,6 @@ private
       # 12am on 10 January becomes 11:59pm on 9 January
       time -= 1.second
     end
-    I18n.l(time, format: "#{time_format} #{I18n.t('formats.call_for_evidence.on')} #{date_format}").gsub(":00", "").gsub("12pm", "midday").gsub("12am #{I18n.t('formats.call_for_evidence.on')} ", "").strip
+    I18n.l(time, format: "#{time_format} #{I18n.t('common.on')} #{date_format}").gsub(":00", "").gsub("12pm", "midday").gsub("12am #{I18n.t('common.on')} ", "").strip
   end
 end

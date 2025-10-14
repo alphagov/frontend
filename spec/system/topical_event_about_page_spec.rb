440240317e6ad4beb94f72ac50bb771cc6243d95
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-14 13:26:44 +0100

    Remove complicated contents list test
    - We shouldn't be having weird exceptions to simple pages. Contents lists in flexible-page derived page
      types just support suppression of h3 level headers, and that's it. That's tested more thoroughly elsewhere.
    - Remove supporting method.

--- spec/system/topical_event_about_page_spec.rb
+++ spec/system/topical_event_about_page_spec.rb
@@ -52,16 +52,6 @@ RSpec.describe "Topical Event About Page" do
     end
   end
 
-#   test "contents list displayed when fewer than three items and first item word count is greater than 100" do
-#     @content_item = get_content_example("topical_event_about_page")
-#     @content_item["details"]["body"] = long_first_item_body
-
-#     stub_content_store_has_item(@content_item["base_path"], @content_item.to_json)
-
-#     visit_with_cachebust @content_item["base_path"]
-#     assert page.has_css?(".gem-c-contents-list")
-#   end
-
 #   test "does not render with the single page notification button" do
 #     setup_and_visit_content_item("topical_event_about_page")
 #     assert_not page.has_css?(".gem-c-single-page-notification-button")
@@ -71,14 +61,6 @@ RSpec.describe "Topical Event About Page" do
 
 #   def topical_event_end_date
 #     Date.parse(@content_item["links"]["parent"][0]["details"]["end_date"])
-#   end
-
-#   def long_first_item_body
-#     "<div class='govspeak'><h2 id='response-in-the-uk'>Item 1</h2>
-#     <p>#{Faker::Lorem.characters(number: 200)}</p>
-#     <p>#{Faker::Lorem.characters(number: 216)}</p>
-#     <h2 id='response-in-africa'>Item 2</h2>
-#     <p>#{Faker::Lorem.sentence}</p></div>"
 #   end
 
 #   def body_with_two_contents_list_items

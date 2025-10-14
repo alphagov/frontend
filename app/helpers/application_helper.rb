257d24d967472f922ffe65ff4d46ffef2aaef106
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-10-14 13:43:28 +0100

    Add topical_event_about_page to contextual breadcrumb exception list
    - Like all flexible pages, these pages handle their own breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,16 @@ module ApplicationHelper
   def show_breadcrumbs?(content_item)
     return false if content_item.nil?
 
-    no_breadcrumbs_for = %w[flexible_page history homepage landing_page service_manual_homepage service_manual_guide service_manual_service_standard service_manual_service_toolkit service_manual_topic]
+    no_breadcrumbs_for = %w[flexible_page
+                            history
+                            homepage
+                            landing_page
+                            service_manual_homepage
+                            service_manual_guide
+                            service_manual_service_standard
+                            service_manual_service_toolkit
+                            service_manual_topic
+                            topical_event_about_page]
 
     return false if no_breadcrumbs_for.include?(content_item.schema_name)
 

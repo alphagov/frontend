7c395463211a4dc3149a3a98183dc48d7371f28a
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-11-04 14:34:34 +0000

    feat: Add auto-setting of attributes from readers
    - It's a common pattern in flexible sections to add a reader attribute and
      then set it from the flexible_section_hash. We can reduce this with a
      bit of magic.
    - At the end of the initializer, we use gather_class_instance_methods to
      get a list of all instance methods introduced in this class (with
      instance_methods(false)), and those added by parents up until the
      Base class itself (we then remove the methods that exist in base
      and which we know don't need to be set)
    - that is used by set_reader_attributes_from_section_hash, which sets
      the matching instance variable from the flexible section hash.
    - Upsides: removes boiler-plate from the sections themselves.
    - Downsides: will sometime initialise things that then get reinitialised
      from custom initializers (see next commits). Too much magic?

--- app/models/flexible_page/flexible_section/base.rb
+++ app/models/flexible_page/flexible_section/base.rb
@@ -1,11 +1,24 @@
 module FlexiblePage::FlexibleSection
   class Base
-    attr_reader :flexible_section_hash, :content_item, :type
+    attr_reader :content_item, :flexible_section_hash, :type
 
     def initialize(flexible_section_hash, content_item)
       @flexible_section_hash = flexible_section_hash
-      @type = flexible_section_hash["type"]
       @content_item = content_item
+      set_reader_attributes_from_section_hash
+    end
+
+    def set_reader_attributes_from_section_hash
+      vars = gather_class_instance_methods(self.class)
+      vars.each do |var|
+        instance_variable_set("@#{var}", flexible_section_hash[var.to_s])
+      end
+    end
+
+    def gather_class_instance_methods(from_class)
+      methods = from_class.instance_methods(false)
+      methods += gather_class_instance_methods(from_class.superclass) unless from_class.name == "FlexiblePage::FlexibleSection::Base"
+      methods - %i[content_item flexible_section_hash set_instance_variables gather_class_instance_methods]
     end
   end
 end

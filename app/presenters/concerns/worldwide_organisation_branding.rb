95ba9f62121dbb5b6858caa385e8cbeeb7694bc4
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-11-10 16:55:18 +0000

    ERB lint on concern

--- app/presenters/concerns/worldwide_organisation_branding.rb
+++ app/presenters/concerns/worldwide_organisation_branding.rb
@@ -1,15 +1,15 @@
 module WorldwideOrganisationBranding
-    DEFAULT_ORGANISATION_LOGO = "single-identity".freeze
+  DEFAULT_ORGANISATION_LOGO = "single-identity".freeze
 
-    def organisation_logo
-      link_to_organisation = defined?(worldwide_organisation) && worldwide_organisation
+  def organisation_logo
+    link_to_organisation = defined?(worldwide_organisation) && worldwide_organisation
 
-      sponsoring_organisation = sponsoring_organisations&.first
-      {
-        name: formatted_title.html_safe,
-        url: link_to_organisation ? worldwide_organisation.base_path : nil,
-        crest: sponsoring_organisation&.dig("details", "logo", "crest") || DEFAULT_ORGANISATION_LOGO,
-        brand: sponsoring_organisation&.dig("details", "brand") || DEFAULT_ORGANISATION_LOGO,
-      }
-    end
+    sponsoring_organisation = sponsoring_organisations&.first
+    {
+      name: formatted_title.html_safe,
+      url: link_to_organisation ? worldwide_organisation.base_path : nil,
+      crest: sponsoring_organisation&.dig("details", "logo", "crest") || DEFAULT_ORGANISATION_LOGO,
+      brand: sponsoring_organisation&.dig("details", "brand") || DEFAULT_ORGANISATION_LOGO,
+    }
+  end
 end

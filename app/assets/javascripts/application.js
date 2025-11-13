ec2db5cd504fda3a0340d428e621d520145e2818
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-11-13 14:42:46 +0000

    Native app install prompt test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,28 @@
 //= require dependencies
 //= require main
 //= require govuk_publishing_components/dependencies
+
+let deferredPrompt
+const btnInstall = document.getElementById('btnInstall')
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault()
+  deferredPrompt = e
+  btnInstall.hidden = false
+})
+
+btnInstall.addEventListener('click', async () => {
+  // Hide the install button
+  btnInstall.style.display = 'none'
+
+  if (deferredPrompt) {
+    deferredPrompt.prompt()
+    const { outcome } = await deferredPrompt.userChoice
+    console.log(
+      outcome === 'accepted'
+        ? 'User accepted the install prompt'
+        : 'User dismissed the install prompt'
+    )
+    deferredPrompt = null // Only one prompt per event
+  }
+})

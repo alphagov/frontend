d73334ef87c9631f9e95d5ac894319bee0d97da2
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-11-14 09:54:38 +0000

    Add manual? check method
    - Copied over the method from government-frontend
    
    Audit trail: https://github.com/alphagov/government-frontend/blob/f0ada45d76fe002bab519ed79e4d5c3ca29311e5/app/presenters/content_item/manual.rb#L67

--- app/models/concerns/manual.rb
+++ app/models/concerns/manual.rb
@@ -4,4 +4,8 @@ module Manual
   def section_groups
     content_store_response.dig("details", "child_section_groups") || []
   end
+
+  def manual?
+    %w[manual manual_section].include?(schema_name)
+  end
 end

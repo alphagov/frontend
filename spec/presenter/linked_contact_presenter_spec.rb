ca6b9bfc2829e39d1df8872e18582043cf74b381
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-11-17 16:12:20 +0000

    Make linked contact presenter tests pass

--- spec/presenter/linked_contact_presenter_spec.rb
+++ spec/presenter/linked_contact_presenter_spec.rb
@@ -1,21 +1,15 @@
-require "test_helper"
+RSpec.describe LinkedContactPresenter do
+  let(:contact_content_item) { WorldwideOffice.new(GovukSchemas::Example.find("worldwide_office", example_name: "worldwide_office")).contact }
 
-class WorldwideOrganisation::LinkedContactPresenterTest < ActiveSupport::TestCase
-  setup do
-    content_item = govuk_content_schema_example("worldwide_office", "worldwide_office")
+  it "#post_address returns nil when nil on the content item" do
+    contact_content_item.content_store_response["details"]["post_addresses"] = nil
 
-    @contact_content_item = content_item.dig("links", "contact").first
+    presented_item = described_class.new(contact_content_item)
+
+    expect(presented_item.post_address).to be_nil
   end
 
-  test "#post_address returns nil when nil on the content item" do
-    @contact_content_item["details"]["post_addresses"] = nil
-
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
-
-    assert_nil presented_item.post_address
-  end
-
-  test "#post_address returns the first post address from the content item as a formatted address" do
+  it "#post_address returns the first post address from the content item as a formatted address" do
     expected_address =
       "<address class=\"govuk-body\"><p class=\"adr\">\n"\
       "<span class=\"fn\">British Embassy Manila</span><br />\n"\
@@ -23,47 +17,47 @@ class WorldwideOrganisation::LinkedContactPresenterTest < ActiveSupport::TestCas
       "<span class=\"postal-code\">1634</span> <span class=\"locality\">Taguig City</span><br />\n"\
       "<span class=\"country-name\">Philippines</span>\n</p>\n</address>"
 
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
+    presented_item = described_class.new(contact_content_item)
 
-    assert_equal expected_address, presented_item.post_address
+    expect(expected_address).to eq(presented_item.post_address)
   end
 
-  test "#email returns nil when nil on the content item" do
-    @contact_content_item["details"]["email_addresses"] = nil
+  it "#email returns nil when nil on the content item" do
+    contact_content_item.content_store_response["details"]["email_addresses"] = nil
 
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
+    presented_item = described_class.new(contact_content_item)
 
-    assert_nil presented_item.email
+    expect(presented_item.email).to be_nil
   end
 
-  test "#email returns the first email address from the content item" do
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
+  it "#email returns the first email address from the content item" do
+    presented_item = described_class.new(contact_content_item)
 
-    assert_equal "ukinthephilippines@fco.gov.uk", presented_item.email
+    expect(presented_item.email).to eq("ukinthephilippines@fco.gov.uk")
   end
 
-  test "#contact_form_link returns nil when nil on the content item" do
-    @contact_content_item["details"]["contact_form_links"] = nil
+  it "#contact_form_link returns nil when nil on the content item" do
+    contact_content_item.content_store_response["details"]["contact_form_links"] = nil
 
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
+    presented_item = described_class.new(contact_content_item)
 
-    assert_nil presented_item.contact_form_link
+    expect(presented_item.contact_form_link).to be_nil
   end
 
-  test "#contact_form_link returns the contact form link from the content item" do
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
+  it "#contact_form_link returns the contact form link from the content item" do
+    presented_item = described_class.new(contact_content_item)
 
-    assert_equal "http://www.gov.uk/contact-consulate-manila", presented_item.contact_form_link
+    expect(presented_item.contact_form_link).to eq("http://www.gov.uk/contact-consulate-manila")
   end
 
-  test "#comments returns the contact description" do
+  it "#comments returns the contact description" do
     expected_comments =
       "<p class=\"govuk-body\">24/7 consular support is available by telephone for all routine enquiries and emergencies. "\
       "Please call +63 (02) 8 858 2200 / +44 20 7136 6857. <br/><br/>Public access to the embassy is by appointment only. "\
       "Please visit https://www.gov.uk/world/philippines or call +63 (02) 8 858 2200.</p>"
 
-    presented_item = WorldwideOrganisation::LinkedContactPresenter.new(@contact_content_item)
+    presented_item = described_class.new(contact_content_item)
 
-    assert_equal expected_comments, presented_item.comments
+    expect(expected_comments).to eq(presented_item.comments)
   end
 end

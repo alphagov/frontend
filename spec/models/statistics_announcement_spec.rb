229601fc24c322efcca09836aceef25a947877aa
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-18 12:56:21 +0000

    Add Updatable concern to model

--- spec/models/statistics_announcement_spec.rb
+++ spec/models/statistics_announcement_spec.rb
@@ -1,2 +1,9 @@
 RSpec.describe StatisticsAnnouncement do
+  let(:official_statistics) { described_class.new(GovukSchemas::Example.find("statistics_announcement", example_name: "official_statistics")) }
+
+  describe "#release_date" do
+    it "returns the release date" do
+      expect(official_statistics.release_date).to eq("20 January 2016 9:30am")
+    end
+  end
 end

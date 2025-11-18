4a21cd35210abb8159e035f6d910d6834b53ef9c
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-18 12:56:21 +0000

    Add Updatable concern to model

--- spec/models/statistics_announcement_spec.rb
+++ spec/models/statistics_announcement_spec.rb
@@ -1,2 +1,9 @@
 RSpec.describe StatisticsAnnouncement do
+  describe "updatable" do
+    let(:statistics_announcement) { described_class.new(GovukSchemas::Example.find("statistics_announcement", example_name: "best-practice-statistics-announcement")) }
+
+    it "knows its updated" do
+      expect(statistics_announcement.updated).to eq(statistics_announcement.public_updated_at)
+    end
+  end
 end

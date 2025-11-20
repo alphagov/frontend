a7f05eae36afa04c0b1711da65ffb7de3ead1a8c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-11-20 14:21:48 +0000

    Add organisation logos
    - can have multiple organisations, see https://www.gov.uk/government/publications/lowland-grazing-offer-countryside-stewardship/lowland-grazing-offer-what-you-need-to-know
    - but if more than one organisation, remove images from being displayed in organisation logo components
    - government-frontend had a whole stack of logic for handling organisations and branding, see https://github.com/alphagov/government-frontend/blob/main/app/presenters/content_item/organisation_branding.rb
    - but this wasn't needed in this context, a simpler solution was possible

--- app/presenters/html_publication_presenter.rb
+++ app/presenters/html_publication_presenter.rb
@@ -4,4 +4,16 @@ class HtmlPublicationPresenter < ContentItemPresenter
 
     PublicationPresenter::PATHS_TO_HIDE.include? content_item.parent.base_path
   end
+
+  def organisation_logos
+    content_item.organisations.map do |organisation|
+      {
+        name: organisation.logo.formatted_title.html_safe,
+        url: organisation.base_path,
+        brand: organisation.brand,
+        crest: organisation.logo.crest,
+        image: content_item.organisations.count == 1 ? organisation.logo.image : nil,
+      }
+    end
+  end
 end

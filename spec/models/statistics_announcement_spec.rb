2b402b0409ccba081f82c1a4fefcd623376e7c8e
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-24 11:49:51 +0000

    Add #release_date to model Audit trail: https://github.com/alphagov/government-frontend/blob/adbbba65aa8355dfed0970c12749f5c34e16c81b/app/presenters/statistics_announcement_presenter.rb#L9

--- spec/models/statistics_announcement_spec.rb
+++ spec/models/statistics_announcement_spec.rb
@@ -1,3 +1,11 @@
 RSpec.describe StatisticsAnnouncement do
+  let(:official_statistics) { described_class.new(GovukSchemas::Example.find("statistics_announcement", example_name: "official_statistics")) }
+
   it_behaves_like "it has updates", "statistics_announcement", "official_statistics"
+
+  describe "#release_date" do
+    it "returns the release date" do
+      expect(official_statistics.release_date).to eq("2016-09-05T14:00:00+01:00")
+    end
+  end
 end

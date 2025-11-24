b5664522e7394b6e73daf29b26bef046d48f015c
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-24 11:51:05 +0000

    Add #important_metadata to presenter Audit trail: https://github.com/alphagov/government-frontend/blob/adbbba65aa8355dfed0970c12749f5c34e16c81b/app/presenters/statistics_announcement_presenter.rb#L27

--- app/presenters/statistics_announcement_presenter.rb
+++ app/presenters/statistics_announcement_presenter.rb
@@ -1,2 +1,14 @@
 class StatisticsAnnouncementPresenter < ContentItemPresenter
+  def important_metadata
+    super.tap do |m|
+      if cancelled?
+        m.merge!(
+          I18n.t("statistics_announcement.proposed_date") => release_date,
+          I18n.t("statistics_announcement.cancellation_date") => cancellation_date,
+        )
+      else
+        m.merge!(I18n.t("statistics_announcement.release_date") => release_date_and_status)
+      end
+    end
+  end
 end

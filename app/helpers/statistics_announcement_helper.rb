ab8b20308b8071104fe5a135b340042dfe8bf5cb
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-26 16:39:47 +0000

    Add StatisticsAnnouncementHelper Audit trail: https://github.com/alphagov/government-frontend/blob/adbbba65aa8355dfed0970c12749f5c34e16c81b/app/helpers/statistics_announcement_helper.rb

---
+++ app/helpers/statistics_announcement_helper.rb
@@ -0,0 +1,35 @@
+module StatisticsAnnouncementHelper
+  def on_in_between_for_release_date(date)
+    return "on #{date}" if date_is_exact_format?(date)
+    return "in #{date}" if date_is_one_month_format?(date)
+    return "between #{replace_on_with_and(date)}" if date_is_two_month_format?(date)
+
+    date
+  end
+
+private
+
+  def replace_on_with_and(date_in_two_month_format)
+    re = /\s(to)\s/
+    date_in_two_month_format.sub(re, " and ")
+  end
+
+  def date_is_two_month_format?(date)
+    date =~ /\A(\w+)\s(to)\s(\w+)/
+  end
+
+  def date_is_one_month_format?(date)
+    date =~ /\A(\w+)\s(\d{1,4})/
+  end
+
+  def date_is_exact_format?(date)
+    date.downcase =~ /\A(\d{1,2})\s(\w+)\s(\d{4})\s(\d{1,2}:\d{1,2})(am|pm)/
+  end
+
+  def nbsp_between_last_two_words(text)
+    return text if text.nil?
+
+    escaped_text = html_escape_once(text.strip)
+    escaped_text.sub(/\s([\w.?!:]+)$/, '&nbsp;\1').html_safe
+  end
+end

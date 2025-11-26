de38617904f6b428f30687856bcf9a111c93f83e
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-26 16:36:56 +0000

    Add #important_metadata presenter method It just needs to return the relevant information about the release date and cancellation date (when applicable). Audit trail: https://github.com/alphagov/government-frontend/blob/adbbba65aa8355dfed0970c12749f5c34e16c81b/app/presenters/statistics_announcement_presenter.rb#L27

--- app/presenters/statistics_announcement_presenter.rb
+++ app/presenters/statistics_announcement_presenter.rb
@@ -1,3 +1,17 @@
 class StatisticsAnnouncementPresenter < ContentItemPresenter
   include StatisticsAnnouncementHelper
+
+  def important_metadata
+    metadata = {}
+    if content_item.cancelled?
+      metadata.merge!(
+        I18n.t("formats.statistics_announcement.proposed_date") => content_item.release_date,
+        I18n.t("formats.statistics_announcement.cancellation_date") => content_item.cancellation_date,
+      )
+    else
+      metadata.merge!(I18n.t("formats.statistics_announcement.release_date") => content_item.release_date_and_status)
+    end
+
+    metadata
+  end
 end

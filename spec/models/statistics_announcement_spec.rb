8db330f31f7bacafc35560a41a24fe14e0f7a417
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-27 20:13:34 +0000

    Remove tests for updateable

--- spec/models/statistics_announcement_spec.rb
+++ spec/models/statistics_announcement_spec.rb
@@ -1,8 +1,6 @@
 RSpec.describe StatisticsAnnouncement do
   let(:official_statistics) { described_class.new(GovukSchemas::Example.find("statistics_announcement", example_name: "official_statistics")) }
 
-  it_behaves_like "it has updates", "statistics_announcement", "official_statistics"
-
   describe "#release_date" do
     it "returns the release date" do
       expect(official_statistics.release_date).to eq("20 January 2016 9:30am")

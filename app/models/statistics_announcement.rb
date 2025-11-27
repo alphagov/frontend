191dd73b8778cc6d6bbba4a41f5ba0ff1a4352b7
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-11-27 12:40:02 +0000

    Add #cancelled? to model Audit trail: https://github.com/alphagov/government-frontend/blob/adbbba65aa8355dfed0970c12749f5c34e16c81b/app/presenters/statistics_announcement_presenter.rb#L49

--- app/models/statistics_announcement.rb
+++ app/models/statistics_announcement.rb
@@ -4,4 +4,14 @@ class StatisticsAnnouncement < ContentItem
   def release_date
     content_store_response.dig("details", "display_date")
   end
+
+  def cancelled?
+    state == "cancelled"
+  end
+
+  private
+
+  def state
+    content_store_response["details"]["state"]
+  end
 end

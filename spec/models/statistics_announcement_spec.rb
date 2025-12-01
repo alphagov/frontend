db4758aa77ecf9a830fec99ff91a2e8a89b8183f
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-12-01 11:58:07 +0000

    Add test for release_date_and_status mothod

--- spec/models/statistics_announcement_spec.rb
+++ spec/models/statistics_announcement_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe StatisticsAnnouncement do
       expect(official_statistics.release_date).to eq("20 January 2016 9:30am")
     end
   end
+
+  describe "#release_date_and_status when confirmed" do
+    it "returns the release date and status" do
+      expect(official_statistics.release_date_and_status).to eq("20 January 2016 9:30am (confirmed)")
+    end
+  end
 end

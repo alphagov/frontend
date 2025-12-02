eda02c338810cfc99138c90a0f9d9a9db5067fe0
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-12-02 17:43:31 +0000

    Add concerns and method to Manual model
    - Added EmphasisedOrganisations concern and contributors method to retrieve the related organisations to update the manual metadata
    - Included Updatable concern to retrieve the initial_publication_date

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -1,2 +1,8 @@
 class Manual < ContentItem
+  include EmphasisedOrganisations
+  include Updatable
+
+  def contributors
+    organisations_ordered_by_emphasis.uniq(&:content_id)
+  end
 end
---
+++ spec/models/manual_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Manual do
+  let(:content_store_response) { GovukSchemas::Example.find("manual", example_name: "content-design") }
+
+  it_behaves_like "it has no updates", "manual", "content-design"
+
+  describe "#contributors" do
+    it "returns the organisations" do
+      content_item = described_class.new(content_store_response)
+
+      organisations = content_store_response["links"]["organisations"]
+      expect(content_item.contributors.count).to eq(1)
+      expect(content_item.contributors[0].title).to eq(organisations[0]["title"])
+    end
+  end
+end

186e4c5826262158b14b05971142ab03123f5094
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2025-12-02 20:59:58 +0000

    Add test for #release_date_and_status when canceled

--- spec/models/statistics_announcement_spec.rb
+++ spec/models/statistics_announcement_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe StatisticsAnnouncement do
     end
   end
 
+  describe "#release_date_and_status when canceled" do
+    let(:cancelled_official_statistics) { described_class.new(GovukSchemas::Example.find("statistics_announcement", example_name: "cancelled_official_statistics")) }
+  
+    it "returns the release date and status" do
+      expect(cancelled_official_statistics.release_date_and_status).to eq("20 January 2016 9:30am")
+    end
+  end
+
   describe "#release_date_changed?" do
     let(:statistics_announcement_date_changed) { described_class.new(GovukSchemas::Example.find("statistics_announcement", example_name: "release_date_changed")) }
 

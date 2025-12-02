547060a8e8a49cb94e670c62accd1d399ecb1e75
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-12-02 17:42:44 +0000

    Add metadata methods and helpers to Manual presenter
    - Added other_metadata and updated_metadata along with helpers to support metadata content
    - The methods other_metadata and updated_metadata are copied over to the presenter from government-frontend
    
    Audit trail for ManualPresenter in government-frontend: https://github.com/alphagov/government-frontend/blob/main/app/presenters/manual_presenter.rb
    Audit trail for manual_metadata: https://github.com/alphagov/government-frontend/blob/main/app/presenters/content_item/manual.rb

--- app/presenters/manual_presenter.rb
+++ app/presenters/manual_presenter.rb
@@ -1,2 +1,23 @@
 class ManualPresenter < ContentItemPresenter
+  include LinkHelper
+  include DateHelper
+
+  def other_metadata
+    updated_metadata(content_item.public_updated_at)
+  end
+
+private
+
+  def updated_metadata(updated_at)
+    current_path = content_item.base_path
+
+    if (content_item.hmrc? || content_item.manual?) && current_path == "#{content_item.base_path}/updates"
+      update_at_text = display_date(updated_at).to_s
+    else
+      updates_link = govuk_styled_link(I18n.t("formats.manuals.see_all_updates"), path: "#{content_item.base_path}/updates")
+      update_at_text = "#{display_date(updated_at)} - #{updates_link}"
+    end
+
+    { I18n.t("formats.manuals.updated") => update_at_text }
+  end
 end

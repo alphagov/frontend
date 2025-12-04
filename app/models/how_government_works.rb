6eb7b524b4532c4459b704b15cd70356e581f80d
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-04 15:48:18 +0000

    Add department and ministerial role count methods to model

--- app/models/how_government_works.rb
+++ app/models/how_government_works.rb
@@ -3,6 +3,14 @@ class HowGovernmentWorks < ContentItem
     linked("current_prime_minister").first
   end
 
+  def department_counts
+    content_store_response.dig("details", "department_counts")
+  end
+
+  def ministerial_role_counts
+    content_store_response.dig("details", "ministerial_role_counts")
+  end
+
   def lead_paragraph
     I18n.t("formats.how_government_works.lead_paragraph").html_safe
   end

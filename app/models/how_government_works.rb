650c1f83784fe927353945d736c16af6284cf806
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-04 15:27:09 +0000

    Add current_prime_minister method to model
    - Just gets a content_item from the linked current_prime_minister
    - In government-frontend this is called "prime_minister" but it's definitely
      the current prime minister, so for the sake of our sanity we rename the method
      to match the content item.

--- app/models/how_government_works.rb
+++ app/models/how_government_works.rb
@@ -1,4 +1,8 @@
 class HowGovernmentWorks < ContentItem
+  def current_prime_minister
+    linked("current_prime_minister").first
+  end
+
   def lead_paragraph
     I18n.t("formats.how_government_works.lead_paragraph").html_safe
   end

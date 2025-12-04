83091cb817e2b9bacc002af123bdf7ca98d6d6e7
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-04 15:27:09 +0000

    Add current_prime_minister method to model
    - Just gets a content_item from the linked current_prime_minister
    - In government-frontend this is called "prime_minister" but it's definitely
      the current prime minister, so for the sake of our sanity we rename the method
      to match the content item.
    - government-frontend also has a couple of utility methods to get the image url
      and alt-text, but we won't copy those across, they're encapsulated a bit in
      the content item's image attribute.
    
    Audit Trail:
    https://github.com/alphagov/government-frontend/blob/main/app/presenters/how_government_works_presenter.rb#L2-L12

--- spec/models/how_government_works_spec.rb
+++ spec/models/how_government_works_spec.rb
@@ -3,6 +3,25 @@ RSpec.describe HowGovernmentWorks do
 
   let(:content_item) { GovukSchemas::Example.find("how_government_works", example_name: "reshuffle-mode-off") }
 
+  describe "#current_prime_minister" do
+    it "returns the current prime minister object key" do
+      expect(how_government_works.current_prime_minister).not_to be_nil
+      expect(how_government_works.current_prime_minister.title).to eq("The Rt Hon Rishi Sunak MP")
+    end
+
+    context "when there isn't a prime minister" do
+      let(:content_item) do
+        GovukSchemas::Example.find("how_government_works", example_name: "reshuffle-mode-off").tap do |item|
+          item["links"].delete("current_prime_minister")
+        end
+      end
+
+      it "returns the prime minister object key" do
+        expect(how_government_works.current_prime_minister).to be_nil
+      end
+    end
+  end
+
   describe "#lead_paragraph" do
     it "returns the localisation key" do
       expect(how_government_works.lead_paragraph).not_to be_blank

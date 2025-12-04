cab561de93623ec83b91bc9676eb152bd72b974e
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-04 16:00:33 +0000

    Add request spec

---
+++ spec/requests/how_government_works_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe "How Government Works" do
+  let(:content_item) { GovukSchemas::Example.find("how_government_works", example_name: "reshuffle-mode-off") }
+  let(:base_path) { content_item.fetch("base_path") }
+
+  before do
+    stub_content_store_has_item(base_path, content_item)
+  end
+
+  describe "GET show" do
+    it "returns 200" do
+      get base_path
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the show template" do
+      get base_path
+
+      expect(response).to render_template(:show)
+    end
+
+    it "sets cache-control headers" do
+      get base_path
+      expect(response).to honour_content_store_ttl
+    end
+  end
+end

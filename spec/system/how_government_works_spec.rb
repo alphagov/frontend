9ab3260ec1ac39f22f67c2463f4eb25173dd5af6
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-04 16:06:58 +0000

    Convert to RSpec and add first test

--- spec/system/how_government_works_spec.rb
+++ spec/system/how_government_works_spec.rb
@@ -1,41 +1,46 @@
-require "test_helper"
+RSpec.describe "How Government Works" do
+  context "when visiting /government/how-goverment-works" do
+    let(:content_item) { GovukSchemas::Example.find(:how_government_works, example_name: "reshuffle-mode-off") }
+    let(:base_path) { content_item["base_path"] }
 
-class HowGovernmentWorksTest < ActionDispatch::IntegrationTest
-  test "includes the title" do
-    setup_and_visit_content_item("reshuffle-mode-off")
+    before { stub_content_store_has_item(base_path, content_item) }
 
-    assert_has_component_title(@content_item["title"])
+    it "displays the title" do
+      visit base_path
+
+      expect(page).to have_title("How government works")
+    end
   end
 
-  test "includes the current prime minister" do
-    setup_and_visit_content_item("reshuffle-mode-off")
+  # test "includes the current prime minister" do
+  #   setup_and_visit_content_item("reshuffle-mode-off")
 
-    assert page.has_text?(@content_item.dig("links", "current_prime_minister", 0, "title"))
-  end
+  #   assert page.has_text?(@content_item.dig("links", "current_prime_minister", 0, "title"))
+  # end
 
-  test "includes the count of ministers" do
-    setup_and_visit_content_item("reshuffle-mode-off")
+  # test "includes the count of ministers" do
+  #   setup_and_visit_content_item("reshuffle-mode-off")
 
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'prime_minister')}.+Prime Minister/m)
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'cabinet_ministers')}.+Cabinet ministers/m)
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'other_ministers')}.+Other ministers/m)
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'total_ministers')}.+Total ministers/m)
-  end
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'prime_minister')}.+Prime Minister/m)
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'cabinet_ministers')}.+Cabinet ministers/m)
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'other_ministers')}.+Other ministers/m)
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'ministerial_role_counts', 'total_ministers')}.+Total ministers/m)
+  # end
 
-  test "includes the count of departments" do
-    setup_and_visit_content_item("reshuffle-mode-off")
+  # test "includes the count of departments" do
+  #   setup_and_visit_content_item("reshuffle-mode-off")
 
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'department_counts', 'ministerial_departments')}.+Ministerial departments/m)
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'department_counts', 'non_ministerial_departments')}.+Non-ministerial departments/m)
-    assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'department_counts', 'agencies_and_other_public_bodies')}.+Agencies and other public bodies/m)
-  end
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'department_counts', 'ministerial_departments')}.+Ministerial departments/m)
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'department_counts', 'non_ministerial_departments')}.+Non-ministerial departments/m)
+  #   assert page.has_selector?(".gem-c-big-number", text: /#{@content_item.dig('details', 'department_counts', 'agencies_and_other_public_bodies')}.+Agencies and other public bodies/m)
+  # end
 
-  test "does not include the count of ministers during a reshuffle" do
-    setup_and_visit_content_item("reshuffle-mode-on")
+  # test "does not include the count of ministers during a reshuffle" do
+  #   setup_and_visit_content_item("reshuffle-mode-on")
 
-    assert_not page.has_selector?(".gem-c-big-number", text: /Prime Minister/m)
-    assert_not page.has_selector?(".gem-c-big-number", text: /Cabinet ministers/m)
-    assert_not page.has_selector?(".gem-c-big-number", text: /Other ministers/m)
-    assert_not page.has_selector?(".gem-c-big-number", text: /Total ministers/m)
-  end
+  #   assert_not page.has_selector?(".gem-c-big-number", text: /Prime Minister/m)
+  #   assert_not page.has_selector?(".gem-c-big-number", text: /Cabinet ministers/m)
+  #   assert_not page.has_selector?(".gem-c-big-number", text: /Other ministers/m)
+  #   assert_not page.has_selector?(".gem-c-big-number", text: /Total ministers/m)
+  # end
 end

71b185507f49eacf60ee5d196857a291124b5290
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-11 21:14:59 +0000

    Add additional headers
    - When policies and / or contact details are present, we need to add
      them to the contents list. Following previous practise, this is a
      presentational change. We can make it by providing a new version
      of the private only_level_two_headers method used in the
      concern.
    
    Audit Trail:
    https://github.com/alphagov/government-frontend/blob/582655dc383c311e46d626a39fd35ddd09874848/app/presenters/working_group_presenter.rb#L32-L37

--- app/presenters/working_group_presenter.rb
+++ app/presenters/working_group_presenter.rb
@@ -1,3 +1,10 @@
 class WorkingGroupPresenter < ContentItemPresenter
   include ContentsList
+
+  def additional_headers
+    additional_headers = []
+    additional_headers << { "id" => "policies", "level" => 2, "text" => I18n.t("formats.working_group.policies") } if content_item.policies.any?
+    additional_headers << { "id" => "contact-details", "level" => 2, "text" => I18n.t("formats.working_group.contact_details") } if content_item.email.present?
+    additional_headers
+  end
 end

6e748a53bd21234c5ca92b3c7401138902766a51
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-12-12 14:41:06 +0000

    Changes to make worldwide_office_presenter tests pass

--- app/presenters/worldwide_office_presenter.rb
+++ app/presenters/worldwide_office_presenter.rb
@@ -1,10 +1,6 @@
 class WorldwideOfficePresenter < ContentItemPresenter
   include GovspeakContentsList
 
-  def formatted_title
-    worldwide_organisation&.formatted_title
-  end
-
   def body
     content_item.content_store_response["details"]["access_and_opening_times"]
   end
@@ -17,10 +13,6 @@ class WorldwideOfficePresenter < ContentItemPresenter
     return unless content_item.worldwide_organisation
 
     WorldwideOrganisationPresenter.new(content_item.worldwide_organisation)
-  end
-
-  def sponsoring_organisations
-    worldwide_organisation&.sponsoring_organisations
   end
 
 private

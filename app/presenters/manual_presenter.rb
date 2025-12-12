7fc6dc4c254eeba5872397afec3f3f586a28af13
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-12-12 17:00:24 +0000

    Update presenter

--- app/presenters/manual_presenter.rb
+++ app/presenters/manual_presenter.rb
@@ -2,15 +2,15 @@ class ManualPresenter < ContentItemPresenter
   include LinkHelper
   include DateHelper
 
-  def other_metadata
-    updated_metadata(content_item.public_updated_at)
+  def other_metadata(request)
+    updated_metadata(content_item.public_updated_at, request)
   end
 
 private
 
-  def updated_metadata(updated_at)
-    current_path = content_item.base_path
-
+  def updated_metadata(updated_at, request)
+    current_path = request&.path
+    Rails.logger.debug "current path is #{current_path}"
     if (content_item.hmrc? || content_item.manual?) && current_path == "#{content_item.base_path}/updates"
       update_at_text = display_date(updated_at).to_s
     else

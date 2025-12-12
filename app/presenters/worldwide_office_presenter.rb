a1db1ec8c43a96a6f72fdb03da44faa57189bd10
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-12-12 14:41:06 +0000

    Changes to make worldwide_office_presenter tests pass
    formatted_title and sponsoring_organisation are removed as they are unused.

--- app/presenters/worldwide_office_presenter.rb
+++ app/presenters/worldwide_office_presenter.rb
@@ -2,10 +2,6 @@ class WorldwideOfficePresenter < ContentItemPresenter
   include WorldwideOrganisationBranding
   include ContentsList
 
-  def formatted_title
-    worldwide_organisation&.formatted_title
-  end
-
   def body
     content_item.content_store_response["details"]["access_and_opening_times"]
   end
@@ -18,10 +14,6 @@ class WorldwideOfficePresenter < ContentItemPresenter
     return unless content_item.worldwide_organisation
 
     WorldwideOrganisationPresenter.new(content_item.worldwide_organisation)
-  end
-
-  def sponsoring_organisations
-    worldwide_organisation&.sponsoring_organisations
   end
 
 private

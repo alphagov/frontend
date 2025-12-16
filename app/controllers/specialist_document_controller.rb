7b7689f7ad45bddaf7fd8cc6f12db8635abfa4a4
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-12-16 16:55:26 +0000

    Ensure that malformed URLs for licence transactions won't attempt to render as specialist documents
    - it's possible to malform a licence transaction url (for example /find-licences/skin-piercing-and-tattooing/.well-known) such that
      they don't match the paths for licence transaction. Since such paths are prefixes, they're routed here, but when they don't match
      the licence finder paths they fall through and (since they're specialist documents) they're matched by the specialist document
      format constraint. But... they won't render because they're missing elements that would be expected by the specialist document.
      Possibly we could add those methods, but realistically we don't want the frontend trying to render malformed URLs, so we add
      a check in that ensures the SpecialistDocument controller won't try to render somthing that isn't a specialist document item.
    
    Audit trail:
    https://govuk.sentry.io/issues/6854003373/?environment=production&project=202225&referrer=project-issue-stream

--- app/controllers/specialist_document_controller.rb
+++ app/controllers/specialist_document_controller.rb
@@ -2,6 +2,8 @@ class SpecialistDocumentController < ContentItemsController
   include Cacheable
 
   def show
+    raise RecordNotFound unless @content_item.instance_of?(SpecialistDocument)
+
     @presenter = SpecialistDocumentPresenter.new(content_item)
   end
 end

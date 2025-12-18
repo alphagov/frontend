0c507502df03375189df998e28026886a97c80fb
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2025-12-18 18:10:09 +0000

    Add Manual updates presenter with the relevant methods
    - As change_notes is a model method that is shared by manuals and hmrc_manuals, this is removed from the presenter and added in a concern in the next commit
    - Also added in a manual_helper method sanitize_manual_update_title copied over from government_frontend as it is only called for manuals from shared _updates partial.
    
    Audit trail(manual_updates_presenter): https://github.com/alphagov/government-frontend/blob/068b7fa23f013c9076a4d26b772934d3a8629cd4/app/presenters/manual_updates_presenter.rb
    Audit trail(manual_updates content_item): https://github.com/alphagov/government-frontend/blob/068b7fa23f013c9076a4d26b772934d3a8629cd4/app/presenters/content_item/manual_updates.rb
    Audit trail(manual_helper): https://github.com/alphagov/government-frontend/blob/068b7fa23f013c9076a4d26b772934d3a8629cd4/app/helpers/manuals_helper.rb#L4

---
+++ app/presenters/manual_updates_presenter.rb
@@ -0,0 +1,72 @@
+class ManualUpdatesPresenter < ContentItemPresenter
+
+  def page_title
+    I18n.t("formats.manuals.updates_page_title", title: manual_page_title)
+  end
+
+  def breadcrumbs
+    [
+      {
+        title: I18n.t("formats.manuals.breadcrumb_contents"),
+        url: base_path,
+      },
+    ]
+  end
+
+  def description
+    I18n.t("formats.manuals.updates_description", title: manual_page_title)
+  end
+
+  def presented_change_notes
+    group_updates_by_year(content_item.change_notes)
+  end
+
+  def sanitize_manual_update_title(title)
+    return "" if title.nil?
+
+    strip_tags(title).gsub(I18n.t("formats.manuals.updates_amendments"), "").gsub(/\s+/, " ").strip
+  end
+
+private
+
+  def updated_at(published_at)
+    Time.zone.parse(published_at).to_date
+  end
+
+  def group_updates_by_year(updates)
+    updates.group_by { |update| updated_at(update["published_at"]).year }
+            .sort_by { |year, _| year }
+            .map { |year, grouped_updates| [year, group_updates_by_day(grouped_updates)] }.reverse
+  end
+
+  def group_updates_by_day(updates)
+    updates.group_by { |update| updated_at(update["published_at"]) }
+            .sort_by { |day, _| day }
+            .map { |day, grouped_updates| [marked_up_date(day), group_updates_by_document(grouped_updates)] }.reverse
+  end
+
+  def group_updates_by_document(updates)
+    updates.group_by { |update| update["base_path"] }
+  end
+
+  def marked_up_date(date)
+    formatted_date = I18n.l(date, format: "%-d %B %Y") if date
+    updates_span = content_tag("span",
+                               I18n.t("formats.manuals.updates_amendments"),
+                               class: "govuk-visually-hidden")
+
+    formatted_date = "#{formatted_date} #{updates_span}"
+    sanitize(formatted_date)
+  end
+
+  def manual_page_title
+    title = content_item.title || ""
+    title += " - " if title.present?
+
+    if content_item.hmrc?
+      I18n.t("formats.manuals.hmrc_title", title:)
+    else
+      I18n.t("formats.manuals.title", title:)
+    end
+  end
+end

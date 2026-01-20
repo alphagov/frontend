70a932cc2b2580f054e4b8c9a0f3250a348b73f8
AshGDS <8880610+AshGDS@users.noreply.github.com>
2026-01-20 14:56:37 +0000

    Adjust contact presenter logic so it can be shared across routes
    Worldwide office was passing through a hash with content_store_response. Worldwide organisation doesn't have a content_store_response layer, so the presenters needed adjusting so that they can both share the code.

--- spec/presenter/linked_contact_presenter_spec.rb
+++ spec/presenter/linked_contact_presenter_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe LinkedContactPresenter do
   it "#post_address returns nil when nil on the content item" do
     contact_content_item.content_store_response["details"]["post_addresses"] = nil
 
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(presented_item.post_address).to be_nil
   end
@@ -17,7 +17,7 @@ RSpec.describe LinkedContactPresenter do
       "<span class=\"postal-code\">1634</span> <span class=\"locality\">Taguig City</span><br />\n"\
       "<span class=\"country-name\">Philippines</span>\n</p>\n</address>"
 
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(expected_address).to eq(presented_item.post_address)
   end
@@ -25,13 +25,13 @@ RSpec.describe LinkedContactPresenter do
   it "#email returns nil when nil on the content item" do
     contact_content_item.content_store_response["details"]["email_addresses"] = nil
 
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(presented_item.email).to be_nil
   end
 
   it "#email returns the first email address from the content item" do
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(presented_item.email).to eq("ukinthephilippines@fco.gov.uk")
   end
@@ -39,13 +39,13 @@ RSpec.describe LinkedContactPresenter do
   it "#contact_form_link returns nil when nil on the content item" do
     contact_content_item.content_store_response["details"]["contact_form_links"] = nil
 
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(presented_item.contact_form_link).to be_nil
   end
 
   it "#contact_form_link returns the contact form link from the content item" do
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(presented_item.contact_form_link).to eq("http://www.gov.uk/contact-consulate-manila")
   end
@@ -56,7 +56,7 @@ RSpec.describe LinkedContactPresenter do
       "Please call +63 (02) 8 858 2200 / +44 20 7136 6857. <br/><br/>Public access to the embassy is by appointment only. "\
       "Please visit https://www.gov.uk/world/philippines or call +63 (02) 8 858 2200.</p>"
 
-    presented_item = described_class.new(contact_content_item)
+    presented_item = described_class.new(contact_content_item.content_store_response)
 
     expect(expected_comments).to eq(presented_item.comments)
   end

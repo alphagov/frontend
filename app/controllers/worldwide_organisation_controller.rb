3d3c9c821ccae1bb1033299659cd814ce734cf3b
AshGDS <8880610+AshGDS@users.noreply.github.com>
2026-01-20 15:16:48 +0000

    Fix translation nav functionality on worldwide_organisation pages

--- app/controllers/worldwide_organisation_controller.rb
+++ app/controllers/worldwide_organisation_controller.rb
@@ -2,6 +2,7 @@ class WorldwideOrganisationController < ContentItemsController
   include Cacheable
 
   def show
+    I18n.locale = @content_item.locale
     @presenter = WorldwideOrganisationPresenter.new(@content_item)
   end
 end

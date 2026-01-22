fd909fd409e97d1cb9150db7ac413fdca49a137c
Sonia Hussain <56222256+CodeSonia@users.noreply.github.com>
2026-01-22 15:34:03 +0000

    Add hmrc_manual route, controller and placeholder view

--- config/routes.rb
+++ config/routes.rb
@@ -203,6 +203,10 @@ Rails.application.routes.draw do
     get "*path", to: "flexible_page#show"
   end
 
+  constraints FullPathFormatRoutingConstraint.new("hmrc_manual") do
+    get "*path", to: "hmrc_manual#show"
+  end
+
   constraints FullPathFormatRoutingConstraint.new("html_publication") do
     get "*path(.:locale)", to: "html_publication#show"
   end

0d6dc578ec116ba8afaa1b82e39acbe181e64288
Sonia Hussain <56222256+CodeSonia@users.noreply.github.com>
2026-01-26 21:13:36 +0000

    Add Updatable concern test to HMRC model test

--- spec/models/hmrc_manual_spec.rb
+++ spec/models/hmrc_manual_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe HmrcManual do
-  let(:content_store_response) { GovukSchemas::Example.find("hmrc_manual", example_name: "hmrc_manual") }
+  let(:content_store_response) { GovukSchemas::Example.find("hmrc_manual", example_name: "vat-government-public-bodies") }
 
-  it_behaves_like "it can have section groups", "hmrc_manual", "hmrc_manual"
-  it_behaves_like "it can be a manual", "hmrc_manual", "hmrc_manual"
-  it_behaves_like "it can be a manual section", "hmrc_manual", "hmrc_manual"
+  it_behaves_like "it can have section groups", "hmrc_manual", "vat-government-public-bodies"
+  it_behaves_like "it can be a manual", "hmrc_manual", "vat-government-public-bodies"
+  it_behaves_like "it can be a manual section", "hmrc_manual", "vat-government-public-bodies"
 end

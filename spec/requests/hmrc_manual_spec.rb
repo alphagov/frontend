c6d75768fb03ded046426c55ff4c3f579afa3bbc
Sonia Hussain <56222256+CodeSonia@users.noreply.github.com>
2026-01-26 12:18:58 +0000

    Add request specs for HMRC manuals & updates page

---
+++ spec/requests/hmrc_manual_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe "HMRC Manual" do
+  let(:content_item) { GovukSchemas::Example.find("hmrc_manual", example_name: "vat-government-public-bodies") }
+  let(:base_path) { content_item.fetch("base_path") }
+
+  before do
+    stub_content_store_does_not_have_item("/hmrc-internal-manuals")
+    stub_content_store_has_item(base_path, content_item)
+  end
+
+  describe "GET show" do
+    it "responds successfully" do
+      get base_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the show template" do
+      get base_path
+      expect(response).to render_template("show")
+    end
+
+    it "sets cache-control headers from the Content Store" do
+      get base_path
+      expect(response).to honour_content_store_ttl
+    end
+  end
+
+  describe "GET updates" do
+    let(:updates_path) { "#{base_path}/updates" }
+
+    before do
+      stub_content_store_has_item(updates_path, content_item)
+    end
+
+    it "responds successfully" do
+      get updates_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the updates template" do
+      get updates_path
+      expect(response).to render_template("updates")
+    end
+
+    it "sets cache-control headers from the Content Store" do
+      get updates_path
+      expect(response).to honour_content_store_ttl
+    end
+  end
+end

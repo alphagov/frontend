bcf10ed158262ba5a350c780a02ddb790019d2a2
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-01-27 15:12:22 +0000

    Add page_title presenter model (WIP)
    - Page titles in manual sections appear to be the manual title,
      section title, the word Guidance. It's quite hard to pin this down in
      government-frontend, so we may come back to this.

--- app/presenters/manual_section_presenter.rb
+++ app/presenters/manual_section_presenter.rb
@@ -1,3 +1,7 @@
 class ManualSectionPresenter < ContentItemPresenter
   include ManualMetadata
+
+  def page_title
+    "#{content_item.manual_title} - #{content_item.title} - Guidance"
+  end
 end
--- spec/presenter/manual_section_presenter_spec.rb
+++ spec/presenter/manual_section_presenter_spec.rb
@@ -1,3 +1,14 @@
 RSpec.describe ManualSectionPresenter do
+  subject(:manual_section_presenter) { described_class.new(content_item) }
+
+  let(:content_item) { ManualSection.new(content_store_response) }
+  let(:content_store_response) { GovukSchemas::Example.find("manual_section", example_name: "what-is-content-design") }
+
   it_behaves_like "it can have manual metadata", Manual
+
+  describe "#page_title" do
+    it "returns the manual name, section name, plus Guidance" do
+      expect(manual_section_presenter.page_title).to eq("Content design: planning, writing and managing content - What is content design? - Guidance")
+    end
+  end
 end

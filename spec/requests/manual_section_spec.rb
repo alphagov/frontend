929ef92767d7480e452b92177d0d3e6bb129255f
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-01-28 10:47:26 +0000

    Add basic request test for manual_section

---
+++ spec/requests/manual_section_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe "Manual Section" do
+  describe "GET section" do
+    context "when a manual section exists" do
+      let(:content_item) { GovukSchemas::Example.find("manual_section", example_name: "what-is-content-design") }
+      let(:base_path) { content_item.fetch("base_path") }
+
+      before do
+        stub_content_store_has_item(base_path, content_item)
+      end
+
+      it "succeeds" do
+        get base_path
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the show template" do
+        get base_path
+
+        expect(response).to render_template(:section)
+      end
+    end
+  end
+end

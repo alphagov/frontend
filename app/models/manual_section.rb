b7f41ce8b718644051fc03faf549b57de6211e3c
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-01-28 11:15:31 +0000

    Add intro and sections methods
    - These will be used to build the content list if needed, and later on
      the contents itself.

--- app/models/manual_section.rb
+++ app/models/manual_section.rb
@@ -1,4 +1,14 @@
 class ManualSection < ContentItem
   include ManualSections
   include ManualLike
+
+  attr_reader :intro, :sections
+
+  def initialize(content_store_response)
+    super
+
+    section_tokenizer = HtmlSectionTokenizer.new(body)
+    @intro = section_tokenizer.intro.first&.dig(:content)
+    @sections = section_tokenizer.sections_with_heading
+  end
 end

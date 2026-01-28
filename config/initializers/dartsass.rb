ddec3c1118fa8d79edd427d800fc0fba8d3ac7ff
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-01-28 09:33:17 +0000

    Stop compiling individual CSS for missions pages
    - we were hugely optimising the styles for the /missions pages by giving each block its own CSS file, but this is overkill
    - stylesheets are pretty small anyway, and haven't changed in the last year (so this model gives us no caching benefit)
    - also makes forthcoming changes to the Sass model easier to manage

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -24,19 +24,6 @@ APP_STYLESHEETS = {
   "views/_travel-advice.scss" => "views/_travel-advice.css",
   "views/_worldwide-organisation.scss" => "views/_worldwide-organisation.css",
   "views/_landing_page.scss" => "views/_landing_page.css",
-  "views/_landing_page/block-error.scss" => "views/_landing_page/block-error.css",
-  "views/_landing_page/box.scss" => "views/_landing_page/box.css",
-  "views/_landing_page/card.scss" => "views/_landing_page/card.css",
-  "views/_landing_page/columns_layout.scss" => "views/_landing_page/columns_layout.css",
-  "views/_landing_page/hero.scss" => "views/_landing_page/hero.css",
-  "views/_landing_page/image.scss" => "views/_landing_page/image.css",
-  "views/_landing_page/logo.scss" => "views/_landing_page/logo.css",
-  "views/_landing_page/featured.scss" => "views/_landing_page/featured.css",
-  "views/_landing_page/main-navigation.scss" => "views/_landing_page/main-navigation.css",
-  "views/_landing_page/map.scss" => "views/_landing_page/map.css",
-  "views/_landing_page/quote.scss" => "views/_landing_page/quote.css",
-  "views/_landing_page/side-navigation.scss" => "views/_landing_page/side-navigation.css",
-  "views/_landing_page/themes/prime-ministers-office-10-downing-street.scss" => "views/_landing_page/themes/prime-ministers-office-10-downing-street.css",
 }.freeze
 
 all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.component_guide_stylesheet)

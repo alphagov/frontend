ec23843797931420bfa7bc868c5d3fb0719a47ad
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-01-29 16:00:54 +0000

    Remove obsolete conditions
    - These conditions are always true (we're always either in a manual, manual_section, hmrc_manual or hmrc_manual_section)

--- app/presenters/concerns/manual_metadata.rb
+++ app/presenters/concerns/manual_metadata.rb
@@ -11,7 +11,7 @@ private
   def updated_metadata(updated_at, request)
     current_path = request&.path
     Rails.logger.debug "current path is #{current_path}"
-    if (content_item.hmrc? || content_item.manual?) && current_path == "#{content_item.base_path}/updates"
+    if current_path == "#{content_item.base_path}/updates"
       update_at_text = display_date(updated_at).to_s
     else
       updates_link = govuk_styled_link(I18n.t("formats.manuals.see_all_updates"), path: "#{content_item.manual_content_item.base_path}/updates")

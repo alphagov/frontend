9231fdfdb5398baca2025abb42713df42b21bed4
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2026-01-30 19:06:25 +0000

    Add manual updates system tests
    Audit trail: https://github.com/alphagov/government-frontend/blob/029a5fcaf56e3094de2bd7932bbc27cd742d4722/test/integration/manual_updates_test.rb

--- spec/system/manual_spec.rb
+++ spec/system/manual_spec.rb
@@ -62,4 +62,58 @@ RSpec.describe "Manual" do
       end
     end
   end
+
+  context "when visiting a manual updates page" do
+    let(:updates_path) { "#{base_path}/updates" }
+
+    before do
+      stub_content_store_has_item(updates_path, content_store_response)
+      visit updates_path
+    end
+
+    it "renders the manual updates page correctly" do
+      expect(page).to have_title("Updates - #{content_store_response['title']}")
+    end
+
+    it "has 1 content id" do
+      content_ids = page.all('[id="content"]')
+      expect(content_ids.count).to eq(1)
+    end
+
+    it "renders manual updates metadata" do
+      within(".gem-c-metadata--inverse") do
+        expect(page).to have_text("From:")
+        expect(page).to have_link("Government Digital Service", href: "/government/organisations/government-digital-service")
+        expect(page).to have_text("Published 27 April 2015")
+        expect(page).to have_text("Updated: 27 April 2015")
+      end
+    end
+
+    it "renders search box" do
+      within ".gem-c-search" do
+        expect(page).to have_css("label.gem-c-search__label", text: "Search this manual")
+      end
+    end
+
+    it "renders back link" do
+      expect(page).to have_link("Back to contents", href: content_store_response["base_path"])
+    end
+
+    it "renders change note updates" do
+      expect(page).to have_css(".gem-c-accordion")
+
+      accordion_sections = page.all(".govuk-accordion__section")
+      expect(accordion_sections.count).to eq(2)
+
+      within accordion_sections[0] do
+        expect(page).to have_link("What is content design?", href: "/guidance/content-design/what-is-content-design")
+        expect(page).to have_text("New section added.")
+      end
+
+      within accordion_sections[1] do
+        expect(page).to have_link("Content types", href: "/guidance/content-design/content-types")
+        expect(page).to have_text("New section added.")
+      end
+    end
+  end
 end

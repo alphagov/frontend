edd6572894fc737bb738967e79631269c5dfbb41
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-03 10:33:25 +0000

    Add show_contents_list? method to presenter
    Audit Trail:
    https://github.com/alphagov/government-frontend/blob/ced639e5061b9fd7ae977e350a726eb00627cc34/app/presenters/manual_section_presenter.rb#L61C1-L67

--- app/presenters/manual_section_presenter.rb
+++ app/presenters/manual_section_presenter.rb
@@ -1,3 +1,9 @@
 class ManualSectionPresenter < ContentItemPresenter
   include ManualMetadata
+
+  MOJ_ORGANISATION_CONTENT_ID = "dcc907d6-433c-42df-9ffb-d9c68be5dc4d".freeze
+
+  def show_contents_list?
+    content_item.organisations.first.content_id == MOJ_ORGANISATION_CONTENT_ID
+  end
 end
--- spec/presenter/manual_section_presenter_spec.rb
+++ spec/presenter/manual_section_presenter_spec.rb
@@ -1,3 +1,26 @@
 RSpec.describe ManualSectionPresenter do
+  subject(:manual_section_presenter) { described_class.new(content_item) }
+
+  let(:content_item) { ManualSection.new(content_store_response) }
+  let(:content_store_response) { GovukSchemas::Example.find("manual_section", example_name: "what-is-content-design") }
+
   it_behaves_like "it can have manual metadata", Manual
+
+  describe "#show_contents_list?" do
+    it "returns false" do
+      expect(manual_section_presenter.show_contents_list?).to be false
+    end
+
+    context "when the manual is published by the Ministry of Justice" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("manual_section", example_name: "what-is-content-design").tap do |item|
+          item["links"]["organisations"][0]["content_id"] = "dcc907d6-433c-42df-9ffb-d9c68be5dc4d"
+        end
+      end
+
+      it "returns true" do
+        expect(manual_section_presenter.show_contents_list?).to be true
+      end
+    end
+  end
 end

ccf68d32569c5c556b2a32f4fb030330e8de29fe
Sonia Hussain <56222256+CodeSonia@users.noreply.github.com>
2026-02-04 15:39:13 +0000

    Update hmrc_manual_spec model test - it don't need the manual sections

--- spec/models/hmrc_manual_spec.rb
+++ spec/models/hmrc_manual_spec.rb
@@ -2,6 +2,5 @@ RSpec.describe HmrcManual do
   let(:content_store_response) { GovukSchemas::Example.find("hmrc_manual", example_name: "vat-government-public-bodies") }
 
   it_behaves_like "it can have section groups", "hmrc_manual", "vat-government-public-bodies"
-  it_behaves_like "it can be a manual", "hmrc_manual", "vat-government-public-bodies"
-  it_behaves_like "it can be a manual section", "hmrc_manual", "vat-government-public-bodies"
+  it_behaves_like "it can have manual updates", "hmrc_manual", "vat-government-public-bodies"
 end

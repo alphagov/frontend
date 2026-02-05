91f8786906cb49732de34929de9c22c8d3cf31a3
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-05 14:42:36 +0000

    Convert initial system tests (WIP)

--- spec/system/topical_event_spec.rb
+++ spec/system/topical_event_spec.rb
@@ -1,28 +1,25 @@
 RSpec.describe "Topical Event pages" do
-  include SearchApiHelpers
-  let(:content_item) { fetch_fixture("topical_event") }
-  let(:base_path) { content_item["base_path"] }
+  include GdsApi::TestHelpers::Search
+
+  let(:content_store_response) { GovukSchemas::Example.find("topical_event", example_name: "western-balkans-summit-london-2018") }
+  let(:base_path) { content_store_response.fetch("base_path") }
 
   before do
-    stub_content_store_has_item(base_path, content_item)
-    stub_search(body: { results: [] })
-
-    allow(Random).to receive(:rand).with(1.0).and_return(1)
+    stub_content_store_has_item(base_path, content_store_response)
+    stub_any_search_to_return_no_results
+    visit base_path
   end
 
   it "sets the page title" do
-    visit base_path
-    expect(page).to have_title("#{content_item['title']} - GOV.UK")
+    expect(page).to have_title("#{content_store_response['title']} - GOV.UK")
   end
 
   it "sets the page description" do
-    visit base_path
-    expect(page).to have_text(content_item["description"])
+    expect(page).to have_text(content_store_response["description"])
   end
 
   it "sets the page meta description" do
-    visit base_path
-    expect(page).to have_selector("meta[name='description'][content='#{content_item['description']}']", visible: :hidden)
+    expect(page).to have_selector("meta[name='description'][content='#{content_store_response['description']}']", visible: :hidden)
   end
 
   it "includes the image and alt text" do

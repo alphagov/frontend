738e05250acf36414953b8095044397ec1863232
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2026-02-05 11:32:48 +0000

    Remove related tests from manual_section_spec

--- spec/models/manual_section_spec.rb
+++ spec/models/manual_section_spec.rb
@@ -10,10 +10,6 @@ RSpec.describe ManualSection do
     stub_content_store_has_item(manual_content_item.fetch("base_path"), manual_content_item)
   end
 
-  it_behaves_like "it can have manual title", "manual_section", "what-is-content-design"
-  it_behaves_like "it can have document heading", "manual_section", "what-is-content-design"
-  it_behaves_like "it can have breadcrumbs", "manual_section", "what-is-content-design"
-  it_behaves_like "it can have manual base path", "manual_section", "what-is-content-design"
   it_behaves_like "it can have section groups", "manual", "content-design"
 
   describe "#contents_outline" do

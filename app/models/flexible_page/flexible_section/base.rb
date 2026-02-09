b820d0c3529e8723b3217ca58004a4e26aee82f2
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-09 14:14:38 +0000

    refactor: alphabetize attributes list

--- app/models/flexible_page/flexible_section/base.rb
+++ app/models/flexible_page/flexible_section/base.rb
@@ -1,11 +1,11 @@
 module FlexiblePage::FlexibleSection
   class Base
-    attr_reader :flexible_section_hash, :content_item, :type
+    attr_reader :content_item, :flexible_section_hash, :type
 
     def initialize(flexible_section_hash, content_item)
+      @content_item = content_item
       @flexible_section_hash = flexible_section_hash
       @type = flexible_section_hash["type"]
-      @content_item = content_item
     end
   end
 end

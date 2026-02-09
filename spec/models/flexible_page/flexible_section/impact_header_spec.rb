4c16f106d1683e618b23e6951062e51f99b864db
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-02-09 12:02:45 +0000

    Add impact header flexible section
    - adds a flexible section for a new header for topical event pages
    - includes breadcrumbs, title, lead paragraph, and optional image
    - layout is half and half unless image is not supplied, in which case falls back to 2/3rds column width for the text

---
+++ spec/models/flexible_page/flexible_section/impact_header_spec.rb
@@ -0,0 +1,73 @@
+RSpec.describe FlexiblePage::FlexibleSection::ImpactHeader do
+  header = {
+    "title" => "This is the page H1",
+    "description" => "This is the lead paragraph",
+    "breadcrumbs" => [
+      {
+        "title" => "Home",
+        "url" => "/",
+      },
+    ],
+    "image" => {
+      "sources" => {
+        "desktop" => "https://assets.publishing.service.gov.uk/media/674725702f94bef8ff48c043/hero_desktop_1x_F_Desktop_HD-50.jpg",
+        "desktop_2x" => "https://assets.publishing.service.gov.uk/media/67472570cf0d45234dd8d0a8/hero_desktop_2x_F_Desktop_HD-50.jpg",
+        "mobile" => "https://assets.publishing.service.gov.uk/media/67472591a72d7eb7f348c047/hero_mobile_1x_F_MOBILE_HD-50.jpg",
+        "mobile_2x" => "https://assets.publishing.service.gov.uk/media/674725912f94bef8ff48c044/hero_mobile_2x_F_MOBILE_HD-50.jpg",
+        "tablet" => "https://assets.publishing.service.gov.uk/media/6747258077462f78091474e2/hero_tablet_1x_F_Tablet_HD-50.jpg",
+        "tablet_2x" => "https://assets.publishing.service.gov.uk/media/67472580886c31e352d8d0b1/hero_tablet_2x_F_Tablet_HD-50.jpg",
+      },
+    },
+  }
+
+  header_without_image = {
+    "title" => "Page title",
+    "description" => "description",
+    "breadcrumbs" => [
+      {
+        "title" => "Home",
+        "url" => "/",
+      },
+    ],
+  }
+
+  subject(:content_hash) do
+    header
+  end
+
+  let(:impact) { described_class.new(content_hash, FlexiblePage.new({})) }
+
+  describe "when complete data is provided" do
+    it "includes page title and description" do
+      expect(impact.breadcrumbs).to eq([{ title: "Home", url: "/" }])
+      expect(impact.title).to eq("This is the page H1")
+      expect(impact.description).to eq("This is the lead paragraph")
+    end
+
+    it "returns image information" do
+      expect(impact.image).to eq({
+        sources: {
+          desktop: "https://assets.publishing.service.gov.uk/media/674725702f94bef8ff48c043/hero_desktop_1x_F_Desktop_HD-50.jpg",
+          desktop_2x: "https://assets.publishing.service.gov.uk/media/67472570cf0d45234dd8d0a8/hero_desktop_2x_F_Desktop_HD-50.jpg",
+          mobile: "https://assets.publishing.service.gov.uk/media/67472591a72d7eb7f348c047/hero_mobile_1x_F_MOBILE_HD-50.jpg",
+          mobile_2x: "https://assets.publishing.service.gov.uk/media/674725912f94bef8ff48c044/hero_mobile_2x_F_MOBILE_HD-50.jpg",
+          tablet: "https://assets.publishing.service.gov.uk/media/6747258077462f78091474e2/hero_tablet_1x_F_Tablet_HD-50.jpg",
+          tablet_2x: "https://assets.publishing.service.gov.uk/media/67472580886c31e352d8d0b1/hero_tablet_2x_F_Tablet_HD-50.jpg",
+        },
+      })
+    end
+  end
+
+  describe "when there is no image" do
+    let(:content_hash) do
+      header_without_image
+    end
+
+    it "returns only the text content" do
+      expect(impact.breadcrumbs).to eq([{ title: "Home", url: "/" }])
+      expect(impact.title).to eq("Page title")
+      expect(impact.description).to eq("description")
+      expect(impact.image).to be(false)
+    end
+  end
+end
---
+++ spec/views/flexible_page/flexible_sections/_impact_header.html.erb_spec.rb
@@ -0,0 +1,78 @@
+RSpec.describe "Impact header flexible section" do
+  let(:flexible_section) do
+    FlexiblePage::FlexibleSection::ImpactHeader.new(
+      {
+        "title" => "page title",
+        "description" => "page description",
+        "breadcrumbs" => [
+          {
+            "title" => "Home",
+            "url" => "/",
+          },
+        ],
+        "image" => {
+          "sources" => {
+            "desktop" => "https://assets.publishing.service.gov.uk/media/674725702f94bef8ff48c043/hero_desktop_1x_F_Desktop_HD-50.jpg",
+            "desktop_2x" => "https://assets.publishing.service.gov.uk/media/67472570cf0d45234dd8d0a8/hero_desktop_2x_F_Desktop_HD-50.jpg",
+            "mobile" => "https://assets.publishing.service.gov.uk/media/67472591a72d7eb7f348c047/hero_mobile_1x_F_MOBILE_HD-50.jpg",
+            "mobile_2x" => "https://assets.publishing.service.gov.uk/media/674725912f94bef8ff48c044/hero_mobile_2x_F_MOBILE_HD-50.jpg",
+            "tablet" => "https://assets.publishing.service.gov.uk/media/6747258077462f78091474e2/hero_tablet_1x_F_Tablet_HD-50.jpg",
+            "tablet_2x" => "https://assets.publishing.service.gov.uk/media/67472580886c31e352d8d0b1/hero_tablet_2x_F_Tablet_HD-50.jpg",
+          },
+        },
+      },
+      nil,
+    )
+  end
+
+  let(:flexible_section_without_image) do
+    FlexiblePage::FlexibleSection::ImpactHeader.new(
+      {
+        "title" => "page title",
+        "description" => "page description",
+        "breadcrumbs" => [
+          {
+            "title" => "Home",
+            "url" => "/",
+          },
+        ],
+      },
+      nil,
+    )
+  end
+
+  describe "an impact header" do
+    before do
+      render(template: "flexible_page/flexible_sections/_impact_header", locals: { flexible_section: })
+    end
+
+    it "renders impact header section" do
+      expect(rendered).to have_selector("[data-flexible-section='impact-header']")
+      expect(rendered).to have_selector(".impact-header .impact-header__container")
+    end
+
+    it "renders the text and image" do
+      expect(rendered).to have_selector(".gem-c-breadcrumbs", text: "Home")
+      expect(rendered).to have_selector(".gem-c-heading", text: "page title")
+      expect(rendered).to have_selector(".gem-c-lead-paragraph", text: "page description")
+      expect(rendered).to have_selector(".impact-header__image[src='https://assets.publishing.service.gov.uk/media/674725702f94bef8ff48c043/hero_desktop_1x_F_Desktop_HD-50.jpg']")
+      expect(rendered).to have_selector(".impact-header__image[alt='']")
+    end
+  end
+
+  describe "an impact header without an image" do
+    before do
+      render(template: "flexible_page/flexible_sections/_impact_header", locals: { flexible_section: flexible_section_without_image })
+    end
+
+    it "renders impact header section" do
+      expect(rendered).to have_selector("[data-flexible-section='impact-header']")
+      expect(rendered).to have_selector(".impact-header.impact-header--grid .govuk-grid-row")
+      expect(rendered).to have_selector(".gem-c-breadcrumbs", text: "Home")
+    end
+
+    it "does not include an image" do
+      expect(rendered).not_to have_selector(".impact-header__image")
+    end
+  end
+end

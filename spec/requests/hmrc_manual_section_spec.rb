c68a02f04aa7694cf5a082316aedc6dd63080862
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-10 13:45:18 +0000

    Stub parent manual in request tests
    - Now we're looking for information from the parent manual in the header, we need to stub
      it in the request tests.

--- spec/requests/hmrc_manual_section_spec.rb
+++ spec/requests/hmrc_manual_section_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "HRMC Manual Section" do
 
       before do
         stub_content_store_has_item(base_path, content_item)
+        manual_content_item = GovukSchemas::Example.find("hmrc_manual", example_name: "vat-government-public-bodies")
+        stub_content_store_has_item(manual_content_item.fetch("base_path"), manual_content_item)
 
         get base_path
       end

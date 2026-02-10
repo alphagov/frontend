4059a5f5a857369d9f60a9341aa6e725102381c8
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-10 14:32:50 +0000

    Add breadcrumbs method to presenter
    - Breadcrumbs are specified in an array in the content item details block (which may be empty), but always start
      with the back to content option which takes you back to the main manual.

--- app/presenters/hmrc_manual_section_presenter.rb
+++ app/presenters/hmrc_manual_section_presenter.rb
@@ -1,3 +1,19 @@
 class HmrcManualSectionPresenter < ContentItemPresenter
   include ManualMetadata
+
+  def breadcrumbs
+    base_breadcrumb = {
+      title: I18n.t("formats.manuals.breadcrumb_contents"),
+      url: content_item.manual_base_path,
+    }
+
+    additional_breadcrumbs = content_item.details["breadcrumbs"].map do |breadcrumb|
+      {
+        title: breadcrumb["section_id"],
+        url: breadcrumb["base_path"],
+      }
+    end
+
+    [base_breadcrumb] + additional_breadcrumbs
+  end
 end
--- spec/presenter/hmrc_manual_section_presenter_spec.rb
+++ spec/presenter/hmrc_manual_section_presenter_spec.rb
@@ -1,3 +1,52 @@
 RSpec.describe HmrcManualSectionPresenter do
+  include ContentStoreHelpers
+
+  subject(:hmrc_manual_section_presenter) { described_class.new(content_item) }
+
+  let(:content_item) { HmrcManualSection.new(content_store_response) }
+  let(:content_store_response) { GovukSchemas::Example.find("hmrc_manual_section", example_name: "vatgpb2000") }
+
+  before do
+    manual_content_item = GovukSchemas::Example.find("hmrc_manual", example_name: "vat-government-public-bodies")
+    stub_content_store_has_item(manual_content_item.fetch("base_path"), manual_content_item)
+  end
+
   it_behaves_like "it can have manual metadata", HmrcManual
+
+  describe "#breadcrumbs" do
+    it "returns a link to the manual" do
+      expect(hmrc_manual_section_presenter.breadcrumbs).to eq([
+        {
+          title: "Back to contents",
+          url: "/hmrc-internal-manuals/vat-government-and-public-bodies",
+        },
+      ])
+    end
+
+    context "when breadcrumbs are present in the details block" do
+      let(:content_store_response) do
+        GovukSchemas::Example.find("hmrc_manual_section", example_name: "vatgpb2000").tap do |item|
+          item["details"]["breadcrumbs"] = [
+            {
+              "base_path" => "/hmrc-internal-manuals/vat-government-and-public-bodies/vatgbp2000",
+              "section_id" => "VAT GBP 2000",
+            },
+          ]
+        end
+      end
+
+      it "appends the specified links to the breadcrumb" do
+        expect(hmrc_manual_section_presenter.breadcrumbs).to eq([
+          {
+            title: "Back to contents",
+            url: "/hmrc-internal-manuals/vat-government-and-public-bodies",
+          },
+          {
+            title: "VAT GBP 2000",
+            url: "/hmrc-internal-manuals/vat-government-and-public-bodies/vatgbp2000",
+          },
+        ])
+      end
+    end
+  end
 end

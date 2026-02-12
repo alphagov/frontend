9513381c5521d1aa70c618da7d504017669a227b
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-12 11:55:20 +0000

    Simplify request test for flexible page
    - Added context doesn't add anything, so remove
    - move the get into the before block to make expectations smaller
    - move let above before/after, as is preferred

--- spec/requests/flexible_page_spec.rb
+++ spec/requests/flexible_page_spec.rb
@@ -1,28 +1,23 @@
 RSpec.describe "FlexiblePage" do
   describe "GET show" do
-    context "when visiting a Flexible page" do
-      before do
-        ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = "true"
-        stub_const("ContentItemLoaders::LocalFileLoader::LOCAL_ITEMS_PATH", "spec/fixtures/local-content-items")
-      end
+    let(:base_path) { "/flexible-page" }
 
-      after do
-        ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = nil
-      end
+    before do
+      ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = "true"
+      stub_const("ContentItemLoaders::LocalFileLoader::LOCAL_ITEMS_PATH", "spec/fixtures/local-content-items")
+      get base_path
+    end
 
-      let(:base_path) { "/flexible-page" }
+    after do
+      ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = nil
+    end
 
-      it "returns 200" do
-        get base_path
+    it "returns 200" do
+      expect(response).to have_http_status(:ok)
+    end
 
-        expect(response).to have_http_status(:ok)
-      end
-
-      it "renders the show template" do
-        get base_path
-
-        expect(response).to render_template("show")
-      end
+    it "renders the show template" do
+      expect(response).to render_template("show")
     end
   end
 end

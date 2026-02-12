e8d8a2f851ab11b1821319c539061cb33e9b1933
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2026-02-12 17:57:46 +0000

    Include contents_list concern in presenter
    - Added concern to access methods to display contents list

--- spec/presenter/worldwide_corporate_information_page_presenter_spec.rb
+++ spec/presenter/worldwide_corporate_information_page_presenter_spec.rb
@@ -1,7 +1,10 @@
 RSpec.describe WorldwideCorporateInformationPagePresenter do
-  let(:content_item) { WorldwideCorporateInformationPage.new(GovukSchemas::Example.find("worldwide_corporate_information_page", example_name: "worldwide_corporate_information_page")) }
+  let(:content_store_response) { GovukSchemas::Example.find("worldwide_corporate_information_page", example_name: "worldwide_corporate_information_page") }
+  let(:content_item) { WorldwideCorporateInformationPage.new(content_store_response) }
 
   let(:presenter) { described_class.new(content_item) }
+
+  it_behaves_like "it can have a contents list", "worldwide_corporate_information_page", "worldwide_corporate_information_page"
 
   it "returns an instance of WorldwideOrganisationPresenter" do
     expect(presenter.worldwide_organisation).to be_a(WorldwideOrganisationPresenter)

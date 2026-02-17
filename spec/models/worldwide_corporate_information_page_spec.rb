850e6e4debf4077e5805138bca3deb72724bec52
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2026-02-17 15:46:23 +0000

    Add headers attribute reader in model
    - Added headers attribute reader to retrieve the headers details that will be required to display the contents-list similar to other routes migrated to frontend

--- spec/models/worldwide_corporate_information_page_spec.rb
+++ spec/models/worldwide_corporate_information_page_spec.rb
@@ -3,6 +3,22 @@ RSpec.describe WorldwideCorporateInformationPage do
 
   let(:content_item) { described_class.new(content_store_response) }
 
+  describe "#headers" do
+    context "when there are headers in the details hash" do
+      it "returns the headers" do
+        expect(content_item.headers).to eq(content_store_response.dig("details", "headers"))
+      end
+    end
+
+    context "when there are no headers in the details hash" do
+      it "returns an empty array" do
+        content_store_response["details"].delete("headers")
+
+        expect(content_item.headers).to eq([])
+      end
+    end
+  end
+
   describe "#worldwide_organisation" do
     it "returns the first worldwide organisation" do
       expect(content_item.worldwide_organisation.content_store_response).to eq(content_store_response.dig("links", "worldwide_organisation", 0))

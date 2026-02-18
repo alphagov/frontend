17837890388aac6be8cdfc91cdec434deacfcd41
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2026-02-18 09:37:06 +0000

    Contract history page to only expect details.images
    We will no longer send a single sidebar_image in the details hash.

--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -4,35 +4,23 @@ RSpec.describe History do
   let(:content_store_response) do
     {
       "title" => "History Page",
-      "details" => details,
+      "details" => {
+        "body" => "Hello",
+        "headers" => [],
+        "images" => images,
+      },
     }
   end
 
-  let(:base_details) do
-    {
-      "body" => "Hello",
-      "headers" => [],
-    }
-  end
-
-  let(:sidebar_image) do
-    {
-      "caption" => "Here's an image!",
-      "url" => "https://assets.publishing.service.gov.uk/image.png",
-    }
-  end
-
-  let(:details) do
-    base_details.merge(
-      "sidebar_image" => sidebar_image,
-    )
-  end
-
-  let(:expected_image_attributes) do
-    {
-      alt: "Here's an image!",
-      src: "https://assets.publishing.service.gov.uk/image.png",
-    }
+  let(:images) do
+    [
+      {
+        "type" => "sidebar",
+        "caption" => "Here's an image!",
+        "url" => "https://assets.publishing.service.gov.uk/image.png",
+        "content_type" => "image/jpeg",
+      },
+    ]
   end
 
   describe "flexible_sections attribute" do
@@ -44,77 +32,55 @@ RSpec.describe History do
     end
 
     context "when details.images has a 'sidebar' type image" do
-      let(:details) do
-        base_details.merge(
-          "images" => [
-            sidebar_image.merge("type" => "sidebar"),
-          ],
-        )
+      let(:expected_image_attributes) do
+        {
+          alt: "Here's an image!",
+          src: "https://assets.publishing.service.gov.uk/image.png",
+        }
       end
 
-      it "uses the 'sidebar' type image from details.images" do
+      it "includes the sidebar image in the rich content" do
         expect(history.flexible_sections.third.image).to have_attributes(expected_image_attributes)
       end
     end
 
     context "when details.images has no 'sidebar' type image" do
-      let(:details) do
-        base_details.merge(
-          "images" => [
-            {
-              "type" => "header",
-              "caption" => "Ignore me",
-              "url" => "https://assets.publishing.service.gov.uk/header-image.png",
-            },
-          ],
-          "sidebar_image" => sidebar_image,
-        )
-      end
-
-      it "falls back to details.sidebar_image" do
-        expect(history.flexible_sections.third.image).to have_attributes(expected_image_attributes)
-      end
-    end
-
-    context "when details.images is empty" do
-      let(:details) do
-        base_details.merge(
-          "sidebar_image" => sidebar_image,
-        )
-      end
-
-      it "falls back to details.sidebar_image" do
-        expect(history.flexible_sections.third.image).to have_attributes(expected_image_attributes)
-      end
-    end
-
-    context "when both details.images and details.sidebar_image are provided" do
-      let(:details) do
-        base_details.merge(
-          "images" => [
-            {
-              "type" => "sidebar",
-              "caption" => "Image from details.images",
-              "url" => "https://assets.publishing.service.gov.uk/images-image.png",
-            },
-          ],
-          "sidebar_image" => {
-            "caption" => "Image from details.sidebar_image",
-            "url" => "https://assets.publishing.service.gov.uk/sidebar-image.png",
+      let(:images) do
+        [
+          {
+            "type" => "header",
+            "caption" => "Ignore me",
+            "url" => "https://assets.publishing.service.gov.uk/header-image.png",
+            "content_type" => "image/jpeg",
           },
-        )
+        ]
       end
 
-      it "prefers details.images over details.sidebar_image" do
-        expect(history.flexible_sections.third.image).to have_attributes(
-          alt: "Image from details.images",
-          src: "https://assets.publishing.service.gov.uk/images-image.png",
-        )
+      it "generates a rich content element without an image" do
+        expect(history.flexible_sections.third).to be_instance_of(FlexiblePage::FlexibleSection::RichContent)
+        expect(history.flexible_sections.third.image).to be_nil
       end
     end
 
-    context "when no sidebar image is provided" do
-      let(:details) { base_details }
+    context "when no images are provided" do
+      let(:images) { [] }
+
+      it "generates a rich content element without an image" do
+        expect(history.flexible_sections.third).to be_instance_of(FlexiblePage::FlexibleSection::RichContent)
+        expect(history.flexible_sections.third.image).to be_nil
+      end
+    end
+
+    context "when the images hash is missing" do
+      let(:content_store_response) do
+        {
+          "title" => "History Page",
+          "details" => {
+            "body" => "Hello",
+            "headers" => [],
+          },
+        }
+      end
 
       it "generates a rich content element without an image" do
         expect(history.flexible_sections.third).to be_instance_of(FlexiblePage::FlexibleSection::RichContent)

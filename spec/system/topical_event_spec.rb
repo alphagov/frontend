0cefaa9846e0148f943660647c5264746a8fbe02
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-23 14:32:54 +0000

    Simplify image tests
    - Since we're using a standardised flexible section (Impact Header) for
      the images on Topical Event Pages we can simplify the image tests,
      assuming the behaviour is covered adequately by the section's view
      tests.

--- spec/system/topical_event_spec.rb
+++ spec/system/topical_event_spec.rb
@@ -22,27 +22,9 @@ RSpec.describe "Topical Event pages" do
     expect(page).to have_selector("meta[name='description'][content='#{content_store_response['description']}']", visible: :hidden)
   end
 
-  # it "includes the image and alt text" do
-  #   visit base_path
-  #   expect(page).to have_css("img[src='https://www.gov.uk/some-image.png'][alt='Text describing the image']")
-  # end
-
-  # it "includes image srcset for various image sizes" do
-  #   visit base_path
-  #   expect(page).to have_css("img[srcset='https://www.gov.uk/some-medium-image.png 2x, https://www.gov.uk/some-high-image.png 3x']")
-  # end
-
-  # context "when the image has no variants" do
-  #   before do
-  #     content_item["details"]["image"].delete("medium_resolution_url")
-  #     content_item["details"]["image"].delete("high_resolution_url")
-  #     stub_content_store_has_item(base_path, content_item)
-  #   end
-  #   it "omits srcset if there are no higher res image sizes" do
-  #     visit base_path
-  #     expect(page).not_to have_css("img[srcset]")
-  #   end
-  # end
+  it "includes an impact header" do
+    expect(page).to have_selector("[data-flexible-section='impact-header']")
+  end
 
   # it "sets the body text" do
   #   visit base_path

a61c2356de18f103fc5401330e25c5f86ba0a247
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-02-26 13:01:45 +0000

    Add atom route and request tests to flexible page controller
    - We could potentially return a 404 if the feed items are empty, but it's valid to return
      an empty atom feed, so we can simply rely on whether there's a route for it in content
      store (there will be for legacy topical events, for instance).
    - To support loading the flexible page for the test we use a symlink to point flexible-page.atom
      to the content for flexible-page (this is a fairly good analogue for what happens in
      production with the router).
    - We add in an empty atom builder here (required for the tests) which we'll fill in in
      subsequent commits.

--- app/controllers/flexible_page_controller.rb
+++ app/controllers/flexible_page_controller.rb
@@ -1,2 +1,11 @@
 class FlexiblePageController < ContentItemsController
+  def show
+    respond_to do |format|
+      format.html
+      format.atom do
+        set_expiry(5.minutes)
+        headers["Access-Control-Allow-Origin"] = "*"
+      end
+    end
+  end
 end
--- spec/requests/flexible_page_spec.rb
+++ spec/requests/flexible_page_spec.rb
@@ -19,5 +19,41 @@ RSpec.describe "FlexiblePage" do
     it "renders the show template" do
       expect(response).to render_template("show")
     end
+
+    describe "atom format" do
+      context "when no feed is present" do
+        before do
+          stub_content_store_does_not_have_item("/no-feed")
+          stub_content_store_does_not_have_item("/no-feed.atom")
+          get "/no-feed.atom"
+        end
+
+        it "returns 404" do
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      context "when feed is present" do
+        before do
+          get "#{base_path}.atom?graphql=false"
+        end
+
+        it "returns 200" do
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "returns correct content-type" do
+          expect(response.headers["Content-Type"]).to eq("application/atom+xml; charset=utf-8")
+        end
+
+        it "sets the cache-control headers to 5 mins" do
+          expect(response.headers["Cache-Control"]).to eq("max-age=#{5.minutes.to_i}, public")
+        end
+
+        it "sets the Access-Control-Allow-Origin header" do
+          expect(response.headers["Access-Control-Allow-Origin"]).to eq("*")
+        end
+      end
+    end
   end
 end

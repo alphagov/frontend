8924f0c08a5c0fc1f922e08fcadf18b5a4d47879
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-03-16 11:01:18 +0000

    Add GA4 tracking to topical events
    - restores GA4 tracking to the topical event pages, which were recently converted to use flexible sections
    - updates the featured flexible section to accept tracking attributes and apply them to the image card links
    - tracking is set at the topical event flexible section model level, so other pages using this flexible section don't have the same tracking, unless explicitly specified

--- spec/views/flexible_page/flexible_sections/_featured.html.erb_spec.rb
+++ spec/views/flexible_page/flexible_sections/_featured.html.erb_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe "Featured flexible section" do
   let(:section_hash) do
     {
       "ordered_featured_documents" => ([featured_document] * featured_count),
+      "ga4_image_card_json" => ga4_image_card_json,
     }
   end
 
@@ -20,6 +21,7 @@ RSpec.describe "Featured flexible section" do
 
   let(:featured_count) { 6 }
   let(:summary) { "This is a summary" }
+  let(:ga4_image_card_json) { nil }
 
   before do
     render(template: "flexible_page/flexible_sections/_featured", locals: { flexible_section: })
@@ -87,6 +89,29 @@ RSpec.describe "Featured flexible section" do
           ".govuk-grid-column-one-third .gem-c-image-card .gem-c-image-card__description",
           text: "This summary is very long, and it will need to be truncated at an appropriate place otherwise it will appear too long in the featured cards and they'll be a...",
         )
+      end
+    end
+
+    context "when GA4 tracking is not included" do
+      let(:ga4_image_card_json) { nil }
+
+      it "has no tracking by default" do
+        expect(rendered).not_to have_selector("[data-module=ga4-link-tracker]")
+      end
+    end
+
+    context "when GA4 tracking is included" do
+      let(:ga4_image_card_json) do
+        {
+          "event_name": "navigation",
+          "type": "image card",
+          "section": "Featured",
+        }
+      end
+
+      it "has tracking" do
+        expect(rendered).to have_selector("[data-module=ga4-link-tracker]")
+        expect(rendered).to have_selector("[data-ga4-link='{\"event_name\":\"navigation\",\"type\":\"image card\",\"section\":\"Featured\"}']")
       end
     end
   end

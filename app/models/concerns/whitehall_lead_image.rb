335470ba9a0b5c42ff2332a9787ed623f33c74e9
eYinka <olayinka.oladele@digital.cabinet-office.gov.uk>
2026-03-19 16:03:32 +0000

    Allow support for the lead image url in both legacy and config driven documents

--- app/models/concerns/whitehall_lead_image.rb
+++ app/models/concerns/whitehall_lead_image.rb
@@ -1,17 +1,27 @@
 module WhitehallLeadImage
   extend ActiveSupport::Concern
 
+  def image
+    lead_image_from_details_images || lead_image_from_details_image
+  end
+
   def image_url
     return unless image
 
-    image.dig("sources", "s300")
+    image.dig("sources", "s300") || image["url"]
   end
 
-  def image
+private
+
+  def lead_image_from_details_images
     images = content_store_response.dig("details", "images")
 
     return nil unless images.is_a?(Array)
 
     images.find { |image| image["type"] == "lead" }
+  end
+
+  def lead_image_from_details_image
+    content_store_response.dig("details", "image")
   end
 end

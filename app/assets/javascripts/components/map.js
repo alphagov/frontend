a0e2ced60aff611601be17eaca0629ff852ee3bc
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-03-25 09:53:48 +0000

    WIP

--- app/assets/javascripts/components/map.js
+++ app/assets/javascripts/components/map.js
@@ -31,8 +31,8 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       }
       const passedConfig = JSON.parse(this.$module.getAttribute('data-config')) || {}
       this.config = window.GOVUK.extendObject(allMapOptions, passedConfig)
-      this.markers = JSON.parse(this.$module.getAttribute('data-markers')) || []
-      this.geojson = this.$module.getAttribute('data-geojson')
+      this.markers = JSON.parse(this.$module.getAttribute('data-markers'))
+      this.geoJsonUrl = this.$module.getAttribute('data-geojson')
     }
 
     init () {
@@ -41,12 +41,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         return
       }
       this.initialiseMap()
-      if (this.markers.length > 0) {
-        this.addMarkers()
-      }
-      if (this.geojson) {
-        this.addMarkersFromGeoJson(this.geojson, this)
-      }
+      this.addAllMarkers()
     }
 
     initialiseMap () {
@@ -58,50 +53,51 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       this.map = L.map(this.map_id, this.config)
       this.map.setView([this.config.centre_lat, this.config.centre_lng], this.config.zoom)
 
-      // // Load and display ZXY tile layer on the map
+      // Load and display ZXY tile layer on the map
       const basemap = L.tileLayer(`https://api.os.uk/maps/raster/v1/zxy/Light_3857/{z}/{x}/{y}.png?key=${this.apiKey}`)
       basemap.addTo(this.map)
     }
 
-    addMarkers () {
-      var popups = []
-      this.markers.forEach(marker => {
-        const popup = L.marker([marker.lat, marker.lng], { alt: marker.alt, icon: this.markerIcon })
-        popups.push(popup)
-        popup.addTo(this.map)
-        if (marker.popup_content) {
-          popup.bindPopup(marker.popup_content, { maxWidth: 200 })
-        }
-      })
-
-      const group = new L.FeatureGroup(popups)
-      this.map.fitBounds(group.getBounds())
-    }
-
-    async addMarkersFromGeoJson (url, that) {
+    async addAllMarkers () {
       try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`)
-        }
-        const result = await response.json()
         var popups = []
 
-        L.geoJSON(result, {
-          pointToLayer: function (feature, latlng) {
-            const marker = L.marker(latlng, { icon: that.markerIcon })
-            popups.push(marker)
-            return marker
-          },
-          onEachFeature: function (feature, layer) {
-            if (feature.properties.popupContent) {
-              layer.bindPopup(feature.properties.popupContent, { maxWidth: 200 })
-            }
+        if (this.geoJsonUrl) {
+          const response = await fetch(this.geoJsonUrl)
+          if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
           }
-        }).addTo(that.map)
+          const result = await response.json()
+          const markerIcon = this.markerIcon
+          L.geoJSON(result, {
+            pointToLayer: function (feature, latlng) {
+              const marker = L.marker(latlng, { icon: markerIcon })
+              popups.push(marker)
+              return marker
+            },
+            onEachFeature: function (feature, layer) {
+              if (feature.properties.popupContent) {
+                layer.bindPopup(feature.properties.popupContent, { maxWidth: 200 })
+              }
+            }
+          }).addTo(this.map)
+        }
 
-        const group = new L.FeatureGroup(popups)
-        that.map.fitBounds(group.getBounds())
+        if (this.markers.length) {
+          this.markers.forEach(marker => {
+            const popup = L.marker([marker.lat, marker.lng], { alt: marker.alt, icon: this.markerIcon })
+            popups.push(popup)
+            popup.addTo(this.map)
+            if (marker.popup_content) {
+              popup.bindPopup(marker.popup_content, { maxWidth: 200 })
+            }
+          })
+        }
+
+        if (popups.length) {
+          const group = new L.FeatureGroup(popups)
+          this.map.fitBounds(group.getBounds())
+        }
       } catch (error) {
         console.error(error)
       }

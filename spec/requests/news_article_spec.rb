1107aacbd546c11f2e33b2ebd91be1d43c90d634
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2026-03-26 12:56:37 +0000

    Remove GraphQL specific tests
    Logic for deciding whether to use Content Store or GraphQL has been
    abstracted into a gem.
    
    Therefore we can remove all tests for this logic from the application.

--- spec/requests/news_article_spec.rb
+++ spec/requests/news_article_spec.rb
@@ -1,48 +1,26 @@
-require "gds_api/test_helpers/publishing_api"
-
 RSpec.describe "News Article" do
   include Capybara::RSpecMatchers
-  include GdsApi::TestHelpers::PublishingApi
 
   let(:content_item) { GovukSchemas::Example.find("news_article", example_name: "news_article") }
   let(:base_path) { content_item.fetch("base_path") }
 
   describe "GET show" do
-    context "when loaded from content store" do
-      before do
-        stub_content_store_has_item(base_path, content_item)
+    before do
+      stub_content_store_has_item(base_path, content_item)
 
-        get base_path, params: { graphql: false }
-      end
-
-      it "succeeds" do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it "renders the show template" do
-        expect(response).to render_template(:show)
-      end
-
-      it "sets cache-control headers" do
-        expect(response).to honour_content_store_ttl
-      end
+      get base_path
     end
 
-    context "when loaded from GraphQL" do
-      before do
-        stub_content_store_has_item(base_path, content_item)
-        stub_publishing_api_graphql_has_item(base_path, content_item)
+    it "succeeds" do
+      expect(response).to have_http_status(:ok)
+    end
 
-        get base_path, params: { graphql: true }
-      end
+    it "renders the show template" do
+      expect(response).to render_template(:show)
+    end
 
-      it "succeeds" do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it "renders the show template" do
-        expect(response).to render_template(:show)
-      end
+    it "sets cache-control headers" do
+      expect(response).to honour_content_store_ttl
     end
   end
 end
--- spec/unit/content_item_loader_spec.rb
+++ spec/unit/content_item_loader_spec.rb
@@ -1,55 +1,9 @@
-require "gds_api/test_helpers/publishing_api"
-
 RSpec.describe ContentItemLoader do
-  include GdsApi::TestHelpers::PublishingApi
   include ContentStoreHelpers
 
   subject(:content_item_loader) { described_class.new }
 
   let!(:item_request) { stub_content_store_has_item("/my-random-item") }
-
-  before do
-    Rails.application.config.graphql_traffic_rates = { "news_article" => 0.5 }
-    Rails.application.config.graphql_allowed_schemas = %w[news_article]
-  end
-
-  shared_examples "rendered from Content Store" do
-    it "calls the Content Store only" do
-      content_item_loader.load(request.path)
-
-      expect(graphql_request).not_to have_been_made
-      expect(item_request).to have_been_made
-    end
-  end
-
-  shared_examples "rendered from Draft Content Store" do
-    it "calls the Draft Content Store only" do
-      ClimateControl.modify(PLEK_HOSTNAME_PREFIX: "draft-") do
-        content_item_loader.load(request.path)
-
-        expect(graphql_request).not_to have_been_made
-        expect(item_request).to have_been_made
-      end
-    end
-  end
-
-  shared_examples "rendered from GraphQL" do
-    it "calls the GraphQL endpoint in additon to Content Store" do
-      content_item_loader.load(request.path)
-
-      expect(item_request).to have_been_made
-      expect(graphql_request).to have_been_made
-    end
-  end
-
-  shared_examples "rendered from GraphQL with query param" do
-    it "calls the GraphQL endpoint only" do
-      content_item_loader.load(request.path)
-
-      expect(item_request).not_to have_been_made
-      expect(graphql_request).to have_been_made
-    end
-  end
 
   describe ".for_request" do
     it "returns a new object per request" do
@@ -121,293 +75,48 @@ RSpec.describe ContentItemLoader do
       end
     end
 
-    context "when the content item schema is in Rails.application.config.graphql_allowed_schemas" do
-      subject(:content_item_loader) { described_class.for_request(request) }
+    context "with ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE=true" do
+      subject(:content_item_loader) { described_class.new }
 
-      let!(:item_request) { stub_content_store_has_item("/my-random-item", { "schema_name" => "news_article" }) }
-      let!(:graphql_request) do
-        stub_publishing_api_graphql_has_item(
-          "/my-random-item",
-          { data: { edition: { schema_name: "news_article" } } },
-        )
+      before do
+        ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = "true"
+        stub_const("ContentItemLoaders::LocalFileLoader::LOCAL_ITEMS_PATH", "spec/fixtures/local-content-items")
       end
 
-      context "when the request is made to the draft deployment" do
-        let(:item_request) { stub_content_store_has_item("/my-random-item", { "schema_name" => "news_article" }, draft: true) }
-
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: {},
-          )
-        end
-
-        include_examples "rendered from Draft Content Store"
+      after do
+        ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = nil
       end
 
-      context "with graphql param=false" do
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: { "graphql" => "false" },
-          )
-        end
+      context "with a local JSON file" do
+        let!(:item_request) { stub_content_store_has_item("/my-json-item") }
 
-        include_examples "rendered from Content Store"
-      end
+        it "loads content from the JSON file instead of the content store" do
+          response = content_item_loader.load("/my-json-item")
 
-      context "when the GraphQL A/B test selects the Content Store variant" do
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: {},
-          )
-        end
-
-        before do
-          allow(Random).to receive(:rand).with(1.0).and_return(1)
-        end
-
-        include_examples "rendered from Content Store"
-
-        context "and with graphql param=false" do
-          let(:request) do
-            instance_double(
-              ActionDispatch::Request,
-              path: "/my-random-item",
-              env: {},
-              params: { "graphql" => "false" },
-            )
-          end
-
-          include_examples "rendered from Content Store"
+          expect(item_request).not_to have_been_made
+          expect(ContentItemFactory.build(response).schema_name).to eq("json_page")
         end
       end
 
-      context "when the GraphQL A/B test selects the GraphQL variant" do
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: {},
-          )
-        end
+      context "with a local YAML file" do
+        let!(:item_request) { stub_content_store_has_item("/my-yaml-item") }
 
-        before do
-          allow(Random).to receive(:rand).with(1.0).and_return(0)
-        end
+        it "loads content from the YAML file instead of the content store" do
+          response = content_item_loader.load("/my-yaml-item")
 
-        include_examples "rendered from GraphQL"
-
-        context "and with graphql param=false" do
-          let(:request) do
-            instance_double(
-              ActionDispatch::Request,
-              path: "/my-random-item",
-              env: {},
-              params: { "graphql" => "false" },
-            )
-          end
-
-          include_examples "rendered from Content Store"
+          expect(item_request).not_to have_been_made
+          expect(ContentItemFactory.build(response).schema_name).to eq("yaml_page")
         end
       end
 
-      context "with graphql param=true" do
-        subject(:content_item_loader) { described_class.for_request(request) }
+      context "with no local file" do
+        let!(:item_request) { stub_content_store_has_item("/my-remote-item") }
 
-        let(:graphql_request) do
-          stub_publishing_api_graphql_has_item(
-            "/my-random-item",
-            { data: { edition: { schema_name: "news_article" } } },
-          )
+        it "returns to loading from the content store" do
+          content_item_loader.load("/my-remote-item")
+
+          expect(item_request).to have_been_made.once
         end
-        let(:request) { instance_double(ActionDispatch::Request, path: "/my-random-item", env: {}, params: { "graphql" => "true" }) }
-
-        include_examples "rendered from GraphQL with query param"
-
-        it "sets the appropriate prometheus labels" do
-          content_item_loader.load("/my-random-item")
-
-          expect(request.env["govuk.prometheus_labels"]).to include({
-            "graphql_status_code" => 200,
-            "graphql_api_timeout" => false,
-          })
-        end
-      end
-
-      context "when given a bad response code from publishing-api" do
-        subject(:content_item_loader) { described_class.for_request(request) }
-
-        let!(:graphql_request) { stub_publishing_api_graphql_does_not_have_item("/my-random-item") }
-        let(:request) { instance_double(ActionDispatch::Request, path: "/my-random-item", env: {}, params: { "graphql" => "true" }) }
-
-        it "falls back to loading from Content Store" do
-          content_item_loader.load("/my-random-item")
-
-          expect(graphql_request).to have_been_made
-          expect(item_request).to have_been_made
-        end
-
-        it "reports bad status codes for graphql requests" do
-          content_item_loader.load("/my-random-item")
-
-          expect(request.env["govuk.prometheus_labels"]["graphql_status_code"]).to eq(404)
-        end
-      end
-
-      context "when GDS API Adapters times-out the request" do
-        subject(:content_item_loader) { described_class.for_request(request) }
-
-        let(:request) { instance_double(ActionDispatch::Request, path: "/my-random-item", env: {}, params: { "graphql" => "true" }) }
-
-        before do
-          publishing_api_adapter = instance_double(GdsApi::PublishingApi)
-          allow(GdsApi).to receive(:publishing_api).and_return(publishing_api_adapter)
-          allow(publishing_api_adapter).to receive(:graphql_live_content_item)
-            .and_raise(GdsApi::TimedOutException)
-        end
-
-        it "falls back to loading from Content Store" do
-          content_item_loader.load("/my-random-item")
-
-          expect(item_request).to have_been_made
-        end
-
-        it "reports bad status codes for graphql requests" do
-          content_item_loader.load("/my-random-item")
-
-          expect(request.env["govuk.prometheus_labels"]["graphql_api_timeout"]).to be(true)
-        end
-      end
-    end
-
-    context "when the content item schema is not in Rails.application.config.graphql_allowed_schemas" do
-      subject(:content_item_loader) { described_class.for_request(request) }
-
-      let!(:item_request) { stub_content_store_has_item("/my-random-item", { "schema_name" => "guide" }) }
-      let(:graphql_request) do
-        stub_publishing_api_graphql_has_item(
-          "/my-random-item",
-          { data: { edition: { schema_name: "some_other_schema" } } },
-        )
-      end
-
-      context "with ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE=true" do
-        subject(:content_item_loader) { described_class.new }
-
-        before do
-          ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = "true"
-          stub_const("ContentItemLoaders::LocalFileLoader::LOCAL_ITEMS_PATH", "spec/fixtures/local-content-items")
-        end
-
-        after do
-          ENV["ALLOW_LOCAL_CONTENT_ITEM_OVERRIDE"] = nil
-        end
-
-        context "with a local JSON file" do
-          let!(:item_request) { stub_content_store_has_item("/my-json-item") }
-
-          it "loads content from the JSON file instead of the content store" do
-            response = content_item_loader.load("/my-json-item")
-
-            expect(item_request).not_to have_been_made
-            expect(ContentItemFactory.build(response).schema_name).to eq("json_page")
-          end
-        end
-
-        context "with a local YAML file" do
-          let!(:item_request) { stub_content_store_has_item("/my-yaml-item") }
-
-          it "loads content from the YAML file instead of the content store" do
-            response = content_item_loader.load("/my-yaml-item")
-
-            expect(item_request).not_to have_been_made
-            expect(ContentItemFactory.build(response).schema_name).to eq("yaml_page")
-          end
-        end
-
-        context "with no local file" do
-          let!(:item_request) { stub_content_store_has_item("/my-remote-item") }
-
-          it "returns to loading from the content store" do
-            content_item_loader.load("/my-remote-item")
-
-            expect(item_request).to have_been_made.once
-          end
-        end
-      end
-
-      context "with graphql param=true" do
-        subject(:content_item_loader) { described_class.for_request(request) }
-
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: { "graphql" => "true" },
-          )
-        end
-
-        before do
-          stub_publishing_api_graphql_has_item(
-            "/my-random-item",
-            { data: { edition: { schema_name: "some_other_schema" } } },
-          )
-        end
-
-        include_examples "rendered from GraphQL with query param"
-
-        it "sets the appropriate prometheus labels" do
-          content_item_loader.load("/my-random-item")
-
-          expect(request.env["govuk.prometheus_labels"]).to include({
-            "graphql_status_code" => 200,
-            "graphql_api_timeout" => false,
-          })
-        end
-      end
-
-      context "when the GraphQL A/B test selects the Content Store variant" do
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: {},
-          )
-        end
-
-        before do
-          allow(Random).to receive(:rand).with(1.0).and_return(1)
-        end
-
-        include_examples "rendered from Content Store"
-      end
-
-      context "when the GraphQL A/B test selects the GraphQL variant" do
-        let(:request) do
-          instance_double(
-            ActionDispatch::Request,
-            path: "/my-random-item",
-            env: {},
-            params: {},
-          )
-        end
-
-        before do
-          allow(Random).to receive(:rand).with(1.0).and_return(0)
-        end
-
-        include_examples "rendered from Content Store"
       end
     end
 

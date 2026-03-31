c67fc61ac1fc961e4966d959fb93ece4d244bf16
kashifatcha <kashif.atcha@digital.cabinet-office.gov.uk>
2026-03-31 15:01:02 +0100

    Add conditional noindex meta tag to specialist documents
    We're now supporting the ability to add a `noindex` meta tag to specialist document pages.
    This is to prevent search engines from indexing certain specialist documents, such as tribunal decisions.
    The schema from Publishing API has been updated to include a new field called `index_documents_in_search_engines`
    which can be set to `false` to indicate that the document should not be indexed by search engines.

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -1,5 +1,5 @@
 class Finder < ContentItem
-  attr_reader :facets, :show_metadata_block, :show_table_of_contents
+  attr_reader :facets, :show_metadata_block, :show_table_of_contents, :index_documents_in_search_engines
 
   def initialize(content_store_response)
     super(content_store_response)
@@ -7,5 +7,6 @@ class Finder < ContentItem
     @facets = content_store_response.dig("details", "facets")
     @show_metadata_block = content_store_response.dig("details", "show_metadata_block")
     @show_table_of_contents = content_store_response.dig("details", "show_table_of_contents")
+    @index_documents_in_search_engines = content_store_response.dig("details", "index_documents_in_search_engines")
   end
 end

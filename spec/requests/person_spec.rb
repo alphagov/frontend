811600be8fda277b38ae517b2f601e242173fa28
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2026-04-07 12:42:02 +0100

    Add request test for person page

---
+++ spec/requests/person_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe "Person" do
+  let(:content_item) {
+    GovukSchemas::Example.find("person", example_name: "person")
+  }
+  let(:base_path) { content_item.fetch("base_path") }
+
+  before do
+    stub_content_store_has_item(base_path, content_item)
+  end
+
+  describe "GET show" do
+    context "when visiting a person page" do
+      it "returns 200" do
+        get base_path
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the show template" do
+        get base_path
+
+        expect(response).to render_template("show")
+      end
+    end
+  end
+end

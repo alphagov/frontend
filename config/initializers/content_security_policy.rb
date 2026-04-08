847e407c21c50a2a0eafb1e91ddbec7211dff751
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-04-08 13:57:39 +0100

    Move policy to global for app

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,1 +1,4 @@
-GovukContentSecurityPolicy.configure
+GovukContentSecurityPolicy.configure do |policy|
+  # The map block makes use of the OS api and inline styles
+  policy.img_src(*policy.img_src, "https://api.os.uk", :data)
+end

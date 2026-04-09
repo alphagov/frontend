895a1caa0720264d143ba5687d181c5229751a48
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-04-09 13:21:12 +0100

    FIX (Table)

--- app/models/flexible_page/flexible_section/table.rb
+++ app/models/flexible_page/flexible_section/table.rb
@@ -1,12 +1,13 @@
 module FlexiblePage::FlexibleSection
   class Table < Base
-    attr_reader :caption, :data
+    attr_reader :caption, :data, :title
 
     def initialize(flexible_section_hash, content_item)
       super
 
+      @title = flexible_section_hash["title"]
       @caption = flexible_section_hash["caption"]
-      @data = flexible_section_hash["data"]
+      @data = flexible_section_hash["data"].deep_symbolize_keys
     end
 
     def hash_for_table_component

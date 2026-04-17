d23095f67bb18910bff45ec1c3af12ac57878c2b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-04-17 09:51:49 +0100

    Include JS and CSS for defra map

---
+++ app/assets/javascripts/components/map/defra-map.js
@@ -0,0 +1,7 @@
+/* this is a separate file so we can exclude it from test coverage in nycrc */
+
+//= require @defra/interactive-map/dist/umd/index.js
+//= require @defra/interactive-map/dist/umd/im-core.js
+//= require @defra/interactive-map/providers/maplibre/dist/umd/index.js
+//= require @defra/interactive-map/providers/maplibre/dist/umd/im-maplibre-framework.js
+//= require @defra/interactive-map/providers/maplibre/dist/umd/im-maplibre-provider.js

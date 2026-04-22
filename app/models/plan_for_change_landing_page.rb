6ea1d90e11448076600e3f4de45114eb71511d58
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-04-22 13:33:11 +0100

    Add basic support for `plan_for_change_landing_page` content items
    - These are identical to `landing_page` content items, but the schema has
      a different name.

---
+++ app/models/plan_for_change_landing_page.rb
@@ -0,0 +1,2 @@
+class PlanForChangeLandingPage < LandingPage
+end
---
+++ spec/requests/plan_for_change_landing_page_spec.rb
@@ -0,0 +1,30 @@
+require "gds_api/test_helpers/search"
+
+RSpec.describe "Plan For Change Landing Page" do
+  include GdsApi::TestHelpers::Search
+
+  describe "GET show" do
+    context "when a content item does exist" do
+      let(:content_item) { GovukSchemas::Example.find("plan_for_change_landing_page", example_name: "plan_for_change_landing_page") }
+      let(:base_path) { content_item.fetch("base_path") }
+
+      before do
+        stub_conditional_loader_returns_content_item_for_path(base_path, content_item)
+        stub_conditional_loader_returns_content_item_for_path(basic_taxon["base_path"], basic_taxon)
+        stub_any_search_to_return_no_results
+      end
+
+      it "succeeds" do
+        get base_path
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the show template" do
+        get base_path
+
+        expect(response).to render_template(:show)
+      end
+    end
+  end
+end

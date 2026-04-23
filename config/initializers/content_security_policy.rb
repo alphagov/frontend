032626b3bfdd5073e108bc2f083e3c2a01dfab6d
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-04-23 16:04:35 +0100

    WIP fix CSP issues

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,6 +1,8 @@
 GovukContentSecurityPolicy.configure do |policy|
+  # The map block makes use of the OS api and inline styles
+  policy.img_src(*policy.img_src, "https://api.os.uk", :data)
   # The map component makes use of maplibre
   policy.img_src(*policy.img_src, "https://tiles.openfreemap.org", :data)
   policy.connect_src(*policy.connect_src, "https://tiles.openfreemap.org", :data)
-  policy.worker_src(*policy.worker_src, "self", :blob)
+  policy.worker_src(*policy.worker_src, "self", :self)
 end

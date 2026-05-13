75667acde67461612dd3dc6ae06450f0252d1f5c
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-05-13 13:25:10 +0100

    Update Featured flexible section
    - We remove the code that massages the data into the correct format,
      assuming we'll be passed the correct data to start with. This code
      can be moved into the TopicalEvent, and if it needs to be used
      by more classes we can make it a helper used by FlexiblePage
      subclasses.

--- spec/models/flexible_page/flexible_section/featured_spec.rb
+++ spec/models/flexible_page/flexible_section/featured_spec.rb
@@ -1,89 +1,97 @@
 RSpec.describe FlexiblePage::FlexibleSection::Featured do
-  featured_item = {
-    "href" => "/href",
-    "image" => {
-      "url" => "/img/src.jpg",
-      "alt_text" => "some meaningful alt text please",
-    },
-    "title" => "Government does things",
-    "summary" => "This a description",
-  }
+  subject(:featured) { described_class.new(ga4_image_card_json:, items:) }
 
-  subject(:content_hash) do
+  let(:ga4_image_card_json) { nil }
+  let(:items) { [featured_document] * featured_count }
+  let(:featured_count) { 6 }
+
+  let(:featured_document) do
     {
-      "ordered_featured_documents" => [featured_item],
+      href: "/not-a-page",
+      image: {
+        alt_text: "some meaningful alt text please",
+        url: "http://test.gov.uk/images/fd.png",
+      },
+      summary: "Summary element",
+      title: "Government does things",
     }
   end
 
-  let(:featured) { described_class.new(content_hash, FlexiblePage.new({})) }
+  describe "#ga4_tracking" do
+    it "returns nil" do
+      expect(featured.ga4_tracking).to be_nil
+    end
 
-  describe "when there is one featured item" do
-    it "shows it full width" do
-      expect(featured.grid_layout_options).to eq({
-        class: "govuk-grid-column-full",
-        break_at_column: 2,
-        large: true,
-      })
+    context "when ga4 tracking info has been supplied" do
+      let(:ga4_image_card_json) do
+        {
+          event_name: "navigation",
+          type: "image card",
+          section: "Featured",
+        }
+      end
+
+      it "returns ga4 tracking attributes in JSON format" do
+        expect(featured.ga4_tracking).to eq({ ga4_link: "{\"event_name\":\"navigation\",\"type\":\"image card\",\"section\":\"Featured\"}", module: "ga4-link-tracker" })
+      end
     end
   end
 
-  describe "when there are two featured items" do
-    let(:content_hash) do
-      {
-        "ordered_featured_documents" => [featured_item, featured_item],
-      }
+  describe "#grid_layout_options" do
+    context "when there is one featured item" do
+      let(:featured_count) { 1 }
+
+      it "shows it full width" do
+        expect(featured.grid_layout_options).to eq({
+          class: "govuk-grid-column-full",
+          break_at_column: 2,
+          large: true,
+        })
+      end
     end
 
-    it "shows them side by side" do
-      expect(featured.grid_layout_options).to eq({
-        class: "govuk-grid-column-one-half",
-        break_at_column: 2,
-      })
-    end
-  end
+    context "when there are two featured items" do
+      let(:featured_count) { 2 }
 
-  describe "when there are three featured items" do
-    let(:content_hash) do
-      {
-        "ordered_featured_documents" => [featured_item, featured_item, featured_item],
-      }
+      it "shows them side by side" do
+        expect(featured.grid_layout_options).to eq({
+          class: "govuk-grid-column-one-half",
+          break_at_column: 2,
+        })
+      end
     end
 
-    it "shows them in one row" do
-      expect(featured.grid_layout_options).to eq({
-        class: "govuk-grid-column-one-third",
-        break_at_column: 3,
-      })
-    end
-  end
+    context "when there are three featured items" do
+      let(:featured_count) { 3 }
 
-  describe "when there are four featured items" do
-    let(:content_hash) do
-      {
-        "ordered_featured_documents" => [featured_item, featured_item, featured_item, featured_item],
-      }
+      it "shows them in one row" do
+        expect(featured.grid_layout_options).to eq({
+          class: "govuk-grid-column-one-third",
+          break_at_column: 3,
+        })
+      end
     end
 
-    it "shows them in two rows" do
-      expect(featured.grid_layout_options).to eq({
-        class: "govuk-grid-column-one-half",
-        break_at_column: 2,
-      })
-    end
-  end
+    context "when there are four featured items" do
+      let(:featured_count) { 4 }
 
-  describe "when there are five featured items" do
-    let(:content_hash) do
-      {
-        "ordered_featured_documents" => [featured_item, featured_item, featured_item, featured_item, featured_item],
-      }
+      it "shows them in two rows" do
+        expect(featured.grid_layout_options).to eq({
+          class: "govuk-grid-column-one-half",
+          break_at_column: 2,
+        })
+      end
     end
 
-    it "shows them in rows of three" do
-      expect(featured.grid_layout_options).to eq({
-        class: "govuk-grid-column-one-third",
-        break_at_column: 3,
-      })
+    context "when there are five or more featured items" do
+      let(:featured_count) { 5 }
+
+      it "shows them in rows of three" do
+        expect(featured.grid_layout_options).to eq({
+          class: "govuk-grid-column-one-third",
+          break_at_column: 3,
+        })
+      end
     end
   end
 end

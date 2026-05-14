63d4160e6459a4bec1774f7accdc4d96036874fc
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-05-14 13:54:02 +0100

    Move JSON loading methods into LocalFileLoader
    - As we're removing YML loading, we can make LocalFileLoader
      (previously the base class for the JSON and YAML loaders) into
      a concrete class that will be called directly.

--- lib/content_item_loaders/local_file_loader.rb
+++ lib/content_item_loaders/local_file_loader.rb
@@ -13,6 +13,14 @@ module ContentItemLoaders
       load_local_file(base_path)
     end
 
+    def local_filename(base_path)
+      Rails.root.join("#{LOCAL_ITEMS_PATH}#{base_path}.json")
+    end
+
+    def load_local_file(base_path)
+      GdsApi::Response.new(OpenStruct.new(code: 200, body: File.read(local_filename(base_path)), headers:))
+    end
+
   private
 
     def headers

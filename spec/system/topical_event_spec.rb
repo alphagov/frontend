37af7e320c185bf4e4cfe7f5644430e54ba86f89
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-05-14 12:32:11 +0100

    Replace TopicalEvent system tests with request tests where possible
    - We can ensure proper behaviour with request tests for a lot of what
      we're looking for in system tests. We already know (thanks to view
      tests) that the flexible sections (if called) are displaying the right
      thing. We can check that the sections' partials are being rendered
      in cheaper request tests, and we have tests that ensure that the
      topical event is passing the correct information to the sections,
      so with those two chained together, we don't need an expensive
      system test to check that the information in the content item is
      getting to the rendered HTML.
    - This also means we don't need to wrap flexible sections in extra
      divs to make their presence testable.

--- spec/system/topical_event_spec.rb
+++ spec/system/topical_event_spec.rb
@@ -21,35 +21,7 @@ RSpec.describe "Topical Event pages" do
     expect(page).to have_title("#{content_store_response['title']} - GOV.UK")
   end
 
-  it "sets the page description" do
-    expect(page).to have_text(content_store_response["description"])
-  end
-
   it "sets the page meta description" do
     expect(page).to have_selector("meta[name='description'][content='#{content_store_response['description']}']", visible: :hidden)
-  end
-
-  it "includes an impact header" do
-    expect(page).to have_selector("[data-flexible-section='impact-header']")
-  end
-
-  it "sets the body text" do
-    expect(page.html).to include(content_store_response.dig("details", "body"))
-  end
-
-  it "includes a link to the about page" do
-    expect(page).to have_link(content_store_response.dig("details", "about_page_link_text"), href: "#{base_path}/about")
-  end
-
-  it "includes featured documents" do
-    expect(page).to have_selector("[data-flexible-section='featured']")
-  end
-
-  it "includes a feed document list" do
-    expect(page).to have_selector("[data-flexible-section='feed_document_list']")
-  end
-
-  it "includes an involved block" do
-    expect(page).to have_selector("[data-flexible-section='involved']")
   end
 end

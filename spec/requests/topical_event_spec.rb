a7a8490a823c0f73b4391d4c93a5695c371b7fcc
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-05-14 13:34:35 +0100

    Move Atom feed request tests to TopicalEvent
    - We don't need to test for whether an atom feed doesn't
      exist because it isn't routed - if it isn't routed, it can't get
      to us anyway.
    - Topical Event is the only thing using atom at the moment,
      and moving the test here means we can remove the
      flexible page request tests (if other pages use this in the
      future we can make this a shared example)

--- spec/requests/topical_event_spec.rb
+++ spec/requests/topical_event_spec.rb
@@ -1,22 +1,22 @@
 RSpec.describe "Topical event page" do
   include GdsApi::TestHelpers::Search
 
+  let(:base_path) { content_item.fetch("base_path") }
+  let(:search_response) do
+    {
+      "results" => [
+        {
+          "title" => "An announcement on Topicals",
+          "link" => "/foo/announcement_one",
+          "display_type" => "some_display_type",
+          "public_timestamp" => "2025-12-01T00:00:01Z",
+        },
+      ],
+    }
+  end
+
   describe "GET show" do
     let(:content_item) { GovukSchemas::Example.find("topical_event", example_name: "western-balkans-summit-london-2018") }
-    let(:base_path) { content_item.fetch("base_path") }
-
-    let(:search_response) do
-      {
-        "results" => [
-          {
-            "title" => "An announcement on Topicals",
-            "link" => "/foo/announcement_one",
-            "display_type" => "some_display_type",
-            "public_timestamp" => "2025-12-01T00:00:01Z",
-          },
-        ],
-      }
-    end
 
     before do
       stub_conditional_loader_returns_content_item_for_path(base_path, content_item)
@@ -55,6 +55,33 @@ RSpec.describe "Topical event page" do
 
     it "includes an involved flexible section" do
       expect(response).to render_template(partial: "flexible_page/flexible_sections/_involved")
+    end
+  end
+
+  describe "GET show (atom format)" do
+    let(:content_item) { GovukSchemas::Example.find("topical_event", example_name: "western-balkans-summit-london-2018") }
+
+    before do
+      stub_conditional_loader_returns_content_item_for_path("#{base_path}.atom", content_item)
+      stub_request(:get, /\A#{Plek.new.find('search-api')}\/search.json/)
+        .to_return(body: search_response.to_json)
+      get "#{base_path}.atom?graphql=false"
+    end
+
+    it "returns 200" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns correct content-type" do
+      expect(response.headers["Content-Type"]).to eq("application/atom+xml; charset=utf-8")
+    end
+
+    it "sets the cache-control headers to 5 mins" do
+      expect(response.headers["Cache-Control"]).to eq("max-age=#{5.minutes.to_i}, public")
+    end
+
+    it "sets the Access-Control-Allow-Origin header" do
+      expect(response.headers["Access-Control-Allow-Origin"]).to eq("*")
     end
   end
 end

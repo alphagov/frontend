c4fbac9eb20f62be2866250d5f1723ac833c0389
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-05-20 09:08:49 +0100

    WIP (Catch ParameterMissing)

--- app/controllers/api/autocompletes_controller.rb
+++ app/controllers/api/autocompletes_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class AutocompletesController < ApplicationController
+    rescue_from ActionController::ParameterMissing, with: :bad_request
+
     def index
       render json: autocomplete_response
     end
@@ -11,6 +13,10 @@ module Api
         .search_api_v2
         .autocomplete(params.require(:q))
         .to_hash
+    end
+
+    def bad_request
+      render status: :bad_request, plain: "400 error: bad request"
     end
   end
 end

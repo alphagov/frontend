b0f0ee114e997f160d7fe96dc5939c29a8b80c8c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-06-03 11:24:55 +0100

    Update how the breadcrumb is called elsewhere

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,7 +2,7 @@ class History < FlexiblePage
   def initialize(content_store_response)
     super
 
-    add_section(Breadcrumbs.new(breadcrumbs:))
+    add_section(Breadcrumbs.new(breadcrumbs_options: { breadcrumbs: }))
     add_section(PageTitle.new(context: "History", heading_text: title, lead_paragraph: content_store_response.dig("details", "lead_paragraph")))
     add_section(SidebarThenContentLayout.new(
                   sidebar: RichContentsList.new(contents_list:, image:),
--- app/models/topical_event_about_page.rb
+++ app/models/topical_event_about_page.rb
@@ -2,7 +2,7 @@ class TopicalEventAboutPage < FlexiblePage
   def initialize(content_store_response)
     super
 
-    add_section(Breadcrumbs.new(breadcrumbs:))
+    add_section(Breadcrumbs.new(breadcrumbs_options: { breadcrumbs: }))
     add_section(PageTitle.new(heading_text: title, lead_paragraph: description))
     add_section(SidebarThenContentLayout.new(
                   sidebar: RichContentsList.new(contents_list:),

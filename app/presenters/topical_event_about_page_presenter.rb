6e85c990e64b20ec21bca357c16ec9a3fbce4358
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-06-08 15:11:44 +0100

    WIP
    Co-authored-by: Ramya <sairamya93@users.noreply.github.com>
    Co-authored-by: Sonia Hussain <CodeSonia@users.noreply.github.com>
    Co-authored-by: deborahchua <deborahchua@users.noreply.github.com>

---
+++ app/presenters/topical_event_about_page_presenter.rb
@@ -0,0 +1,33 @@
+class TopicalEventAboutPagePresenter < FlexibleContent
+  attr_reader :flexible_sections
+
+  def initialize(content_item)
+    about = content_item.about
+
+    @flexible_sections = []
+
+    @flexible_sections << Breadcrumbs.new(breadcrumbs:)
+
+    @flexible_sections << PageTitle.new(heading_text: about.title, lead_paragraph: about.description)
+
+    @flexible_sections << SidebarThenContentLayout.new(
+      sidebar: RichContentsList.new(contents_list: about.contents_list),
+      content: Govspeak.new(govspeak: about.body),
+    )
+  end
+
+private
+
+  def breadcrumbs
+    [
+      {
+        title: "Home",
+        url: "/",
+      },
+      {
+        title: content_item.title,
+        url: content_item.base_path,
+      },
+    ]
+  end
+end

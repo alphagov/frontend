7b59e93c63424588a83fe531b57f755ff927fe25
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-06-10 14:58:51 +0100

    Use component for cards flexible section
    - replace the code for the cards flexible section with the cards component, newly updated to work as required in the flexible section with a new option
    - will likely remove the entire flexible section once the infrastructure supports it and just use the component

--- app/models/flexible_page/flexible_section/cards.rb
+++ app/models/flexible_page/flexible_section/cards.rb
@@ -2,15 +2,10 @@ module FlexiblePage::FlexibleSection
   class Cards < Base
     attr_reader :items
 
-    def initialize(items:, heading_level: nil)
+    def initialize(items:)
       super
 
       @items = items
-      @heading_level = [2, 3, 4, 5, 6].include?(heading_level) ? heading_level : 2
-    end
-
-    def heading
-      "h#{@heading_level}"
     end
   end
 end

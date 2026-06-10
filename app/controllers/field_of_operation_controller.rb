de01f73a01f56c2b973c15e07c671fd2d73bfed4
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-06-10 13:21:12 +0100

    Update field_of_operation to use the new layout
    - As an example of using the new layout, we update the field_of_operation to use this new
      layout. The heading/org logo go in the header slot, the contents list in the sidebar slot,
      and the main body in the contents slot.

--- app/controllers/field_of_operation_controller.rb
+++ app/controllers/field_of_operation_controller.rb
@@ -1,5 +1,8 @@
 class FieldOfOperationController < ContentItemsController
   include Cacheable
+
+  layout "header_sidebar_content"
+
   def show
     @presenter = FieldOfOperationPresenter.new(@content_item)
   end

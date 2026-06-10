e9df424fbb92960aeace04008d43796d21223a9e
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-06-10 14:58:51 +0100

    Use component for cards flexible section
    - replace the code for the cards flexible section with the cards component, newly updated to work as required in the flexible section with a new option
    - will likely remove the entire flexible section once the infrastructure supports it and just use the component

--- spec/models/flexible_page/flexible_section/cards_spec.rb
+++ spec/models/flexible_page/flexible_section/cards_spec.rb
@@ -1,45 +1,26 @@
 RSpec.describe FlexiblePage::FlexibleSection::Cards do
-  subject(:cards) { described_class.new(items:, heading_level:) }
+  subject(:cards) { described_class.new(items:) }
 
-  let(:heading_level) { nil }
   let(:items) do
     [
       {
-        title: "Writing implements",
-        href: "/example",
+        link: {
+          text: "Writing implements",
+          path: "/example",
+        },
         description: "Pens, pencils, markers, crayons, chalk.",
       },
       {
-        title: "Bones present in the human foot",
-        href: "/example",
+        link: {
+          text: "Bones present in the human foot",
+          path: "/example",
+        },
         description: "Talus, Calcaneus, Cuboid, Cuneiforms, Navicular, Metatarsals, Phalanges.",
       },
     ]
   end
 
-  describe "#initialize" do
-    it "sets the attributes from the parameters" do
-      expect(cards.items).to eq(items)
-    end
-
-    it "sets the heading level correctly" do
-      expect(cards.heading).to eq("h2")
-    end
-  end
-
-  describe "with a custom heading level" do
-    let(:heading_level) { 3 }
-
-    it "sets the heading level correctly" do
-      expect(cards.heading).to eq("h3")
-    end
-  end
-
-  describe "with an invalid heading level" do
-    let(:heading_level) { 3000 }
-
-    it "sets the heading level correctly" do
-      expect(cards.heading).to eq("h2")
-    end
+  it "initialize sets the attributes from the parameters" do
+    expect(cards.items).to eq(items)
   end
 end

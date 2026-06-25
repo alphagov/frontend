6bea926beb1c8e2d359ed3aecbd7ca6d6752381f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-06-25 15:50:40 +0100

    FIX (model)

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,26 +1,11 @@
 class History < FlexiblePage
+  include HasImages
+
   def contents_outline
     ContentsOutline.new((content_store_response.dig("details", "headers") || []).map { |header| header.except("headers").deep_symbolize_keys })
   end
 
   def lead_paragraph
     content_store_response.dig("details", "lead_paragraph")
-  end
-
-private
-
-  def image
-    images = content_store_response.dig("details", "images")
-
-    return nil unless images
-
-    sidebar_image = images.find { |image| image["type"] == "sidebar" }
-
-    return nil unless sidebar_image
-
-    {
-      alt: sidebar_image["caption"],
-      src: sidebar_image.dig("sources", "s960"),
-    }
   end
 end

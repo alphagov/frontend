2ae534c140bdcab0724874e78dc0ce325a8931e5
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-07-09 12:50:05 +0100

    Add page_title_options method to history presenter and use in view

--- app/presenters/history_presenter.rb
+++ app/presenters/history_presenter.rb
@@ -7,4 +7,12 @@ class HistoryPresenter < ContentItemPresenter
       },
     ]
   end
+
+  def page_title_options
+    {
+      context: "History",
+      heading_text: content_item.title,
+      lead_paragraph: content_item.lead_paragraph,
+    }
+  end
 end
--- spec/presenter/history_presenter_spec.rb
+++ spec/presenter/history_presenter_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe HistoryPresenter do
       expect(history_presenter.breadcrumbs.count).to eq(2)
     end
   end
+
+  describe "#page_title_options" do
+    it "has a context" do
+      expect(history_presenter.page_title_options[:context]).to eq("History")
+    end
+
+    it "has options derived from the content item" do
+      expect(history_presenter.page_title_options[:heading_text]).to eq(content_store_response["title"])
+      expect(history_presenter.page_title_options[:lead_paragraph]).to eq(content_store_response["details"]["lead_paragraph"])
+    end
+  end
 end

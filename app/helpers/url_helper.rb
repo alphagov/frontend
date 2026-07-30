983eb01b394f6124af052433ea00c02a9bfba007
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-07-30 13:38:33 +0100

    Add #add_parameter_if_present method to UrlHelper
    - Safely adds a passthrough parameter to a link.

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,4 +1,13 @@
 module UrlHelper
+  def add_parameter_if_present(url, parameter)
+    return url if params[parameter].nil?
+
+    parsed_url = URI.parse(url)
+    new_query_array = URI.decode_www_form(parsed_url.query || "") << [parameter, params[parameter]]
+    parsed_url.query = URI.encode_www_form(new_query_array)
+    parsed_url.to_s
+  end
+
   def canonical_url(path)
     Plek.new.website_root + path
   end

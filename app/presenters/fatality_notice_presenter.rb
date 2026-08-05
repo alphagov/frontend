a6feecf3a2e67cb773719bdc9edfc98b82bd67fd
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-08-05 11:42:56 +0100

    Move metadata into page_title
    - pass the details for the metadata to the page_title partial, which is already set up to accept and display them
    - this isn't quite visually correct yet as it needs a new option on the metadata component to add a border top, coming in this PR: https://github.com/alphagov/govuk_publishing_components/pull/5592

--- app/presenters/fatality_notice_presenter.rb
+++ app/presenters/fatality_notice_presenter.rb
@@ -1,2 +1,14 @@
 class FatalityNoticePresenter < ContentItemPresenter
+  include LinkHelper
+  include DateHelper
+
+  def page_title_options
+    super.merge({
+      metadata: {
+        from: govuk_styled_links_list(contributor_links),
+        first_published: display_date(content_item.initial_publication_date),
+        last_updated: display_date(content_item.updated),
+      },
+    })
+  end
 end

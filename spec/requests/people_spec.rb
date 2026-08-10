b8df4968a5d7929eefdc3bb0318547b86f86fbce
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2026-08-10 10:48:55 +0100

    Add person page request coverage
    Add a request-level regression test for the new route and renderer using a valid person schema example and Frontend's conditional content-item loader. Search is stubbed because announcements are part of the page render.
    
    This is the modern request-spec equivalent of the route/render coverage implicit in the Collections feature test.
    
    Audit trail: https://github.com/alphagov/collections/blob/a754e84a8f69531bfcc1dbcc2e9be0fe144e9046/spec/features/person_spec.rb

---
+++ spec/requests/people_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe "Person pages" do
+  include GdsApi::TestHelpers::Search
+
+  let(:content_item) do
+    GovukSchemas::RandomExample.for_schema(frontend_schema: "person").merge(
+      "title" => "Rufus Scrimgeour",
+      "base_path" => "/government/people/rufus-scrimgeour",
+    )
+  end
+  let(:base_path) { content_item.fetch("base_path") }
+
+  before do
+    stub_conditional_loader_returns_content_item_for_path(base_path, content_item)
+    stub_any_search_to_return_no_results
+    get base_path
+  end
+
+  it "returns successfully" do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "renders the people show template" do
+    expect(response).to render_template("people/show")
+  end
+
+  it "sets public cache headers" do
+    expect(response.headers["Cache-Control"]).to include("public")
+  end
+end

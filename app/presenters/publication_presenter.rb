ca1c7e0936ca5759ca03da28d5d71c7c202d8e59
AshGDS <8880610+AshGDS@users.noreply.github.com>
2026-08-20 11:51:18 +0100

    Add extra routes to be excluded from search engines

--- app/presenters/publication_presenter.rb
+++ app/presenters/publication_presenter.rb
@@ -5,6 +5,10 @@ class PublicationPresenter < ContentItemPresenter
     /government/publications/govuk-app-testing-privacy-notice-how-we-use-your-data
     /government/publications/govuk-test-app-privacy-notice
     /government/publications/pension-credit-claim-form--2
+    /government/publications/hpv-self-testing-kit-instructions
+    /government/publications/hpv-self-testing-a-self-test-to-help-protect-against-cervical-cancer
+    /government/publications/hpv-self-testing-easy-read-letter-templates
+    /government/publications/hpv-self-testing-easy-guides
   ].freeze
 
   def hide_from_search_engines?

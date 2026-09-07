0cb9332512f7af072abfebefe156411f02baf5e2
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-07 13:59:45 +0100

    Update controller and presenter
    - switches to using layout
    - updates presenter with required methods and detail

--- app/controllers/document_collection_controller.rb
+++ app/controllers/document_collection_controller.rb
@@ -1,6 +1,7 @@
 class DocumentCollectionController < ContentItemsController
   include Cacheable
   include Personalisable
+  layout "header_content_sidebar"
 
   def show
     @content_item_presenter = DocumentCollectionPresenter.new(content_item)

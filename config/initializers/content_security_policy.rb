00b72cd048c9e6c42a328619c316c05d4297f5df
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-09-10 10:16:18 +0100

    Revert "Allow 'script' hosts of .publishing.service.gov.uk on draft stack"
    This reverts commit 93f735724630f2bb8180b55d5d03585bf6e85faa.
    
    The script does have a Location redirect, but it doesn't get followed
    so the auth flow never completes.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,17 +1,7 @@
-require_relative "../../app/helpers/draft_helper"
-
-# rubocop:disable Style/MixinUsage
-include DraftHelper
-# rubocop:enable Style/MixinUsage
-
 GovukContentSecurityPolicy.configure do |policy|
   # The map block makes use of the OS api and inline styles
   policy.img_src(*policy.img_src, "https://api.os.uk", :data)
   # The map component makes use of maplibre
   policy.img_src(*policy.img_src, "https://tiles.openfreemap.org", :data)
   policy.connect_src(*policy.connect_src, "https://tiles.openfreemap.org", :data)
-
-  # The draft stack uses a parser-blocking script tag to force the Asset
-  # Manager authentication handshake before the rest of the page is parsed.
-  policy.script_src(*policy.script_src, "*.publishing.service.gov.uk") if draft_host?
 end

915b74ec57368381a37e0871882e97a114e367ad
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-11 10:14:31 +0100

    WIP split out more JS due to namespace conflicts

--- app/javascript/components/map/defra.js
+++
@@ -1,5 +0,0 @@
-//= require @defra/interactive-map/dist/esm/index.js
-//= require @defra/interactive-map/dist/esm/im-core.js
-//= require @defra/interactive-map/dist/esm/im-shell.js
-
-// require @defra/interactive-map/dist/umd/index.js

e0485e3a72b52f3263c196181a051644979692b9
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-11 08:42:47 +0100

    WIP

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "@defra/interactive-map", to: "@defra/interactive-map/dist/esm/index.js"
-pin "@defra/interactive-map/providers/maplibre", to: "@defra/interactive-map/providers/maplibre/dist/esm/index.js"
-pin "@defra/interactive-map/plugins/interact", to: "@defra/interactive-map/plugins/interact/dist/esm/index.js"
+# Pin the default entry point required by javascript_importmap_tags
+pin "application", preload: true
+
+pin "@defra/interactive-map", to: "./node_modules/@defra/interactive-map/dist/esm/index.js"
+pin "@defra/interactive-map/providers/maplibre", to: "./node_modules/@defra/interactive-map/providers/maplibre/dist/esm/index.js"
+pin "@defra/interactive-map/plugins/interact", to: "./node_modules/@defra/interactive-map/plugins/interact/dist/esm/index.js"
 pin "components/map", to: "components/map.js"

8f69097bd2701f1ff092e831060fe30bbdb5b028
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-14 13:56:48 +0100

    Update view

--- app/presenters/speech_presenter.rb
+++ app/presenters/speech_presenter.rb
@@ -1,6 +1,20 @@
 class SpeechPresenter < ContentItemPresenter
+  include LinkHelper
+  include DateHelper
+
   def use_contextual_components?
     true
+  end
+
+  def page_title_options
+    super.merge({
+      metadata: {
+        from: govuk_styled_links_list(speech_contributor_links),
+        first_published: display_date(content_item.initial_publication_date),
+        last_updated: display_date(content_item.updated),
+        see_updates_link: true,
+      },
+    })
   end
 
   def speech_contributor_links

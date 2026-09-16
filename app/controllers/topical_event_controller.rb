e2a092a780380cd57f556cbfd3a5567f24894084
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-16 11:28:52 +0100

    Move flexible views to topical events folder
    - We don't need these generic flexible page views, so first off move them to the appropriate layout and remove the override render from the controller.
    - Nothing else is now using these views, but they're still using the flexible sections from their new location, so we can't delete those yet.

--- app/controllers/topical_event_controller.rb
+++ app/controllers/topical_event_controller.rb
@@ -3,9 +3,7 @@ class TopicalEventController < FlexiblePageController
 
   def show
     respond_to do |format|
-      format.html do
-        render "flexible_page/show"
-      end
+      format.html
       format.atom do
         set_expiry(5.minutes)
         headers["Access-Control-Allow-Origin"] = "*"

f0818cd9ad5a5da2c3712b990b05282739266d15
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-16 15:59:58 +0100

    SQUASH (add path)

--- app/presenters/topical_event_presenter.rb
+++ app/presenters/topical_event_presenter.rb
@@ -1,6 +1,10 @@
 class TopicalEventPresenter < ContentItemPresenter
   include ImpactHeader
 
+  def about_page_path
+    "#{content_item.base_path}/about"
+  end
+
   def about_page_link_text
     content_item.details["about_page_link_text"]
   end

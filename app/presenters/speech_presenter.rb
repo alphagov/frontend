467a30f8d2c67bf6ac4a0de9d2d2cb896f52ea7d
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-16 11:27:07 +0100

    Remove published dates component
    - moves the page history into the metadata component at the top of the page, and removes the published dates component at the foot

--- app/presenters/speech_presenter.rb
+++ app/presenters/speech_presenter.rb
@@ -12,7 +12,7 @@ class SpeechPresenter < ContentItemPresenter
         from: govuk_styled_links_list(speech_contributor_links),
         first_published: display_date(content_item.initial_publication_date),
         last_updated: display_date(content_item.updated),
-        see_updates_link: true,
+        page_history: formatted_history(content_item.history),
       },
     })
   end

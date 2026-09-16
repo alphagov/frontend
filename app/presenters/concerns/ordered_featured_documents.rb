4feb20756961cadc9897cc49a8fb3a9d9a9ecaf1
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-16 16:45:28 +0100

    Add ordered featured documents concern
    - Likely to be a common feature of minisites, topical events, and others, so make a concern that will convert them into suitable formats.

---
+++ app/presenters/concerns/ordered_featured_documents.rb
@@ -0,0 +1,13 @@
+module OrderedFeaturedDocuments
+  def ordered_featured_documents_for_image_cards
+    (details["ordered_featured_documents"] || []).map do |i|
+      {
+        href: i["href"],
+        image_src: i["image"]["url"],
+        image_alt: i["image"]["alt_text"],
+        heading_text: i["title"],
+        description: i["summary"],
+      }
+    end
+  end
+end

d03d3f87d2d3fed29a63e1f072defce3da38cc40
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-17 19:02:51 +0100

    WIP

--- spec/models/topical_event_spec.rb
+++ spec/models/topical_event_spec.rb
@@ -2,4 +2,22 @@ RSpec.describe TopicalEvent do
   subject(:topical_event) { described_class.new(content_store_response) }
 
   let(:content_store_response) { GovukSchemas::Example.find("topical_event", example_name: "western-balkans-summit-london-2018") }
+
+  describe "#header_image" do
+    it "returns nil" do
+      expect(topical_event.header_image).to be_nil
+    end
+  end
+
+  describe "#logo_image" do
+    it "returns nil" do
+      expect(topical_event.logo_image).to be_nil
+    end
+  end
+
+  describe "#legacy_logo" do
+    it "returns nil" do
+      expect(topical_event.legacy_logo).to be_nil
+    end
+  end
 end

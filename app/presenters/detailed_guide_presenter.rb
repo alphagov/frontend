31781f00154e26922a8a23390ab7e2ba4630ef07
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-17 14:35:34 +0100

    Update controller and presenter
    - set new layout
    - add required methods

--- app/presenters/detailed_guide_presenter.rb
+++ app/presenters/detailed_guide_presenter.rb
@@ -1,10 +1,28 @@
 class DetailedGuidePresenter < ContentItemPresenter
   include ContentsList
+  include DateHelper
+  include LinkHelper
 
   PATHS_TO_HIDE = %w[
     /guidance/about-govuk-chat
     /guidance/govuk-chat-terms-and-conditions
   ].freeze
+
+  def use_contextual_components?
+    true
+  end
+
+  def page_title_options
+    super.merge({
+      metadata: {
+        from: govuk_styled_links_list(contributor_links),
+        first_published: display_date(content_item.first_public_at || content_item.first_published_at),
+        last_updated: display_date(content_item.updated),
+        see_updates_link: true,
+      },
+      logo: logo,
+    })
+  end
 
   def logo
     return unless content_item.image

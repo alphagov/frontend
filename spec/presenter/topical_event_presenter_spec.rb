cb83b1f4035b922bd31e47bda02d3004830647f7
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-17 14:53:58 +0100

    SQUASH (add about page path to presenter)

--- spec/presenter/topical_event_presenter_spec.rb
+++ spec/presenter/topical_event_presenter_spec.rb
@@ -1,6 +1,12 @@
 RSpec.describe TopicalEventPresenter do
-  subject(:topical_even_presenter) { described_class.new(content_item) }
+  subject(:topical_event_presenter) { described_class.new(content_item) }
 
   let(:content_item) { StatisticsAnnouncement.new(content_store_response) }
   let(:content_store_response) { GovukSchemas::Example.find("topical_event", example_name: "topical_event") }
+
+  describe "#about_page_path" do
+    it "returns the base path with about appended" do
+      expect(topical_event_presenter.about_page_path).to eq("#{content_store_response['base_path']}/about")
+    end
+  end
 end

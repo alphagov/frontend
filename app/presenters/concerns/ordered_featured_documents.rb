6f74f65d9a02ec67603596cd4b8270bf298ce4cc
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-09-17 09:21:20 +0100

    SQUASH (ordered_featured_documents shared)

--- app/presenters/concerns/ordered_featured_documents.rb
+++ app/presenters/concerns/ordered_featured_documents.rb
@@ -1,12 +1,12 @@
 module OrderedFeaturedDocuments
   def ordered_featured_documents_for_image_cards
-    (details["ordered_featured_documents"] || []).map do |i|
+    (content_item.details["ordered_featured_documents"] || []).map do |i|
       {
         href: i["href"],
         image_src: i["image"]["url"],
         image_alt: i["image"]["alt_text"],
         heading_text: i["title"],
-        description: sanitize(i["summary"]).truncate(160, separator: " "),
+        description: ActionController::Base.helpers.sanitize(i["summary"]).truncate(160, separator: " "),
       }
     end
   end

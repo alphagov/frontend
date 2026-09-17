05ddcac35589c999196b94237bc59f67997b2476
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-17 12:58:05 +0100

    Update controller and presenter
    - switch to new layout and add required methods

--- app/presenters/consultation_presenter.rb
+++ app/presenters/consultation_presenter.rb
@@ -1,4 +1,22 @@
 class ConsultationPresenter < ContentItemPresenter
+  include LinkHelper
+  include DateHelper
+
+  def use_contextual_components?
+    true
+  end
+
+  def page_title_options
+    super.merge({
+      metadata: {
+        from: govuk_styled_links_list(contributor_links),
+        first_published: display_date(content_item.initial_publication_date),
+        last_updated: display_date(content_item.updated),
+        see_updates_link: true,
+      },
+    })
+  end
+
   def notice_title
     if content_item.unopened?
       I18n.t("formats.consultation.not_open_yet")

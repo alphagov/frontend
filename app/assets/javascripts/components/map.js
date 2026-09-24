e1041244fd6fb93ecc7fc6fa611dc051de1ff442
Andy Sellick <andy.sellick@dsit.gov.uk>
2026-09-24 16:46:03 +0100

    Add further key information
    - adds the key icon and name into both the map popup when the user clicks on a marker, and the list of markers beneath
    - trying to make it super easy for both sighted and non-sighted users to understand the classification of map markers, if a classification exists

--- app/assets/javascripts/components/map.js
+++ app/assets/javascripts/components/map.js
@@ -125,11 +125,11 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       const heading = `h${this.headingLevel}`
       let popupContent = `<${heading} class="govuk-heading-s govuk-!-margin-bottom-2">${feature.properties.name}</${heading}>`
       if (feature.properties.description) {
-        popupContent = `${popupContent} ${feature.properties.description}`
+        popupContent = `${popupContent} <p class="govuk-body govuk-!-margin-bottom-2">${feature.properties.description}</p>`
       }
       feature.marker = feature.marker || {}
       if (feature.marker.name) {
-        popupContent = `${popupContent} <span class="govuk-visually-hidden">Categorised under: ${feature.marker.name}</span>`
+        popupContent = `${popupContent} <p class="govuk-body-s"><span class="app-c-map__key app-c-map__key--${feature.marker.symbol} app-c-map__key--${feature.marker.colour}"></span> Categorised under: ${feature.marker.name}</p>`
       }
       popupContent = this.removeScript(popupContent)
       return popupContent
@@ -205,8 +205,9 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
           if (allowedColours[colour]) {
             marker.marker.backgroundColor = allowedColours[colour]
+          } else {
+            delete marker.marker.colour
           }
-          delete marker.marker.colour
 
           const symbol = marker.marker.symbol || false
           if (!(symbol && allowedSymbols.includes(symbol))) {
